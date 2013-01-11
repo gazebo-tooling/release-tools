08a4860552d57475eacb5fb41d64813cd721f012
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-01-11 10:35:09 +0100

    Import all jenkins scripts from web interface
     - Duplicate scripts were merged
     - Naming tries to mimic the jenkins jobs

---
+++ jenkins-scripts/gazebo-default-doc.bash
@@ -0,0 +1,126 @@
+#!/bin/bash -x
+
+###################################################
+# Boilerplate.
+# DO NOT MODIFY
+
+#stop on error
+set -e
+
+distro=precise
+arch=amd64
+base=/var/cache/pbuilder-$distro-$arch
+
+aptconffile=$WORKSPACE/apt.conf
+
+#increment this value if you have changed something that will invalidate base tarballs. #TODO this will need cleanup eventually.
+basetgz_version=2
+
+rootdir=$base/apt-conf-$basetgz_version
+
+basetgz=$base/base-$basetgz_version.tgz
+output_dir=$WORKSPACE/output
+work_dir=$WORKSPACE/work
+
+sudo apt-get update
+sudo apt-get install -y pbuilder python-empy python-argparse debhelper # todo move to server setup, or confirm it's there
+
+if [ -e $WORKSPACE/catkin-debs ]
+then
+  rm -rf $WORKSPACE/catkin-debs
+fi
+
+git clone git://github.com/willowgarage/catkin-debs.git $WORKSPACE/catkin-debs -b master --depth 1
+
+
+cd $WORKSPACE/catkin-debs
+. setup.sh
+
+#setup the cross platform apt environment
+# using sudo since this is shared with pbuilder and if pbuilder is interupted it will leave a sudo only lock file.  Otherwise sudo is not necessary. 
+# And you can't chown it even with sudo and recursive 
+sudo PYTHONPATH=$PYTHONPATH $WORKSPACE/catkin-debs/scripts/setup_apt_root.py $distro $arch $rootdir --local-conf-dir $WORKSPACE
+
+sudo rm -rf $output_dir
+mkdir -p $output_dir
+
+sudo rm -rf $work_dir
+mkdir -p $work_dir
+cd $work_dir
+
+sudo apt-get update -c $aptconffile
+
+# Grab a newer version of pbuilder, because the one that ships with Lucid suffers from a bug when using --execute
+# https://bugs.launchpad.net/ubuntu/+source/pbuilder/+bug/811016
+rm -f $WORKSPACE/pbuilder
+wget -O $WORKSPACE/pbuilder http://bazaar.launchpad.net/~vcs-imports/pbuilder/trunk/download/head:/pbuilder/pbuilder
+chmod a+x $WORKSPACE/pbuilder
+
+# Setup the pbuilder environment if not existing, or update
+if [ ! -e $basetgz ] || [ ! -s $basetgz ] 
+then
+  #make sure the base dir exists
+  sudo mkdir -p $base
+  #create the base image
+  sudo $WORKSPACE/pbuilder create \
+    --distribution $distro \
+    --aptconfdir $rootdir/etc/apt \
+    --basetgz $basetgz \
+    --architecture $arch
+else
+  sudo $WORKSPACE/pbuilder --update --basetgz $basetgz
+fi
+
+# Boilerplate.
+# DO NOT MODIFY
+###################################################
+
+cat > build.sh << DELIM
+###################################################
+# Make project-specific changes here
+#
+set -ex
+
+# Step 1: install everything you need
+
+# Required stuff for Gazebo
+apt-get install -y build-essential libtinyxml-dev libtbb-dev libxml2-dev libqt4-dev pkg-config  libprotoc-dev libfreeimage-dev libprotobuf-dev protobuf-compiler libboost-all-dev freeglut3-dev cmake libogre-dev libcurl4-openssl-dev libtar-dev
+# Optional stuff for Gazebo
+apt-get install -y robot-player-dev libcegui-mk2-dev libavformat-dev libavcodec-dev libltdl-dev libgtest-dev
+#TODO: pare down to just the following install; blocked on https://bitbucket.org/osrf/gazebo/issue/27/
+apt-get install -y doxygen graphviz texlive-latex-base texlive-latex-extra texlive-latex-recommended latex-xcolor texlive-fonts-recommended
+
+# Step 2: configure and build
+
+# Normal cmake routine for Gazebo
+rm -rf $WORKSPACE/build
+mkdir -p $WORKSPACE/build
+cd $WORKSPACE/build
+cmake $WORKSPACE/gazebo
+make doc
+make gzsdf
+GAZEBO_RESOURCE_PATH=`pwd`/../gazebo/gazebo ./tools/gzsdf doc > dev.html
+
+# Step 3: upload docs
+apt-get install -y openssh-client
+ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@gazebosim.org sudo rm -rf /var/www/api/dev /tmp/gazebo_dev /tmp/dev.html
+scp -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa -r doxygen/html ubuntu@gazebosim.org:/tmp/gazebo_dev
+ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@gazebosim.org sudo mv /tmp/gazebo_dev /var/www/api/dev
+scp -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa doxygen/latex/gazebo-[0-9]*.[0-9]*.[0-9]*.pdf ubuntu@gazebosim.org:/tmp/gazebo-dev.pdf
+ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@gazebosim.org sudo mv /tmp/gazebo-dev.pdf /var/www/api/gazebo-dev.pdf
+scp -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa dev.html ubuntu@gazebosim.org:/tmp/dev.html
+ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@gazebosim.org sudo mv /tmp/dev.html /var/www/sdf/dev.html
+DELIM
+
+# Copy in my ssh keys, to allow the above ssh/scp calls to work; not sure this is the best way to do it, 
+# but it shouldn't be a security issue, as only Jenkins users can see the contents of the workspace
+cp $HOME/.ssh/id_rsa $WORKSPACE
+
+# Make project-specific changes here
+###################################################
+
+sudo $WORKSPACE/pbuilder  --execute \
+    --bindmounts $WORKSPACE \
+    --basetgz $basetgz \
+    -- build.sh
+
---
+++ jenkins-scripts/lib/debbuild-base.bash
@@ -0,0 +1,152 @@
+#!/bin/bash -x
+
+###################################################
+# Boilerplate.
+# DO NOT MODIFY
+
+#stop on error
+set -e
+
+distro=precise
+arch=amd64
+base=/var/cache/pbuilder-$distro-$arch
+
+aptconffile=$WORKSPACE/apt.conf
+
+#increment this value if you have changed something that will invalidate base tarballs. #TODO this will need cleanup eventually.
+basetgz_version=2
+
+rootdir=$base/apt-conf-$basetgz_version
+
+basetgz=$base/base-$basetgz_version.tgz
+output_dir=$WORKSPACE/output
+work_dir=$WORKSPACE/work
+
+sudo apt-get update
+sudo apt-get install -y pbuilder python-empy python-argparse debhelper # todo move to server setup, or confirm it's there
+
+if [ -e $WORKSPACE/catkin-debs ]
+then
+  rm -rf $WORKSPACE/catkin-debs
+fi
+
+git clone git://github.com/willowgarage/catkin-debs.git $WORKSPACE/catkin-debs -b master --depth 1
+
+
+cd $WORKSPACE/catkin-debs
+. setup.sh
+
+#setup the cross platform apt environment
+# using sudo since this is shared with pbuilder and if pbuilder is interupted it will leave a sudo only lock file.  Otherwise sudo is not necessary. 
+# And you can't chown it even with sudo and recursive 
+sudo PYTHONPATH=$PYTHONPATH $WORKSPACE/catkin-debs/scripts/setup_apt_root.py $distro $arch $rootdir --local-conf-dir $WORKSPACE
+
+sudo rm -rf $output_dir
+mkdir -p $output_dir
+
+sudo rm -rf $work_dir
+mkdir -p $work_dir
+cd $work_dir
+
+sudo apt-get update -c $aptconffile
+
+# Grab a newer version of pbuilder, because the one that ships with Lucid suffers from a bug when using --execute
+# https://bugs.launchpad.net/ubuntu/+source/pbuilder/+bug/811016
+rm -f $WORKSPACE/pbuilder
+wget -O $WORKSPACE/pbuilder http://bazaar.launchpad.net/~vcs-imports/pbuilder/trunk/download/head:/pbuilder/pbuilder
+chmod a+x $WORKSPACE/pbuilder
+
+# Setup the pbuilder environment if not existing, or update
+if [ ! -e $basetgz ] || [ ! -s $basetgz ] 
+then
+  #make sure the base dir exists
+  sudo mkdir -p $base
+  #create the base image
+  sudo $WORKSPACE/pbuilder create \
+    --distribution $distro \
+    --aptconfdir $rootdir/etc/apt \
+    --basetgz $basetgz \
+    --architecture $arch
+else
+  sudo $WORKSPACE/pbuilder --update --basetgz $basetgz
+fi
+
+# Boilerplate.
+# DO NOT MODIFY
+###################################################
+
+cat > build.sh << DELIM
+###################################################
+# Make project-specific changes here
+#
+set -ex
+
+# Install deb-building tools
+apt-get install -y pbuilder debootstrap devscripts ubuntu-dev-tools mercurial debhelper reprepro wget
+
+# get ROS repo's key, to be used in creating the pbuilder chroot (to allow it to install packages from that repo)
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | apt-key add -
+# Also get drc repo's key, to be used in getting Gazebo
+sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu precise main" > /etc/apt/sources.list.d/drc-latest.list'
+wget http://packages.osrfoundation.org/drc.key -O - | apt-key add -
+apt-get update
+
+# Step 0: create/update distro-specific pbuilder environment
+pbuilder-dist $DISTRO $ARCH create --othermirror "deb http://packages.ros.org/ros/ubuntu precise main|deb http://packages.osrfoundation.org/drc/ubuntu precise main" --keyring /etc/apt/trusted.gpg --debootstrapopts --keyring=/etc/apt/trusted.gpg
+
+# Step 0: Clean up
+rm -rf $WORKSPACE/build
+mkdir -p $WORKSPACE/build
+
+# Step 1: Get the source tarball
+cd $WORKSPACE/build
+wget --quiet -O ${PACKAGE_ALIAS}_$VERSION.orig.tar.bz2 $SOURCE_TARBALL_URI
+
+# Step 3: unpack tarball
+rm -rf $PACKAGE-$VERSION
+tar xf ${PACKAGE_ALIAS}_$VERSION.orig.tar.bz2
+cd $PACKAGE-$VERSION
+
+# Step 4: add debian/ subdirectory with necessary metadata files to unpacked source tarball
+rm -rf /tmp/$PACKAGE-release
+hg clone https://bitbucket.org/osrf/$PACKAGE-release /tmp/$PACKAGE-release
+cd /tmp/$PACKAGE-release
+hg up $RELEASE_REPO_BRANCH
+cd $WORKSPACE/build/$PACKAGE-$VERSION
+cp -a /tmp/$PACKAGE-release/ubuntu/* .
+
+# Step 5: use debuild to create source package
+#TODO: create non-passphrase-protected keys and remove the -uc and -us args to debuild
+debuild -S -uc -us
+
+# Step 6: use pbuilder-dist to create binary package(s)
+pbuilder-dist $DISTRO $ARCH build ../*.dsc
+
+# Step 7: upload resulting .deb
+sudo apt-get install -y openssh-client
+cd /var/packages/gazebo/ubuntu
+if [ -f /var/lib/jenkins/pbuilder/${DISTRO}-${ARCH}_result/${PACKAGE_ALIAS}_${VERSION}${UBUNTU_VERSION}_$ARCH.deb ]; then
+  GNUPGHOME=$WORKSPACE/gnupg reprepro includedeb $DISTRO /var/lib/jenkins/pbuilder/${DISTRO}-${ARCH}_result/${PACKAGE_ALIAS}_${VERSION}${UBUNTU_VERSION}_$ARCH.deb
+  scp -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa /var/lib/jenkins/pbuilder/${DISTRO}-${ARCH}_result/${PACKAGE_ALIAS}_${VERSION}${UBUNTU_VERSION}_$ARCH.deb ubuntu@gazebosim.org:/var/www/assets/distributions
+else
+  GNUPGHOME=$WORKSPACE/gnupg reprepro includedeb $DISTRO /var/lib/jenkins/pbuilder/${DISTRO}_result/${PACKAGE_ALIAS}_${VERSION}${UBUNTU_VERSION}_$ARCH.deb
+  scp -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa /var/lib/jenkins/pbuilder/${DISTRO}_result/${PACKAGE_ALIAS}_${VERSION}${UBUNTU_VERSION}_$ARCH.deb ubuntu@gazebosim.org:/var/www/assets/distributions
+fi
+DELIM
+
+# Copy in my GPG key, to allow reprepro to sign the debs it builds.
+rm -rf $WORKSPACE/gnupg
+cp -a $HOME/.gnupg $WORKSPACE/gnupg
+
+# Copy in my ssh keys, to allow the above ssh/scp calls to work; not sure this is the best way to do it, 
+# but it shouldn't be a security issue, as only Jenkins users can see the contents of the workspace
+cp $HOME/.ssh/id_rsa $WORKSPACE
+#
+# Make project-specific changes here
+###################################################
+
+sudo $WORKSPACE/pbuilder  --execute \
+    --bindmounts "$WORKSPACE /var/packages/gazebo/ubuntu" \
+    --basetgz $basetgz \
+    -- build.sh
