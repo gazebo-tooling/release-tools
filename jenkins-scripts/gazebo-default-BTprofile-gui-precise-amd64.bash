012cc84ee3c66d505a5bdb457edbde31416aabcd
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-02-14 19:11:50 +0100

    New script to run profile cmake build type with support for DISPLAY

---
+++ jenkins-scripts/gazebo-default-BTprofile-gui-precise-amd64.bash
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+export DISPLAY=$(ps aux | grep "X :" | grep -v grep | awk '{ print $12 }')
+export GZ_BUILD_TYPE=Profile
+
+. ${SCRIPT_DIR}/lib/gazebo-base-default.bash
