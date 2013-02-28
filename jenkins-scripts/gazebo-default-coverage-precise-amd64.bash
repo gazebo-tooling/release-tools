d0153fa41c4b21bd781d0bd886f34b533e844a5f
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-02-28 19:10:39 +0100

    Added script for coverage default gazebo

---
+++ jenkins-scripts/gazebo-default-coverage-precise-amd64.bash
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+export DISPLAY=$(ps aux | grep "X :" | grep -v grep | awk '{ print $12 }')
+export COVERAGE_ENABLED=true
+. ${SCRIPT_DIR}/lib/gazebo-base-default.bash
