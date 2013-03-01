c96fdf9beb0f1f35773d66b4b41fa39aa037fd29
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-03-01 18:57:27 +0100

    Support for single and multidistro with only one file

---
+++ jenkins-scripts/lib/debbuild-base-multidistribution.bash
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+
+export RELEASE_REPO_DIRECTORY=${DISTRO}
+. ./debbuild-meta-base.bash
--- jenkins-scripts/lib/debbuild-base.bash
+++ jenkins-scripts/lib/debbuild-base.bash
@@ -1,4 +1,5 @@
 #!/bin/bash -x
 # TODO: this script should be removed when all software is migrated to multidistribution
 
+export RELEASE_REPO_DIRECTORY=ubuntu
 . ./debbuild-meta-base.bash
