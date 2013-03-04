ab1fa340800cf2cb8eb1bc8023f93a42e37f9867
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-03-04 19:14:49 +0100

    Use -y in apt-get install

--- jenkins-scripts/cloudsim-client-tools-default-devel-precise.bash
+++ jenkins-scripts/cloudsim-client-tools-default-devel-precise.bash
@@ -12,7 +12,7 @@ cat > build.sh << DELIM
 set -ex
 
 # Install NoseXUnit
-apt-get install --upgrade python-nose
+apt-get install -y python-nose
 
 cd $WORKSPACE/cloudsim-client-tools/vrc_shaping
 nosetests --with-xunit vrc_sniffer
