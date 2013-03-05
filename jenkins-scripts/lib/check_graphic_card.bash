934b0fcd94ff5fca5816f820536b3911dc9b0e4f
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-03-05 20:01:47 +0100

    Initial support to look for graphic card in the host and install it on the chroot

---
+++ jenkins-scripts/lib/check_graphic_card.bash
@@ -0,0 +1,17 @@
+GRAPHIC_CARD_FOUND=false
+GRAPHIC_CARD_PKG=""
+
+# Check for Nvidia stuff
+NVIDIA_SUPPORT_RESULT=$(lspci -v | grep nvidia | grep "Kernel driver in use: nvidia")
+if [ -n "${NVIDIA_SUPPORT_RESULT}" ]; then
+    export GRAPHIC_CARD_PKG=$(lspci -v | grep nvidia | grep "Kernel modules:" | awk '{ print$3 }' | tr -d ',')
+    if [ -z ${GRAPHIC_CARD_PKG} ]; then
+	echo "Nvidia support found but not the module in use"
+	exit 1
+    fi
+    echo "Nvidia found using package ${GRAPHIC_CARD_PKG}"
+    export GRAPHIC_CARD_FOUND=true
+fi
+
+# Check for ati stuff
+# TODO
