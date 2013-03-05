26248e1372c9ee0866edae6db3de730ead492e60
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-03-05 20:47:15 +0100

    Basic suppor for ATI
    Fix problem with name in nvidia package

--- jenkins-scripts/lib/check_graphic_card.bash
+++ jenkins-scripts/lib/check_graphic_card.bash
@@ -4,7 +4,7 @@ GRAPHIC_CARD_PKG=""
 # Check for Nvidia stuff
 NVIDIA_SUPPORT_RESULT=$(lspci -v | grep nvidia | grep "Kernel driver in use: nvidia")
 if [ -n "${NVIDIA_SUPPORT_RESULT}" ]; then
-    export GRAPHIC_CARD_PKG=$(lspci -v | grep nvidia | grep "Kernel modules:" | awk '{ print$3 }' | tr -d ',')
+    export GRAPHIC_CARD_PKG=$(lspci -v | grep nvidia | grep "Kernel modules:" | awk '{ print$3 }' | tr -d ','| sed -e s:_:-:)
     if [ -z ${GRAPHIC_CARD_PKG} ]; then
 	echo "Nvidia support found but not the module in use"
 	exit 1
@@ -14,4 +14,10 @@ if [ -n "${NVIDIA_SUPPORT_RESULT}" ]; then
 fi
 
 # Check for ati stuff
-# TODO
+ATI_SUPPORT_RESULT=$(lspci -v | grep "ATI" | grep "VGA")
+if [ -n "${ATI_SUPPORT_RESULT}" ]; then
+    # TODO search for correct version of fglrx
+    export GRAPHIC_CARD_PKG=fglrx
+    echo "ATI found using package ${GRAPHIC_CARD_PKG}"
+    export GRAPHIC_CARD_FOUND=true
+fi
