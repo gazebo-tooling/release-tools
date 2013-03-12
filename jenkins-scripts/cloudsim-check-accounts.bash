f02248aaf7a256ff7637bf5ac380232d1d63b068
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-03-12 17:35:07 +0100

    New script to check current used AWS accounts

---
+++ jenkins-scripts/cloudsim-check-accounts.bash
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+. ${SCRIPT_DIR}/lib/boilerplate_prepare.sh
+
+set +x # keep password secret
+MAIL_PASS=`cat $HOME/mail_pass`
+set -x # back to debug
+
+cat > build.sh << DELIM
+###################################################
+# Make project-specific changes here
+#
+set -ex
+
+# Install dependencies
+apt-get install -y python-pip
+pip install --upgrade boto
+
+# Testing
+cde $WORKSPACE/cloudsim
+set +x # keep password secret
+./bin/cloud-check.py $WORKSPACE/aws_accounts.yaml jrivero@osrfoundation.org 24 osrfbuild@osrfoundation.org $MAIL_PASS
+set -x # back to debug
+DELIM
+
+# Copy in my boto config file, to allow launching of AWS machines.
+cp $HOME/boto.test-osrfoundation.org $WORKSPACE/boto.ini
+
+# Make project-specific changes here
+###################################################
+
+rm -fr $WORKSPACE/aws_accounts.yaml
+cp -a $HOME/aws_accounts.yaml $WORKSPACE/aws_accounts.yaml
+
+sudo $WORKSPACE/pbuilder  --execute \
+    --bindmounts $WORKSPACE \
+    --basetgz $basetgz \
+    -- build.sh
