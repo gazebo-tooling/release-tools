7f2ae47f6ae79f7b1b2b7167736c6f55cb823d40
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-03-13 00:10:29 +0100

    Initial support for double compilation

--- jenkins-scripts/drcsim-default-gazebo-default-devel-quantal-amd64.bash
+++ jenkins-scripts/drcsim-default-gazebo-default-devel-quantal-amd64.bash
@@ -4,9 +4,7 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-export DISTRO=precise
-export ROS_DISTRO=fuerte
-
-export GZ_BUILD_TYPE=Release
+export DISTRO=quantal
+export ROS_DISTRO=groovy
 
 . ${SCRIPT_DIR}/lib/drcsim-default-gazebo-default-base.bash
