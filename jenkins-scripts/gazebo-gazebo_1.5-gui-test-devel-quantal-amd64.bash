e2d988735dbd7f7c7ef7116cce6339ad8bdfb0bc
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-03-13 17:28:25 +0100

    Remove quantal version of gazebo default script and use standard

--- jenkins-scripts/gazebo-gazebo_1.5-gui-test-devel-quantal-amd64.bash
+++ jenkins-scripts/gazebo-gazebo_1.5-gui-test-devel-quantal-amd64.bash
@@ -6,4 +6,8 @@ SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
 # Hack to pick from current processes the DISPLAY available
 export DISPLAY=$(ps aux | grep "X :" | grep -v grep | awk '{ print $12 }')
-. ${SCRIPT_DIR}/lib/gazebo-base-default-quantal.bash
+
+export DISTRO=quantal
+export ROS_DISTRO=groovy
+
+. ${SCRIPT_DIR}/lib/gazebo-base-default.bash
