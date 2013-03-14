97cd0bfab76e2e2770a184fef4f2b7fffde9a1b7
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-03-14 14:47:52 +0100

    Migrate default-doc to use common boilerplate

--- jenkins-scripts/gazebo-default-doc.bash
+++ jenkins-scripts/gazebo-default-doc.bash
@@ -1,79 +1,9 @@
 #!/bin/bash -x
 
-###################################################
-# Boilerplate.
-# DO NOT MODIFY
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-#stop on error
-set -e
-
-distro=precise
-arch=amd64
-base=/var/cache/pbuilder-$distro-$arch
-
-aptconffile=$WORKSPACE/apt.conf
-
-#increment this value if you have changed something that will invalidate base tarballs. #TODO this will need cleanup eventually.
-basetgz_version=2
-
-rootdir=$base/apt-conf-$basetgz_version
-
-basetgz=$base/base-$basetgz_version.tgz
-output_dir=$WORKSPACE/output
-work_dir=$WORKSPACE/work
-
-sudo apt-get update
-sudo apt-get install -y pbuilder python-empy python-argparse debhelper # todo move to server setup, or confirm it's there
-
-if [ -e $WORKSPACE/catkin-debs ]
-then
-  rm -rf $WORKSPACE/catkin-debs
-fi
-
-git clone git://github.com/willowgarage/catkin-debs.git $WORKSPACE/catkin-debs -b master --depth 1
-
-
-cd $WORKSPACE/catkin-debs
-. setup.sh
-
-#setup the cross platform apt environment
-# using sudo since this is shared with pbuilder and if pbuilder is interupted it will leave a sudo only lock file.  Otherwise sudo is not necessary. 
-# And you can't chown it even with sudo and recursive 
-sudo PYTHONPATH=$PYTHONPATH $WORKSPACE/catkin-debs/scripts/setup_apt_root.py $distro $arch $rootdir --local-conf-dir $WORKSPACE
-
-sudo rm -rf $output_dir
-mkdir -p $output_dir
-
-sudo rm -rf $work_dir
-mkdir -p $work_dir
-cd $work_dir
-
-sudo apt-get update -c $aptconffile
-
-# Grab a newer version of pbuilder, because the one that ships with Lucid suffers from a bug when using --execute
-# https://bugs.launchpad.net/ubuntu/+source/pbuilder/+bug/811016
-rm -f $WORKSPACE/pbuilder
-wget -O $WORKSPACE/pbuilder http://bazaar.launchpad.net/~vcs-imports/pbuilder/trunk/download/head:/pbuilder/pbuilder
-chmod a+x $WORKSPACE/pbuilder
-
-# Setup the pbuilder environment if not existing, or update
-if [ ! -e $basetgz ] || [ ! -s $basetgz ] 
-then
-  #make sure the base dir exists
-  sudo mkdir -p $base
-  #create the base image
-  sudo $WORKSPACE/pbuilder create \
-    --distribution $distro \
-    --aptconfdir $rootdir/etc/apt \
-    --basetgz $basetgz \
-    --architecture $arch
-else
-  sudo $WORKSPACE/pbuilder --update --basetgz $basetgz
-fi
-
-# Boilerplate.
-# DO NOT MODIFY
-###################################################
+. ${SCRIPT_DIR}/lib/boilerplate_prepare.sh
 
 cat > build.sh << DELIM
 ###################################################
@@ -119,7 +49,7 @@ cp $HOME/.ssh/id_rsa $WORKSPACE
 # Make project-specific changes here
 ###################################################
 
-sudo $WORKSPACE/pbuilder  --execute \
+sudo pbuilder  --execute \
     --bindmounts $WORKSPACE \
     --basetgz $basetgz \
     -- build.sh
