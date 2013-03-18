2f9be1cba15de9610e816ecc91f49ebdf499741a
Carlos Agüero <caguero@osrfoundation.org>
2013-03-18 12:57:07 -0700

    Boilerplate updated for cloudsim.bash

--- jenkins-scripts/cloudsim.bash
+++ jenkins-scripts/cloudsim.bash
@@ -1,79 +1,14 @@
 #!/bin/bash -x
 
-###################################################
-# Boilerplate.
-# DO NOT MODIFY
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-#stop on error
-set -e
+. ${SCRIPT_DIR}/lib/boilerplate_prepare.sh
 
-distro=precise
-arch=amd64
-base=/var/cache/pbuilder-$distro-$arch
+set +x # keep password secret
+MAIL_PASS=`cat $HOME/mail_pass`
+set -x # back to debug
 
-aptconffile=$WORKSPACE/apt.conf
-
-#increment this value if you have changed something that will invalidate base tarballs. #TODO this will need cleanup eventually.
-basetgz_version=2
-
-rootdir=$base/apt-conf-$basetgz_version
-
-basetgz=$base/base-$basetgz_version.tgz
-output_dir=$WORKSPACE/output
-work_dir=$WORKSPACE/work
-
-sudo apt-get update
-sudo apt-get install -y pbuilder python-empy python-argparse debhelper # todo move to server setup, or confirm it's there
-
-if [ -e $WORKSPACE/catkin-debs ]
-then
-  rm -rf $WORKSPACE/catkin-debs
-fi
-
-git clone git://github.com/willowgarage/catkin-debs.git $WORKSPACE/catkin-debs -b master --depth 1
-
-
-cd $WORKSPACE/catkin-debs
-. setup.sh
-
-#setup the cross platform apt environment
-# using sudo since this is shared with pbuilder and if pbuilder is interupted it will leave a sudo only lock file.  Otherwise sudo is not necessary. 
-# And you can't chown it even with sudo and recursive 
-sudo PYTHONPATH=$PYTHONPATH $WORKSPACE/catkin-debs/scripts/setup_apt_root.py $distro $arch $rootdir --local-conf-dir $WORKSPACE --repo ros@http://packages.ros.org/ros/ubuntu
-
-sudo rm -rf $output_dir
-mkdir -p $output_dir
-
-sudo rm -rf $work_dir
-mkdir -p $work_dir
-cd $work_dir
-
-sudo apt-get update -c $aptconffile
-
-# Grab a newer version of pbuilder, because the one that ships with Lucid suffers from a bug when using --execute
-# https://bugs.launchpad.net/ubuntu/+source/pbuilder/+bug/811016
-rm -f pbuilder
-wget -O pbuilder http://bazaar.launchpad.net/~vcs-imports/pbuilder/trunk/download/head:/pbuilder/pbuilder
-chmod a+x pbuilder
-
-# Setup the pbuilder environment if not existing, or update
-if [ ! -e $basetgz ] || [ ! -s $basetgz ] 
-then
-  #make sure the base dir exists
-  sudo mkdir -p $base
-  #create the base image
-  sudo pbuilder create \
-    --distribution $distro \
-    --aptconfdir $rootdir/etc/apt \
-    --basetgz $basetgz \
-    --architecture $arch
-else
-  sudo pbuilder --update --basetgz $basetgz
-fi
-
-# Boilerplate.
-# DO NOT MODIFY
-###################################################
 
 cat > build.sh << DELIM
 ###################################################
