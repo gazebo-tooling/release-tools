9595e8b0c367b19d428b1501713a52171950be67
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-03-25 17:29:44 +0100

    Add script for use with quantal gui tests

---
+++ jenkins-scripts/gazebo-default-gui-test-devel-quantal-amd64.bash
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+# Hack to pick from current processes the DISPLAY available
+export DISPLAY=$(ps aux | grep "X :" | grep -v grep | awk '{ print $12 }')
+
+export DISTRO=quantal
+export ROS_DISTRO=groovy
+
+. ${SCRIPT_DIR}/lib/gazebo-base-default.bash
