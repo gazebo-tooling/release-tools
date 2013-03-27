d3ef343ad7094adde26913eed24cd90b875ed69a
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-03-27 19:47:51 +0100

    Initial support for sdf package

---
+++ jenkins-scripts/lib/sdf-default.bash
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+set -e
+
+. ${SCRIPT_DIR}/lib/boilerplate_prepare.sh
+
+cat > build.sh << DELIM
+###################################################
+# Make project-specific changes here
+#
+set -ex
+
+# Step 1: install everything you need
+apt-get install -s cmake build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev libboost-iostreams-dev libtinyxml-dev
+
+# Step 2: configure and build
+rm -rf $WORKSPACE/build
+mkdir -p $WORKSPACE/build
+cd $WORKSPACE/build
+ls $WORKSPACE
+cmake $WORKSPACE/sdf
+make -j3
+make install
+make test ARGS="-VV" || true
+DELIM
+
+# Make project-specific changes here
+###################################################
+
+sudo $WORKSPACE/pbuilder  --execute \
+    --bindmounts $WORKSPACE \
+    --basetgz $basetgz \
+    -- build.sh
