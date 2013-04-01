05eba711d6ca8585dad1c4ad95bf2d73c25f8406
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-04-02 00:03:02 +0200

    Initial support for nightly checkers

---
+++ jenkins-scripts/drcsim-nightly-checker-precise-amd64.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+export DISTRO=precise
+export ROS_DISTRO=fuerte
+
+export SOFTWARE_UNDER_TEST=drcsim
+
+. ${SCRIPT_DIR}/lib/install-checker-nightly.bash
---
+++ jenkins-scripts/lib/install-checker-nightly.bash
@@ -0,0 +1,43 @@
+#!/bin/bash -x
+set -e
+
+if [ -z ${SOFTWARE_UNDER_TEST} ]; then
+    echo "Need to define the SOFTWARE_UNDER_TEST variable"
+    exit 1
+fi
+
+. ${SCRIPT_DIR}/lib/boilerplate_prepare.sh
+
+cat > build.sh << DELIM
+###################################################
+# Make project-specific changes here
+#
+set -ex
+
+# get ROS repo's key
+apt-get install -y wget
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu ${DISTRO} main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | apt-key add -
+# Also get drc repo's key, to be used in getting Gazebo
+sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu ${DISTRO} main" > /etc/apt/sources.list.d/drc-latest.list'
+wget http://packages.osrfoundation.org/drc.key -O - | apt-key add -
+apt-get update
+
+# Step 1: install everything you need
+apt-get install -y ${SOFTWARE_UNDER_TEST}-nightly
+
+# Step 2: configure and build
+rm -rf $WORKSPACE/build
+mkdir -p $WORKSPACE/build
+cd $WORKSPACE/build
+cmake $WORKSPACE/${SOFTWARE_UNDER_TEST}
+make test ARGS="-VV" || true
+DELIM
+
+# Make project-specific changes here
+###################################################
+
+sudo pbuilder  --execute \
+    --bindmounts $WORKSPACE \
+    --basetgz $basetgz \
+    -- build.sh
