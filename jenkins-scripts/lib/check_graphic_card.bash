28fc2cf9cedd4a6a493a05d0be10321fb1107c66
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-04-13 21:26:41 +0200

    Check for synchornism in dri pkgs versions between host and chroot

--- jenkins-scripts/lib/check_graphic_card.bash
+++ jenkins-scripts/lib/check_graphic_card.bash
@@ -8,7 +8,8 @@ if [ -n "$(lspci -v | grep nvidia | head -n 2 | grep "Kernel driver in use: nvid
 	echo "Nvidia support found but not the module in use"
 	exit 1
     fi
-    echo "Nvidia found using package ${GRAPHIC_CARD_PKG}"
+    # Check for host installed version
+    export GRAPHIC_CARD_NAME="Nvidia"
     export GRAPHIC_CARD_FOUND=true
 fi
 
@@ -17,6 +18,11 @@ ATI_SUPPORT_RESULT=
 if [ -n "$(lspci -v | grep "ATI" | grep "VGA")" ]; then
     # TODO search for correct version of fglrx
     export GRAPHIC_CARD_PKG=fglrx
-    echo "ATI found using package ${GRAPHIC_CARD_PKG}"
+    export GRAPHIC_CARD_NAME="ATI"
     export GRAPHIC_CARD_FOUND=true
 fi
+
+# Get version of package 
+export GRAPHIC_CARD_PKG_VERSION=$(dpkg -l | grep "^ii.*${GRAPHIC_CARD_PKG}\ " | awk '{ print $3 }')
+echo "${GRAPHIC_CARD_NAME} found using package ${GRAPHIC_CARD_PKG} (${GRAPHIC_CARD_PKG_VERSION})"
+
