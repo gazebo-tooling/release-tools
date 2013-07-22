406bcfe9fc5a8c9e3374e3823493593f57a8f9f9
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-07-22 16:47:35 +0200

    New raring scripts

---
+++ jenkins-scripts/gazebo-default-devel-raring-amd64.bash
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+export DISTRO=raring
+
+. ${SCRIPT_DIR}/lib/gazebo-base-default.bash
