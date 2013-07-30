18beed8af4aad51201f10fbb51b03e8d8801fe4b
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-07-30 12:02:15 +0200

    Proper ros dependencies for ftcsim

--- jenkins-scripts/lib/ftcsim-default.bash
+++ jenkins-scripts/lib/ftcsim-default.bash
@@ -19,7 +19,7 @@ wget http://packages.osrfoundation.org/drc.key -O - | apt-key add -
 apt-get update
 
 # Step 1: install everything you need
-apt-get install -y python pkg-config cmake build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev libboost-iostreams-dev libtinyxml-dev cppcheck gazebo ros-${ROS_DISTRO}-ros-comm
+apt-get install -y python pkg-config cmake build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev libboost-iostreams-dev libtinyxml-dev cppcheck gazebo ros-${ROS_DISTRO}-sensors-msgs ros-${ROS_DISTRO}-image-transport
 
 # Step 2: configure and build
 if [ $DISTRO = quantal ]; then
