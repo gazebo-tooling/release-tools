565413ee0d27bbd27e2d237f6b8de83bc7698cce
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-07-30 11:37:31 +0200

    ROS support for ftcsim

--- jenkins-scripts/lib/ftcsim-default.bash
+++ jenkins-scripts/lib/ftcsim-default.bash
@@ -9,20 +9,39 @@ cat > build.sh << DELIM
 #
 set -ex
 
+# get ROS repo's key
 apt-get install -y wget
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu ${DISTRO} main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | apt-key add -
+# Also get drc repo's key, to be used in getting Gazebo
 sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu ${DISTRO} main" > /etc/apt/sources.list.d/drc-latest.list'
 wget http://packages.osrfoundation.org/drc.key -O - | apt-key add -
 apt-get update
 
 # Step 1: install everything you need
-apt-get install -y python pkg-config cmake build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev libboost-iostreams-dev libtinyxml-dev cppcheck gazebo
+apt-get install -y python pkg-config cmake build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev libboost-iostreams-dev libtinyxml-dev cppcheck gazebo ros-${ROS_DISTRO}-ros-base
 
 # Step 2: configure and build
+if [ $DISTRO = quantal ]; then
+    rosdep init 
+    # Hack for not failing when github is down
+    update_done=false
+    seconds_waiting=0
+    while (! \$update_done); do
+      rosdep update && update_done=true
+      sleep 1
+      seconds_waiting=$((seconds_waiting+1))
+      [ \$seconds_waiting -gt 60 ] && exit 1
+    done
+fi
+
+. /opt/ros/${ROS_DISTRO}/setup.sh
+. /usr/share/gazebo/setup.sh
 rm -rf $WORKSPACE/build
 mkdir -p $WORKSPACE/build
 cd $WORKSPACE/build
 cmake $WORKSPACE/ftcsim
-make -j3
+make -j${MAKE_JOBS}
 make install
 make test ARGS="-VV" || true
 
