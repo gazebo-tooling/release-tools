c77aafe88b9302ada00aac4e7132b9e5c6e04dd7
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-08-16 20:01:00 +0200

    Adapt depencies for including raring

--- jenkins-scripts/lib/dependencies_archive.sh
+++ jenkins-scripts/lib/dependencies_archive.sh
@@ -20,8 +20,8 @@ fi
 # image-transport-plugins is needed to properly advertise compressed image topics
 DRCSIM_BASE_DEPENDENCIES="ros-${ROS_DISTRO}-pr2-mechanism ros-${ROS_DISTRO}-std-msgs ros-${ROS_DISTRO}-common-msgs ros-${ROS_DISTRO}-image-common ros-${ROS_DISTRO}-geometry ros-${ROS_DISTRO}-pr2-controllers ros-${ROS_DISTRO}-geometry-experimental ros-${ROS_DISTRO}-robot-model-visualization ros-${ROS_DISTRO}-image-pipeline ros-${ROS_DISTRO}-console-bridge ${GAZEBO_DEB_PACKAGE} ros-${ROS_DISTRO}-image-transport-plugins ros-${ROS_DISTRO}-urdfdom"
 
-# Extra dependencies for groovy
-if [[ ${ROS_DISTRO} == 'groovy' ]]; then
+# Extra dependencies for groovy/hydro
+if [[ ${ROS_DISTRO} != 'precise' ]]; then
      DRCSIM_BASE_DEPENDENCIES="${DRCSIM_BASE_DEPENDENCIES} ros-groovy-compressed-depth-image-transport ros-groovy-compressed-image-transport ros-groovy-theora-image-transport"
 fi
 
