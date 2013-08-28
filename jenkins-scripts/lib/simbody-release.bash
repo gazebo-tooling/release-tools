571df3e59e2fd26e584adf905138077e0d19eac1
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-08-28 16:30:06 +0200

    New script to build simbody upstream

---
+++ jenkins-scripts/lib/simbody-release.bash
@@ -0,0 +1,91 @@
+#!/bin/bash -x
+
+
+. ${SCRIPT_DIR}/lib/boilerplate_prepare.sh
+
+cat > build.sh << DELIM
+###################################################
+# Make project-specific changes here
+#
+#!/usr/bin/env bash
+set -ex
+
+# ccache is sometimes broken and has now reason to be used here
+# http://lists.debian.org/debian-devel/2012/05/msg00240.html
+echo "unset CCACHEDIR" >> /etc/pbuilderrc
+
+# Install deb-building tools
+apt-get install -y pbuilder fakeroot debootstrap devscripts dh-make ubuntu-dev-tools debhelper wget git
+
+# Hack to avoid problem with non updated 
+if [ $DISTRO = 'precise' ]; then
+  echo "Skipping pbuilder check for outdated info"
+  sed -i -e 's:UbuntuDistroInfo().devel():self.target_distro:g' /usr/bin/pbuilder-dist
+fi
+
+# Step 0: create/update distro-specific pbuilder environment
+pbuilder-dist $DISTRO $ARCH create /etc/apt/trusted.gpg --debootstrapopts --keyring=/etc/apt/trusted.gpg
+
+# Step 0: Clean up
+rm -rf $WORKSPACE/build
+mkdir -p $WORKSPACE/build
+cd $WORKSPACE/build
+
+# Step 1: Get the source (nightly builds or tarball)
+git clone https://github.com/j-rivero/simbody.git $WORKSPACE/simbody
+cd $WORKSPACE/simbody
+
+# Step 5: use debuild to create source package
+debuild -S -uc -us --source-option=--include-binaries -j${MAKE_JOBS}
+
+# Step 6: use pbuilder-dist to create binary package(s)
+pbuilder-dist $DISTRO $ARCH build ../*.dsc -j${MAKE_JOBS}
+
+# Set proper package names
+PKG_NAME=${PACKAGE_ALIAS}_${VERSION}-${RELEASE_VERSION}~${DISTRO}_${ARCH}.deb
+DBG_PKG_NAME=${PACKAGE_ALIAS}-dbg_${VERSION}-${RELEASE_VERSION}~${DISTRO}_${ARCH}.deb
+
+mkdir -p $WORKSPACE/pkgs
+rm -fr $WORKSPACE/pkgs/*
+
+# Both paths are need, beacuse i386 use a different path
+MAIN_PKGS="/var/lib/jenkins/pbuilder/${DISTRO}_result/\${PKG_NAME} /var/lib/jenkins/pbuilder/${DISTRO}-${ARCH}_result/\${PKG_NAME}"
+DEBUG_PKGS="/var/lib/jenkins/pbuilder/${DISTRO}_result/\${DBG_PKG_NAME} /var/lib/jenkins/pbuilder/${DISTRO}-${ARCH}_result/\${DBG_PKG_NAME}"
+
+FOUND_PKG=0
+for pkg in \${MAIN_PKGS}; do
+    echo "looking for \$pkg"
+    if [ -f \${pkg} ]; then
+        echo "found \$pkg"
+	# Check for correctly generated packages size > 3Kb
+        test -z \$(find \$pkg -size +3k) && exit 1
+	cp \${pkg} $WORKSPACE/pkgs
+        FOUND_PKG=1
+        break;
+    fi
+done
+test \$FOUND_PKG -eq 1 || exit 1
+
+FOUND_PKG=0
+for pkg in \${DEBUG_PKGS}; do
+    if [ -f \${pkg} ]; then
+        # Check for correctly generated debug packages size > 3Kb
+        # when not valid instructions in rules/control it generates 1.5K package
+        test -z \$(find \$pkg -size +3k) && exit 1
+        cp \${pkg} $WORKSPACE/pkgs
+        FOUND_PKG=1
+        break;
+    fi
+done
+test \$FOUND_PKG -eq 1 || echo "No debug packages found. No upload"
+DELIM
+
+#
+# Make project-specific changes here
+###################################################
+
+sudo mkdir -p /var/packages/gazebo/ubuntu
+sudo pbuilder  --execute \
+    --bindmounts "$WORKSPACE /var/packages/gazebo/ubuntu" \
+    --basetgz $basetgz \
+    -- build.sh
