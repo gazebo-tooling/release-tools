3fe4722932100f14416be98184a1c8b62b820d45
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-09-04 01:07:55 +0200

    Use upstream real repository

--- jenkins-scripts/lib/simbody-release-base.bash
+++ jenkins-scripts/lib/simbody-release-base.bash
@@ -33,8 +33,7 @@ cd $WORKSPACE/build
 
 # Step 1: Get the source (nightly builds or tarball)
 rm -fr $WORKSPACE/simbody
-# git clone https://github.com/simbody/simbody.git -b simbody-${VERSION} $WORKSPACE/simbody
-git clone https://github.com/j-rivero/simbody.git $WORKSPACE/simbody
+git clone https://github.com/simbody/simbody.git -b simbody-${VERSION} $WORKSPACE/simbody
 rm -fr $WORKSPACE/*.orig.*
 cd $WORKSPACE/simbody
 # Use current distro
