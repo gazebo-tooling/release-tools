3d823fd861b30b6c254caca3dafdccb1c0c58540
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-09-09 18:08:51 +0200

    Final fixes to run abi-checker

--- jenkins-scripts/lib/sdformat-default.bash
+++ jenkins-scripts/lib/sdformat-default.bash
@@ -9,22 +9,60 @@ cat > build.sh << DELIM
 #
 set -ex
 
+apt-get install -y wget
+sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu ${DISTRO} main" > /etc/apt/sources.list.d/drc-latest.list'
+wget http://packages.osrfoundation.org/drc.key -O - | apt-key add -
+apt-get update
+
 # Step 1: install everything you need
-apt-get install -y python cmake build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev libboost-iostreams-dev libtinyxml-dev cppcheck
+apt-get install -y python cmake build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev libboost-iostreams-dev libtinyxml-dev cppcheck sdformat exuberant-ctags git 
 
 # Step 2: configure and build
 rm -rf $WORKSPACE/build
 mkdir -p $WORKSPACE/build
 cd $WORKSPACE/build
-cmake $WORKSPACE/sdformat
+cmake $WORKSPACE/sdformat -DCMAKE_INSTALL_PREFIX=/usr/local
 make -j3
 make install
 make test ARGS="-VV" || true
 
-# Step 3: code check
-cd $WORKSPACE/sdformat
-sh tools/code_check.sh -xmldir $WORKSPACE/build/cppcheck_results || true
-cat $WORKSPACE/build/cppcheck_results/*.xml
+# Install abi-compliance-checker.git
+cd $WORKSPACE
+git clone git://github.com/lvc/abi-compliance-checker.git  
+cd abi-compliance-checker
+perl Makefile.pl -install --prefix=/usr
+
+mkdir -p $WORKSPACE/abi_checker
+cd $WORKSPACE/abi_checker
+cat > pkg.xml << CURRENT_DELIM
+ <version>
+     development
+ </version>
+   
+ <headers>
+    /usr/include/sdformat-1.4/
+ </headers>
+   
+ <libs>
+    /usr/lib/
+ </libs>
+CURRENT_DELIM
+
+cat > devel.xml << DEVEL_DELIM
+ <version>
+     development
+ </version>
+   
+ <headers>
+    /usr/local/include/sdformat-1.4//
+ </headers>
+   
+ <libs>
+    /usr/local/lib/
+ </libs>
+DEVEL_DELIM
+
+abi-compliance-checker -lib sdformat -old pkg.xml -new devel.xml
 DELIM
 
 # Make project-specific changes here
