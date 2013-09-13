bcd48628c282ef0e42eaa9088c9aeffbe4683493
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-09-13 13:27:50 +0200

    Migrate script from precise -> quantal

--- jenkins-scripts/gazebo-default-sdformat-default-devel-quantal-amd64.bash
+++ jenkins-scripts/gazebo-default-sdformat-default-devel-quantal-amd64.bash
@@ -4,7 +4,6 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-export DISTRO=precise
-export ROS_DISTRO=fuerte
+export DISTRO=quantal
 
 . ${SCRIPT_DIR}/lib/gazebo-default-sdformat-default-base.bash
