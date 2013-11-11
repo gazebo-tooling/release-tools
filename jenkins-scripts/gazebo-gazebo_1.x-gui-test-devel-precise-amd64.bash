2c72260b8fe724e035b934ea7b41f5a457c0c8c5
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-11-11 19:18:41 +0100

    Fix gui test

--- jenkins-scripts/gazebo-gazebo_1.x-gui-test-devel-precise-amd64.bash
+++ jenkins-scripts/gazebo-gazebo_1.x-gui-test-devel-precise-amd64.bash
@@ -4,6 +4,9 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
+# Hack to pick from current processes the DISPLAY available
+export DISPLAY=$(ps aux | grep "X :" | grep -v grep | awk '{ print $12 }')
+
 export DISTRO=precise
 export ROS_DISTRO=fuerte
 export USE_OLD_SDFORMAT=true
