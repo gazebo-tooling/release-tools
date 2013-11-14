aac98f0ec259d8c72251d3b919baa559c524638c
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-11-14 03:42:42 +0100

    Export ROS_VERSION depending on distro

--- jenkins-scripts/drcsim-release-checker.bash
+++ jenkins-scripts/drcsim-release-checker.bash
@@ -4,4 +4,18 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
+if [[ $DISTRO = 'precise' ]]; then
+  export ROS_DISTRO=groovy
+elif [[ $DISTRO = 'quantal' ]]; then
+  export ROS_DISTRO=groovy
+elif [[ $DISTRO = 'raring' ]]; then
+  export ROS_DISTRO=hydro
+elif [[ $DISTRO = 'saucy' ]]; then
+  # saucy is not supported in drcsim yet
+  exit 0
+else
+  echo "Unknow ubuntu distro. Fix your script"
+  exit 1
+fi
+
 . ${SCRIPT_DIR}/lib/drcsim-check-release.bash
