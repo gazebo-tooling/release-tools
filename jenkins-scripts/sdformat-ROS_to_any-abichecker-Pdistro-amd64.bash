5fab31153d1f54802d4f976e5a016fe536440bd1
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-11-22 18:56:25 +0100

    General Pdistro file

--- jenkins-scripts/sdformat-ROS_to_any-abichecker-Pdistro-amd64.bash
+++ jenkins-scripts/sdformat-ROS_to_any-abichecker-Pdistro-amd64.bash
@@ -4,7 +4,10 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-export DISTRO=quantal
+if [[ -z ${DISTRO} ]]; then
+  export DISTRO=quantal
+fi
+
 export REPO_TO_USE=ROS
 
 . ${SCRIPT_DIR}/lib/sdformat-any-base.bash
