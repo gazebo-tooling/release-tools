5c8c92d55bb80903cba060b4bcc4bf6345ea694d
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-12-02 23:25:26 +0100

    Avoid to check in ros-gazebo-pkgs

--- jenkins-scripts/lib/debbuild-bloom-base.bash
+++ jenkins-scripts/lib/debbuild-bloom-base.bash
@@ -106,8 +106,10 @@ PKGS=\`find /var/lib/jenkins/pbuilder -name *.deb || true\`
 FOUND_PKG=0
 for pkg in \${PKGS}; do
     echo "found \$pkg"
-    # Check for correctly generated packages size > 3Kb
-    test -z \$(find \$pkg -size +3k) && echo "WARNING: empty package?" && exit 1
+    if [ $PACKAGE != 'gazebo-ros-pkgs' ]; then
+      # Check for correctly generated packages size > 3Kb
+      test -z \$(find \$pkg -size +3k) && echo "WARNING: empty package?" && exit 1
+    fi
     cp \${pkg} $WORKSPACE/pkgs
     FOUND_PKG=1
 done
