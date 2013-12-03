b320c099220b8dfd6e9328661d149a595efdd029
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-12-03 01:23:48 +0100

    Do not run check for graphic card in drcsim release

--- jenkins-scripts/lib/drcsim-check-release.bash
+++ jenkins-scripts/lib/drcsim-check-release.bash
@@ -24,19 +24,8 @@ sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu ${DISTRO} main" > 
 wget http://packages.osrfoundation.org/drc.key -O - | apt-key add -
 apt-get update
 
-# Step 1: install everything you need
 
-# Optional stuff. Check for graphic card support
-if ${GRAPHIC_CARD_FOUND}; then
-    apt-get install -y ${GRAPHIC_CARD_PKG}
-    # Check to be sure version of kernel graphic card support is the same.
-    # It will kill DRI otherwise
-    CHROOT_GRAPHIC_CARD_PKG_VERSION=\$(dpkg -l | grep "^ii.*${GRAPHIC_CARD_PKG}\ " | awk '{ print \$3 }' | sed 's:-.*::')
-    if [ "\${CHROOT_GRAPHIC_CARD_PKG_VERSION}" != "${GRAPHIC_CARD_PKG_VERSION}" ]; then
-       echo "Package ${GRAPHIC_CARD_PKG} has different version in chroot and host system. Maybe you need to update your host" 
-       exit 1
-    fi
-fi
+# Do not run optional stuff: check for graphic card support
 
 # Step 2: configure and build
 
