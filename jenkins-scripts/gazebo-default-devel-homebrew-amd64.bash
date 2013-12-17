e12e468802503d816328cd8e05513d837ea673d4
Steve Peters <scpeters@osrfoundation.org>
2013-12-16 17:16:39 -0800

    Refactor homebrew script slightly, add a gazebo script as well

--- jenkins-scripts/gazebo-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/gazebo-default-devel-homebrew-amd64.bash
@@ -4,5 +4,5 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-. ${SCRIPT_DIR}/lib/project-default-devel-homebrew-amd64.bash sdformat
+. ${SCRIPT_DIR}/lib/project-default-devel-homebrew-amd64.bash gazebo
 
---
+++ jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+set -e
+
+# Get project name as first argument to this script
+PROJECT=$1
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+# Step 1. Set up homebrew
+RUN_DIR=$(mktemp -d ${HOME}/jenkins.XXXX)
+echo "Install into: ${RUN_DIR}"
+cd $RUN_DIR
+# Install homebrew
+curl -L -o homebrew.tar.gz https://github.com/Homebrew/homebrew/tarball/master 
+tar --strip 1 -xzf homebrew.tar.gz
+# Need to create cache so the system one (without permissions) is not used
+mkdir -p ${HOME}/Library/Caches/Homebrew
+
+# Step 2. Install ${PROJECT}
+${RUN_DIR}/bin/brew tap osrf/simulation
+${RUN_DIR}/bin/brew install `${RUN_DIR}/bin/brew deps ${PROJECT}`
+
+# Step 3. Manually compile and install ${PROJECT}
+cd ${WORKSPACE}/${PROJECT}
+mkdir -p ${WORKSPACE}/build
+cd ${WORKSPACE}/build
+${RUN_DIR}/bin/cmake ${WORKSPACE}/${PROJECT} \
+      -DCMAKE_INSTALL_PREFIX=${RUN_DIR}/Cellar/${PROJECT}/HEAD \
+      -DCMAKE_PREFIX_PATH=${RUN_DIR}
+
+make -j${MAKE_JOBS} install
+${RUN_DIR}/bin/brew link ${PROJECT}
+
+# Step 4. Testing
+make test ARGS="-VV" || true
+
+# Step 5. Clean up
+rm -fr ${RUN_DIR}
