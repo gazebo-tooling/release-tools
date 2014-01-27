259bc15363fe6af5c77bd5f575b4af21671188de
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-01-27 13:07:30 +0100

    New support for multiros distributions

---
+++ jenkins-scripts/multidistribution-rosdistro-debbuild.bash
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+export RELEASE_REPO_DIRECTORY=${DISTRO}/${ROS_DISTRO}
+export ENABLE_ROS=true
+. ${SCRIPT_DIR}/lib/debbuild-base.bash
+
