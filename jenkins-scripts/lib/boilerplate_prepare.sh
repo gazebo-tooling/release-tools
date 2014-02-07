be7c62aada6e2793a50eb892c1c29a2904f5e37d
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-02-07 13:53:23 +0100

    Fix workaround

--- jenkins-scripts/lib/boilerplate_prepare.sh
+++ jenkins-scripts/lib/boilerplate_prepare.sh
@@ -31,9 +31,6 @@ fi
 . ${SCRIPT_DIR}/lib/dependencies_archive.sh
       
 
-distro=${DISTRO}
-arch=amd64
-base=/var/cache/pbuilder-$distro-$arch
 
 # Workaround for precise pbuilder-dist segfault
 # https://bitbucket.org/osrf/release-tools/issue/22
@@ -43,8 +40,12 @@ fi
 
 if $WORKAROUND_PBUILDER_BUG && [[ $DISTRO == 'precise' ]]; then
   distro=raring
+else
+  distro=${DISTRO}
 fi
 
+arch=amd64
+base=/var/cache/pbuilder-$distro-$arch
 aptconffile=$WORKSPACE/apt.conf
 
 #increment this value if you have changed something that will invalidate base tarballs. #TODO this will need cleanup eventually.
