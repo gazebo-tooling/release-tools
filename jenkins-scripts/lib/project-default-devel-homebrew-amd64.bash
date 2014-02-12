8e710a88a9bf502f07c700e21813ac25ee5a502a
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-02-12 20:25:12 +0100

    Proper params to ln

--- jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
@@ -37,7 +37,7 @@ if [[ $PROJECT == 'gazebo' ]]; then
   # The bottle has some hardcoded files in qmake configurations. Hack them.
   # see https://bitbucket.org/osrf/release-tools/pull-request/30
   rm -fr ${HOME}/jenkins.R7cR
-  ln -s ${HOME}/jenkins.R7cR ${RUN_DIR}
+  ln -s ${RUN_DIR} ${HOME}/jenkins.R7cR 
 fi
 # Process the package dependencies
 ${RUN_DIR}/bin/brew install ${PROJECT} --only-dependencies
