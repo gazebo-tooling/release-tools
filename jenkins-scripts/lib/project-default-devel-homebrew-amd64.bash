f34d306339b08481be1eec9eb07e27cd9653f75c
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-02-12 17:56:48 +0100

    Replace bad refrences in qt bottle

--- jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
@@ -33,6 +33,10 @@ if [[ $PROJECT == 'gazebo' ]]; then
   fi
 
   ${RUN_DIR}/bin/brew install "${LOCAL_CELLAR}/qt-4.8.5.mavericks.bottle.tar.gz"
+  # The bottle has some hardcoded files in qmake configurations. Hack them.
+  # see https://bitbucket.org/osrf/release-tools/pull-request/30
+  find ${RUN_DIR}/Cellar ${RUN_DIR}/lib -name '*.prl' -exec \
+      sed -i -e "s:jenkins.R7cR:${RUN_DIR}:g" {} \;
 fi
 # Process the package dependencies
 ${RUN_DIR}/bin/brew install ${PROJECT} --only-dependencies
