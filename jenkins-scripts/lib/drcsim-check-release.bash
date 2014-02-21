146b0890bb1499304cfd614839c61337c8a84bd1
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-02-21 20:21:32 +0100

    Need graphic card support

--- jenkins-scripts/lib/drcsim-check-release.bash
+++ jenkins-scripts/lib/drcsim-check-release.bash
@@ -24,7 +24,17 @@ sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu ${DISTRO} main" > 
 wget http://packages.osrfoundation.org/drc.key -O - | apt-key add -
 apt-get update
 
-# Do not run optional stuff: check for graphic card support
+# Optional stuff. Check for graphic card support
+if ${GRAPHIC_CARD_FOUND}; then
+    apt-get install -y ${GRAPHIC_CARD_PKG}
+    # Check to be sure version of kernel graphic card support is the same.
+    # It will kill DRI otherwise
+    CHROOT_GRAPHIC_CARD_PKG_VERSION=\$(dpkg -l | grep "^ii.*${GRAPHIC_CARD_PKG}\ " | awk '{ print \$3 }' | sed 's:-.*::')
+    if [ "\${CHROOT_GRAPHIC_CARD_PKG_VERSION}" != "${GRAPHIC_CARD_PKG_VERSION}" ]; then
+       echo "Package ${GRAPHIC_CARD_PKG} has different version in chroot and host system. Maybe you need to update your host" 
+       exit 1
+    fi
+fi
 
 DRCSIM_PKG=drcsim
 # check for several distros on precise
