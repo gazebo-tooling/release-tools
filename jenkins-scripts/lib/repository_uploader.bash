5213f9e480111e7669b681acb4475a7b5c32b561
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-02-28 17:02:32 +0100

    Support for repeated _all.deb packages

--- jenkins-scripts/lib/repository_uploader.bash
+++ jenkins-scripts/lib/repository_uploader.bash
@@ -16,10 +16,46 @@ fi
 # Place in reprepro directory
 cd /var/packages/gazebo/ubuntu
 
+upload_package()
+{
+    local pkg=$1
+
+    sudo GNUPGHOME=$HOME/.gnupg reprepro --nothingiserror includedeb $DISTRO ${pkg}
+    scp -o StrictHostKeyChecking=no -i $HOME/.ssh/id_rsa ${pkg} \
+               ubuntu@gazebosim.org:/var/www/assets/distributions
+}
+
 for pkg in `ls $WORKSPACE/pkgs/*.deb`; do
-  sudo GNUPGHOME=$HOME/.gnupg reprepro --nothingiserror includedeb $DISTRO ${pkg}
-  scp -o StrictHostKeyChecking=no -i $HOME/.ssh/id_rsa ${pkg} ubuntu@gazebosim.org:/var/www/assets/distributions
+  # Get components from pkg
+  pkg_name=${pkg/_*} # get the first part only
+  pkg_suffix=${pkg##*_} # amd64.deb, i386.deb, all.deb
+  pkg_version=${pkg#*_} # remove package name
+  pkg_version=${pkg_version/_*} # remove package suffix
+
+  case ${pkg_suffix} in
+      i386.deb | amd64.deb)
+	  upload_package ${pkg}
+      ;;
+      all.deb)
+	# Check if the package already exists. i386 and amd64 generates the same binaries.
+	# all should be multiarch, so supposed to work on every platform
+	$existing_version=$(sudo GNUPGHOME=/var/lib/jenkins/.gnupg/ ls ${pkg_name} | grep ${DISTRO} | awk '{ print $3 }')
+	if [[ ${existing_version} == ${pkg_version} ]]; then
+	    echo "${pkg} for ${DISTRO} is already in the repo"
+	    echo "SKIP UPLOAD"
+	    continue
+	fi
+	upload_package ${pkg}
+      ;;
+      *)
+	  echo "ERROR: unknown pkg_suffix: ${pkg_suffix}"
+	  exit 1
+      ;;
+  esac
 done
+
+
+
 
 
 rm -fr $WORKSPACE/pkgs/*.deb
