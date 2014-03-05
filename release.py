02916adbbd087a4d5fed2a254d7c358488ddcd86
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-03-05 19:28:18 +0100

    Change support for Saucy and introduce Trusty

--- release.py
+++ release.py
@@ -17,8 +17,8 @@ UPLOAD_DEST = 'ubuntu@gazebosim.org:/var/www/assets/distributions'
 DOWNLOAD_URI = 'http://gazebosim.org/assets/distributions/'
 
 UBUNTU_ARCHS = ['amd64', 'i386']
-UBUNTU_DISTROS = ['precise', 'quantal', 'raring']
-UBUNTU_DISTROS_EXPERIMENTAL = ['saucy']
+UBUNTU_DISTROS = ['precise', 'quantal', 'raring','saucy']
+UBUNTU_DISTROS_EXPERIMENTAL = ['trusty']
 
 DRY_RUN = False
 NIGHTLY = False
