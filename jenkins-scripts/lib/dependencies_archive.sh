a7c44397b53712d4a4f61ff44ef15ac79d5ab623
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-04-10 02:25:11 +0200

    Update prerelease package

--- jenkins-scripts/lib/dependencies_archive.sh
+++ jenkins-scripts/lib/dependencies_archive.sh
@@ -35,7 +35,7 @@ fi
 if ${USE_OLD_SDFORMAT}; then
     sdformat_pkg="sdformat"
 else
-    sdformat_pkg="libsdformat-dev-prerelease"
+    sdformat_pkg="libsdformat2-dev-prerelease"
 fi
 
 # GAZEBO related dependencies
