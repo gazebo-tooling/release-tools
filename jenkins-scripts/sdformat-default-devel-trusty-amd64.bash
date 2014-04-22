bfe8cf1af371f10dbdf0dcfded823f74e1c8b2de
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-04-22 02:35:34 +0200

    Update sdformat status

--- jenkins-scripts/sdformat-default-devel-trusty-amd64.bash
+++ jenkins-scripts/sdformat-default-devel-trusty-amd64.bash
@@ -4,6 +4,6 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-export DISTRO=raring
+export DISTRO=trusty
 
 . ${SCRIPT_DIR}/lib/sdformat-default.bash
