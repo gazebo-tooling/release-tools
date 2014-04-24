cd95dce02fd51a9606496fd03b4e4539ed0df8cd
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-04-24 17:30:04 +0200

    add new precise script

---
+++ jenkins-scripts/gazebo-default-coverage-precise-amd64.bash
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+export DISPLAY=$(ps aux | grep "X :" | grep -v grep | awk '{ print $12 }')
+export DISTRO=precise
+export COVERAGE_ENABLED=true
+. ${SCRIPT_DIR}/lib/gazebo-base-default.bash
