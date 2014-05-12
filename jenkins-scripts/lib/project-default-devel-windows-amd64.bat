ded0a7311d51b80b9770ba17589ce9ef858cbcc9
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-05-12 14:48:38 +0200

    Fix typo and launch compilation

--- jenkins-scripts/lib/project-default-devel-windows-amd64.bat
+++ jenkins-scripts/lib/project-default-devel-windows-amd64.bat
@@ -3,8 +3,8 @@ REM Windows standard file
 call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 echo %WORKSPACE%
 cd %WORKSPACE%
-mkidr build
+mkdir build
 cd build
 cmake ..
 dir
-msbuild
+msbuild ALL_BUILD.vcxproj  
