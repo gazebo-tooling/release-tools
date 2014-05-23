d2dd5da428cec71dd4dec8848be2416082ace725
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-05-23 16:48:37 +0200

    Use CMAKE_BUILD_TYPE

--- jenkins-scripts/lib/project-default-devel-windows-amd64.bat
+++ jenkins-scripts/lib/project-default-devel-windows-amd64.bat
@@ -6,6 +6,6 @@ cd %WORKSPACE%
 mkdir build
 cd build
 REM Remeber to pass the generator for a proper Win64 built
-cmake .. -G"Visual Studio 12 Win64" -DBOOST_ROOT:STRING="C:\local\boost_1_55_0" -DBOOST_LIBRARYDIR:STRING="C:\local\boost_1_55_0\lib64-msvc-12.0"
+cmake .. -G"Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE:STRING="Release" -DBOOST_ROOT:STRING="C:\local\boost_1_55_0" -DBOOST_LIBRARYDIR:STRING="C:\local\boost_1_55_0\lib64-msvc-12.0"
 
 msbuild ALL_BUILD.vcxproj
