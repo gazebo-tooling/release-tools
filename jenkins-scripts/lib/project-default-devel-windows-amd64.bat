a8bc0bef9b7301474bd34c51f948298cb00c3989
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-05-23 16:28:03 +0200

    Define cmake generator

--- jenkins-scripts/lib/project-default-devel-windows-amd64.bat
+++ jenkins-scripts/lib/project-default-devel-windows-amd64.bat
@@ -5,5 +5,6 @@ echo %WORKSPACE%
 cd %WORKSPACE%
 mkdir build
 cd build
-cmake .. -DBOOST_ROOT:STRING="C:\local\boost_1_55_0" -DBOOST_LIBRARYDIR:STRING="C:\local\boost_1_55_0\lib64-msvc-12.0"
-msbuild ALL_BUILD.vcxproj  
+cmake .. -G"Visual Studio 12 Win64" -DBOOST_ROOT:STRING="C:\local\boost_1_55_0" -DBOOST_LIBRARYDIR:STRING="C:\local\boost_1_55_0\lib64-msvc-12.0"
+
+msbuild ALL_BUILD.vcxproj
