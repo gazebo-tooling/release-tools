f12d1dc1754a796f79bf58d626f0f6b9c0f4e3ed
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-06-17 18:41:27 +0200

    Script to use with ignition packages

---
+++ jenkins-scripts/multidistribution-ignition-debbuild.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+export RELEASE_REPO_DIRECTORY=${DISTRO}
+export ENABLE_ROS=false
+export WORKAROUND_PBUILDER_BUG=true
+export BITBUCKET_REPO=ignitionrobotics
+
+. ${SCRIPT_DIR}/lib/debbuild-base.bash
