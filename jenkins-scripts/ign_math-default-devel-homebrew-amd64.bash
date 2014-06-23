3e04ad76e214e191b743e77ee09eb325f1e345fd
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-06-23 18:07:02 +0200

    Fix project names

--- jenkins-scripts/ign_math-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/ign_math-default-devel-homebrew-amd64.bash
@@ -4,4 +4,4 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-. ${SCRIPT_DIR}/lib/project-default-devel-homebrew-amd64.bash ign-math
+. ${SCRIPT_DIR}/lib/project-default-devel-homebrew-amd64.bash ignition-math
