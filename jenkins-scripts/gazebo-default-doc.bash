6e212b8ebafc7b279789f6e44ff8af180aaa6f18
Nate Koenig <natekoenig@gmail.com>
2014-07-08 10:00:15 -0700

    Removing latex documentation build

--- jenkins-scripts/gazebo-default-doc.bash
+++ jenkins-scripts/gazebo-default-doc.bash
@@ -24,7 +24,7 @@ apt-get install -y ${BASE_DEPENDENCIES} ${GAZEBO_BASE_DEPENDENCIES}
 # Optional stuff for Gazebo
 apt-get install -y ${GAZEBO_EXTRA_DEPENDENCIES}
 #TODO: pare down to just the following install; blocked on https://bitbucket.org/osrf/gazebo/issue/27/
-apt-get install -y doxygen graphviz texlive-latex-base texlive-latex-extra texlive-latex-recommended latex-xcolor texlive-fonts-recommended
+apt-get install -y doxygen graphviz
 
 # Step 2: configure and build
 
@@ -42,8 +42,6 @@ apt-get install -y openssh-client
 ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@gazebosim.org sudo rm -rf /var/www/api/dev /tmp/gazebo_dev /tmp/dev.html
 scp -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa -r doxygen/html ubuntu@gazebosim.org:/tmp/gazebo_dev
 ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@gazebosim.org sudo mv /tmp/gazebo_dev /var/www/api/dev
-scp -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa doxygen/latex/gazebo-[0-9]*.[0-9]*.[0-9]*.pdf ubuntu@gazebosim.org:/tmp/gazebo-dev.pdf
-ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@gazebosim.org sudo mv /tmp/gazebo-dev.pdf /var/www/api/gazebo-dev.pdf
 scp -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa dev.html ubuntu@gazebosim.org:/tmp/dev.html
 ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@gazebosim.org sudo mv /tmp/dev.html /var/www/sdf/dev.html
 DELIM
