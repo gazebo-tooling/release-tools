ba9660a361c262174b268bc76888f1d7e3269957
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-07-17 01:59:28 +0200

    Migrate from precise to trusty

--- jenkins-scripts/gazebo-default-sdformat-default-devel-trusty-amd64.bash
+++ jenkins-scripts/gazebo-default-sdformat-default-devel-trusty-amd64.bash
@@ -4,7 +4,7 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-export DISTRO=precise
+export DISTRO=trusty
 export DART_USE_4_VERSION=true
 
 . ${SCRIPT_DIR}/lib/gazebo-default-sdformat-default-base.bash
