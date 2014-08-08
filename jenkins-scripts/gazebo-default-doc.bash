fd3996a6605765ec0b1b3f3fbb7ebae4e610e5d5
Nate Koenig <natekoenig@gmail.com>
2014-08-08 11:52:34 -0700

    Move upload to old.gazebosim.org

--- jenkins-scripts/gazebo-default-doc.bash
+++ jenkins-scripts/gazebo-default-doc.bash
@@ -39,11 +39,11 @@ make doc
 
 # Step 3: upload docs
 apt-get install -y openssh-client
-ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@gazebosim.org sudo rm -rf /var/www/api/dev /tmp/gazebo_dev /tmp/dev.html
-scp -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa -r doxygen/html ubuntu@gazebosim.org:/tmp/gazebo_dev
-ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@gazebosim.org sudo mv /tmp/gazebo_dev /var/www/api/dev
-scp -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa dev.html ubuntu@gazebosim.org:/tmp/dev.html
-ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@gazebosim.org sudo mv /tmp/dev.html /var/www/sdf/dev.html
+ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@old.gazebosim.org sudo rm -rf /var/www/api/dev /tmp/gazebo_dev /tmp/dev.html
+scp -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa -r doxygen/html ubuntu@old.gazebosim.org:/tmp/gazebo_dev
+ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@old.gazebosim.org sudo mv /tmp/gazebo_dev /var/www/api/dev
+scp -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa dev.html ubuntu@old.gazebosim.org:/tmp/dev.html
+ssh -o StrictHostKeyChecking=no -i $WORKSPACE/id_rsa ubuntu@old.gazebosim.org sudo mv /tmp/dev.html /var/www/sdf/dev.html
 DELIM
 
 # Copy in my ssh keys, to allow the above ssh/scp calls to work; not sure this is the best way to do it, 
