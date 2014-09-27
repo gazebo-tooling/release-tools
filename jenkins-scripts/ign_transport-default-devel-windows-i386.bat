7f1cea790c876062366f306eafc12dc585d8e3ab
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-09-27 19:21:46 +0200

    Need to set path for protobuf

--- jenkins-scripts/ign_transport-default-devel-windows-i386.bat
+++ jenkins-scripts/ign_transport-default-devel-windows-i386.bat
@@ -3,6 +3,7 @@ set SCRIPT_DIR="%~dp0"
 call "%SCRIPT_DIR%/lib/windows_configuration.bat"
 REM x64 or x86
 set PLATFORM_TO_BUILD=x86
+set PATH=%PATH%;C:\local\protobuf_2.6.0
 set ARG_CMAKE_FLAGS=%CMAKE_ZEROMQ_FLAGS% %CMAKE_PROTOBUF_FLAGS% %CMAKE_CPPZMQ_FLAGS%
 
 call "%SCRIPT_DIR%/lib/project-default-devel-windows.bat"
