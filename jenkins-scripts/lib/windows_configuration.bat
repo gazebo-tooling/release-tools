759f081a98024a0759743873cd1b70866703550d
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-09-27 18:39:22 +0200

    Fix quoting

--- jenkins-scripts/lib/windows_configuration.bat
+++ jenkins-scripts/lib/windows_configuration.bat
@@ -1,7 +1,7 @@
-set VS32bits_CMAKE_GEN='-G"Visual Studio 12"'
-set VS64bits_CMAKE_GEN='-G"Visual Studio 12 Win64"'
+set VS32bits_CMAKE_GEN=-G"Visual Studio 12"
+set VS64bits_CMAKE_GEN=-G"Visual Studio 12 Win64"
 
-set VS_DEFAULT_FLAGS='/p:Configuration=Release -DCMAKE_BUILD_TYPE:STRING="Release"'
+set VS_DEFAULT_FLAGS="/p:Configuration=Release -DCMAKE_BUILD_TYPE:STRING=Release"
 
 REM Software avalibale on windows node
-set WINNODE_BOOST_ROOT='-DBOOST_ROOT:STRING="C:\local\boost_1_55_0" -DBOOST_LIBRARYDIR:STRING="C:\local\boost_1_55_0\lib64-msvc-12.0"'
+set ARG_CMAKE_FLAGS='-DBOOST_ROOT:STRING="C:\local\boost_1_55_0" -DBOOST_LIBRARYDIR:STRING="C:\local\boost_1_55_0\lib64-msvc-12.0"'
