f699b426f9dc06524ea7222eb023504b0e2295d5
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-09-27 18:14:42 +0200

    Need script dir in windows

--- jenkins-scripts/ign_transport-default-devel-windows-i386.bat
+++ jenkins-scripts/ign_transport-default-devel-windows-i386.bat
@@ -1,7 +1,12 @@
 #!/bin/bash -x
 
-call "lib/windows_configuration.bat"
+echo" %~dp0"
+echo" %~dp"
+
+set SCRIPT_DIR="%~dp"
+
+call "%SCRIPT_DIR%/lib/windows_configuration.bat"
 set PLATFORM_TO_BUILD="32"
 set ARG_CMAKE_ARGS="%WINNODE_BOOST_ROOT%"
 
-call "lib/project-default-devel-windows.bat"
+call "%SCRIPT_DIR%/lib/project-default-devel-windows.bat"
