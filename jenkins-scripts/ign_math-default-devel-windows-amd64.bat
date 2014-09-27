12aa43cd64ad54a67852c1575cb77667912ddf15
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-09-27 19:00:38 +0200

    Update scripts with dependencies

--- jenkins-scripts/ign_math-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_math-default-devel-windows-amd64.bat
@@ -3,6 +3,6 @@ set SCRIPT_DIR="%~dp0"
 call "%SCRIPT_DIR%/lib/windows_configuration.bat"
 REM x64 or x86
 set PLATFORM_TO_BUILD=x64
-set ARG_CMAKE_FLAGS=%CMAKE_BOOST_FLAGS%
+set ARG_CMAKE_FLAGS=
 
 call "%SCRIPT_DIR%/lib/project-default-devel-windows.bat"
