0f0d6ddb5d9406b80261b94e53ca2baefe5030d4
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-09-27 19:05:34 +0200

    Use 32bits for ig-math

--- jenkins-scripts/lib/windows_configuration.bat
+++ jenkins-scripts/lib/windows_configuration.bat
@@ -8,6 +8,5 @@ REM Software avalibale on windows node
 set CMAKE_BOOST_FLAGS=-DBOOST_ROOT:STRING="C:\local\boost_1_55_0" -DBOOST_LIBRARYDIR:STRING="C:\local\boost_1_55_0\lib64-msvc-12.0"
 
 set CMAKE_ZEROMQ_FLAGS=-DZeroMQ_ROOT_DIR="C:\Program Files (x86)\ZeroMQ 4.0.4" 
-set CMAKE_PROTOBUF_FLAGS=-DPROTOBUF_SRC_ROOT_FOLDER="C:\local\protobuf-2.6.0" -
-SET CMAKE_CPPZMQ_FLAGS=-DCPPZMQ_HEADER_PATH="C:\local\cppzmq"
-
+set CMAKE_PROTOBUF_FLAGS=-DPROTOBUF_SRC_ROOT_FOLDER="C:\local\protobuf-2.6.0"
+Set CMAKE_CPPZMQ_FLAGS=-DCPPZMQ_HEADER_PATH="C:\local\cppzmq"
