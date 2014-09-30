ac4c7bdfb5ac187509563232b1457c1bf151cd8e
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-09-30 14:27:31 +0200

    Debug message

--- jenkins-scripts/lib/drcsim-base.bash
+++ jenkins-scripts/lib/drcsim-base.bash
@@ -62,6 +62,7 @@ mkdir -p $WORKSPACE/build $WORKSPACE/install
 cd $WORKSPACE/build
 
 # Do not use atlassiminterface in 32 bist
+echo "Check for atlassimitnerface in 32 bits"
 if [ "$DISTRO" = "i386" ]; then
     EXTRA_ARGS="-DUSE_ATLASSIMINTERFACE_BLOB:BOOL=False"
 fi
