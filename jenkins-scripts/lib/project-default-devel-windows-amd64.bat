f989152495ce274ffb51fb479dd5cb5156dc39e9
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-10-02 18:46:36 +0200

    Added testing phase to windows

--- jenkins-scripts/lib/project-default-devel-windows-amd64.bat
+++ jenkins-scripts/lib/project-default-devel-windows-amd64.bat
@@ -9,3 +9,6 @@ REM Remeber to pass the generator for a proper Win64 built
 cmake .. -G"Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE:STRING="Release" -DBOOST_ROOT:STRING="C:\local\boost_1_55_0" -DBOOST_LIBRARYDIR:STRING="C:\local\boost_1_55_0\lib64-msvc-12.0"
 
 msbuild ALL_BUILD.vcxproj
+
+REM Need to find a way of running test from msbuild passing ARGS=-VV
+ctest -C "Release" --extra-verbose || true
