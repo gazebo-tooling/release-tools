0b864ca30cb5061d1abb70a1640c044765ae0c1d
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-10-17 23:50:55 +0200

    Define proper PATH for software and use it in ign-transport

--- jenkins-scripts/ign_transport-default-devel-windows-i386.bat
+++ jenkins-scripts/ign_transport-default-devel-windows-i386.bat
@@ -4,5 +4,6 @@ call "%SCRIPT_DIR%/lib/windows_configuration.bat"
 REM x64 or x86
 set PLATFORM_TO_BUILD=x86
 set ARG_CMAKE_FLAGS=%CMAKE_ZEROMQ_FLAGS% %CMAKE_PROTOBUF_FLAGS% %CMAKE_CPPZMQ_FLAGS%
+set PATH=%PATH%;%ZEROMQ_DLL_PATH%;%PROTOBUF_DLL_PATH%
 
 call "%SCRIPT_DIR%/lib/project-default-devel-windows.bat"
--- jenkins-scripts/lib/windows_configuration.bat
+++ jenkins-scripts/lib/windows_configuration.bat
@@ -7,9 +7,15 @@ set VS_DEFAULT_MSBUILD_FLAGS=/p:Configuration=Release
 REM Software avalibale on windows node
 set CMAKE_BOOST_FLAGS=-DBOOST_ROOT:STRING="C:\local\boost_1_55_0" -DBOOST_LIBRARYDIR:STRING="C:\local\boost_1_55_0\lib64-msvc-12.0"
 
-set CMAKE_ZEROMQ_FLAGS=-DZeroMQ_ROOT_DIR="C:\Program Files (x86)\ZeroMQ 4.0.4" 
+REM Protobuf variables
 set CMAKE_PROTOBUF_FLAGS=-DPROTOBUF_SRC_ROOT_FOLDER="C:\local\protobuf-2.6.0"
-set PATH=%PATH%;C:/local/protobuf-2.6.0/vsprojects/Release/
+set PROTOBUF_DLL_PATH="C:/local/protobuf-2.6.0/vsprojects/Release/"
+
+REM ZeroMQ variables
+set CMAKE_ZEROMQ_FLAGS=-DZeroMQ_ROOT_DIR="C:\Program Files (x86)\ZeroMQ 4.0.4" 
+set ZEROMQ_DLL_PATH="C:\Program Files (x86)\ZeroMQ 4.0.4\bin"
+
+REM cppzmq header 
 set CMAKE_CPPZMQ_FLAGS=-DCPPZMQ_HEADER_PATH="C:\local\cppzmq"
 
 echo %PATH%
