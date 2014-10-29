6c68217401b048f00348f978fe631e8514f866a8
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-10-28 18:56:37 -0700

    Functions at the end of the file

--- jenkins-scripts/ign_transport-default-devel-windows-i386.bat
+++ jenkins-scripts/ign_transport-default-devel-windows-i386.bat
@@ -1,5 +1,45 @@
 @echo on
 
+set SCRIPT_DIR="%~dp0"
+call "%SCRIPT_DIR%/lib/windows_library.bat"
+
+REM i386 for the moment to ignition-transport
+set PLATFORM_TO_BUILD=x86
+
+IF %PLATFORM_TO_BUILD% == x86 (
+  echo "Using 32bits VS configuration"
+) ELSE (
+  echo "Using 64bits VS configuration"
+)
+
+REM Configure the VC++ compilation
+call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" %PLATFORM_TO_BUILD%
+
+cd "C:\Temp"
+del workspace /q /s /f 
+mkdir workspace
+cd workspace
+
+call:wget http://packages.osrfoundation.org/win32/deps/cppzmq-noarch.zip cppzmq-noarch.zip
+call:wget http://packages.osrfoundation.org/win32/deps/protobuf-2.6.0-win32-vc12.zip protobuf-2.6.0-win32-vc12.zip
+call:wget http://packages.osrfoundation.org/win32/deps/zeromq-3.2.4-x86.zip zeromq-3.2.4-x86.zip
+
+REM get the unzip script for our library
+call:create_unzip_script
+call:unzip cppzmq-noarch.zip
+call:unzip protobuf-2.6.0-win32-vc12.zip
+call:unzip zeromq-3.2.4-x86.zip
+
+REM TODO: mercurial autoinstalled in windows if not present? Is that even possible?
+hg clone https://bitbucket.org/ignitionrobotics/ign-transport
+cd ign-transport
+
+mkdir build
+cd build
+..\configure || goto:error
+nmake || goto:error
+nmake install || goto:error
+
 REM TODO: remove this copy/paste when loading function from a file is working
 REM TODO: http://www.dostips.com/forum/viewtopic.php?f=3&t=1626&hilit=%20library 
 
@@ -65,43 +105,3 @@ GOTO:EOF
 echo Failed with error #%errorlevel%.
 exit /b %errorlevel%
 GOTO:EOF
-
-set SCRIPT_DIR="%~dp0"
-call "%SCRIPT_DIR%/lib/windows_library.bat"
-
-REM i386 for the moment to ignition-transport
-set PLATFORM_TO_BUILD=x86
-
-IF %PLATFORM_TO_BUILD% == x86 (
-  echo "Using 32bits VS configuration"
-) ELSE (
-  echo "Using 64bits VS configuration"
-)
-
-REM Configure the VC++ compilation
-call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" %PLATFORM_TO_BUILD%
-
-cd "C:\Temp"
-del workspace /q /s /f 
-mkdir workspace
-cd workspace
-
-call:wget http://packages.osrfoundation.org/win32/deps/cppzmq-noarch.zip cppzmq-noarch.zip
-call:wget http://packages.osrfoundation.org/win32/deps/protobuf-2.6.0-win32-vc12.zip protobuf-2.6.0-win32-vc12.zip
-call:wget http://packages.osrfoundation.org/win32/deps/zeromq-3.2.4-x86.zip zeromq-3.2.4-x86.zip
-
-REM get the unzip script for our library
-call:create_unzip_script
-call:unzip cppzmq-noarch.zip
-call:unzip protobuf-2.6.0-win32-vc12.zip
-call:unzip zeromq-3.2.4-x86.zip
-
-REM TODO: mercurial autoinstalled in windows if not present? Is that even possible?
-hg clone https://bitbucket.org/ignitionrobotics/ign-transport
-cd ign-transport
-
-mkdir build
-cd build
-..\configure || goto:error
-nmake || goto:error
-nmake install || goto:error
