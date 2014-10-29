5520c9b31808e48f2a630bd9445d7c628111bc48
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-10-28 18:01:46 -0700

    Change to windows script

--- jenkins-scripts/ign_transport-default-devel-windows-i386.bat
+++ jenkins-scripts/ign_transport-default-devel-windows-i386.bat
@@ -1,4 +1,5 @@
 set SCRIPT_DIR="%~dp0"
+call "%SCRIPT_DIR%/lib/windows_library.bat"
 
 REM i386 for the moment to ignition-transport
 set PLATFORM_TO_BUILD=x86
@@ -19,59 +20,15 @@ del workspace /s
 mkdir workspace
 cd workspace
 
-REM ##################################
-REM Unzip script from http://jagaroth.livejournal.com/69147.html 
-REM
-REM Changing working folder back to current directory for Vista & 7 compatibility
-%~d0
-CD %~dp0
-REM Folder changed
+call:wget http://packages.osrfoundation.org/win32/deps/cppzmq-noarch.zip cppzmq-noarch.zip
+call:wget http://packages.osrfoundation.org/win32/deps/protobuf-2.6.0-win32-vc12.zip protobuf-2.6.0-win32-vc12.zip
+call:wget http://packages.osrfoundation.org/win32/deps/zeromq-3.2.4-x86.zip zeromq-3.2.4-x86.zip
 
-REM This script upzip's files...
-> j_unzip.vbs ECHO '
->> j_unzip.vbs ECHO ' Dim ArgObj, var1, var2
->> j_unzip.vbs ECHO Set ArgObj = WScript.Arguments
->> j_unzip.vbs ECHO.
->> j_unzip.vbs ECHO If (Wscript.Arguments.Count ^> 0) Then
->> j_unzip.vbs ECHO. var1 = ArgObj(0)
->> j_unzip.vbs ECHO Else
->> j_unzip.vbs ECHO. var1 = ""
->> j_unzip.vbs ECHO End if
->> j_unzip.vbs ECHO.
->> j_unzip.vbs ECHO If var1 = "" then
->> j_unzip.vbs ECHO. strFileZIP = "example.zip"
->> j_unzip.vbs ECHO Else
->> j_unzip.vbs ECHO. strFileZIP = var1
->> j_unzip.vbs ECHO End if
->> j_unzip.vbs ECHO.
->> j_unzip.vbs ECHO 'The location of the zip file.
->> j_unzip.vbs ECHO REM Set WshShell = CreateObject("Wscript.Shell")
->> j_unzip.vbs ECHO REM CurDir = WshShell.ExpandEnvironmentStrings("%%cd%%")
->> j_unzip.vbs ECHO Dim sCurPath
->> j_unzip.vbs ECHO sCurPath = CreateObject("Scripting.FileSystemObject").GetAbsolutePathName(".")
->> j_unzip.vbs ECHO strZipFile = sCurPath ^& "\" ^& strFileZIP
->> j_unzip.vbs ECHO 'The folder the contents should be extracted to.
->> j_unzip.vbs ECHO outFolder = sCurPath ^& "\"
->> j_unzip.vbs ECHO.
->> j_unzip.vbs ECHO. WScript.Echo ( "Extracting file " ^& strFileZIP)
->> j_unzip.vbs ECHO.
->> j_unzip.vbs ECHO Set objShell = CreateObject( "Shell.Application" )
->> j_unzip.vbs ECHO Set objSource = objShell.NameSpace(strZipFile).Items()
->> j_unzip.vbs ECHO Set objTarget = objShell.NameSpace(outFolder)
->> j_unzip.vbs ECHO intOptions = 256
->> j_unzip.vbs ECHO objTarget.CopyHere objSource, intOptions
->> j_unzip.vbs ECHO.
->> j_unzip.vbs ECHO. WScrip.Echo ( "Extracted." )
->> j_unzip.vbs ECHO.
-REM ##################################
-
-bitsadmin /transfer mydownloadjob /download /priority normal http://packages.osrfoundation.org/win32/deps/cppzmq-noarch.zip %cd%\cppzmq-noarch.zip
-bitsadmin /transfer mydownloadjob /download /priority normal http://packages.osrfoundation.org/win32/deps/protobuf-2.6.0-win32-vc12.zip %cd%\protobuf-2.6.0-win32-vc12.zip
-bitsadmin /transfer mydownloadjob /download /priority normal http://packages.osrfoundation.org/win32/deps/zeromq-3.2.4-x86.zip %cd%\zeromq-3.2.4-x86.zip
-
-cscript //B j_unzip.vbs cppzmq-noarch.zip
-cscript //B j_unzip.vbs protobuf-2.6.0-win32-vc12.zip
-cscript //B j_unzip.vbs zeromq-3.2.4-x86.zip
+REM get the unzip script for our library
+call:create_unzip_script
+call:unzip cppzmq-noarch.zip
+call:unzip protobuf-2.6.0-win32-vc12.zip
+call:unzip zeromq-3.2.4-x86.zip
 
 REM TODO: mercurial autoinstalled in windows if not present? Is that even possible?
 hg clone https://bitbucket.org/ignitionrobotics/ign-transport
--- jenkins-scripts/lib/windows_configuration.bat
+++ jenkins-scripts/lib/windows_configuration.bat
@@ -1,19 +1,57 @@
-set VS32bits_CMAKE_GEN=-G"Visual Studio 12"
-set VS64bits_CMAKE_GEN=-G"Visual Studio 12 Win64"
+:create_unzip_script - Create a script to unzip files
+REM ##################################
+REM Unzip script from http://jagaroth.livejournal.com/69147.html 
+REM
+REM Changing working folder back to current directory for Vista & 7 compatibility
+%~d0
+CD %~dp0
+REM Folder changed
 
-set VS_DEFAULT_CMAKE_FLAGS=-DCMAKE_BUILD_TYPE:STRING=Release
-set VS_DEFAULT_MSBUILD_FLAGS=/p:Configuration=Release
+REM This script upzip's files...
+> j_unzip.vbs ECHO '
+>> j_unzip.vbs ECHO ' Dim ArgObj, var1, var2
+>> j_unzip.vbs ECHO Set ArgObj = WScript.Arguments
+>> j_unzip.vbs ECHO.
+>> j_unzip.vbs ECHO If (Wscript.Arguments.Count ^> 0) Then
+>> j_unzip.vbs ECHO. var1 = ArgObj(0)
+>> j_unzip.vbs ECHO Else
+>> j_unzip.vbs ECHO. var1 = ""
+>> j_unzip.vbs ECHO End if
+>> j_unzip.vbs ECHO.
+>> j_unzip.vbs ECHO If var1 = "" then
+>> j_unzip.vbs ECHO. strFileZIP = "example.zip"
+>> j_unzip.vbs ECHO Else
+>> j_unzip.vbs ECHO. strFileZIP = var1
+>> j_unzip.vbs ECHO End if
+>> j_unzip.vbs ECHO.
+>> j_unzip.vbs ECHO 'The location of the zip file.
+>> j_unzip.vbs ECHO REM Set WshShell = CreateObject("Wscript.Shell")
+>> j_unzip.vbs ECHO REM CurDir = WshShell.ExpandEnvironmentStrings("%%cd%%")
+>> j_unzip.vbs ECHO Dim sCurPath
+>> j_unzip.vbs ECHO sCurPath = CreateObject("Scripting.FileSystemObject").GetAbsolutePathName(".")
+>> j_unzip.vbs ECHO strZipFile = sCurPath ^& "\" ^& strFileZIP
+>> j_unzip.vbs ECHO 'The folder the contents should be extracted to.
+>> j_unzip.vbs ECHO outFolder = sCurPath ^& "\"
+>> j_unzip.vbs ECHO.
+>> j_unzip.vbs ECHO. WScript.Echo ( "Extracting file " ^& strFileZIP)
+>> j_unzip.vbs ECHO.
+>> j_unzip.vbs ECHO Set objShell = CreateObject( "Shell.Application" )
+>> j_unzip.vbs ECHO Set objSource = objShell.NameSpace(strZipFile).Items()
+>> j_unzip.vbs ECHO Set objTarget = objShell.NameSpace(outFolder)
+>> j_unzip.vbs ECHO intOptions = 256
+>> j_unzip.vbs ECHO objTarget.CopyHere objSource, intOptions
+>> j_unzip.vbs ECHO.
+>> j_unzip.vbs ECHO. WScrip.Echo ( "Extracted." )
+>> j_unzip.vbs ECHO.
+REM ##################################
+GOTO:EOF
 
-REM Software avalibale on windows node
-set CMAKE_BOOST_FLAGS=-DBOOST_ROOT:STRING="C:\local\boost_1_55_0" -DBOOST_LIBRARYDIR:STRING="C:\local\boost_1_55_0\lib64-msvc-12.0"
+:wget - Download internet file in your current directory. [URL] [filename]
+REM Downloading %~1
+bitsadmin /transfer mydownloadjob /download /priority normal %~1 %cd%\%~2
+GOTO:EOF
 
-REM Protobuf variables
-set CMAKE_PROTOBUF_FLAGS=-DPROTOBUF_SRC_ROOT_FOLDER="C:\local\protobuf-2.6.0"
-set PROTOBUF_DLL_PATH=C:/local/protobuf-2.6.0/vsprojects/Release/
-
-REM ZeroMQ variables
-set CMAKE_ZEROMQ_FLAGS=-DZeroMQ_ROOT_DIR="C:\Program Files (x86)\ZeroMQ 4.0.4" 
-set ZEROMQ_DLL_PATH=C:\Program Files (x86)\ZeroMQ 4.0.4\bin
-
-REM cppzmq header 
-set CMAKE_CPPZMQ_FLAGS=-DCPPZMQ_HEADER_PATH="C:\local\cppzmq"
+REM TODO: automatically create the download script if it does not exists
+:unzip - Unzip file [zip_file]
+cscript //B j_unzip.vbs %~1
+GOTO:EOF
