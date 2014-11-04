5993ede74d58484d7053ed69627fd4066044f6b8
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-11-04 15:22:19 -0800

    Final fixes on windows

--- jenkins-scripts/ign_transport-default-devel-windows-i386.bat
+++ jenkins-scripts/ign_transport-default-devel-windows-i386.bat
@@ -1,18 +1,6 @@
 @echo on
 
 set SCRIPT_DIR="%~dp0"
-
-set win_lib="%SCRIPT_DIR%/lib/windows_library.bat"
-
-REM i386 for the moment to ignition-transport
 set PLATFORM_TO_BUILD=x86
-
-IF %PLATFORM_TO_BUILD% == x86 (
-  echo "Using 32bits VS configuration"
-  set BITNESS=32
-) ELSE (
-  echo "Using 64bits VS configuration"
-  set BITNESS=64
-)
 
 call %SCRIPT_DIR%/lib/ign_transport-base-windows.bat
--- jenkins-scripts/lib/ign_transport-base-windows.bat
+++ jenkins-scripts/lib/ign_transport-base-windows.bat
@@ -1,5 +1,31 @@
+set win_lib="%SCRIPT_DIR%/lib/windows_library.bat"
+
+IF %PLATFORM_TO_BUILD% == x86 
+(
+  echo "Using 32bits VS configuration"
+  set BITNESS=32
+) ELSE (
+  echo "Using 64bits VS configuration"
+  set BITNESS=64
+)
+
 REM Configure the VC++ compilation
-call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" %PLATFORM_TO_BUILD%
+set MSVC_ON_WIN64="c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+set MSVC_ON_WIN32="c:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+
+IF exist %MSVC_ON_WIN64%
+( 
+   call %MSVC_ON_WIN64% %PLATFORM_TO_BUILD%
+)
+ELSE IF exist %MSVC_ON_WIN32%
+(
+   call %MSVC_ON_WIN32% %PLATFORM_TO_BUILD%
+)
+ELSE
+(
+   echo "Could not find the vcvarsall.bat file"
+   exit -1
+)
 
 IF exist workspace ( rmdir /s /q workspace ) 
 mkdir workspace
@@ -16,10 +42,7 @@ call %win_lib% :unzip cppzmq-noarch.zip || goto:error
 call %win_lib% :unzip protobuf-2.6.0-win%BITNESS%-vc12.zip || goto:error
 call %win_lib% :unzip zeromq-3.2.4-%PLATFORM_TO_BUILD%.zip || goto:error
 
-echo "Cloning ignition transport"
-REM TODO: mercurial autoinstalled in windows if not present? Is that even possible?
-hg clone https://bitbucket.org/ignitionrobotics/ign-transport > clone_ign_transport.log
-cd ign-transport
+cd %WORKSPACE%
 
 echo "Compiling"
 mkdir build
@@ -31,3 +54,5 @@ nmake install || goto:error
 echo "Running tests"
 REM Need to find a way of running test from the standard make test (not working)
 ctest -C "Release" --verbose --extra-verbose || exit 0
+
+echo %PWD%
