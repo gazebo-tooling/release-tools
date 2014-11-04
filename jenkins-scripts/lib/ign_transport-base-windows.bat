0bcb57f7d100d1fbe21642d6906d5759a34f038b
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-11-04 14:53:16 -0800

    Refactor ignition transport

---
+++ jenkins-scripts/lib/ign_transport-base-windows.bat
@@ -0,0 +1,33 @@
+REM Configure the VC++ compilation
+call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" %PLATFORM_TO_BUILD%
+
+rmdir /s /q workspace  || goto:error
+mkdir workspace
+cd workspace
+
+echo "Download libraries"
+call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/cppzmq-noarch.zip cppzmq-noarch.zip
+call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/protobuf-2.6.0-win32-vc12.zip protobuf-2.6.0-win32-vc12.zip
+call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/zeromq-3.2.4-x86.zip zeromq-3.2.4-x86.zip
+
+echo "Uncompressing libraries"
+call %win_lib% :create_unzip_script || goto:error
+call %win_lib% :unzip cppzmq-noarch.zip || goto:error
+call %win_lib% :unzip protobuf-2.6.0-win32-vc12.zip || goto:error
+call %win_lib% :unzip zeromq-3.2.4-x86.zip || goto:error
+
+echo "Cloning ignition transport"
+REM TODO: mercurial autoinstalled in windows if not present? Is that even possible?
+hg clone https://bitbucket.org/ignitionrobotics/ign-transport > clone_ign_transport.log
+cd ign-transport
+
+echo "Compiling"
+mkdir build
+cd build
+call "..\configure.bat" || goto:error
+nmake || goto:error
+nmake install || goto:error
+
+echo "Running tests"
+REM Need to find a way of running test from the standard make test (not working)
+ctest -C "Release" --verbose --extra-verbose || exit 0
