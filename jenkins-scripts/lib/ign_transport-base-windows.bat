edeeec086f1d7b74e5cb84931342bf8b732f09c3
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-11-04 15:33:06 -0800

    Fix windows if structures

--- jenkins-scripts/lib/ign_transport-base-windows.bat
+++ jenkins-scripts/lib/ign_transport-base-windows.bat
@@ -1,7 +1,6 @@
 set win_lib=%SCRIPT_DIR%\lib\windows_library.bat
 
-IF %PLATFORM_TO_BUILD% == x86 
-(
+IF %PLATFORM_TO_BUILD% == x86 (
   echo "Using 32bits VS configuration"
   set BITNESS=32
 ) ELSE (
@@ -13,16 +12,13 @@ echo "Configure the VC++ compilation"
 set MSVC_ON_WIN64=c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat
 set MSVC_ON_WIN32=c:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat
 
-IF exist "%MSVC_ON_WIN64%"
-( 
+IF exist "%MSVC_ON_WIN64%" ( 
    call "%MSVC_ON_WIN64%" %PLATFORM_TO_BUILD%
 )
-ELSE IF exist "%MSVC_ON_WIN32%"
-(
+ELSE IF exist "%MSVC_ON_WIN32%" (
    call "%MSVC_ON_WIN32%" %PLATFORM_TO_BUILD%
 )
-ELSE
-(
+ELSE (
    echo "Could not find the vcvarsall.bat file"
    exit -1
 )
