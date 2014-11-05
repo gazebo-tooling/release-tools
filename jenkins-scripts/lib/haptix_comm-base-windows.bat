92d2cc79ba21f3981e7591d2f33ea02b7375ec01
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-11-04 22:46:34 -0800

    Fix typo

--- jenkins-scripts/lib/haptix_comm-base-windows.bat
+++ jenkins-scripts/lib/haptix_comm-base-windows.bat
@@ -1,6 +1,6 @@
 :: Supposed to has been called ignition-base before
 
-cd %WORKSPACE%/workspace/haptix-comv || goto %win_lib% :error
+cd %WORKSPACE%/workspace/haptix-comm || goto %win_lib% :error
 
 echo "Compiling haptix"
 mkdir build
