cd46a206704ed5d53a4532076b2a88ac1dbe9f25
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-11-04 19:04:43 -0800

    Initial implementation

---
+++ jenkins-scripts/haptix_comm-default-devel-windows-amd64.bat
@@ -0,0 +1,12 @@
+@echo on
+
+set SCRIPT_DIR=%~dp0
+set PLATFORM_TO_BUILD=amd64
+
+:: We need ignition first
+hg clone https://bitbucket.org/ignitionrobotics/ign-transport
+call %SCRIPT_DIR%/lib/ign_transport-base-windows.bat
+
+cd %WORKSPACE%/workspace
+mv haptix-comm %WORKSPACE%/workspace
+call %SCRIPT_DIR%/lib/haptix_comm-base-windows.bat
---
+++ jenkins-scripts/lib/haptix_comm-base-windows.bat
@@ -0,0 +1,15 @@
+:: Supposed to has been called ignition-base before
+
+cd haptix-comm || goto %win_lib% :error
+
+
+echo "Compiling haptix"
+mkdir build
+cd build
+call "..\configure.bat" Release %BITNESS% || goto %win_lib% :error
+nmake || goto %win_lib% :error
+nmake install || goto %win_lib% :error
+
+echo "Running tests"
+REM Need to find a way of running test from the standard make test (not working)
+ctest -C "Release" --verbose --extra-verbose || exit 0
