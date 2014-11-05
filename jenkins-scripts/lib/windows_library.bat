018f30a88035735ca0707c5afe66ce680416ec4a
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-11-04 17:58:49 -0800

    Fix problems with keywords

--- jenkins-scripts/lib/windows_library.bat
+++ jenkins-scripts/lib/windows_library.bat
@@ -3,6 +3,42 @@ call :%*
 exit /b
 
 :: ##################################
+:configure_msvc_compiler
+::
+::
+
+:: By default should be the same
+set MSVC_KEYWORD=%PLATFORM_TO_BUILD%
+
+IF %PLATFORM_TO_BUILD% == x86 (
+  echo "Using 32bits VS configuration"
+  set BITNESS=32
+) ELSE (
+  REM Visual studio is accepting many keywords to compile for 64bits
+  REM We need to set x86_amd64 to make express version to be able to
+  REM Cross compile from x86 -> amd64
+  echo "Using 64bits VS configuration"
+  set BITNESS=64
+  set MSVC_KEYWORD=x86_amd64
+  set PLATFORM_TO_BUILD=amd64
+)
+
+echo "Configure the VC++ compilation"
+set MSVC_ON_WIN64=c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat
+set MSVC_ON_WIN32=c:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat
+
+IF exist "%MSVC_ON_WIN64%" ( 
+   call "%MSVC_ON_WIN64%" %MSVC_KEYWORD% || goto %win_lib% :error
+) ELSE IF exist "%MSVC_ON_WIN32%" (
+   call "%MSVC_ON_WIN32%" %MSVC_KEYWORD% || goto %win_lib% :error
+) ELSE (
+   echo "Could not find the vcvarsall.bat file"
+   exit -1
+)
+goto :EOF
+
+
+:: ##################################
 :create_unzip_script - Create the unzip script to run unzip command
 ::
 REM Unzip script from http://jagaroth.livejournal.com/69147.html 
