ec4a6085f5d496a7da469e6bdb1cc60f7526932b
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-11-04 17:16:08 -0800

    Fix amd64 build

--- jenkins-scripts/lib/ign_transport-base-windows.bat
+++ jenkins-scripts/lib/ign_transport-base-windows.bat
@@ -3,9 +3,12 @@ set win_lib=%SCRIPT_DIR%\lib\windows_library.bat
 IF %PLATFORM_TO_BUILD% == x86 (
   echo "Using 32bits VS configuration"
   set BITNESS=32
-) ELSE (
+) ELSE IF %PLATFORM_TO_BUILD% == amd64 (
   echo "Using 64bits VS configuration"
   set BITNESS=64
+) ELSE (
+  echo "Please use x86 or amd64 as platform values"
+  goto %win_lib% :error
 )
 
 echo "Configure the VC++ compilation"
@@ -13,16 +16,16 @@ set MSVC_ON_WIN64=c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsa
 set MSVC_ON_WIN32=c:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat
 
 IF exist "%MSVC_ON_WIN64%" ( 
-   call "%MSVC_ON_WIN64%" %PLATFORM_TO_BUILD% || goto:error
+   call "%MSVC_ON_WIN64%" %PLATFORM_TO_BUILD% || goto %win_lib% :error
 ) ELSE IF exist "%MSVC_ON_WIN32%" (
-   call "%MSVC_ON_WIN32%" %PLATFORM_TO_BUILD% || goto:error
+   call "%MSVC_ON_WIN32%" %PLATFORM_TO_BUILD% || goto %win_lib% :error
 ) ELSE (
    echo "Could not find the vcvarsall.bat file"
    exit -1
 )
 
-IF exist workspace ( rmdir /s /q workspace ) 
-mkdir workspace
+IF exist workspace ( rmdir /s /q workspace ) || goto %win_lib% :error
+mkdir workspace 
 cd workspace
 
 echo "Download libraries"
@@ -31,10 +34,10 @@ call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/protobuf-2.6.0
 call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/zeromq-3.2.4-%PLATFORM_TO_BUILD%.zip zeromq-3.2.4-%PLATFORM_TO_BUILD%.zip
 
 echo "Uncompressing libraries"
-call %win_lib% :create_unzip_script || goto:error
-call %win_lib% :unzip cppzmq-noarch.zip || goto:error
-call %win_lib% :unzip protobuf-2.6.0-win%BITNESS%-vc12.zip || goto:error
-call %win_lib% :unzip zeromq-3.2.4-%PLATFORM_TO_BUILD%.zip || goto:error
+call %win_lib% :create_unzip_script
+call %win_lib% :unzip cppzmq-noarch.zip
+call %win_lib% :unzip protobuf-2.6.0-win%BITNESS%-vc12.zip
+call %win_lib% :unzip zeromq-3.2.4-%PLATFORM_TO_BUILD%.zip
 
 REM Note that your jenkins job should put source in %WORKSPACE%/ign-transport
 echo "Move sources so we agree with configure.bat layout"
@@ -44,9 +47,9 @@ cd ign-transport
 echo "Compiling"
 mkdir build
 cd build
-call "..\configure.bat" || goto:error
-nmake || goto:error
-nmake install || goto:error
+call "..\configure.bat" || goto %win_lib% :error
+nmake || goto %win_lib% :error
+nmake install || goto %win_lib% :error
 
 echo "Running tests"
 REM Need to find a way of running test from the standard make test (not working)
