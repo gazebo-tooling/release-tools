df0840679d45f2e2378cf245d24e45fba347da61
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-11-06 14:58:16 -0800

    move instead of mv

--- jenkins-scripts/haptix_comm-default-devel-windows-amd64.bat
+++ jenkins-scripts/haptix_comm-default-devel-windows-amd64.bat
@@ -9,5 +9,5 @@ hg clone https://bitbucket.org/ignitionrobotics/ign-transport
 call %SCRIPT_DIR%/lib/ign_transport-base-windows.bat
 
 cd %WORKSPACE%/workspace
-mv haptix-comm %WORKSPACE%/workspace
+move haptix-comm %WORKSPACE%/workspace
 call %SCRIPT_DIR%/lib/haptix_comm-base-windows.bat
