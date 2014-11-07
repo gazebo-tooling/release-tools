2efa9d4a0febb40660027b664077dc5245f8d781
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-11-06 16:52:35 -0800

    Changes also in amd64

--- jenkins-scripts/haptix_comm-default-devel-windows-amd64.bat
+++ jenkins-scripts/haptix_comm-default-devel-windows-amd64.bat
@@ -4,10 +4,4 @@ set SCRIPT_DIR=%~dp0
 set PLATFORM_TO_BUILD=amd64
 set IGN_TEST_DISABLE=TRUE
 
-:: We need ignition first
-hg clone https://bitbucket.org/ignitionrobotics/ign-transport
-call %SCRIPT_DIR%/lib/ign_transport-base-windows.bat
-
-cd %WORKSPACE%
-move haptix-comm %WORKSPACE%/workspace
 call %SCRIPT_DIR%/lib/haptix_comm-base-windows.bat
