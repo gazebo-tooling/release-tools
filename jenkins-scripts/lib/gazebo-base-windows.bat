982bbe0e7910aa0ad431d2d48ddff13ab978b836
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-12-03 00:42:37 +0100

    Install curl

--- jenkins-scripts/lib/gazebo-base-windows.bat
+++ jenkins-scripts/lib/gazebo-base-windows.bat
@@ -13,6 +13,7 @@ call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/protobuf-2.6.0
 call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/OgreSDK_vc11_x64_v1-9-0unstable.zip OgreSDK_vc11_x64_v1-9-0unstable.zip
 call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/FreeImage-vc12-x64-release-debug.zip FreeImage-vc12-x64-release-debug.zip
 call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/tbb43_20141023oss_win.zip tbb43_20141023oss_win.zip
+call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/libcurl-vc12-x64-release-static-ipv6-sspi-winssl.zip libcurl-vc12-x64-release-static-ipv6-sspi-winssl.zip
 
 echo "Uncompressing libraries"
 call %win_lib% :download_7za
@@ -21,6 +22,7 @@ call %win_lib% :unzip_7za protobuf-2.6.0-win%BITNESS%-vc12.zip
 call %win_lib% :unzip_7za OgreSDK_vc11_x64_v1-9-0unstable.zip
 call %win_lib% :unzip_7za FreeImage-vc12-x64-release-debug.zip
 call %win_lib% :unzip_7za tbb43_20141023oss_win.zip
+call %win_lib% :unzip_7za libcurl-vc12-x64-release-static-ipv6-sspi-winssl.zip
 
 echo "Compile sdformat special branch"
 hg clone https://bitbucket.org/osrf/sdformat
