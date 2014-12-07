c37673e86425a841510a2e3e346039719ac3210a
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-12-07 02:11:28 +0100

    Install missing deps

--- jenkins-scripts/lib/gazebo-base-windows.bat
+++ jenkins-scripts/lib/gazebo-base-windows.bat
@@ -12,27 +12,32 @@ mkdir workspace
 cd workspace
 
 echo "Download libraries"
-call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/boost_1_56_0.zip boost_1_56_0.zip
-call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/protobuf-2.6.0-win%BITNESS%-vc12.zip protobuf-2.6.0-win%BITNESS%-vc12.zip
-call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/ogre_src_v1-8-1-vc12-x64-release-debug.zip ogre_src_v1-8-1-vc12-x64-release-debug.zip
 call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/FreeImage-vc12-x64-release-debug.zip FreeImage-vc12-x64-release-debug.zip
-call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/tbb43_20141023oss_win.zip tbb43_20141023oss_win.zip
-call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/libcurl-vc12-x64-release-debug-static-ipv6-sspi-winssl.zip libcurl-vc12-x64-release-debug-static-ipv6-sspi-winssl.zip	
+call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/boost_1_56_0.zip boost_1_56_0.zip
+call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/bzip2-1.0.6-vc12-x64-release-debug.zip bzip2-1.0.6-vc12-x64-release-debug.zip
 call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/dlfcn-win32-vc12-x64-release-debug.zip dlfcn-win32-vc12-x64-release-debug.zip
+call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/freetype-2.4.0-vc12-x64-release-debug.zip freetype-2.4.0-vc12-x64-release-debug.zip
+call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/libcurl-vc12-x64-release-debug-static-ipv6-sspi-winssl.zip libcurl-vc12-x64-release-debug-static-ipv6-sspi-winssl.zip	
+call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/ogre_src_v1-8-1-vc12-x64-release-debug.zip ogre_src_v1-8-1-vc12-x64-release-debug.zip
+call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/protobuf-2.6.0-win%BITNESS%-vc12.zip protobuf-2.6.0-win%BITNESS%-vc12.zip
+call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/tbb43_20141023oss_win.zip tbb43_20141023oss_win.zip
 call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/zziplib-0.13.62-vc12-x64-release-debug.zip zziplib-0.13.62-vc12-x64-release-debug.zip
 call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/zlib-1.2.8-vc12-x64-release-debug.zip zlib-1.2.8-vc12-x64-release-debug.zip
 
 echo "Uncompressing libraries"
 call %win_lib% :download_7za
-call %win_lib% :unzip_7za boost_1_56_0.zip 
-call %win_lib% :unzip_7za protobuf-2.6.0-win%BITNESS%-vc12.zip
 call %win_lib% :unzip_7za FreeImage-vc12-x64-release-debug.zip
-call %win_lib% :unzip_7za ogre_src_v1-8-1-vc12-x64-release-debug.zip
-call %win_lib% :unzip_7za tbb43_20141023oss_win.zip
-call %win_lib% :unzip_7za libcurl-vc12-x64-release-debug-static-ipv6-sspi-winssl.zip
+call %win_lib% :unzip_7za boost_1_56_0.zip 
+call %win_lib% :unzip_7za bzip2-1.0.6-vc12-x64-release-debug.zip
 call %win_lib% :unzip_7za dlfcn-win32-vc12-x64-release-debug.zip
+call %win_lib% :unzip_7za freetype-2.4.0-vc12-x64-release-debug.zip
+call %win_lib% :unzip_7za libcurl-vc12-x64-release-debug-static-ipv6-sspi-winssl.zip
+call %win_lib% :unzip_7za ogre_src_v1-8-1-vc12-x64-release-debug.zip
+call %win_lib% :unzip_7za protobuf-2.6.0-win%BITNESS%-vc12.zip
+call %win_lib% :unzip_7za tbb43_20141023oss_win.zip
 call %win_lib% :unzip_7za zlib-1.2.8-vc12-x64-release-debug.zip
 call %win_lib% :unzip_7za zziplib-0.13.62-vc12-x64-release-debug.zip
+
 
 
 echo "Compile sdformat special branch"
