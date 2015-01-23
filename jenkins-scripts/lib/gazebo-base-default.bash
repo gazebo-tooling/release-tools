acbb8c6274e44204d501880b3bfa1222478df5c0
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-01-23 21:18:06 +0100

    Add check on GAZEBO_MAJOR_VERSION

--- jenkins-scripts/lib/gazebo-base-default.bash
+++ jenkins-scripts/lib/gazebo-base-default.bash
@@ -16,6 +16,12 @@ GAZEBO_MAJOR_VERSION=`\
   grep 'set.*GAZEBO_MAJOR_VERSION ' ${WORKSPACE}/gazebo/CMakeLists.txt | \
   tr -d 'a-zA-Z _()'`
 
+# Check gazebo version between 1-9 
+if ! [[ ${GAZEBO_MAJOR_VERSION} =~ ^-?[1-9]$ ]]; then
+   echo "Error! GAZEBO_MAJOR_VERSION is not between 1 and 9, check the detection"
+   exit -1
+fi
+
 . ${SCRIPT_DIR}/lib/boilerplate_prepare.sh
 
 cat > build.sh << DELIM
