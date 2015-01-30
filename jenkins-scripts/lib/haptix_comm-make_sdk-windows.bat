28ac18988f0bb6ab5d30161c8be21033c0d8269e
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-01-30 19:57:22 +0100

    Remove zmq lib

--- jenkins-scripts/lib/haptix_comm-make_sdk-windows.bat
+++ jenkins-scripts/lib/haptix_comm-make_sdk-windows.bat
@@ -164,11 +164,11 @@ for %%b in (Debug, Release) do (
     :: MEX generation
 
     :: TODO: check for the absolute path if really exists
-    IF %%b == Debug (
-      set zmq_lib=libzmq-v120-mt-gd-3_2_4.lib
-    ) else (
-      set zmq_lib=libzmq-v120-mt-3_2_4.lib
-    )
+    :: IF %%b == Debug (
+    ::  set zmq_lib=libzmq-v120-mt-gd-3_2_4.lib
+    :: ) else (
+    ::  set zmq_lib=libzmq-v120-mt-3_2_4.lib
+    :: )
 
     "C:\Program files\MATLAB\R2014b\bin\mex" "!installdir!\matlab\hx_getdeviceinfo.c" -I"!installdir!\haptix-comm\!build_type!\include" -L"!installdir!\haptix-comm\!build_type!\lib" -lhaptix-comm -lhaptix_msgs -L"!installdir!\deps\protobuf-2.6.0-win%BITNESS%-vc12\vsprojects\!build_type!" -lprotobuf  -L"!installdir!\deps\ZeroMQ 3.2.4\lib" -l%zmq_lib% -I"!installdir!\deps\ign-transport\!build_type!\include" -L"!installdir!\deps\ign-transport\!build_type!\lib" -lignition-transport -lws2_32 -lIphlpapi -v || goto :error
 
