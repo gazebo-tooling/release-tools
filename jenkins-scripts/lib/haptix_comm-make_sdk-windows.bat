d4f06cecc4c823f8e6c4bf3453a50476d44079b8
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-01-31 03:14:23 +0100

    Proper zmq lib

--- jenkins-scripts/lib/haptix_comm-make_sdk-windows.bat
+++ jenkins-scripts/lib/haptix_comm-make_sdk-windows.bat
@@ -166,6 +166,13 @@ echo "inside the loop"
     :: - haptix-comm MATLAB stuff
     xcopy "haptix-comm\matlab\*" "!installdir!\matlab"
 
+    :: TODO: check for the absolute path if really exists
+    IF "%%b" == "Debug" (
+      set zmq_lib=libzmq-v120-mt-gd-3_2_4.lib
+    ) else (
+      set zmq_lib=libzmq-v120-mt-3_2_4.lib
+    )
+
     "C:\Program files\MATLAB\R2014b\bin\mex" "!installdir!\matlab\hx_getdeviceinfo.c" -I"!installdir!\haptix-comm\!build_type!\include" -L"!installdir!\haptix-comm\!build_type!\lib" -lhaptix-comm -lhaptix_msgs -L"!installdir!\deps\protobuf-2.6.0-win%BITNESS%-vc12\vsprojects\!build_type!" -lprotobuf  -L"!installdir!\deps\ZeroMQ 3.2.4\lib" -l%zmq_lib% -I"!installdir!\deps\ign-transport\!build_type!\include" -L"!installdir!\deps\ign-transport\!build_type!\lib" -lignition-transport -lws2_32 -lIphlpapi -v || goto :error
 
     :: TODO: need hx_update.c
