a545541f7415c02f4f5c2b8f23354cb84f568060
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-02-12 22:33:06 +0100

    Added utopìc version of ignition-transport

---
+++ jenkins-scripts/ign_transport-default-devel-utopic-amd64.bash
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+export DISTRO=utopic
+export REPO_DIRECTORY="ignition-transport"
+export PKG_DEPENDENCIES_VAR_NAME="IGN_TRANSPORT_DEPENDENCIES"
+
+. ${SCRIPT_DIR}/lib/generic-job.bash
