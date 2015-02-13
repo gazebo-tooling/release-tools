5b71f17010b5c5d2d3f42d9808058ea4923a8a3c
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-02-13 20:38:49 +0100

    Move api checking to trusty

--- jenkins-scripts/gazebo-default-api_checker-trusty-amd64.bash
+++ jenkins-scripts/gazebo-default-api_checker-trusty-amd64.bash
@@ -4,7 +4,6 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-export DISTRO=precise
-export ROS_DISTRO=hydro
+export DISTRO=trusty
 
 . ${SCRIPT_DIR}/lib/gazebo-api_checker-default.bash
