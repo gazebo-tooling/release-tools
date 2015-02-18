b63105a02c597d0236257e705d4282bdbfdf939e
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-02-18 19:27:43 +0100

    Added example test compilattion

--- jenkins-scripts/lib/haptix_comm-base-windows.bat
+++ jenkins-scripts/lib/haptix_comm-base-windows.bat
@@ -32,6 +32,14 @@ call "..\configure.bat" Release %BITNESS% || goto %win_lib% :error
 nmake || goto %win_lib% :error
 nmake install || goto %win_lib% :error
 
+echo "Compiling example ..."
+cd ..\example
+mkdir build
+cd build
+call "..\configure.bat" Release %BITNESS% || goto %win_lib% :error
+nmake || goto %win_lib% :error
+
 echo "Running tests"
 REM Need to find a way of running test from the standard make test (not working)
+cd %WORKSPACE%/workspace/haptix-comm/build
 ctest -C "Release" --verbose --extra-verbose || exit 0
