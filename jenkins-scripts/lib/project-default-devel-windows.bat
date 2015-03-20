c9433ec4e8c8772825329ea229ee9279b3d7109b
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-03-20 02:09:07 +0100

    Fix generic job and setup sections

--- jenkins-scripts/lib/project-default-devel-windows.bat
+++ jenkins-scripts/lib/project-default-devel-windows.bat
@@ -2,51 +2,61 @@ REM Windows standard file to build Visual Studio projects
 
 set win_lib=%SCRIPT_DIR%\lib\windows_library.bat
 
-IF %PLATFORM_TO_BUILD% == x86 (
-  echo "Using 32bits VS configuration"
-  set VS_CMAKE_GEN=%VS32bits_CMAKE_GEN%
-) ELSE (
-  echo "Using 64bits VS configuration"
-  set VS_CMAKE_GEN=%VS64bits_CMAKE_GEN%
-)
+:: Call vcvarsall and all the friends
+echo # BEGIN SECTION: configure the MSVC compiler
+call %win_lib% :configure_msvc_compiler
+echo # END SECTION
 
-REM Configure the VC++ compilation
-call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" %PLATFORM_TO_BUILD%
-
-echo %WORKSPACE%
+echo # BEGIN SECTION: setup workspace
 cd %WORKSPACE%
-REM Reset the workspace directory if exists
-if exist workspace ( rmdir workspace /s /q )
+IF exist workspace ( rmdir /s /q workspace ) || goto %win_lib% :error
 mkdir workspace
-cd workspace || goto :error
+cd workspace
+echo # END SECTION
 
-echo "Download dependency if needed"
+echo # BEGIN SECTION: downloading and unzip dependency %DEPENDENCY_PKG%
 REM Todo: support multiple dependencies
 if defined DEPENDENCY_PKG (
   call %win_lib% :download_7za
   call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/%DEPENDENCY_PKG% %DEPENDENCY_PKG% || goto :error
   call %win_lib% :unzip_7za %DEPENDENCY_PKG% %DEPENDENCY_PKG% > install_boost.log || goto:error
 )
+echo # END SECTION
 
-echo "Copy sources inside workspace"
+echo # BEGIN SECTION: move %VCS_DIRECTORY% source to workspace
 xcopy %WORKSPACE%\%VCS_DIRECTORY% %VCS_DIRECTORY% /s /e /i > xcopy.log || goto :error
 cd %VCS_DIRECTORY% || goto :error
 mkdir build
 cd build
+echo # END SECTION
 
 if exist ..\configure.bat (
-  echo "Found configure.bat running it"
+  echo # BEGIN SECTION: configuring %VCS_DIRECTORY% using configure.bat
   call ..\configure.bat || goto :error
 ) else (
-  echo "cmake .. %VS_CMAKE_GEN% %VS_DEFAULT_CMAKE_FLAGS% %ARG_CMAKE_FLAGS%"
+  echo # BEGIN SECTION: configuring %VCS_DIRECTORY% using cmake 
   cmake .. %VS_CMAKE_GEN% %VS_DEFAULT_CMAKE_FLAGS% %ARG_CMAKE_FLAGS% || goto :error
 )
+echo # END SECTION
 
-REM Running the compilation
-msbuild %VS_DEFAULT_MSBUILD_FLAGS% ALL_BUILD.vcxproj || goto :error
+echo # BEGIN SECTION: compiling %VCS_DIRECTORY%
+nmake || goto %win_lib% :error
+echo # END SECTION
+echo # BEGIN SECTION: installing %VCS_DIRECTORY%
+nmake install || goto %win_lib% :error
+echo # END SECTION
 
-REM Need to find a way of running test from msbuild passing ARGS=-VV
-ctest -C "Release" --verbose --extra-verbose || echo "tests failed"
+echo # BEGIN SECTION: running tests
+set TEST_RESULT_PATH=%WORKSPACE%/test_results
+
+if exist %TEST_RESULT_PATH% ( rmdir /s %TEST_RESULT_PATH% )
+REM Need to find a way of running test from the standard make test (not working)
+cd %WORKSPACE%/workspace/haptix-comm/build
+ctest -C "Release" --verbose --extra-verbose || echo "test failed"
+echo # END SECTION
+echo # BEGIN SECTION: export testing results
+move test_results %TEST_RESULT_PATH% || goto :error
+echo # END SECTION
 
 if NOT DEFINED %KEEP_WORKSPACE% (
    echo # BEGIN SECTION: clean up workspace
@@ -57,6 +67,5 @@ if NOT DEFINED %KEEP_WORKSPACE% (
 :error - error routine
 ::
 echo Failed with error #%errorlevel%.
-if exist workspace ( rmdir workspace /s /q )
 exit /b %errorlevel%
 goto :EOF
