2f91b8ef02bb243994be16a2841f6d1be85cbc45
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-03-31 00:47:50 +0200

    Move path fix to proper place

--- jenkins-scripts/lib/windows_library.bat
+++ jenkins-scripts/lib/windows_library.bat
@@ -2,13 +2,13 @@
 call :%*
 exit /b
 
-:: See: https://issues.jenkins-ci.org/browse/JENKINS-11992
-set path=%path:"=%
-
 :: ##################################
 :configure_msvc_compiler
 ::
 ::
+
+:: See: https://issues.jenkins-ci.org/browse/JENKINS-11992
+set path=%path:"=%
 
 :: By default should be the same
 set MSVC_KEYWORD=%PLATFORM_TO_BUILD%
