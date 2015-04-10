0678b3b4b934c7d8fa0606203b018865e95a15a3
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-04-10 22:19:05 +0200

    Initial implementation of versioned sdks

--- jenkins-scripts/lib/haptix_comm-make_sdk-windows.bat
+++ jenkins-scripts/lib/haptix_comm-make_sdk-windows.bat
@@ -117,6 +117,7 @@ for %%b in (Debug, Release) do (
     call ..\configure %%b %BITNESS%
     nmake VERBOSE=1 > ign-transport.log || goto :error
     nmake install
+    set /p IGNTRANSPORT_VERSION=<VERSION
     cd ..\..
     echo # END SECTION
 
@@ -128,6 +129,7 @@ for %%b in (Debug, Release) do (
     call ..\configure %%b %BITNESS%
     nmake VERBOSE=1 > haptix.log || goto :error
     nmake install
+    set /p HAPTIX_VERSION=<VERSION
     echo # END SECTION
    
     echo # BEGIN SECTION: build haptix-comm examples in %%b
@@ -143,7 +145,7 @@ for %%b in (Debug, Release) do (
 
     cd ..\..\..
 
-    echo # BEGIN SECTION: generate zip file in %%b
+    echo # BEGIN SECTION: generate zip (version %HAPTIX_VERSION%) in %%b
     :: Package it all up
     :: Our goal here is to create an "install" layout for all the stuff
     :: needed to use haptix-comm.  That layout can be then be zipped and
@@ -152,9 +154,9 @@ for %%b in (Debug, Release) do (
     :: why the ! var ! is being used. For more information, please read:
     :: http://ss64.com/nt/delayedexpansion.html
     set "build_type=%%b"
-    set "installdir=%cwd%\hx_gz_sdk_!build_type!"
+    set "installdir=%cwd%\hx_gz_sdk-%HAPTIX_VERSION%-!build_type!"
     
-    set "sdk_zip_file=%WORKSPACE%\hx_gz_sdk-!build_type!-%haptix_hash%-win%BITNESS%.zip"
+    set "sdk_zip_file=%WORKSPACE%\hx_gz_sdk-!build_type!-%HAPTIX_VERSION%-win%BITNESS%.zip"
 
     echo " * Build type             : !build_type!"
     echo " * Installation directory : !installdir!"
