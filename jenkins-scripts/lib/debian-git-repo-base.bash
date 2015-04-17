17b5c6b0a3643f917eda319457ffddd28dbaf880
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-04-17 02:19:12 +0200

    Need to use key properly

--- jenkins-scripts/lib/debian-git-repo-base.bash
+++ jenkins-scripts/lib/debian-git-repo-base.bash
@@ -35,10 +35,8 @@ apt-get install -y pbuilder fakeroot debootstrap devscripts dh-make ubuntu-dev-t
 
 # Also get gazebo repo's key, to be used in getting Gazebo
 sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu $DISTRO main" > /etc/apt/sources.list.d/gazebo.list'
-apt-get update
-
-# Needed for pbuilder
 wget http://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
+apt-get update
 
 # Hack to avoid problem with non updated 
 if [ $DISTRO = 'precise' ]; then
