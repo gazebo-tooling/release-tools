a0ec132d61df2039daa513258eb88bdb1b51ec4e
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-05-07 01:31:19 +0200

    Export the original tarball

--- jenkins-scripts/docker/lib/debbuild-base.bash
+++ jenkins-scripts/docker/lib/debbuild-base.bash
@@ -119,6 +119,7 @@ debuild --no-tgz-check -uc -us -S --source-option=--include-binaries
 
 cp ../*.dsc $WORKSPACE/pkgs
 cp ../*.orig.* $WORKSPACE/pkgs
+cp ../*.tar.* $WORKSPACE/pkgs
 # debian is only generated in quilt format, native does not have it
 cp ../*.debian.* $WORKSPACE/pkgs || true
 echo '# END SECTION'
