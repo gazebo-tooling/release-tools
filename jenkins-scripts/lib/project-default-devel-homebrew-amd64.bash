ebb1da670046270c2d8563240446c86c9d0bd92b
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-05-07 13:12:00 +0200

    Initial work

--- jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
@@ -12,34 +12,30 @@ PROJECT_ARGS=${2}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
 # Step 1. Set up homebrew
-RUN_DIR=$(mktemp -d ${HOME}/jenkins.XXXX)
-echo "Install into: ${RUN_DIR}"
-cd $RUN_DIR
-# Install homebrew
-curl -L -o homebrew.tar.gz https://github.com/Homebrew/homebrew/tarball/master 
-tar --strip 1 -xzf homebrew.tar.gz
+echo '# BEGIN SECTION: clean up /usr/local'
+
+echo '# END SECTION'
+
+echo '# BEGIN SECTION: install latest homebrew'
+
+echo '# END SECTION'
 
 # Need to create cache so the system one (without permissions) is not used
 LOCAL_CELLAR=${HOME}/Library/Caches/Homebrew
 mkdir -p ${LOCAL_CELLAR}
 
+echo '# BEGIN SECTION: brew information'
 # Run brew update to get latest versions of formulae
-${RUN_DIR}/bin/brew update
-
+brew update
 # Run brew config to print system information
-${RUN_DIR}/bin/brew config
-
+brew config
 # Run brew doctor to check for problems with the system
-${RUN_DIR}/bin/brew doctor || true
+brew doctor || true
+echo '# END SECTION'
 
-# Step 2. Install dependencies of ${PROJECT}
-${RUN_DIR}/bin/brew tap osrf/simulation
-
-# Unlink system dependencies first
-for dep in `/usr/local/bin/brew deps ${PROJECT} ${PROJECT_ARGS}`
-do
-  /usr/local/bin/brew unlink ${dep} || true
-done || true
+echo '# BEGIN SECTION: setup the osrf/simulation tap'
+brew tap osrf/simulation
+echo '# END SECTION'
 
 IS_A_HEAD_FORMULA=${IS_A_HEAD_PROJECT:-false}
 HEAD_STR=""
@@ -47,12 +43,15 @@ if $IS_A_HEAD_PROJECT; then
     HEAD_STR="--HEAD"
 fi
 
+echo "# BEGIN SECTION: install ${PROJECT} dependencies"
 # Process the package dependencies
 # Run twice! details about why in:
 # https://github.com/osrf/homebrew-simulation/pull/18#issuecomment-45041755 
 ${RUN_DIR}/bin/brew install ${HEAD_STR} ${PROJECT} ${PROJECT_ARGS} --only-dependencies
 ${RUN_DIR}/bin/brew install ${HEAD_STR} ${PROJECT} ${PROJECT_ARGS} --only-dependencies
+echo '# END SECTION'
 
+echo "# BEGIN SECTION: configuring ${PROJECT}"
 # Step 3. Manually compile and install ${PROJECT}
 cd ${WORKSPACE}/${PROJECT}
 # Need the sudo since the test are running with roots perms to access to GUI
@@ -60,14 +59,6 @@ sudo rm -fr ${WORKSPACE}/build
 mkdir -p ${WORKSPACE}/build
 cd ${WORKSPACE}/build
  
-
-# Mimic the homebrew variables
-export PKG_CONFIG_PATH=${RUN_DIR}/lib/pkgconfig
-export DYLD_FALLBACK_LIBRARY_PATH="$DYLD_FALLBACK_LIBRARY_PATH:${RUN_DIR}/lib"
-export PATH="${PATH}:${RUN_DIR}/bin"
-export C_INCLUDE_PATH="${C_INCLUDE_PATH}:${RUN_DIR}/include"
-export CPLUS_INCLUDE_PATH="${CPLUS_INCLUDE_PATH}:${RUN_DIR}/include"
-
 # add X11 path so glxinfo can be found
 export PATH="${PATH}:/opt/X11/bin"
 
@@ -83,25 +74,23 @@ ${RUN_DIR}/bin/cmake ${WORKSPACE}/${PROJECT} \
       -DCMAKE_INSTALL_PREFIX=${RUN_DIR}/Cellar/${PROJECT}/HEAD \
       -DCMAKE_PREFIX_PATH=${RUN_DIR} \
       -DBOOST_ROOT=${RUN_DIR}
+echo '# END SECTION'
 
+echo "# BEGIN SECTION: compile ${PROJECT}"
 make -j${MAKE_JOBS} install
-${RUN_DIR}/bin/brew link ${PROJECT}
+brew link ${PROJECT}
+brew doctor
+echo '# END SECTION'
 
-cat > test_run.sh << DELIM
-cd $WORKSPACE/build/
-export PKG_CONFIG_PATH=${RUN_DIR}/lib/pkgconfig
-export DYLD_FALLBACK_LIBRARY_PATH=${RUN_DIR}/lib
-export BOOST_ROOT=${RUN_DIR}
-export PATH="${PATH}:${RUN_DIR}/bin"
-export CMAKE_PREFIX_PATH=${RUN_DIR}
-
+echo "# BEGIN SECTION: run tests"
 # Need to clean up models before run tests (issue 27)
 rm -fr \$HOME/.gazebo/models
-make test ARGS="-VV" || true
-DELIM
 
-chmod +x test_run.sh
-sudo  ./test_run.sh
+cd $WORKSPACE/build/
+# May need sudo to run tests?
+make test ARGS="-VV" || true
+echo '# END SECTION'
 
 # Step 5. Clean up
 rm -fr ${RUN_DIR}
+echo '# END SECTION'
