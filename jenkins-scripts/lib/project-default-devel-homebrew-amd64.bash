2f79e038f56b1fa8289300c12678b1ce34c939eb
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-05-11 18:40:13 +0200

    Properly remove of previous installation

--- jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
@@ -14,6 +14,7 @@ SCRIPT_DIR="${SCRIPT_DIR%/*}"
 # Step 1. Set up homebrew
 echo '# BEGIN SECTION: clean up /usr/local'
 sudo chown -R jenkins /usr/local 
+rm -rf /usr/local/Cellar /usr/local/.git && brew cleanup
 cd /usr/local && git clean -fdx
 echo '# END SECTION'
 
