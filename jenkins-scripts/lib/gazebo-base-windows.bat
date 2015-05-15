482389d5786c3ac37121a5d31a4092cf8b6e68d8
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-05-15 15:46:58 +0200

    Import the jom tool

--- jenkins-scripts/lib/gazebo-base-windows.bat
+++ jenkins-scripts/lib/gazebo-base-windows.bat
@@ -76,6 +76,7 @@ call "..\configure.bat" Release %BITNESS% || goto %win_lib% :error
 echo # END SECTION
 
 echo # BEGIN SECTION: compiling gazebo
+copy %WORKSPACE%\workspace\jom.exe .
 jom -j%MAKE_JOBS% || goto :error
 echo # END SECTION
 
