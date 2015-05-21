cfa9edb561b0b46c9a14dcbd2a0eeb3168c9d836
Steve Peters <scpeters@osrfoundation.org>
2015-05-21 10:46:31 -0700

    Allow building against tags, not just branches

--- jenkins-scripts/lib/sdformat-base-windows.bat
+++ jenkins-scripts/lib/sdformat-base-windows.bat
@@ -34,7 +34,7 @@ echo # END SECTION
 IF %USE_IGNITION_ZIP% == FALSE (
   echo # BEGIN SECTION: compile and install ign-math
   IF exist %WORKSPACE%\ign-math ( rmdir /s /q %WORKSPACE%\ign-math ) || goto :error
-  hg clone https://bitbucket.org/ignitionrobotics/ign-math %WORKSPACE%\ign-math -b %IGNMATH_BRANCH% || goto :error
+  hg clone https://bitbucket.org/ignitionrobotics/ign-math %WORKSPACE%\ign-math -u %IGNMATH_BRANCH% || goto :error
   set VCS_DIRECTORY=ign-math
   set KEEP_WORKSPACE=TRUE
   call "%SCRIPT_DIR%\lib\project-default-devel-windows.bat"
