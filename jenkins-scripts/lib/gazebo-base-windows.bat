17e71c2f6df5a7a0f2aed9387dfa1478e47b4134
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-05-28 01:54:36 +0200

    Include ign-math2 in the gazebo compilation on windows

--- jenkins-scripts/lib/gazebo-base-windows.bat
+++ jenkins-scripts/lib/gazebo-base-windows.bat
@@ -28,6 +28,7 @@ call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/protobuf-2.6.0
 call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/tbb43_20141023oss_win.zip tbb43_20141023oss_win.zip
 call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/zziplib-0.13.62-vc12-x64-release-debug.zip zziplib-0.13.62-vc12-x64-release-debug.zip
 call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/zlib-1.2.8-vc12-x64-release-debug.zip zlib-1.2.8-vc12-x64-release-debug.zip
+call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/ign-math2.zip ign-math2.zip
 call %win_lib% :wget http://download.qt-project.org/official_releases/jom/jom.zip jom.zip
 
 call %win_lib% :download_7za
@@ -42,6 +43,7 @@ call %win_lib% :unzip_7za protobuf-2.6.0-win%BITNESS%-vc12.zip
 call %win_lib% :unzip_7za tbb43_20141023oss_win.zip
 call %win_lib% :unzip_7za zlib-1.2.8-vc12-x64-release-debug.zip
 call %win_lib% :unzip_7za zziplib-0.13.62-vc12-x64-release-debug.zip
+call %win_lib% :unzip_7za ign-math2.zip
 call %win_lib% :unzip_7za jom.zip
 echo # END SECTION
 ) ELSE (
