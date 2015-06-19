9e207d6f47454a3e82fb8cf7573588405ab610a2
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-06-19 03:23:42 +0200

    Need to define ROS variable

--- jenkins-scripts/multidistribution-gcc48-debbuild.bash
+++ jenkins-scripts/multidistribution-gcc48-debbuild.bash
@@ -6,6 +6,7 @@ SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
 export RELEASE_REPO_DIRECTORY=${DISTRO}
 export WORKAROUND_PBUILDER_BUG=true
+export ENABLE_ROS=false
 export NEED_GCC48_COMPILER=true
 
 . ${SCRIPT_DIR}/lib/debbuild-base.bash
