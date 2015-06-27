d23bc385353e33847cedbf7d252b48939c0e5f2a
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-06-27 18:06:37 +0200

    Remove unedded else

--- jenkins-scripts/lib/ros_gazebo_pkgs-check-release.bash
+++ jenkins-scripts/lib/ros_gazebo_pkgs-check-release.bash
@@ -82,12 +82,11 @@ catkin_make -j${MAKE_JOBS}
 SHELL=/bin/sh . $WORKSPACE/ws/devel/setup.sh
 
 # Precise coreutils does not support preserve-status
-if [ $DISTRO = 'precise' ]; then
-  # TODO: all the testing on precise X in our jenkins is currently segfaulting
-  # docker can not run on Precise without altering the base packages
-  # Previous attemps to get GPU acceleration for intel in chroot:
-  # http://build.osrfoundation.org/job/ros_gazebo_pkgs-release-testing-broken-intel/
-else
+# TODO: all the testing on precise X in our jenkins is currently segfaulting
+# docker can not run on Precise without altering the base packages
+# Previous attemps to get GPU acceleration for intel in chroot:
+# http://build.osrfoundation.org/job/ros_gazebo_pkgs-release-testing-broken-intel/
+if [ $DISTRO != 'precise' ]; then
   # timeout --preserve-status 180 roslaunch gazebo_ros shapes_world.launch extra_gazebo_args:="--verbose"
   timeout --preserve-status 180 roslaunch rrbot_gazebo rrbot_world.launch headless:=true extra_gazebo_args:="--verbose"
   if [ $? != 0 ]; then
