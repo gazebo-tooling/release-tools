e5ba0eb2268a8884fe64fecbd54b757d7ffc2974
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-09-01 23:41:06 +0000

    split package list into multiples lines

--- jenkins-scripts/cygwin/_ros1_roscygwin_compilation.bash
+++ jenkins-scripts/cygwin/_ros1_roscygwin_compilation.bash
@@ -10,7 +10,9 @@ apt-cyg install python make cmake gdb git patch unzip pkg-config gcc-g++ libtool
 echo '# END SECTION'
 
 echo "# BEGIN SECTION: Install library dependencies"
-apt-cyg install libpoco-devel libboost-devel libboost_python-devel libGLU-devel libgtk2.0-devel libcurl-devel libjpeg-devel libfltk-devel libX11-devel libXext-devel libfreetype-devel libxml2-devel libqhull-devel
+apt-cyg install libpoco-devel libboost-devel libboost_python-devel libGLU-devel \ 
+                libgtk2.0-devel libcurl-devel libjpeg-devel libfltk-devel \
+				libX11-devel libXext-devel libfreetype-devel libxml2-devel libqhull-devel
 echo '# END SECTION'
 
 echo "# BEGIN SECTION: Prepare script sources"
