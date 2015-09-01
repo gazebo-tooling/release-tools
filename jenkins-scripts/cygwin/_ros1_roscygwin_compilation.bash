4d5aa4a9dea842de00b7a3056adc6e2f625acc2b
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-09-01 19:24:47 +0200

    Added the cygwin ros scripts

---
+++ jenkins-scripts/cygwin/_ros1_roscygwin_compilation.bash
@@ -0,0 +1,34 @@
+#/bin/bash
+
+echo "# BEGIN SECTION: Install script dependencies"
+apt-cyg update
+apt-cyg install wget mercurial tar gawk xz bzip2 
+echo '# END SECTION'
+
+echo "# BEGIN SECTION: Install tool dependencies"
+apt-cyg install python make cmake gdb git patch unzip pkg-config gcc-g++ libtool
+echo '# END SECTION'
+
+echo "# BEGIN SECTION: Install library dependencies"
+apt-cyg install libpoco-devel libboost-devel libboost_python-devel libGLU-devel libgtk2.0-devel libcurl-devel libjpeg-devel libfltk-devel libX11-devel libXext-devel libfreetype-devel libxml2-devel libqhull-devel
+echo '# END SECTION'
+
+echo "# BEGIN SECTION: Prepare script sources"
+
+# TODO: At his moment the script is located in a cygwin fixed dir
+# need to check it out from the repository
+CHECKOUT_PATH=${HOME}/ros_cygwin-master
+SCRIPTS_INSTALL_DIR=/opt/rosscripts
+
+# Cleanup the installs paths
+mkdir -p /opt
+rm -fr ${SCRIPTS_INSTALL_DIR}
+
+# Copy script repo into /opt
+cp -a ${CHECKOUT_PATH}/autobuild ${SCRIPTS_INSTALL_DIR}
+echo '# END SECTION'
+
+echo "# BEGIN SECTION: Run ros_build_isolated.bat"
+cd ${SCRIPTS_INSTALL_DIR}
+./build_ros_isolated.sh
+echo "# END SECTION"
---
+++ jenkins-scripts/cygwin/dsl/roscywgin_compilation.dsl
@@ -0,0 +1,15 @@
+job('roscygwin-ci_daily-cygwin64') {
+    scm {
+        hg('http://bibucket.org/osrf/release-tools','${RTOOLS_BRANCH}')
+    }
+    triggers {
+       cron('@daily')
+    }
+    parameters {
+       stringParam('RTOOLS_BRANCH','default','release-tool branch to use')
+    }
+    steps {
+       systemGroovyCommand("build.setDescription('RTOOLS_BRANCH: ' + build.buildVariableResolver.resolve('RTOOLS_BRANCH'));")
+       shell("${WORKSPACE}/jenkins-scripts/cygwin/_ros1_roscygwin_compilation.bash")
+   }
+}
