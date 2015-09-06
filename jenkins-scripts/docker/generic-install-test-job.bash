8a89f0ad323a99df8dcabdce3c5e8524a34781d6
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-09-06 03:16:33 +0200

    New names

--- jenkins-scripts/docker/generic-install-test-job.bash
+++ jenkins-scripts/docker/generic-install-test-job.bash
@@ -4,4 +4,4 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-. ${SCRIPT_DIR}/lib/install-base.bash
+. ${SCRIPT_DIR}/lib/generic-install-base.bash
