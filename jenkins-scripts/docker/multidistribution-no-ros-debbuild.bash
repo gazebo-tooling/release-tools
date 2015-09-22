4c464147674d2546ed603523979a155ef511ca8e
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-09-22 03:42:29 +0200

    Implement mentor2 repo

--- jenkins-scripts/docker/multidistribution-no-ros-debbuild.bash
+++ jenkins-scripts/docker/multidistribution-no-ros-debbuild.bash
@@ -5,6 +5,12 @@
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
 export ENABLE_ROS=false
+
+# Need to find a better place to handle project repository constraints
+if [[ -n ${UPLOAD_TO_REPO} ]] && [[ ${UPLOAD_TO_REPO} == "mentor2" ]]; then
+  OSRF_REPOS_TO_USE="mentor2"
+fi
+
 export OSRF_REPOS_TO_USE=${OSRF_REPOS_TO_USE:=stable}
 
 . ${SCRIPT_DIR}/lib/debbuild-base.bash
