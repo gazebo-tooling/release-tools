78af345502242ff1125dfd962fc7ccf747dcd596
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-09-30 18:27:23 +0200

    Remove cppcheck from UNIX

--- jenkins-scripts/dsl/_configs_/GenericUNIXCompilation.groovy
+++ jenkins-scripts/dsl/_configs_/GenericUNIXCompilation.groovy
@@ -29,45 +29,6 @@ class GenericUNIXCompilation
                    testDataPublishers()
               }
            }
-
-           // cppcheck is not implemented. Use configure for it
-           configure { project ->
-             project / publishers / 'org.jenkinsci.plugins.cppcheck.CppcheckPublisher' / cppcheckConfig {
-               pattern('build/cppcheck_results/*.xml')
-               ignoreBlankFiles true
-               allowNoReport false
-
-               configSeverityEvaluation {
-                     threshold 0
-                     newThreshold()
-                     failureThreshold()
-                 newFailureThreshold()
-                 healthy()
-                 unHealthy()
-                 severityError true
-                 severityWarning true
-                 severityStyle true
-                 severityPerformance true
-                 severityInformation true
-                 severityNoCategory false
-                 severityPortability false
-               }
-
-                     configGraph {
-                 xSize 500
-                 ySize 200
-                 numBuildsInGraph 0
-                 displayAllErrors true
-                 displayErrorSeverity false
-                 displayWarningSeverity false
-                 displayStyleSeverity false
-                 displayPerformanceSeverity false
-                 displayInformationSeverity false
-                 displayNoCategorySeverity false
-                 displayPortabilitySeverity false
-               } // end of configGraph
-             } // end of cppcheckconfig
-           } // end of configure
         } // end of publishers
       } // end of job
    } // end of create
