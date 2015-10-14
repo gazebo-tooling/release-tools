e14ffc748aaba543f43b1a4c37f6907421f02561
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-10-15 01:09:00 +0200

    Workaround for ignition packages

--- jenkins-scripts/ign_transport-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/ign_transport-default-devel-homebrew-amd64.bash
@@ -6,4 +6,4 @@ SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
 IS_A_HEAD_FORMULA=true
 
-. ${SCRIPT_DIR}/lib/project-default-devel-homebrew-amd64.bash ign-transport
+. ${SCRIPT_DIR}/lib/project-default-devel-homebrew-amd64.bash ignition-transport
