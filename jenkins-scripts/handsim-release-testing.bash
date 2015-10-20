9847d82d4731481ca181f9ff94d89900862f6925
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-10-20 21:56:17 +0200

    Forgot to add the scripts

---
+++ jenkins-scripts/handsim-release-testing.bash
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+. ${SCRIPT_DIR}/lib/handsim-check-release.bash
---
+++ jenkins-scripts/lib/handsim-check-release.bash
@@ -0,0 +1,59 @@
+#!/bin/bash -x
+
+# Use always GPU in drcsim project
+export GPU_SUPPORT_NEEDED=true
+
+# Do not use the subprocess_reaper in debbuild. Seems not as needed as in
+# testing jobs and seems to be slow at the end of jenkins jobs
+export ENABLE_REAPER=false
+
+. ${SCRIPT_DIR}/lib/boilerplate_prepare.sh
+
+cat > build.sh << DELIM
+###################################################
+# Make project-specific changes here
+#
+set -ex
+
+apt-get install -y wget
+# Also get drc repo's key, to be used in getting Gazebo
+sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu ${DISTRO} main" > /etc/apt/sources.list.d/drc-latest.list'
+wget http://packages.osrfoundation.org/drc.key -O - | apt-key add -
+apt-get update
+
+# check for graphic card support
+GRAPHIC_TESTS=false
+if [ $GRAPHIC_CARD_NAME = Nvidia ] && [ $DISTRO = trusty ]; then
+    GRAPHIC_TESTS=true
+
+    if ${GRAPHIC_CARD_FOUND}; then
+	apt-get install -y ${GRAPHIC_CARD_PKG}
+	# Check to be sure version of kernel graphic card support is the same.
+	# It will kill DRI otherwise
+	CHROOT_GRAPHIC_CARD_PKG_VERSION=\$(dpkg -l | grep "^ii.*${GRAPHIC_CARD_PKG}\ " | awk '{ print \$3 }' | sed 's:-.*::')
+	if [ "\${CHROOT_GRAPHIC_CARD_PKG_VERSION}" != "${GRAPHIC_CARD_PKG_VERSION}" ]; then
+	   echo "Package ${GRAPHIC_CARD_PKG} has different version in chroot and host system. Maybe you need to update your host" 
+	   exit 1
+	fi
+    fi
+fi
+
+apt-get install -y handsim
+
+# In our nvidia machines, run the test to launch altas
+# Seems like there is no failure in runs on precise pbuilder in
+# our trusty machine. So we do not check for GRAPHIC_TESTS=true
+mkdir -p \$HOME/.gazebo
+timeout 180 gazebo worlds/arat.world || cat \$HOME/.gazebo/gzserver.log && echo "Failure response in the launch command"
+
+echo "180 testing seconds finished successfully"
+
+DELIM
+
+# Make project-specific changes here
+###################################################
+
+sudo pbuilder  --execute \
+    --bindmounts $WORKSPACE \
+    --basetgz $basetgz \
+    -- build.sh
