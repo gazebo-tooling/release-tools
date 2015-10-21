e9209c44e5bf803adc459a02f7c19276b07d8bf7
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-10-21 17:53:23 +0200

    Adde ign-math compilation script

---
+++ jenkins-scripts/docker/ign_math-compilation.bash
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+if [[ -z ${ARCH} ]]; then
+  echo "ARCH variable not set!"
+  exit 1
+fi
+
+if [[ -z ${DISTRO} ]]; then
+  echo "DISTRO variable not set!"
+  exit 1
+fi
+
+export BUILDING_SOFTWARE_DIRECTORY="ign-math"
+export BUILDING_JOB_REPOSITORIES="stable"
+
+. ${SCRIPT_DIR}/lib/generic-building-base.bash
