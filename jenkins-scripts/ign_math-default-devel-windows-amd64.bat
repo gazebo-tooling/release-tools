ec6492a5976781368d94c68a7f3aad872f1eda27
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-10-21 18:08:09 +0200

    Fix VCS directory

--- jenkins-scripts/ign_math-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_math-default-devel-windows-amd64.bat
@@ -2,7 +2,7 @@ set SCRIPT_DIR="%~dp0"
 
 call "%SCRIPT_DIR%/lib/windows_configuration.bat"
 
-set VCS_DIRECTORY=ignition-math
+set VCS_DIRECTORY=ign-math
 set PLATFORM_TO_BUILD=x86_amd64
 
 call "%SCRIPT_DIR%/lib/project-default-devel-windows.bat"
