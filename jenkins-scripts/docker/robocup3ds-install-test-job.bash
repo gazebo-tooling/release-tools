c02e1722acb3ec6e1f8ad03d2981a355de0a5b9d
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-10-23 00:37:00 +0200

    Give a try to the simulator

--- jenkins-scripts/docker/robocup3ds-install-test-job.bash
+++ jenkins-scripts/docker/robocup3ds-install-test-job.bash
@@ -19,7 +19,14 @@ DELIM
 """
 
 INSTALL_JOB_POSTINSTALL_HOOK="""
+echo '# BEGIN SECTION: run the one-liner installation'
 curl -ssL https://bitbucket.org/osrf/release-tools/raw/default/one-line-installations/robocup3ds.sh | sh
+echo '# END SECTION'
+
+
+echo '# BEGIN SECTION: test the script'
+timeout --preserve-status 180 gazebo-robocup3ds --verbose
+echo '# END SECTION'
 """
 
 . ${SCRIPT_DIR}/lib/generic-install-base.bash
