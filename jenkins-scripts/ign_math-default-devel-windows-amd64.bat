e40fdc10d46544878d5ef2f69d44c0dbd0ba113d
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-11-04 00:48:33 +0100

    Reset workspace in ign-transport

--- jenkins-scripts/ign_math-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_math-default-devel-windows-amd64.bat
@@ -4,5 +4,6 @@ call "%SCRIPT_DIR%/lib/windows_configuration.bat"
 
 set VCS_DIRECTORY=ign-math
 set PLATFORM_TO_BUILD=x86_amd64
+set IGN_CLEAN_WORKSPACE=true
 
 call "%SCRIPT_DIR%/lib/project-default-devel-windows.bat"
