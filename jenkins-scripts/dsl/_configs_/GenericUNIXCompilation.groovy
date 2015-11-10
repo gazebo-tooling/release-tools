65b9260bf0674d04240fce26887c7db7a30efd89
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-11-10 21:27:03 +0100

    Remove all actions from GenericUNIXCompilation

--- jenkins-scripts/dsl/_configs_/GenericUNIXCompilation.groovy
+++ jenkins-scripts/dsl/_configs_/GenericUNIXCompilation.groovy
@@ -16,19 +16,5 @@ class GenericUNIXCompilation
  
      GenericCompilation.create(job)
 
-     job.with
-     {
-        publishers
-        {
-           // junit plugin is not implemented. Use configure for it
-           configure { project ->
-              project / publishers << 'hudson.tasks.junit.JUnitResultArchiver' {
-                   testResults('build/test_results/*.xml')
-                   keepLongStdio false
-                   testDataPublishers()
-              }
-           }
-        } // end of publishers
-      } // end of job
    } // end of create
 } // end of class
