58e8723ad2c87069a2b88cb017335ac88a5d3034
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-11-25 01:30:54 +0100

    Fix syntax

--- jenkins-scripts/dsl/_configs_/OSRFLinuxPerformance.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFLinuxPerformance.groovy
@@ -37,27 +37,26 @@ class OSRFLinuxABI
       {
         configure { project ->
           project / publishers / 'hudson.plugins.performance.PerformancePublisher' {
-              errorFailedThreshold 0
-              errorUnstableThreshold 0
-              errorUnstableResponseTimeThreshold ''
-              relativeFailedThresholdPositive '20.0'
-              relativeFailedThresholdNegative '-100.0'
-              relativeUnstableThresholdPositive '10.0'
-              relativeUnstableThresholdNegative '-50.0'
-              nthBuildNumber 0
-              modeRelativeThresholds false
-              configType ART
-              modeOfThreshold true
-              compareBuildPrevious true
-              modePerformancePerTestCase true              
-              
-              parsers {
-                'hudson.plugins.performance.JUnitParser' {
-                  glob 'build/test_results/PERFORMANCE_*.xml'
-                }
-              } // end of parsers
-           } // end of project
-         } // end of congfigure
+            errorFailedThreshold 0
+            errorUnstableThreshold 0
+            errorUnstableResponseTimeThreshold ''
+            relativeFailedThresholdPositive '20.0'
+            relativeFailedThresholdNegative '-100.0'
+            relativeUnstableThresholdPositive '10.0'
+            relativeUnstableThresholdNegative '-50.0'
+            nthBuildNumber 0
+            modeRelativeThresholds false
+            configType ART
+            modeOfThreshold true
+            compareBuildPrevious true
+            modePerformancePerTestCase true              
+            
+            parsers {
+              'hudson.plugins.performance.JUnitParser' {
+                glob 'build/test_results/PERFORMANCE_*.xml'
+            } // end of parsers
+          } // end of project
+        } // end of congfigure
       } // end of publishers
     } // end of with
   } // end of create
