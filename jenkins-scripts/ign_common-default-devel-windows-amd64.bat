7b8d70fa0de135888fd84eebf6af07f5d33c7f37
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-12-01 22:53:44 +0100

    Added ignition-math to list of dependencies

--- jenkins-scripts/ign_common-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_common-default-devel-windows-amd64.bat
@@ -5,6 +5,6 @@ call "%SCRIPT_DIR%/lib/windows_configuration.bat"
 set VCS_DIRECTORY=ign-common
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
-set DEPENDENCY_PKG=boost_1_56_0.zip,FreeImage-vc12-x64-release-debug.zip
+set DEPENDENCY_PKG=boost_1_56_0.zip,FreeImage-vc12-x64-release-debug.zip,ign-math2.zip
 
 call "%SCRIPT_DIR%/lib/project-default-devel-windows.bat"
