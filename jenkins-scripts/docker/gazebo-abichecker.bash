e3375c9913e612c82b223bd44e923299ffd2cf39
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-12-16 18:00:47 +0100

    Remove duplicate code. Reusegazebo version hook

--- jenkins-scripts/docker/gazebo-abichecker.bash
+++ jenkins-scripts/docker/gazebo-abichecker.bash
@@ -14,8 +14,13 @@ if [[ -z ${DISTRO} ]]; then
   exit 1
 fi
 
+${SCRIPT_DIR}/lib/_gazebo_version_hook.bash
+
 export ABI_JOB_SOFTWARE_NAME="gazebo"
 export ABI_JOB_REPOS="stable"
+if ${NEED_PRERELEASE}; then
+  ABI_JOB_REPOS="${ABI_JOB_REPOS} prerelease"
+fi
 export ABI_JOB_PKG_DEPENDENCIES_VAR_NAME="GAZEBO_BASE_DEPENDECIES"
 
 . ${SCRIPT_DIR}/lib/generic-abi-base.bash
--- jenkins-scripts/docker/lib/gazebo-base-default.bash
+++ jenkins-scripts/docker/lib/gazebo-base-default.bash
@@ -11,25 +11,7 @@ else
     GZ_CMAKE_BUILD_TYPE="-DCMAKE_BUILD_TYPE=${GZ_BUILD_TYPE}"
 fi
 
-# Identify GAZEBO_MAJOR_VERSION to help with dependency resolution
-GAZEBO_MAJOR_VERSION=`\
-  grep 'set.*GAZEBO_MAJOR_VERSION ' ${WORKSPACE}/gazebo/CMakeLists.txt | \
-  tr -d 'a-zA-Z _()'`
-
-# Check gazebo version is integer
-if ! [[ ${GAZEBO_MAJOR_VERSION} =~ ^-?[0-9]+$ ]]; then
-  echo "Error! GAZEBO_MAJOR_VERSION is not an integer, check the detection"
-  exit -1
-fi
-
-NEED_PRERELEASE=false
-if [[ $GAZEBO_MAJOR_VERSION -ge 7 ]]; then
-  # Need prerelease repo to get sdformat4 during the development cycle
-  # 20160125 release date of gazebo7
-  if [[ $(date +%Y%m%d) -le 20160125 ]]; then
-    NEED_PRERELEASE=true
-  fi
-fi
+${SCRIPT_DIR}/lib/_gazebo_version_hook.bash
 
 echo '# BEGIN SECTION: setup the testing enviroment'
 # Define the name to be used in docker
