e824122f54d8ac3535796bd7ffa601274e19043a
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-12-24 02:15:23 +0100

    Refactor

--- jenkins-scripts/lib/homebrew_bottle_pullrequest.bash
+++ jenkins-scripts/lib/homebrew_bottle_pullrequest.bash
@@ -1,65 +1,35 @@
 #!/bin/bash -x
-set -e
 
-[[ -L ${0} ]] && SCRIPT_LIBDIR=$(readlink ${0}) || SCRIPT_LIBDIR=${0}
-SCRIPT_LIBDIR="${SCRIPT_LIBDIR%/*}"
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-get_formula()
-{
-  local filename=${1}
-
-  echo ${filename/-*}
-}
-
-get_osX_distribution()
-{
-  local hash_line=${1}
-
-  echo ${hash_line/*:}
-}
-
-export PATH="/usr/local/bin:$PATH"
-
-PKG_DIR=${WORKSPACE}/pkgs
+set +e
 
 echo '# BEGIN SECTION: check variables'
-if [ -z "${PULL_REQUEST_URL}" ]; then
-    echo PULL_REQUEST_URL not specified
-    exit -1
+if [ -z "${PACKAGE_ALIAS}" ]; then
+  echo PACKAGE_ALIAS not specified
+  exit -1
+fi
+
+if [ -z "${DISTRO}" ]; then
+  echo DISTRO not specified
+  exit -1
+fi
+
+if [ -z "${VERSION}" ]; then
+  echo VERSION not specified
+  exit -1
+fi
+
+if [ -z "${NEW_HASH_LINE}" ]; then
+  echo NEW_HASH_LINE not specified
+  exit -1
 fi
 echo '# END SECTION'
 
-echo '# BEGIN SECTION: clean up environment'
-rm -fr ${PKG_DIR} && mkdir -p ${PKG_DIR}
-. ${SCRIPT_LIBDIR}/_homebrew_cleanup.bash
-echo '# END SECTION'
+. ${SCRIPT_DIR}/lib/_homebrew_github_setup.bash
 
-echo '# BEGIN SECTION: run test-bot'
-# return always true since audit fails to run gzserver
-# can not find a way of disabling it
-( brew test-bot           \
-    --tap=osrf/simulation \
-    --bottle              \
-    --ci-pr               \
-    --verbose ${PULL_REQUEST_URL} ) || true
-echo '# END SECTION'
+sed -i -e "s/sha256.*=>.*${DISTRO}/${NEW_HASH_LINE}/g" ${FORMULA_PATH}
 
-echo '# BEGIN SECTION: export bottle'
-if [[ $(find . -name *.bottle.* | wc -l) -lt 2 ]]; then
- echo "Can not find the two bottle files"
- exit -1
-fi
-
-mv *.bottle.tar.gz ${PKG_DIR}
-mv *.bottle.rb ${PKG_DIR}
-
-FILENAME=$(ls *.bottle.rb)
-FORMULA=$(get_formula ${FILENAME})
-NEW_HASH_LINE=$(grep 'sha256.*=>' $FILENAME)
-DISTRO=$(get_osX_distribution ${NEW_HASH_LINE})
-
-echo "PACKAGE_ALIAS=${FORMULA}" > exported_vars.properties
-echo "NEW_HASH_LINE=${NEW_HASH_LINE}" >> exported_vars.properties
-echo "DISTRO=${DISTRO}" >> exported_vars.properties
-
-echo '# END SECTION'
+. ${SCRIPT_DIR}/lib/_homebrew_github_commit.bash
