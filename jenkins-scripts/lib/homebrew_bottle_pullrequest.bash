f23e43c7d6c572eb0b0f33916c2818281fc81761
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-12-28 03:04:58 +0100

    run in bash

--- jenkins-scripts/lib/homebrew_bottle_pullrequest.bash
+++ jenkins-scripts/lib/homebrew_bottle_pullrequest.bash
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 # Knowing Script dir beware of symlink
-[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0})
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
 set +e
