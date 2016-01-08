cc028b5ce5c636314844bdaea7cc7d9efd6f8093
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-01-08 18:28:15 +0100

    Fix nightly version when creating orig file

--- jenkins-scripts/docker/lib/debbuild-base.bash
+++ jenkins-scripts/docker/lib/debbuild-base.bash
@@ -114,7 +114,7 @@ cd \`find $WORKSPACE/build -mindepth 1 -type d |head -n 1\`
 # If use the quilt 3.0 format for debian (drcsim) it needs a tar.gz with sources
 if $NIGHTLY_MODE; then
   rm -fr .hg*
-  echo | dh_make -s --createorig -p ${PACKAGE_ALIAS}_\${UPSTREAM_VERSION}~hg\${TIMESTAMP}r\${REV} > /dev/null
+  echo | dh_make -s --createorig -p ${PACKAGE_ALIAS}_\${UPSTREAM_VERSION}+hg\${TIMESTAMP}r\${REV} > /dev/null
 fi
 
 # Adding extra directories to code. debian has no problem but some extra directories
