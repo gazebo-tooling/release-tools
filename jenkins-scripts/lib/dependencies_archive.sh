bace60dbb9dc95b8cdaed44dd00dffc1ff3fa329
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-02-05 20:24:30 +0100

    Use ignition transport 1.0.0 in jenkins jobs

--- jenkins-scripts/lib/dependencies_archive.sh
+++ jenkins-scripts/lib/dependencies_archive.sh
@@ -154,7 +154,7 @@ fi
 
 if [[ ${GAZEBO_MAJOR_VERSION} -ge 7 ]]; then
     GAZEBO_BASE_DEPENDENCIES_NO_SDFORMAT="${GAZEBO_BASE_DEPENDENCIES_NO_SDFORMAT} \\
-                              libignition-transport0-dev"
+                              libignition-transport-dev"
 fi
 
 # libtinyxml2-dev is not on precise
@@ -342,7 +342,7 @@ HAPTIX_COMM_DEPENDENCIES_WITHOUT_IGN="pkg-config  \\
                           protobuf-compiler       \\
                           liboctave-dev"
 HAPTIX_COMM_DEPENDENCIES="${HAPTIX_COMM_DEPENDENCIES_WITHOUT_IGN} \\
-                          libignition-transport0-dev"
+                          libignition-transport-dev"
 #
 # HANDSIM
 #
