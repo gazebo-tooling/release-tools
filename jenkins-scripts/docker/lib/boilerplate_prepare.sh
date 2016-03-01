9fa8188cc9cfba3f66f95e938630987083cd67db
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-03-02 00:35:45 +0100

    Do not check for docker

--- jenkins-scripts/docker/lib/boilerplate_prepare.sh
+++ jenkins-scripts/docker/lib/boilerplate_prepare.sh
@@ -101,7 +101,8 @@ fi
 output_dir=$WORKSPACE/output
 work_dir=$WORKSPACE/work
 
-NEEDED_HOST_PACKAGES="mercurial docker-engine python-setuptools python-psutil qemu-user-static gpgv"
+# TODO: Check for docker package
+NEEDED_HOST_PACKAGES="mercurial python-setuptools python-psutil qemu-user-static gpgv"
 # python-argparse is integrated in libpython2.7-stdlib since raring
 # Check for precise in the HOST system (not valid DISTRO variable)
 if [[ $(lsb_release -sr | cut -c 1-5) == '12.04' ]]; then
