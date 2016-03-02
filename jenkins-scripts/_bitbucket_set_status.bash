5c3567f7817c5b1e5ca745ae5d9a5d3672a80f4f
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-03-02 20:19:57 +0100

    Implement the publish of post-build status in bitbucket

--- jenkins-scripts/_bitbucket_set_status.bash
+++ jenkins-scripts/_bitbucket_set_status.bash
@@ -1,47 +1,37 @@
 #!/bin/bash -xe
 
-echo '# BEGIN SECTION: sending bitbucket status: inprogress'
-export NEEDED_HOST_PACKAGES="python-yaml python-six python-uritemplate python-requests python-requests-oauthlib python-pip"
-export CONFIG_FILE_PATH="$WORKSPACE/config_pybitbucket.yml"
-export BITBUCKET_USER_PASS_FILE="/var/lib/jenkins/osrf_jenkins_bitbucket_user_pass"
-export REPO_SHORT_NAME=`echo ${SRC_REPO} | sed s:.*\.org/::`
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+STATUS=${1}
+
+# Source bitbucket configs
+. ${SCRIPT_DIR}/_bitbucket_configs.bash
+
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: $0 [inprogress|ok|failed]"
+    exit 1
+fi
+
+if [[ ! -f $BITBUCKET_BUILD_STATUS_FILE ]]; then
+    echo "BITBUCKET_BUILD_STATUS_FILE does not correspond to a file"
+    echo "content: ${BITBUCKET_BUILD_STATUS_FILE}"
+    exit 1
+fi
 
 if [[ ! -f ${BITBUCKET_USER_PASS_FILE} ]]; then
-  echo "Bitbucket user pass not found in file \${BITBUCKET_USER_PASS_FILE}"
+  echo "Bitbucket user pass not found in file: ${BITBUCKET_USER_PASS_FILE}"
   exit 1
 fi
 
-# Check if they are already installed in the host. 
-# dpkg-query will return an error in stderr if a package has never been in the
-# system. It will return a header composed by several lines started with |, +++
-# and 'Desired' the rest of lines is composed by: ^rc or ^un if the package is
-# not in the system. ^in if it is installed
-QUERY_RESULT=$(dpkg-query --list g++ 2>&1 | grep -v ^ii | grep -v '|' | grep -v '^\+++' | grep -v '^Desired')
-if [[ -z ${QUERY_RESULT} ]]; then
-  # Trick to not run apt-get update if there is no error in installation
-  sudo apt-get install -y ${NEEDED_HOST_PACKAGES} || { sudo apt-get update && sudo apt-get install -y ${NEEDED_HOST_PACKAGES}; }
-  # python-future package is available only from Ubuntu wily on
-  pip install future
-fi
-
-echo "Generating config file ..."
-cat > $CONFIG_FILE_PATH << DELIM_CONFIG
-bitbucket_origin:
-  repository_name: ${REPO_SHORT_NAME}
-  sha: ${MERCURIAL_REVISION_SHORT}
-jenkins_job:
-  name: ${JOB_NAME}
-  url: ${BUILD_URL}
-DELIM_CONFIG
-cat $CONFIG_FILE_PATH
-
-echo "Running the set_status_from_file in hidden mode"
+echo "BEGIN SECTION: build status in bitbucket: ${STATUS} (hidden)"
 set +x # keep password secret
-BITBUCKET_USER_PASS=`cat $BITBUCKET_USER_PASS_FILE`
+BITBUCKET_USER_PASS=$(cat ${BITBUCKET_USER_PASS_FILE})
 ${WORKSPACE}/scripts/jenkins-scripts/python-bitbucket/set_status_from_file.py \
     --user osrf_jenkins  \
     --pass ${BITBUCKET_USER_PASS} \
-    --status inprogress \
-    --load_from_file ${CONFIG_FILE_PATH} >& pybitbucket.log
+    --status ${STATUS} \
+    --load_from_file ${BITBUCKET_BUILD_STATUS_FILE} >& ${WORKSPACE}/pybitbucket.log
 set -x # back to debug
 echo '# END SECTION'
--- jenkins-scripts/dsl/_configs_/OSRFLinuxCompilationAny.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFLinuxCompilationAny.groovy
@@ -25,16 +25,73 @@ class OSRFLinuxCompilationAny
         shell("""\
         #!/bin/bash -xe
 
-        /bin/bash -xe ./scripts/jenkins-scripts/_bitbucket_set_status.bash
+        /bin/bash -xe ./scripts/jenkins-scripts/_bitbucket_create_build_status_file.bash
+        /bin/bash -xe ./scripts/jenkins-scripts/_bitbucket_set_status.bash inprogress
         """.stripIndent())
       }
 
       parameters
       {
-        stringParam('DEST_BRANCH','default',
+        stringParam('DEST_BRANCH','default2',
                     'Destination branch where the pull request will be merged.' +
                     'Mostly used to decide if calling to ABI checker')
       }
-    }
-  }
-}
+
+      /* To implement the set of build status this implementation uses
+         post-build scripts. With current versions it was broken the option of
+         including different Tasks or TaskProperties blocks in the Postbuildtask
+         plugin. In the PostScript plugin there is no option to code the
+         'Unstable' state. This approaches uses both in this way:
+           - Postbuildtask to implement all failures options
+             (configure block since DSL code available can not code
+              different LogProperties)
+           - PostBuildScript to implement the ok status
+      */
+      configure { project ->
+        project / publishers << 'hudson.plugins.postbuildtask.PostbuildTask' {
+          tasks
+          {
+            "hudson.plugins.postbuildtask.TaskProperties"
+            {
+              logTexts {
+                "hudson.plugins.postbuildtask.LogProperties" {
+                  logText('marked build as failure')
+                  operator('OR')
+                }
+                "hudson.plugins.postbuildtask.LogProperties" {
+                  logText('Build was aborted')
+                  operator('OR')
+                }
+                "hudson.plugins.postbuildtask.LogProperties" {
+                              logText('result to UNSTABLE')
+                  operator('OR')
+                }
+                "hudson.plugins.postbuildtask.LogProperties" {
+                  logText('result is FAILURE')
+                  operator('OR')
+                 }
+               }
+            EscalateStatus(false)
+            RunIfJobSuccessful(false)
+            script('/bin/bash -xe ./scripts/jenkins-scripts/_bitbucket_set_status.bash failure')
+            } // end of TaskProperties
+          } // end of tasks
+        } // end of project
+      } // end of configure
+
+      publishers {
+        postBuildScripts {
+          steps {
+            shell("""\
+            #!/bin/bash -xe
+
+            /bin/bash -xe ./scripts/jenkins-scripts/_bitbucket_set_status.bash ok
+            """.stripIndent())
+          }
+          onlyIfBuildSucceeds(true)
+        }
+      } // end of publishers
+
+    } // end of with
+  } // end of create method
+} // end of class
