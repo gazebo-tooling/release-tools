fb7629ee2c82fce7c725ee22c94846c1ebb96c8b
Steve Peters <scpeters@osrfoundation.org>
2016-03-03 13:28:29 -0800

    fix redirecting to output file

--- jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
@@ -122,10 +122,13 @@ echo contents of test_results
 ls test_results
 echo contents of test_results0
 ls test_results0
+mkdir test_results_tmp
 for i in \$(ls test_results); do
   echo looking for flaky tests in test_results0/\$i and test_results/\$i
   python ${SCRIPT_DIR}/lib/flaky_junit_merge.py test_results0/\$i test_results/\$i
-  python ${SCRIPT_DIR}/lib/flaky_junit_merge.py test_results0/\$i test_results/\$i > test_results0/\$i
+  python ${SCRIPT_DIR}/lib/flaky_junit_merge.py test_results0/\$i test_results/\$i > test_results_tmp/\$i
+  mv test_results_tmp/\$i test_results0
+  cat test_results0/\$i
 done
 mv test_results test_results1
 mv test_results0 test_results
