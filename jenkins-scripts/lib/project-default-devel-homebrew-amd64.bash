32542e667da2e5105e939d432deff58b5a9d106b
Steve Peters <scpeters@osrfoundation.org>
2016-03-03 17:56:57 -0800

    only rerun failed tests if something failed the first time

--- jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
@@ -115,23 +115,24 @@ export CMAKE_PREFIX_PATH=${RUN_DIR}
 
 # Need to clean up models before run tests (issue 27)
 rm -fr \$HOME/.gazebo/models test_results*
-make test ARGS="-VV" || true
-mv test_results test_results0
-make test ARGS="-VV --rerun-failed" || true
-echo contents of test_results
-ls test_results
-echo contents of test_results0
-ls test_results0
-mkdir test_results_tmp
-for i in \$(ls test_results); do
-  echo looking for flaky tests in test_results0/\$i and test_results/\$i
-  python ${WORKSPACE}/scripts/jenkins-scripts/lib/flaky_junit_merge.py test_results0/\$i test_results/\$i
-  python ${WORKSPACE}/scripts/jenkins-scripts/lib/flaky_junit_merge.py test_results0/\$i test_results/\$i > test_results_tmp/\$i
-  mv test_results_tmp/\$i test_results0
-  cat test_results0/\$i
-done
-mv test_results test_results1
-mv test_results0 test_results
+if ! make test ARGS="-VV"; then
+  mv test_results test_results0
+  make test ARGS="-VV --rerun-failed" || true
+  echo contents of test_results
+  ls test_results
+  echo contents of test_results0
+  ls test_results0
+  mkdir test_results_tmp
+  for i in \$(ls test_results); do
+    echo looking for flaky tests in test_results0/\$i and test_results/\$i
+    python ${WORKSPACE}/scripts/jenkins-scripts/lib/flaky_junit_merge.py \
+      test_results0/\$i test_results/\$i \
+      > test_results_tmp/\$i
+    mv test_results_tmp/\$i test_results0
+  done
+  mv test_results test_results1
+  mv test_results0 test_results
+fi
 DELIM
 
 chmod +x test_run.sh
