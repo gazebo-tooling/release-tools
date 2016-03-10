fc13f74c6b00fbb4a78eb4ba31e3edeec5f6264e
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-03-10 02:17:15 +0100

    Implement the retry to get X variable

--- jenkins-scripts/lib/check_graphic_card.bash
+++ jenkins-scripts/lib/check_graphic_card.bash
@@ -1,6 +1,22 @@
 GRAPHIC_CARD_FOUND=false
 GRAPHIC_CARD_PKG=""
 
+export_display_variable()
+{
+    # Hack to found the current display (if available) two steps:
+    # Check for /tmp/.X11-unix/ socket and check if the process is running
+    for i in `ls /tmp/.X11-unix/ | sed -e 's@^X@:@'`
+    do
+      # grep can fail so let's disable the fail or error during its call
+      set +e
+      ps aux | grep bin/X.*$i | grep -v grep
+      set -e
+      if [ $? -eq 0 ] ; then
+	export DISPLAY=$i
+      fi
+    done
+}
+
 if [ -z ${GPU_SUPPORT_NEEDED} ]; then
     GPU_SUPPORT_NEEDED=false
 fi
@@ -9,18 +25,8 @@ if ! ${GPU_SUPPORT_NEEDED}; then
     return
 fi
 
-# Hack to found the current display (if available) two steps:
-# Check for /tmp/.X11-unix/ socket and check if the process is running
-for i in `ls /tmp/.X11-unix/ | sed -e 's@^X@:@'`
-do
-  # grep can fail so let's disable the fail or error during its call
-  set +e
-  ps aux | grep bin/X.*$i | grep -v grep
-  set -e
-  if [ $? -eq 0 ] ; then
-    export DISPLAY=$i
-  fi
-done
+# First try to get the display variable
+export_display_variable
 
 # Check for Nvidia stuff
 if [ -n "$(lspci -v | grep nvidia | head -n 2 | grep "Kernel driver in use: nvidia")" ]; then
@@ -63,6 +69,12 @@ if $GPU_SUPPORT_NEEDED; then
       echo "GPU support needed by the script but DISPLAY var is empty"
       # Try to restart lightdm. It should stop the script in the case of failure
       sudo service lightdm restart
+      # Second try to get display variable
+      export_display_variable
+      if [[ ${DISPLAY} == "" ]]; then
+	  echo "Imposible to get DISPLAY variable. Check your system"
+	  exit 1
+      fi
     fi
     
     # Check if the GPU support was found when not 
