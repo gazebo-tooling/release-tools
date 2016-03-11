386a0bb2257dd9c14fb60f132b66e6059155a28f
Steven Peters <scpeters@osrfoundation.org>
2016-03-11 22:41:04 +0000

    remove extra `

--- jenkins-scripts/lib/check_graphic_card.bash
+++ jenkins-scripts/lib/check_graphic_card.bash
@@ -5,7 +5,7 @@ export_display_variable()
 {
     # Hack to found the current display (if available) two steps:
     # Check for /tmp/.X11-unix/ socket and check if the process is running
-    for i in $(ls /tmp/.X11-unix/ | sed -e 's@^X@:@')`
+    for i in $(ls /tmp/.X11-unix/ | sed -e 's@^X@:@')
     do
       # grep can fail so let's disable the fail or error during its call
       set +e
