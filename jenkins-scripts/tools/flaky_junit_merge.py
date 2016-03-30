8bb19006480caae7c71d9f5ac57667de7da123e9
Steve Peters <scpeters@osrfoundation.org>
2016-03-30 15:11:47 -0700

    flaky_junit_merge: handle pairs of files with testsuite as root element, include examples

--- jenkins-scripts/tools/flaky_junit_merge.py
+++ jenkins-scripts/tools/flaky_junit_merge.py
@@ -39,21 +39,39 @@ f = open(sys.argv[2], 'r')
 xml2 = etree.fromstring(f.read())
 f.close()
 
-if xml1.tag != 'testsuites':
-    print('root tag should be testsuites', file=sys.stderr)
-    exit
+if xml1.tag == 'testsuites':
+    testsuites = xml1.getchildren()
+elif xml1.tag == 'testsuite':
+    testsuites = [xml1]
+else:
+    print("root tag of %s should be testsuite or testsuites" % (sys.argv[1]),
+          file=sys.stderr)
+    exit()
+
+if xml2.tag == 'testsuites':
+    root2 = xml2
+elif xml2.tag == 'testsuite':
+    root2 = etree.Element('testsuites')
+    root2.append(xml2)
+else:
+    print("root tag of %s should be testsuite or testsuites" % (sys.argv[2]),
+          file=sys.stderr)
+    exit()
 
 # iterate over <testsuite> tags in xml1
-for ts in xml1.getchildren():
+for ts in testsuites:
     if ts.tag != 'testsuite':
-        print('child tags should be testsuite', file=sys.stderr)
+        print('expected testsuite tag', file=sys.stderr)
         continue
-    # find <testsuite> tag with matching name attribute in xml2
-    ts2 = xml2.findall(".//testsuite[@name='%s']" % (ts.attrib['name']))[0]
+    # find <testsuite> tag with matching name attribute in root2
+    ts2 = root2.findall(".//testsuite[@name='%s']" % (ts.attrib['name']))[0]
     # iterate over <testcase> tags in <testsuite> from xml1
     for tc in ts.getchildren():
-        # find matching <testcase> tag from xml2
-        tc2 = xml2.findall(".//testsuite[@name='%s']/testcase[@name='%s']" % (ts.attrib['name'], tc.attrib['name']))[0]
+        if tc.tag != 'testcase':
+            print('expected testcase tag', file=sys.stderr)
+            continue
+        # find matching <testcase> tag from root2
+        tc2 = root2.findall(".//testsuite[@name='%s']/testcase[@name='%s']" % (ts.attrib['name'], tc.attrib['name']))[0]
         failures1 = countFailures(tc)
         failures2 = countFailures(tc2)
         if failures1 > 0 and failures2 == 0:
