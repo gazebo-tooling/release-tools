66f73b79fb4062cc22b1cd93b7f6607dbf38b5da
Steve Peters <scpeters@osrfoundation.org>
2016-03-30 23:29:02 -0700

    re-enable RERUN_FAILED_TESTS for ign-math

--- jenkins-scripts/ign_math-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/ign_math-default-devel-homebrew-amd64.bash
@@ -4,4 +4,6 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
+RERUN_FAILED_TESTS=1
+
 . ${SCRIPT_DIR}/lib/project-default-devel-homebrew-amd64.bash ignition-math
