b9183a9f05f9317bcabcbac977284b6899ef9921
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-04-01 22:19:30 +0200

    Revert status and avoid builds to fail

--- jenkins-scripts/_bitbucket_configs.bash
+++ jenkins-scripts/_bitbucket_configs.bash
@@ -1,7 +1,4 @@
 # Variables exported by the system
-# BUILD_STATUS_FILE need to be defined here if scripts are used from our current scripts and not as a job
-if [[ -z ${BITBUCKET_BUILD_STATUS_FILE} ]]; then
-  export BITBUCKET_BUILD_STATUS_FILE="$WORKSPACE/config_pybitbucket.yml"
-fi
+export BITBUCKET_BUILD_STATUS_FILE="$WORKSPACE/config_pybitbucket.yml"
 export BITBUCKET_USER_PASS_FILE="/var/lib/jenkins/osrf_jenkins_bitbucket_user_pass"
 export BITBUCKET_LOG_FILE="${WORKSPACE}/pybitbucket.log"
