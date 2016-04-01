bdbad4a84b2e847e09e912ebb76bbe62fbb1435a
Steve Peters <scpeters@osrfoundation.org>
2016-04-01 14:09:19 -0700

    Adjust flaky_junit_merge.py to handle when testsuite is root element, with example files

--- jenkins-scripts/tools/flaky_junit_merge.py
+++ jenkins-scripts/tools/flaky_junit_merge.py
@@ -41,21 +41,44 @@ f = open(fileName2, 'r')
 xml2 = etree.fromstring(f.read())
 f.close()
 
-if xml1.tag != 'testsuites':
-    print('root tag should be testsuites', file=sys.stderr)
-    exit
+# we want to iterate over the testsuite elements
+if xml1.tag == 'testsuites':
+    testsuiteArray = xml1.getchildren()
+elif xml1.tag == 'testsuite':
+    # just add the whole doc to a list
+    testsuiteArray = [xml1]
+else:
+    print("root tag of %s should be testsuite or testsuites" % (fileName1),
+          file=sys.stderr)
+    exit()
+
+# we search for testsuite elements in the second file with XPath,
+# which means testsuite can't be the root element
+# so create root2 with root tag testsuites to contain the testsuite if necessary
+if xml2.tag == 'testsuites':
+    root2 = xml2
+elif xml2.tag == 'testsuite':
+    root2 = etree.Element('testsuites')
+    root2.append(xml2)
+else:
+    print("root tag of %s should be testsuite or testsuites" % (fileName2),
+          file=sys.stderr)
+    exit()
 
 # iterate over <testsuite> tags in xml1
-for ts in xml1.getchildren():
+for ts in testsuiteArray:
     if ts.tag != 'testsuite':
-        print('child tags should be testsuite', file=sys.stderr)
+        print('expected testsuite tag', file=sys.stderr)
         continue
-    # find <testsuite> tag with matching name attribute in xml2
-    ts2 = xml2.findall(".//testsuite[@name='%s']" % (ts.attrib['name']))[0]
+    # find <testsuite> tag with matching name attribute in root2
+    ts2 = root2.findall(".//testsuite[@name='%s']" % (ts.attrib['name']))[0]
     # iterate over <testcase> tags in <testsuite> from xml1
     for tc in ts.getchildren():
-        # find matching <testcase> tag from xml2
-        tc2 = xml2.findall(".//testsuite[@name='%s']/testcase[@name='%s']" % (ts.attrib['name'], tc.attrib['name']))[0]
+        if tc.tag != 'testcase':
+            print('expected testcase tag', file=sys.stderr)
+            continue
+        # find matching <testcase> tag from root2
+        tc2 = root2.findall(".//testsuite[@name='%s']/testcase[@name='%s']" % (ts.attrib['name'], tc.attrib['name']))[0]
         failures1 = countFailures(tc)
         failures2 = countFailures(tc2)
         if failures1 > 0 and failures2 == 0:
