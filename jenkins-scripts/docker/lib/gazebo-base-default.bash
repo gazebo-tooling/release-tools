7ec37404e3712fc2f9d1727918a1e8f6bd647af5
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-04-04 01:11:31 +0200

    Better message

--- jenkins-scripts/docker/lib/gazebo-base-default.bash
+++ jenkins-scripts/docker/lib/gazebo-base-default.bash
@@ -66,6 +66,7 @@ for dep_uppercase in $GAZEBO_OSRF_DEPS; do
       [[ -z ${dep_branch} ]] && dep_branch='default'
 cat >> build.sh << DELIM_BUILD_DEPS  
     echo "#BEGIN SECTION: building dependency: ${dep} (${dep_branch})"
+    echo '# END SECTION'
     rm -fr $WORKSPACE/$dep
 
     if [[ ${dep/ign} == ${dep} ]]; then
