b96ad3dad79629830bfb223a25010cf26480d0f5
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-04-04 13:16:55 +0200

    Use directly the bitness, coded in the script name

--- jenkins-scripts/ign_msgs-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_msgs-default-devel-windows-amd64.bat
@@ -6,6 +6,6 @@ call "%SCRIPT_DIR%/lib/windows_configuration.bat"
 set VCS_DIRECTORY=ign-msgs
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
-set DEPENDENCY_PKG=protobuf-2.6.0-win%BITNESS%-vc12.zip
+set DEPENDENCY_PKG=protobuf-2.6.0-win64-vc12.zip
 
 call "%SCRIPT_DIR%/lib/project-default-devel-windows.bat"
