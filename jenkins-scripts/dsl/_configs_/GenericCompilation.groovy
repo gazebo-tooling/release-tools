dbecc6d61bd680c14096d63049c039e57179db9d
Steven Peters <scpeters@osrfoundation.org>
2016-04-05 21:35:45 +0000

    fix whitespace in GenericCompilation.groovy

--- jenkins-scripts/dsl/_configs_/GenericCompilation.groovy
+++ jenkins-scripts/dsl/_configs_/GenericCompilation.groovy
@@ -50,9 +50,9 @@ class GenericCompilation
                 project / publishers << 'hudson.tasks.junit.JUnitResultArchiver' {
                      testResults('build/test_results/*.xml')
                      keepLongStdio false
-                   testDataPublishers {
-                     publishFlakyTestsReport()
-                   }
+                     testDataPublishers {
+                       publishFlakyTestsReport()
+                     }
                 }
             }
           } // end of publishers
