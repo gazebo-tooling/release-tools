18fa1a491b91d02683068c4bbf4e4e569861d6a7
Steve Peters <scpeters@osrfoundation.org>
2016-04-06 10:29:34 -0700

    fix whitespace in regular expressions

--- jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
@@ -128,8 +128,8 @@ if ! make test ARGS="-VV" && [[ "${RERUN_FAILED_TESTS}" -gt 0 ]]; then
   echo Failed tests:
   ctest -N --rerun-failed
   FAILED_TESTS=$(ctest -N --rerun-failed \
-    | grep 'Test #[0-9][0-9]*:' \
-    | sed -e 's@^ *Test #[0-9]*: *@@' \
+    | grep 'Test  *#[0-9][0-9]*:' \
+    | sed -e 's@^ *Test  *#[0-9]*: *@@' \
   )
   for i in ${FAILED_TESTS}; do
     make test ARGS="-VV -R ${i}" || true
