d9259bfd5516945a90c6fac76ac2f4e373653e0c
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-04-06 22:23:18 +0200

    Different fixes based on feedback in the PR

--- jenkins-scripts/lib/homebrew_bottle_creation.bash
+++ jenkins-scripts/lib/homebrew_bottle_creation.bash
@@ -8,8 +8,6 @@ export PATH="/usr/local/bin:$PATH"
 
 PKG_DIR=${WORKSPACE}/pkgs
 
-find . -name '*.bottle.*' | wc -l
-
 echo '# BEGIN SECTION: check variables'
 if [ -z "${PULL_REQUEST_URL}" ]; then
     echo PULL_REQUEST_URL not specified
@@ -23,19 +21,13 @@ rm -fr ${PKG_DIR} && mkdir -p ${PKG_DIR}
 echo '# END SECTION'
 
 echo '# BEGIN SECTION: run test-bot'
-# return always true since audit fails to run gzserver
-# can not find a way of disabling it
-( brew test-bot             \
-    --tap=osrf/simulation \
-    --bottle              \
-    --ci-pr               \
-    --verbose ${PULL_REQUEST_URL} ) || true
+brew test-bot --tap=osrf/simulation --ci-pr ${PULL_REQUEST_URL}
 echo '# END SECTION'
 
 echo '# BEGIN SECTION: export bottle'
 if [[ $(find . -name '*.bottle.*' | wc -l | sed 's/^ *//') != 2 ]]; then
- echo "Can not find the two bottle files"
- exit -1
+  echo "Can not find the two bottle files"
+  exit -1
 fi
 
 mv *.bottle.tar.gz ${PKG_DIR}
