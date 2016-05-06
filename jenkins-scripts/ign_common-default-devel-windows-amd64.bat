e485f73872daca198a2b2ced90d64047dfc4f691
Nate Koenig <natekoenig@gmail.com>
2016-05-06 07:40:25 -0700

    Compile ign-math from default

--- jenkins-scripts/ign_common-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_common-default-devel-windows-amd64.bat
@@ -5,6 +5,6 @@ call "%SCRIPT_DIR%/lib/windows_configuration.bat"
 set VCS_DIRECTORY=ign-common
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
-set DEPENDENCY_PKG=FreeImage-vc12-x64-release-debug.zip,ign-math2.zip
+set DEPENDENCY_PKG=FreeImage-vc12-x64-release-debug.zip
 
 call "%SCRIPT_DIR%/lib/ign_common-base-windows.bat"
