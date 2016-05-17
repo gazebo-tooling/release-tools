607b6eb0bcd0ebba34752fc5e5c1ff16d958d1c2
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-05-17 17:25:29 +0200

    One more maxdepth

--- jenkins-scripts/docker/lib/docker_run.bash
+++ jenkins-scripts/docker/lib/docker_run.bash
@@ -32,7 +32,7 @@ sudo docker stop ${CID} || true
 sudo docker rm ${CID} || true
 
 # Export results out of build directory, to WORKSPACE
-for d in $(find ${WORKSPACE}/build -name '*_results' -type d); do
+for d in $(find ${WORKSPACE}/build -maxdepth 1 -name '*_results' -type d); do
     sudo mv ${d} ${WORKSPACE}/
     sudo chown -R jenkins ${WORKSPACE}/*_results
 done
