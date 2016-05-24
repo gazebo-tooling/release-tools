a8a60e17cdba1cc51564fcb8dd33a525b7b5fecf
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-05-24 21:21:44 +0200

    Fix problem with quoting

--- jenkins-scripts/dsl/_configs_/OSRFCIWorkFlow.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFCIWorkFlow.groovy
@@ -39,8 +39,8 @@ class OSRFCIWorkFlow
     * args input map contains:
     *    String build_status,
     *    String build_desc = "",
-    *    String build_name = 'ENV.JOB_NAME',
-    *    String build_url  = 'ENV.BUILD_URL'
+    *    String build_name = 'env.JOB_NAME',
+    *    String build_url  = 'env.BUILD_URL'
     */
    static String script_code_set_code(Map args)
    {
@@ -49,10 +49,10 @@ class OSRFCIWorkFlow
          args.build_desc = ''
 
       if (! args.containsKey('build_name'))
-         args.build_name = 'ENV.JOB_NAME'
+         args.build_name = 'env.JOB_NAME'
 
       if (! args.containsKey('build_url'))
-         args.build_url = 'ENV.BUILD_URL'
+         args.build_url = 'env.BUILD_URL'
 
      return """\
 
@@ -65,10 +65,10 @@ class OSRFCIWorkFlow
                     [[\$class: 'StringParameterValue', name: 'RTOOLS_BRANCH',          value: "\$RTOOLS_BRANCH"],
                      [\$class: 'StringParameterValue', name: 'JENKINS_BUILD_REPO',     value: "\$SRC_REPO"],
                      [\$class: 'StringParameterValue', name: 'JENKINS_BUILD_HG_HASH',  value: env.MERCURIAL_REVISION_SHORT],
-                     [\$class: 'StringParameterValue', name: 'JENKINS_BUILD_JOB_NAME', value: "${args.build_name}"],
-                     [\$class: 'StringParameterValue', name: 'JENKINS_BUILD_URL',      value: "${args.build_url}"],
-                     [\$class: 'StringParameterValue', name: 'JENKINS_BUILD_DESC',     value: "${args.build_desc}"],
-                     [\$class: 'StringParameterValue', name: 'BITBUCKET_STATUS',       value: "${args.build_status}"]]
+                     [\$class: 'StringParameterValue', name: 'JENKINS_BUILD_JOB_NAME', value: ${args.build_name}],
+                     [\$class: 'StringParameterValue', name: 'JENKINS_BUILD_URL',      value: ${args.build_url}],
+                     [\$class: 'StringParameterValue', name: 'JENKINS_BUILD_DESC',     value: ${args.build_desc}],
+                     [\$class: 'StringParameterValue', name: 'BITBUCKET_STATUS',       value: ${args.build_status}]]
          }
      """
    }
