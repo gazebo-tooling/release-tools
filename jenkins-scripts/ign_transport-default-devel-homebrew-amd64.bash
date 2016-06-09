ea5a021b4a61761100039b4bd5d1d36e843a1b60
Steve Peters <scpeters@osrfoundation.org>
2016-06-09 13:57:22 -0700

    Support ignition-transportN in homebrew

--- jenkins-scripts/ign_transport-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/ign_transport-default-devel-homebrew-amd64.bash
@@ -4,7 +4,19 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-IS_A_HEAD_FORMULA=true
+# Identify IGNITION_TRANSPORT_MAJOR_VERSION to help with dependency resolution
+IGNITION_TRANSPORT_MAJOR_VERSION=`\
+  grep 'set.*IGNITION_TRANSPORT_MAJOR_VERSION ' ${WORKSPACE}/gazebo/CMakeLists.txt | \
+  tr -d 'a-zA-Z _()'`
+# Drop version number if it is 1 (ignition-transport 1.X is in ignition-transport.rb)
+if [ $IGNITION_TRANSPORT_MAJOR_VERSION -eq 1 ]; then
+  IGNITION_TRANSPORT_MAJOR_VERSION=""
+else
+  rm -rf ${WORKSPACE}/ign-transport${IGNITION_TRANSPORT_MAJOR_VERSION}
+  cp -R ${WORKSPACE}/ign-transport \
+        ${WORKSPACE}/ign-transport${IGNITION_TRANSPORT_MAJOR_VERSION}
+fi
 
-. ${SCRIPT_DIR}/lib/project-default-devel-homebrew-amd64.bash ignition-transport
+. ${SCRIPT_DIR}/lib/project-default-devel-homebrew-amd64.bash \
+  ignition-transport${IGNITION_TRANSPORT_MAJOR_VERSION}
 
