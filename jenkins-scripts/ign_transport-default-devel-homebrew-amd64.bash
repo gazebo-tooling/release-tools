9fd6b984bf6c1e9beee189e465774387c4debecc
Steve Peters <scpeters@osrfoundation.org>
2016-06-09 14:02:14 -0700

    fix name of MAJOR_VERSION variable

--- jenkins-scripts/ign_transport-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/ign_transport-default-devel-homebrew-amd64.bash
@@ -4,19 +4,19 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-# Identify IGNITION_TRANSPORT_MAJOR_VERSION to help with dependency resolution
-IGNITION_TRANSPORT_MAJOR_VERSION=`\
-  grep 'set.*IGNITION_TRANSPORT_MAJOR_VERSION ' ${WORKSPACE}/ign-transport/CMakeLists.txt | \
+# Identify PROJECT_MAJOR_VERSION to help with dependency resolution
+PROJECT_MAJOR_VERSION=`\
+  grep 'set.*PROJECT_MAJOR_VERSION ' ${WORKSPACE}/ign-transport/CMakeLists.txt | \
   tr -d 'a-zA-Z _()'`
 # Drop version number if it is 1 (ignition-transport 1.X is in ignition-transport.rb)
-if [ $IGNITION_TRANSPORT_MAJOR_VERSION -eq 1 ]; then
-  IGNITION_TRANSPORT_MAJOR_VERSION=""
+if [ $PROJECT_MAJOR_VERSION -eq 1 ]; then
+  PROJECT_MAJOR_VERSION=""
 else
-  rm -rf ${WORKSPACE}/ign-transport${IGNITION_TRANSPORT_MAJOR_VERSION}
+  rm -rf ${WORKSPACE}/ign-transport${PROJECT_MAJOR_VERSION}
   cp -R ${WORKSPACE}/ign-transport \
-        ${WORKSPACE}/ign-transport${IGNITION_TRANSPORT_MAJOR_VERSION}
+        ${WORKSPACE}/ign-transport${PROJECT_MAJOR_VERSION}
 fi
 
 . ${SCRIPT_DIR}/lib/project-default-devel-homebrew-amd64.bash \
-  ignition-transport${IGNITION_TRANSPORT_MAJOR_VERSION}
+  ignition-transport${PROJECT_MAJOR_VERSION}
 
