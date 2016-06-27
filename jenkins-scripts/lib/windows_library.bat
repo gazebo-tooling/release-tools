e88a7e534be52113feae80c0ba0a3d4c83596113
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-06-27 20:04:23 +0200

    Fix 7za unzip command

--- jenkins-scripts/lib/windows_library.bat
+++ jenkins-scripts/lib/windows_library.bat
@@ -113,6 +113,8 @@ goto :EOF
 :unzip_7za - Unzip using 7za
 ::
 :: arg1 - File to unzip
+echo Uncompressing %~1 
+IF NOT exist %~1 ( echo "Zip file does not exists: %~1" && goto :error )
 7za.exe x %~1 || goto :error
 goto :EOF
 
