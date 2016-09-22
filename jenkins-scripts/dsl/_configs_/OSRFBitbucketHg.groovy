53dfdb1f5cc168324b53107eff9bef5fdc825615
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-09-23 00:42:02 +0200

    Fix configure block to correctly set browser

--- jenkins-scripts/dsl/_configs_/OSRFBitbucketHg.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFBitbucketHg.groovy
@@ -4,8 +4,8 @@ import javaposse.jobdsl.dsl.Job
 
 class OSRFBitbucketHg
 {
-  static void create(Job job, String repo, 
-                              String rev    = 'default', 
+  static void create(Job job, String repo,
+                              String rev    = 'default',
                               String subdir = 'NOT-DEFINED-USE-DEFAULT')
   {
     String software_name = repo.tokenize('/').last()
@@ -23,8 +23,8 @@ class OSRFBitbucketHg
         hg(repo) {
           branch(rev)
           subdirectory(subdir)
-          configure { project ->
-           project / "browser" / "bitBucket" / "url" << repo 
+        configure { project ->
+           project / browser(class: 'hudson.plugins.mercurial.browser.BitBucket') / "url" << repo
           }
         }
       }
