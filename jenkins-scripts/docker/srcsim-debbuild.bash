ac3bd44b0d3bfc50a1ddbc76d961e38a0d9d8dc6
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-10-24 12:53:23 +0200

    Fix indentation

--- jenkins-scripts/docker/srcsim-debbuild.bash
+++ jenkins-scripts/docker/srcsim-debbuild.bash
@@ -8,10 +8,8 @@ export RELEASE_REPO_DIRECTORY=${DISTRO}
 export ENABLE_ROS=true
 
 # Use src temporary repo
-export DOCKER_POSTINSTALL_HOOK="""
-# import the SRC repo
-echo \"deb http://52.53.157.231/src ${DISTRO} main\" > \\
-                                           /etc/apt/sources.list.d/src.list && \\
+export DOCKER_POSTINSTALL_HOOK="""\
+echo \"deb http://52.53.157.231/src ${DISTRO} main\" > /etc/apt/sources.list.d/src.list && \\
 wget -qO - http://52.53.157.231/src/src.key | sudo apt-key add - && \\
 apt-get update
 """
