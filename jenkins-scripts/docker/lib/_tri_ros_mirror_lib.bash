177c0dc85f3c498e095893dd73f8dd48c98c6a5c
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-11-22 23:28:08 +0100

    Import ROS keys if needed

--- jenkins-scripts/docker/lib/_tri_ros_mirror_lib.bash
+++ jenkins-scripts/docker/lib/_tri_ros_mirror_lib.bash
@@ -10,6 +10,13 @@ APLTY_CMD="sudo aptly -config=/etc/aptly.conf"
 APLTY_MIRROR_NAME="ros_stable"
 APLTY_PUBLISH_REPO_PREFIX="ros-stable-trusty-pkgs"
 
+# Be sure that we have ROS keys
+GPG_CMD="sudo gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net" 
+
+if [[ -z $(${GPG_CMD} --list-keys | grep "B01FA116") ]]; then
+  ${GPG_CMD} --recv-keys "5523BAEEB01FA116"
+fi
+
 generate_snapshot_info()
 {
   local snapshots_name=${1}
