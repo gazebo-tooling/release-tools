9557b4f10aeb5b445394afbd0674cdca8df7944f
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-11-22 21:44:06 +0100

    Need to scape vars

--- jenkins-scripts/docker/tri_ros_mirror_desktop_full_install.bash
+++ jenkins-scripts/docker/tri_ros_mirror_desktop_full_install.bash
@@ -16,8 +16,8 @@ INSTALL_JOB_PREINSTALL_HOOK="""
 # import the TRI ROS mirror
 
 export IP='54.183.148.69:8080'
-wget http://${IP}/repo.key -O - | apt-key add -
-echo \"deb http://${IP}/${APLTY_PUBLISH_REPO_PREFIX} ${DISTRO} main\" >\\
+wget http://\${IP}/repo.key -O - | apt-key add -
+echo \"deb http://\${IP}/${APLTY_PUBLISH_REPO_PREFIX} ${DISTRO} main\" >\\
                           /etc/apt/sources.list.d/tri_ros_mirror.list
 sudo apt-get update
 """
