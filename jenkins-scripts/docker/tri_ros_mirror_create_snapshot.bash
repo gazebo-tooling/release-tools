148a8dd81c0c64a0113d07b15be6167b70c7c960
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-11-23 22:32:07 +0100

    Replace mkdir command

--- jenkins-scripts/docker/tri_ros_mirror_create_snapshot.bash
+++ jenkins-scripts/docker/tri_ros_mirror_create_snapshot.bash
@@ -9,8 +9,6 @@ export USE_APTLY_MANAGEMENT=true
 # TRI ROS Mirror common library
 . ${SCRIPT_DIR}/lib/_tri_ros_mirror_lib.bash
 
-mkdir -p ${WORKSPACE}/info
-
 # Update the current aptly mirror
 ${APLTY_CMD} mirror update ${APLTY_MIRROR_NAME} 
 
@@ -23,5 +21,6 @@ if [[ -n ${SNAPSHOT_TAG} ]]; then
 fi
 ${APLTY_CMD} snapshot create ${snapshot_name} from mirror ${APLTY_MIRROR_NAME}
 
+mkdir -p ${WORKSPACE}/info
 echo ${snapshot_name} > $WORKSPACE/info/new_snapshot_name
 generate_snapshot_info ${snapshot_name}
