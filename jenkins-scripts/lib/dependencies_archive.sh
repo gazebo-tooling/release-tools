bd55d20c4e15e2d0abe0d6e334627cd60c5bac88
nkoenig <nkoenig@osrfoundation.org>
2016-12-19 12:33:49 -0800

    Install ignition-transport3

--- jenkins-scripts/lib/dependencies_archive.sh
+++ jenkins-scripts/lib/dependencies_archive.sh
@@ -183,6 +183,7 @@ fi
 if [[ ${GAZEBO_MAJOR_VERSION} -ge 8 ]]; then
     GAZEBO_BASE_DEPENDENCIES_NO_SDFORMAT="${GAZEBO_BASE_DEPENDENCIES_NO_SDFORMAT} \\
                                          libignition-transport2-dev \\
+                                         libignition-transport3-dev \\
                                          libignition-msgs-dev"
 fi
 
