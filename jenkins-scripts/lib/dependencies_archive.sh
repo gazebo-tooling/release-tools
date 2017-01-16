a26b16f91e0c77a78e7a55b1e2567d535727d320
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-01-16 18:19:31 +0100

    Use ign-math3 for ign-common and ign-msgs

--- jenkins-scripts/lib/dependencies_archive.sh
+++ jenkins-scripts/lib/dependencies_archive.sh
@@ -425,7 +425,7 @@ IGN_COMMON_DEPENDENCIES="pkg-config            \\
                          python                \\
                          ruby-ronn             \\
                          uuid-dev              \\
-                         libignition-math2-dev \\
+                         libignition-math3-dev \\
                          libfreeimage-dev      \\
                          libgts-dev            \\
                          libavformat-dev       \\
