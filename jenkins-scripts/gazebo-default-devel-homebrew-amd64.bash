ff0d6e4fc64096c3d862f2fb471695174caa1b91
Steve Peters <scpeters@osrfoundation.org>
2017-02-03 16:19:29 -0800

    use the -j compiler flag for the gazebo make tests command on homebrew

--- jenkins-scripts/gazebo-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/gazebo-default-devel-homebrew-amd64.bash
@@ -25,7 +25,7 @@ if [[ $GAZEBO_MAJOR_VERSION -ge 8 ]]; then
   export CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}:/usr/local/opt/qt5
 PRE_TESTS_EXECUTION_HOOK="""
 echo '#BEGIN SECTION: compile the tests suite'
-make tests \${MAKE_VERBOSE_STR}
+make tests -j\${MAKE_JOBS} \${MAKE_VERBOSE_STR}
 echo '# END SECTION'
 """
 fi
