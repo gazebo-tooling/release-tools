eb935142e997a6b16e8c129ecbfc0075bf2df803
Shane Loretz <sloretz@osrfoundation.org>
2017-02-07 08:42:52 -0800

    Revert change that breaks builds on some debian systems due to apt-get bug

--- jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
+++ jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
@@ -202,17 +202,17 @@ cat >> Dockerfile << DELIM_DOCKER3
 # The rm after the fail of apt-get update is a workaround to deal with the error:
 # Could not open file *_Packages.diff_Index - open (2: No such file or directory)
 RUN echo "${MONTH_YEAR_STR}" \
- && apt-get update \
+ && apt-get update || rm -rf /var/lib/apt/lists/* && apt-get update \
  && apt-get install -y ${PACKAGES_CACHE_AND_CHECK_UPDATES} \
  && apt-get clean \
- && ( rm -rf /var/lib/apt/lists/*  || true )
+ && rm -rf /var/lib/apt/lists/*
 # This is killing the cache so we get the most recent packages if there
 # was any update
 RUN echo "Invalidating cache $(( ( RANDOM % 100000 )  + 1 ))" \
- && apt-get update \
+ && apt-get update || rm -rf /var/lib/apt/lists/* && apt-get update \
  && apt-get install -y ${PACKAGES_CACHE_AND_CHECK_UPDATES} \
  && apt-get clean \
- && ( rm -rf /var/lib/apt/lists/*  || true )
+ && rm -rf /var/lib/apt/lists/*
 # Map the workspace into the container
 RUN mkdir -p ${WORKSPACE}
 DELIM_DOCKER3
