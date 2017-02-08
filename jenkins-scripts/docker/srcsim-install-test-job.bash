8db771eca7e82c2d20e0f69cf95eec48ad2eb27f
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-02-08 23:26:41 +0100

    Fix duplicate var name

--- jenkins-scripts/docker/srcsim-install-test-job.bash
+++ jenkins-scripts/docker/srcsim-install-test-job.bash
@@ -36,6 +36,6 @@ fi
 echo '# END SECTION'
 """
 # Need bc to proper testing and parsing the time
-export DEPENDENCY_PKGS DEPENDENCY_PKGS="wget bc"
+export DEPENDENCY_PKGS="wget bc"
 
 . ${SCRIPT_DIR}/lib/generic-install-base.bash
