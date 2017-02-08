5be8cd3b212dd3df9bfea4cee52792ed659bb25e
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-02-08 22:56:51 +0100

    Missing fi

--- jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
+++ jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
@@ -165,6 +165,7 @@ RUN echo "deb http://packages.ros.org/ros/ubuntu ${DISTRO} main" > \\
                                                 /etc/apt/sources.list.d/ros.list
 RUN apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 421C365BD9FF1F717815A3895523BAEEB01FA116
 DELIM_ROS_REPO
+fi
 
 if [ `expr length "${DOCKER_PREINSTALL_HOOK}"` -gt 1 ]; then
 cat >> Dockerfile << DELIM_WORKAROUND_PRE_HOOK
