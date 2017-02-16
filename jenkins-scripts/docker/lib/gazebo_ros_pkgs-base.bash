efdfd17b7a19091b8bd67c118d9482d83caa037a
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-02-16 17:25:44 +0100

    Need to place CATKIN_EXTRA_ARGS before ros build generation

--- jenkins-scripts/docker/lib/gazebo_ros_pkgs-base.bash
+++ jenkins-scripts/docker/lib/gazebo_ros_pkgs-base.bash
@@ -11,12 +11,12 @@ DOCKER_JOB_NAME="gazebo_ros_pkgs_ci"
 . ${SCRIPT_DIR}/lib/boilerplate_prepare.sh
 
 # Generate the first part of the build.sh file for ROS
+CATKIN_EXTRA_ARGS="--cmake-args -DENABLE_DISPLAY_TESTS:BOOL=ON"
 . ${SCRIPT_DIR}/lib/_ros_setup_buildsh.bash "gazebo_ros_pkgs"
 
 # don't have rosdep at this point and want gazebo to be cached by docker
 DEPENDENCY_PKGS="${ROS_GAZEBO_PKGS_DEPENDENCIES} ${_GZ_ROS_PACKAGES}"
 USE_ROS_REPO=true
-export CATKIN_EXTRA_ARGS="--cmake-args -DENABLE_DISPLAY_TESTS:BOOL=ON"
 
 . ${SCRIPT_DIR}/lib/docker_generate_dockerfile.bash
 . ${SCRIPT_DIR}/lib/docker_run.bash
