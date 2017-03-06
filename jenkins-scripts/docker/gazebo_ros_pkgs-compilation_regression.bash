2c8e7375b3b10f4c35b267a99f1e015c85fe4a08
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-03-06 13:36:23 +0100

    Checkout proper branch

--- jenkins-scripts/docker/gazebo_ros_pkgs-compilation_regression.bash
+++ jenkins-scripts/docker/gazebo_ros_pkgs-compilation_regression.bash
@@ -25,6 +25,6 @@ if [[ -z ${ROS_DISTRO} ]]; then
 fi
 
 ROS_WS_PREBUILD_HOOK="""
-git clone https://github.com/tu-darmstadt-ros-pkg/hector_gazebo
+git clone https://github.com/tu-darmstadt-ros-pkg/hector_gazebo -b ${ROS_DISTRO}-devel
 """
 . ${SCRIPT_DIR}/lib/gazebo_ros_pkgs-base.bash
