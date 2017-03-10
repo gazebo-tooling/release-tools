8e74bd2c74ade5407b31ce105095e1ab7a2a9775
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-03-10 21:09:09 +0100

    Need to update apt to get srcsim repo

--- one-line-installations/srcsim.sh
+++ one-line-installations/srcsim.sh
@@ -205,7 +205,7 @@ do_install() {
 	  $sh_c "wget -O - http://srcsim.gazebosim.org/src/src.key | sudo apt-key add -"
           $sh_c "echo deb http://srcsim.gazebosim.org/src ${dist_version} main > /etc/apt/sources.list.d/src-latest.list"
           # 2. Install the SRCSim package
-          $sh_c 'apt-get install -y -q srcsim'
+          $sh_c 'apt-get update; apt-get install -y -q srcsim'
           # 3. Change owner of ihmc_ros_java_adapter
           $sh_c "chown -R $USER:$USER /opt/ros/indigo/share/ihmc_ros_java_adapter"
 		  # 3. Add the ros group and add the user to it
