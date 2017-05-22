2549d04d3b9ce7aa457cd77024bffe952f0cf04f
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-05-22 14:41:49 +0200

    Need to install docker inside docker

--- jenkins-scripts/docker/ariac-docker-installation.bash
+++ jenkins-scripts/docker/ariac-docker-installation.bash
@@ -14,13 +14,25 @@ fi
 
 INSTALL_JOB_PREINSTALL_HOOK="""
 # run the test to install team system
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce
+
 cd ${WORKSPACE}/ariac-docker
 bash -x ./prepare_ariac_system.bash ${ROS_DISTRO}
 """
 
 INSTALL_JOB_POSTINSTALL_HOOK="""
 """
+
 # Need bc to proper testing and parsing the time
-export DEPENDENCY_PKGS DEPENDENCY_PKGS=""
+export DEPENDENCY_PKGS DEPENDENCY_PKGS="apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common"
 
 . ${SCRIPT_DIR}/lib/generic-install-base.bash
