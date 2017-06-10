ad1663b8f15f16b69796c6dd6b38a1415a096778
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-06-10 18:27:44 +0200

    pre-clean docker cache

--- jenkins-scripts/docker/ariac-docker-installation.bash
+++ jenkins-scripts/docker/ariac-docker-installation.bash
@@ -16,6 +16,12 @@ fi
 export DOCKER_DO_NOT_CACHE=true
 
 INSTALL_JOB_PREINSTALL_HOOK="""
+
+echo '# BEGIN SECTION: clean docker cache to get disk space'
+wget https://raw.githubusercontent.com/spotify/docker-gc/master/docker-gc
+sudo bash -c \"GRACE_PERIOD_SECONDS=86400 bash docker-gc\"
+echo '# END SECTION'
+
 echo '# BEGIN SECTION: workaround on docker ip overlaping'
 find ${WORKSPACE}/ariac-docker -name '*.bash' -exec sed -i -e 's:172\.18:172.19:g' {} \\;
 grep 172.19 ${WORKSPACE}/ariac-docker/ariac-server/run_container.bash 
