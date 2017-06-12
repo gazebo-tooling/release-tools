468c5402b31715edbf229d2be34a5602ed9624a5
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-06-12 19:05:58 +0200

    Use docker to build bloom packages

---
+++ jenkins-scripts/docker/bloom-debbuild.bash
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+if [[ -z ${ARCH} ]]; then
+  echo "ARCH variable not set!"
+  exit 1
+fi
+
+if [[ -z ${DISTRO} ]]; then
+  echo "DISTRO variable not set!"
+  exit 1
+fi
+
+if [[ -z ${ROS_DISTRO} ]]; then
+  echo "ROS_DISTRO variable not set!"
+  exit 1
+fi
+
+export ENABLE_ROS=true
+export OSRF_REPOS_TO_USE=${OSRF_REPOS_TO_USE:=stable}
+
+. ${SCRIPT_DIR}/lib/debbuild-bloom-base.bash
