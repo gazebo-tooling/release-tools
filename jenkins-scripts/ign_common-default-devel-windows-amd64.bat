1d6dba6964876c220a91ec75d80c9b1aa81824ee
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-07-11 19:27:19 +0200

    Include libdl dependency for ign-common windows build

--- jenkins-scripts/ign_common-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_common-default-devel-windows-amd64.bat
@@ -5,6 +5,6 @@ call "%SCRIPT_DIR%/lib/windows_configuration.bat"
 set VCS_DIRECTORY=ign-common
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
-set DEPENDENCY_PKG=FreeImage-vc12-x64-release-debug.zip
+set DEPEN_PKGS=FreeImage-vc12-x64-release-debug.zip dlfcn-win32-vc12-x64-release-debug.zip
 
 call "%SCRIPT_DIR%/lib/ign_common-base-windows.bat"
