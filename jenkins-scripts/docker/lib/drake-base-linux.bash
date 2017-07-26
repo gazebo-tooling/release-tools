171c8f1fcd367129d03db30356b3b6b6710794c9
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-07-26 21:41:38 +0200

    Fix the bazel installation

--- jenkins-scripts/docker/lib/drake-base-linux.bash
+++ jenkins-scripts/docker/lib/drake-base-linux.bash
@@ -8,10 +8,11 @@ echo '# END SECTION'
 cat > build.sh << DELIM
 ###################################################
 #
+set -ex
 
 echo '# BEGIN SECTION: install bazel'
-echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
 apt-get update
 apt-get install bazel
 echo '# END SECTION'
