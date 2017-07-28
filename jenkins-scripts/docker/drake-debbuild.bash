2f5aa128c1d41a56cca6aff2b213363339ae7b2f
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-07-28 12:28:57 +0200

    Fix script

--- jenkins-scripts/docker/drake-debbuild.bash
+++ jenkins-scripts/docker/drake-debbuild.bash
@@ -6,11 +6,11 @@ SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
 # Needs it own script to be able to install basel as prehook
 DOCKER_PREINSTALL_HOOK="""\
-echo '# BEGIN SECTION: install bazel'
-echo \"deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8\" | tee /etc/apt/sources.list.d/bazel.list
-curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
-apt-get update
-apt-get install -y bazel
+echo '# BEGIN SECTION: install bazel' && \\
+echo \"deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8\" | tee /etc/apt/sources.list.d/bazel.list && \\
+curl https://bazel.build/bazel-release.pub.gpg | apt-key add - && \\
+apt-get update && \\
+apt-get install -y bazel && \\
 echo '# END SECTION'
 """
 
