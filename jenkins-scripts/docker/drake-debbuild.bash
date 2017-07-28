25356f824e126174dd574168696d7fe4828504ec
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-07-28 13:06:25 +0200

    Need to overwrite file by a bug in java9

--- jenkins-scripts/docker/drake-debbuild.bash
+++ jenkins-scripts/docker/drake-debbuild.bash
@@ -5,12 +5,13 @@
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
 # Needs it own script to be able to install basel as prehook
+# Overwite for Bug: https://askubuntu.com/questions/769467/can-not-install-openjdk-9-jdk-because-it-tries-to-overwrite-file-aready-includ 
 DOCKER_POSTINSTALL_HOOK="""\
 echo '# BEGIN SECTION: install bazel' && \\
 echo \"deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8\" | tee /etc/apt/sources.list.d/bazel.list && \\
 wget -qO - https://bazel.build/bazel-release.pub.gpg | apt-key add - && \\
 apt-get update && \\
-apt-get install -y bazel && \\
+apt-get install -o Dpkg::Options::=\"--force-overwrite\" -y bazel && \\
 echo '# END SECTION'
 """
 
