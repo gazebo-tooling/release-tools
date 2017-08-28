7e614316819bdcce99027cb7262db4aa63912699
chapulina <burajiru.no.chapulina@gmail.com>
2017-08-28 16:49:27 -0700

    remove debug, add mercurial to deps

--- jenkins-scripts/docker/lib/_generic_linux_compilation_build.sh.bash
+++ jenkins-scripts/docker/lib/_generic_linux_compilation_build.sh.bash
@@ -16,8 +16,6 @@ fi
 [[ -z $GENERIC_ENABLE_CPPCHECK ]] && GENERIC_ENABLE_CPPCHECK=true
 [[ -z $GENERIC_ENABLE_TESTS ]] && GENERIC_ENABLE_TESTS=true
 
-echo "YOU'RE HERE!"
-
 cat > build.sh << DELIM_HEADER
 #!/bin/bash
 set -ex
@@ -26,12 +24,8 @@ DELIM_HEADER
 # Process the source build of dependencies if needed
 OSRF_DEPS="SDFORMAT IGN_MATH IGN_TRANSPORT IGN_GUI IGN_COMMON"
 for dep_uppercase in $OSRF_DEPS; do
-echo "NOW HERE!"
-echo "MATH? $BUILD_IGN_MATH"
-echo "COMMON? $BUILD_IGN_COMMON"
-echo "MSGS? $BUILD_IGN_MSGS"
   dep=`echo $dep_uppercase | tr '[:upper:]' '[:lower:]'`
-  EXTRA_PACKAGES="${EXTRA_PACKAGES} mercurial"
+  DEPENDENCY_PKGS="${DEPENDENCY_PKGS} mercurial"
   dependecy_installation=true #"\$BUILD_$dep_uppercase"
 
   if [[ -n ${dependecy_installation} ]] && ${dependecy_installation}; then
