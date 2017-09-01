71ae761f31a8ecd45018e4a24a258832751d4b6d
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-09-02 00:08:38 +0200

    Complete the job by running: compilatio, test, install and example

--- jenkins-scripts/docker/lib/drake-base-linux.bash
+++ jenkins-scripts/docker/lib/drake-base-linux.bash
@@ -17,13 +17,34 @@ ${DRAKE_BAZEL_INSTALL}
 
 echo '# BEGIN SECTION: compilation'
 cd ${WORKSPACE}/repo
-bazel run :install --jobs=${MAKE_JOBS} -- ${WORKSPACE}/install
+bazel build --jobs=${MAKE_JOBS}//...
+echo '# END SECTION'
+
+echo '# BEGIN SECTION: tests'
+bazel test //...
+echo '# END SECTION'
+
+echo '# BEGIN SECTION: install'
+bazel run :install --jobs=${MAKE_JOBS} -- /opt/drake
+echo '# END SECTION'
+
+echo '# BEGIN SECTION: particle test'
+cd ${WORKSPACE}
+git clone https://github.com/RobotLocomotion/drake-shambhala
+cd drake-shambhala/drake_cmake_installed
+mkdir build
+cd build
+cmake ..
+make -j${MAKE_JOBS}
+cd src/particles
+timeout --preserve-status 5 ./uniformly_accelerated_particle_demo
 echo '# END SECTION'
 DELIM
 
 SOFTWARE_DIR="repo"
 OSRF_REPOS_TO_USE="stable"
-DEPENDENCY_PKGS="${BASE_DEPENDENCIES} \
+DEPENDENCY_PKGS="git \
+                 ${BASE_DEPENDENCIES} \
                  ${DRAKE_DEPENDENCIES}"
 
 . ${SCRIPT_DIR}/lib/docker_generate_dockerfile.bash
