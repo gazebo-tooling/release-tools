34fd275fde0434872d2b21b40082cd713abf5f45
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-09-06 18:53:10 +0200

    Implement sdformat_version_hook use in the ABI job for sdformat

--- jenkins-scripts/docker/sdformat-compilation.bash
+++ jenkins-scripts/docker/sdformat-compilation.bash
@@ -14,16 +14,7 @@ if [[ -z ${DISTRO} ]]; then
   exit 1
 fi
 
-# Identify SDFORMAT_MAJOR_VERSION to help with dependency resolution
-SDFORMAT_MAJOR_VERSION=`\
-  grep 'set.*SDF_MAJOR_VERSION ' ${WORKSPACE}/sdformat/CMakeLists.txt | \
-  tr -d 'a-zA-Z _()'`
-
-# Check SDFORMAT version is integer
-if ! [[ ${SDFORMAT_MAJOR_VERSION} =~ ^-?[0-9]+$ ]]; then
-  echo "Error! SDFORMAT_MAJOR_VERSION is not an integer, check the detection"
-  exit -1
-fi
+. ${SCRIPT_DIR}/lib/_sdformat_version_hook.bash
 
 export BUILDING_SOFTWARE_DIRECTORY="sdformat"
 export BUILDING_PKG_DEPENDENCIES_VAR_NAME="SDFORMAT_BASE_DEPENDENCIES"
