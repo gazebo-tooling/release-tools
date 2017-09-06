8d630b10b51c30823f13af2f6b44a00c261798b1
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-09-07 01:29:52 +0200

    Fix clean up commands

--- jenkins-scripts/docker/lib/eigen-abi-checker.bash
+++ jenkins-scripts/docker/lib/eigen-abi-checker.bash
@@ -105,7 +105,8 @@ echo '# END SECTION'
 
 echo '# BEGIN SECTION: run the ABI checker'
 # cleanup optional support
-find  /usr/local -name *Cholmod* -d -exec {} \;
+find  /usr/local -name *Cholmod* -d -exec rm {} \;
+find  /usr/local -name *SuperLU* -d -exec rm {} \;
 # clean previous reports
 REPORTS_DIR=$WORKSPACE/reports/
 rm -fr \${REPORTS_DIR} && mkdir -p \${REPORTS_DIR}
