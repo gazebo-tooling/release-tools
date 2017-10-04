b8a5cdcab957a3aac794a753c4795091b4b46d83
Carlos Agüero <caguero@osrfoundation.org>
2017-10-04 16:34:58 -0700

    Compile ign-tools

--- jenkins-scripts/docker/ign_transport-compilation.bash
+++ jenkins-scripts/docker/ign_transport-compilation.bash
@@ -16,6 +16,16 @@ fi
 
 export BUILDING_SOFTWARE_DIRECTORY="ign-transport"
 export BUILDING_PKG_DEPENDENCIES_VAR_NAME="IGN_TRANSPORT_DEPENDENCIES"
+# Install Ignition Tools while we release a .deb package.
+# ToDo: Remove this env variable after releasing Ignition Tools.
+export DOCKER_POSTINSTALL_HOOK="""\
+hg clone https://bitbucket.org/osrf/ign-tools &&  \\
+mkdir ign-tools/build && \\
+cd ign-tools/build &&  \\
+cmake .. && \\
+make install && \\
+cd ../..
+"""
 export BUILDING_JOB_REPOSITORIES="stable"
 
 . ${SCRIPT_DIR}/lib/generic-building-base.bash
