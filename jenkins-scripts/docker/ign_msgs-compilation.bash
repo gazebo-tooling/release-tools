68d7f877ddc614e8d7778c4a5e8029a57c473041
Steve Peters <scpeters@openrobotics.org>
2017-10-17 22:02:22 -0700

    use prerelease repo for ign-msgs until Dec 1

--- jenkins-scripts/docker/ign_msgs-compilation.bash
+++ jenkins-scripts/docker/ign_msgs-compilation.bash
@@ -18,5 +18,9 @@ export BUILDING_SOFTWARE_DIRECTORY="ign-msgs"
 export BUILDING_DEPENDENCIES="libprotobuf-dev libprotoc-dev protobuf-compiler ruby ruby-dev libignition-math4-dev"
 export DOCKER_POSTINSTALL_HOOK="gem install protobuf"
 export BUILDING_JOB_REPOSITORIES="stable"
+if [[ $(date +%Y%m%d) -le 20171201 ]]; then
+  ## need prerelease repo to get ignition-cmake during the development cycle
+  export BUILDING_JOB_REPOSITORIES="${BUILDING_JOB_REPOSITORIES} prerelease"
+fi
 
 . ${SCRIPT_DIR}/lib/generic-building-base.bash
