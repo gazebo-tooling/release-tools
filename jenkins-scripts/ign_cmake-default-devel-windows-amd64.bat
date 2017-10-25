6520c93724cccc3878b73add7aec73ff18d4d1ac
Steven Peters <scpeters@osrfoundation.org>
2017-10-25 20:07:55 +0000

    ign_cmake-default-devel-windows-amd64.bat: enable testing

--- jenkins-scripts/ign_cmake-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_cmake-default-devel-windows-amd64.bat
@@ -4,7 +4,4 @@ set VCS_DIRECTORY=ign-cmake
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
-:: The ign-cmake CI does not currently support testing
-set ENABLE_TESTS=FALSE
-
 call "%SCRIPT_DIR%/lib/generic-default-devel-windows.bat"
