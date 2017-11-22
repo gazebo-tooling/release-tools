a613103a18ba5ec63294e276ac0d3d378a7a5242
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-11-22 12:35:40 +0100

    Fix Xenial build

--- jenkins-scripts/lib/dependencies_archive.sh
+++ jenkins-scripts/lib/dependencies_archive.sh
@@ -507,6 +507,8 @@ IGN_RENDERING_DEPENDENCIES="${ogre_pkg}\\
                             freeglut3-dev\\
                             libx11-dev"
 
+IGN_SENSORS_DEPENDENCIES="libignition-math4-dev      \\
+                          libignition-transport3-dev"
 #
 # HAPTIX
 #
