6b81014072f17c0c601e364cde42a0eda3e6324c
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-12-07 21:44:31 +0100

    New common from pre-release

--- jenkins-scripts/docker/ign_fuel-tools-compilation.bash
+++ jenkins-scripts/docker/ign_fuel-tools-compilation.bash
@@ -17,5 +17,9 @@ fi
 export BUILDING_SOFTWARE_DIRECTORY="ign-fuel-tools"
 export BUILDING_DEPENDENCIES="libignition-common-dev libzip-dev libjsoncpp-dev libcurl4-openssl-dev"
 export BUILDING_JOB_REPOSITORIES="stable"
+if [[ $(date +%Y%m%d) -le 20171215 ]]; then
+  ## need prerelease repo to get ignition-cmake during the development cycle
+  export BUILDING_JOB_REPOSITORIES="${BUILDING_JOB_REPOSITORIES} prerelease"
+fi
 
 . ${SCRIPT_DIR}/lib/generic-building-base.bash
