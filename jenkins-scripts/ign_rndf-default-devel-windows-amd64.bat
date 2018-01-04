032fc1e5445a9c4d0cb2eb6ea00170b17c1e7323
Chris Lalancette <clalancette@openrobotics.org>
2018-01-04 19:51:09 +0000

    Change the ignition-rndf dependencies.

--- jenkins-scripts/ign_rndf-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_rndf-default-devel-windows-amd64.bat
@@ -6,6 +6,6 @@ call "%SCRIPT_DIR%\lib\windows_configuration.bat"
 set VCS_DIRECTORY=ign-rndf
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
-set DEPEN_PKGS=ign-math3.zip
+set DEPEN_PKGS=ign-math4.zip ign-cmake0.zip
 
 call "%SCRIPT_DIR%\lib\project-default-devel-windows.bat"
