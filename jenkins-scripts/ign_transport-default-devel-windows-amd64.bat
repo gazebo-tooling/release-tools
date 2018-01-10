1012740f85fc666496d886af49c056a0bf223643
Steve Peters <scpeters@openrobotics.org>
2018-01-10 15:28:26 -0800

    call different ign-transport windows script based on detected major version

--- jenkins-scripts/ign_transport-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_transport-default-devel-windows-amd64.bat
@@ -6,4 +6,9 @@ set VCS_DIRECTORY=ign-transport
 set PLATFORM_TO_BUILD=amd64
 set IGN_CLEAN_WORKSPACE=true
 
-call %SCRIPT_DIR%/lib/generic-default-devel-windows.bat
+for /f %%i in ('python %SCRIPT_DIR%\tools\detect_ignition_major_version.py %WORKSPACE%\%VCS_DIRECTORY\CMakeLists.txt%') do set IGN_TRANSPORT_MAJOR_VERSION=%%i
+if %IGN_TRANSPORT_MAJOR_VERSION% GEQ 4 (
+  call %SCRIPT_DIR%/lib/generic-default-devel-windows.bat
+) else (
+  call %SCRIPT_DIR%/lib/ign_transport-base-windows.bat
+)
