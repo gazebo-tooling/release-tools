5ee23646157f89a471125cac5ec5038eb6c1963a
Steve Peters <scpeters@openrobotics.org>
2018-01-10 14:51:37 -0800

    add python script for detecting major version of an ignition package

---
+++ jenkins-scripts/tools/detect_ignition_major_version.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import re
+import sys
+
+if len(sys.argv) != 2:
+    print('need to specify location of CMakeLists.txt', file=sys.stderr)
+    exit()
+fileName = sys.argv[1]
+
+f = open(fileName, 'r')
+txt = f.read()
+f.close()
+
+old_version = re.search('set *\( *PROJECT_MAJOR_VERSION +(\d+)', txt)
+if old_version:
+    print(old_version.group(1) )
+
+ign_cmake_version = re.search('project *\( *ignition-[a-z\-]+(\d+)', txt)
+if ign_cmake_version:
+    print(ign_cmake_version.group(1))
