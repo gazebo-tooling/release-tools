604fe3e4224c817e34284032ace07ecbd6553953
chapulina <burajiru.no.chapulina@gmail.com>
2018-01-16 12:28:15 -0800

    Use releases for ign-sensors

--- jenkins-scripts/docker/ign_sensors-compilation.bash
+++ jenkins-scripts/docker/ign_sensors-compilation.bash
@@ -20,10 +20,6 @@ export BUILDING_JOB_REPOSITORIES="stable prerelease"
 
 # TODO: stop building dependencies from source after there's a release
 export BUILD_SDFORMAT=true # Need 6.x version
-export BUILD_IGN_COMMON=true
-# Need dependencies for ign_msgs
-export BUILDING_DEPENDENCIES="protobuf-compiler libprotobuf-dev libprotoc-dev"
-export BUILD_IGN_MSGS=true
 export BUILD_IGN_RENDERING=true
 
 export GPU_SUPPORT_NEEDED=true
