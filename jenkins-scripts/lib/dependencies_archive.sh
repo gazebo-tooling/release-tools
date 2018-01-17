b15ace412923d00c43353c5cbec4e1e4424456e8
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-01-17 23:01:52 +0100

    Move dependencies to lib archive

--- jenkins-scripts/lib/dependencies_archive.sh
+++ jenkins-scripts/lib/dependencies_archive.sh
@@ -521,7 +521,10 @@ IGN_RENDERING_DEPENDENCIES="${ogre_pkg}\\
                             freeglut3-dev\\
                             libx11-dev"
 
-IGN_SENSORS_DEPENDENCIES="libignition-common-dev     \\
+IGN_SENSORS_DEPENDENCIES="protobuf-compiler          \\
+                          libprotobuf-dev            \\
+                          libprotoc-dev              \\
+                          libignition-common-dev     \\
                           libignition-math4-dev      \\
                           libignition-msgs-dev       \\
                           libignition-transport3-dev"
