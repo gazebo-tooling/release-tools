2ac52f65428afc3a34289a55268479f2f13d1691
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-01-18 23:54:08 +0100

    Support for artful instead of zesty

--- jenkins-scripts/dsl/_configs_/Globals.groovy
+++ jenkins-scripts/dsl/_configs_/Globals.groovy
@@ -72,7 +72,7 @@ class Globals
 
    static ArrayList get_other_supported_distros()
    {
-     return [ 'trusty', 'zesty' ]
+     return [ 'trusty', 'artful' ]
    }
 
    static ArrayList get_supported_arches()
