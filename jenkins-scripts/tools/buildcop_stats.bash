7ccbc6f3c1abd402a05377f790a8329cff0318c8
Steve Peters <scpeters@openrobotics.org>
2018-01-29 15:34:10 -0800

    add helper script for generating gazebo buildcop statistics

---
+++ jenkins-scripts/tools/buildcop_stats.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl https://build.osrfoundation.org/view/main/view/BuildCopFail/api/json 2>/dev/null | python -c '\
+import json, sys;
+jobs = json.loads(sys.stdin.read())["jobs"];
+print("total: %d" % len(jobs));
+for c in ["blue", "yellow", "red", "aborted"]:
+    jc = [j for j in jobs if j["color"] == c]
+    print("%s: %d/%d, %.1f%%" % (c, len(jc), len(jobs), 100*float(len(jc)) / len(jobs)))'
