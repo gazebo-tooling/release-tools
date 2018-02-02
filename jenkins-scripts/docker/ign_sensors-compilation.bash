4dcd4993af22773c1f7328e0581fc624b6266e29
Steve Peters <scpeters@openrobotics.org>
2018-02-01 16:55:57 -0800

    don't need prerelease or sdformat from source for ign-sensors anymore

--- jenkins-scripts/docker/ign_sensors-compilation.bash
+++ jenkins-scripts/docker/ign_sensors-compilation.bash
@@ -16,9 +16,8 @@ fi
 
 export BUILDING_SOFTWARE_DIRECTORY="ign-sensors"
 export BUILDING_PKG_DEPENDENCIES_VAR_NAME="IGN_SENSORS_DEPENDENCIES"
-export BUILDING_JOB_REPOSITORIES="stable prerelease"
+export BUILDING_JOB_REPOSITORIES="stable"
 # TODO: stop building dependencies from source after there's a release
-export BUILD_SDFORMAT=true # Need 6.x version
 export BUILD_IGN_RENDERING=true
 
 export GPU_SUPPORT_NEEDED=true
