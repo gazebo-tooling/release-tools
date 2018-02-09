5753c5a090273d28f51f80ca53fe07519a88c8ce
Steven! Ragnarök <steven@nuclearsandwich.com>
2018-02-09 11:56:29 -0800

    Add a simple compilation script based on srcsim's.

---
+++ jenkins-scripts/docker/servicesim-compilation.bash
@@ -0,0 +1,60 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+if [[ -z ${ARCH} ]]; then
+  echo "ARCH variable not set!"
+  exit 1
+fi
+
+if [[ -z ${DISTRO} ]]; then
+  echo "DISTRO variable not set!"
+  exit 1
+fi
+
+case ${DISTRO} in
+  'xenial')
+    ROS_DISTRO=kinetic
+    PKG_VERSION="8"
+    ;;
+  *)
+    echo "Unsupported DISTRO: ${DISTRO}"
+    exit 1
+esac
+
+export GPU_SUPPORT_NEEDED=true
+
+# Do not use the subprocess_reaper in debbuild. Seems not as needed as in
+# testing jobs and seems to be slow at the end of jenkins jobs
+export ENABLE_REAPER=false
+
+DOCKER_JOB_NAME="servicesim_ci"
+. ${SCRIPT_DIR}/lib/boilerplate_prepare.sh
+
+# Import servicesim library (none currently)
+#. ${SCRIPT_DIR}/lib/_servicesim_lib.bash
+
+export ROS_SETUP_PREINSTALL_HOOK="""
+#${SERVICESIM_SETUP_REPOSITORIES} # currently in progress
+## Needed for rosdep
+apt-get install -y ros-${ROS_DISTRO}-ros-base ${ROS_CATKIN_BASE}
+## Need to get all srcsim dependencies (from package) No source pkg so
+## can not use build-dep
+#apt-get install -y servicesim && apt-get remove -y servicesim # currently in progress
+"""
+
+export ROS_SETUP_POSTINSTALL_HOOK="""
+"""
+
+# Generate the first part of the build.sh file for ROS
+. ${SCRIPT_DIR}/lib/_ros_setup_buildsh.bash "servicesim"
+
+# don't have rosdep at this point and want gazebo to be cached by docker
+DEPENDENCY_PKGS="libgazebo${PKG_VERSION}-dev"
+USE_ROS_REPO=true
+OSRF_REPOS_TO_USE="stable"
+
+. ${SCRIPT_DIR}/lib/docker_generate_dockerfile.bash
+. ${SCRIPT_DIR}/lib/docker_run.bash
