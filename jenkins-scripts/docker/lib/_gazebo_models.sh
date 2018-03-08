0c1a7a08d75b9c554b455af54b652747a6dd5fe2
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-03-08 18:14:18 +0100

    Use curl instead of wget

--- jenkins-scripts/docker/lib/_gazebo_models.sh
+++ jenkins-scripts/docker/lib/_gazebo_models.sh
@@ -1,5 +1,5 @@
 GAZEBO_MODEL_INSTALLATION="""
-wget -P /tmp/ https://bitbucket.org/osrf/gazebo_models/get/default.tar.gz && \
+curl http://example.com --output /tmp/default.tar.gz
 mkdir -p ~/.gazebo/models
 tar -xvf /tmp/default.tar.gz -C ~/.gazebo/models --strip 1
 rm /tmp/default.tar.gz"""
