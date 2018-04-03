892638fc2639ec3e5e2d5e099d9ee317f5b576a3
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-04-03 20:15:31 +0200

    Missing dependency for gl libraries

--- jenkins-scripts/docker/ign_rendering-compilation.bash
+++ jenkins-scripts/docker/ign_rendering-compilation.bash
@@ -16,7 +16,7 @@ fi
 
 export BUILDING_SOFTWARE_DIRECTORY="ign-rendering"
 export BUILDING_JOB_REPOSITORIES="stable"
-export BUILDING_DEPENDENCIES="libignition-cmake1-dev libignition-common-dev libignition-math4-dev libfreeimage-dev libx11-dev mesa-common-dev mesa-utils libogre-1.9-dev"
+export BUILDING_DEPENDENCIES="libignition-cmake1-dev libignition-common-dev libignition-math4-dev libfreeimage-dev libx11-dev mesa-common-dev mesa-utils libglu1-mesa-dev libogre-1.9-dev"
 if [[ $(date +%Y%m%d) -le 20180415 ]]; then
   ## need prerelease repo to get ignition-cmake during the development cycle
   export BUILDING_JOB_REPOSITORIES="${BUILDING_JOB_REPOSITORIES} prerelease"
