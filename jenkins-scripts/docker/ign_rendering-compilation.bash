0de05720d711fa12917a1112c5baf9855b68734c
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-04-03 18:18:18 +0200

    Need prerelease repo for ign-cmake1

--- jenkins-scripts/docker/ign_rendering-compilation.bash
+++ jenkins-scripts/docker/ign_rendering-compilation.bash
@@ -17,7 +17,7 @@ fi
 export BUILDING_SOFTWARE_DIRECTORY="ign-rendering"
 export BUILDING_JOB_REPOSITORIES="stable"
 export BUILDING_DEPENDENCIES="libignition-cmake1-dev libignition-common-dev libignition-math4-dev libfreeimage-dev libx11-dev mesa-common-dev mesa-utils libogre-1.9-dev"
-if [[ $(date +%Y%m%d) -le 20180115 ]]; then
+if [[ $(date +%Y%m%d) -le 20180415 ]]; then
   ## need prerelease repo to get ignition-cmake during the development cycle
   export BUILDING_JOB_REPOSITORIES="${BUILDING_JOB_REPOSITORIES} prerelease"
 fi
