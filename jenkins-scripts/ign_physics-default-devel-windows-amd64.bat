469147006e11253a340bda4cd7463c163965b2f5
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-04-20 18:01:04 +0200

    Migrate ign_physics to colcon

--- jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
@@ -4,4 +4,7 @@ set VCS_DIRECTORY=ign-physics
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
-call "%SCRIPT_DIR%/lib/generic-default-devel-windows.bat"
+:: This needs to be migrated to DSL to get multi-major versions correctly
+set GAZEBODISTRO_FILE="ign-physics0.yaml"
+
+call "%SCRIPT_DIR%/lib/concon-default-devel-windows.bat"
