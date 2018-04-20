4bc168dc2e2271ec01d5384302cad4cd720de5f8
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-04-20 16:12:48 +0200

    Fix dependency name

--- jenkins-scripts/ign_rendering-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_rendering-default-devel-windows-amd64.bat
@@ -5,7 +5,7 @@ set VCS_DIRECTORY=ign-rendering
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 :: dlfcn
-set DEPEN_PKGS="dlfcn"
+set DEPEN_PKGS="dlfcn-win32"
 :: This needs to be migrated to DSL to get multi-major versions correctly
 set GAZEBODISTRO_FILE="ign-rendering0.yaml"
 
