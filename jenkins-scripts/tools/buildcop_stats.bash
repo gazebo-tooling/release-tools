3a98017405e2bf08ff24b1f5068a26aa37f61069
Steve Peters <scpeters@openrobotics.org>
2018-04-20 11:57:13 -0700

    add list of reasons builds might be failing

--- jenkins-scripts/tools/buildcop_stats.bash
+++ jenkins-scripts/tools/buildcop_stats.bash
@@ -13,7 +13,35 @@ for c in ["blue", "yellow", "red", "aborted", "notbuilt"]:
     jobs_colors[c] = jc
     print("| %s | %d/%d | %.1f%% |  |" % (c, len(jc), len(jobs), 100*float(len(jc)) / len(jobs)))
 print("\n## [Failing Builds](https://build.osrfoundation.org/view/main/view/BuildCopFail/)\n")
+
+# list of reasons build might be failing
+alwaysFailsBecause = {}
+alwaysFailsBecause["gazebo-ci-default-artful-amd64-gpu-none"] = "Problems with libblas. Pending SRU in Ubuntu"
+alwaysFailsBecause["gazebo-ci-default-windows7-amd64"] = \
+  "Both pending some fixes under the scope of [PR 529 in release-tools](https://bitbucket.org/osrf/release-tools/pull-requests/529/fix-windows-gazebo-build/diff)"
+alwaysFailsBecause["gazebo-ci-gazebo9-windows7-amd64"] = \
+  "Both pending some fixes under the scope of [PR 529 in release-tools](https://bitbucket.org/osrf/release-tools/pull-requests/529/fix-windows-gazebo-build/diff)"
+alwaysFailsBecause["gazebo-performance-default-xenial-amd64"] = \
+  "Performance problems documented in [gazebo issue 2320](https://bitbucket.org/osrf/gazebo/issues/2320/performance_transport_stress-test-times)"
+alwaysFailsBecause["ignition_gui-ci-default-windows7-amd64"] = "Awaiting Windows implementation. In jrivero TODO list"
+alwaysFailsBecause["ignition_rendering-ci-default-windows7-amd64"] = "Awaiting Windows implementation. In jrivero TODO list"
+alwaysFailsBecause["ignition_sensors-ci-default-windows7-amd64"] = "Awaiting Windows implementation. In jrivero TODO list"
+
+print("\n### Builds with no record of passing\n")
 for j in jobs_colors["red"]:
     name = j["name"]
     url = j["url"]
-    print("* [![Build Status](%s/badge/icon)](%s) [%s](%s)\n" % (url, url, name, url))'
+    if alwaysFailsBecause.has_key(name):
+        print("\n* %s\n" % alwaysFailsBecause[name])
+        print("    * [![Build Status](%s/badge/icon)](%s) [%s](%s)\n" % (url, url, name, url))
+
+print("\n### Builds that have succeeded in the past, but are failing now\n")
+print("\n* internal compiler error, started a new build\n")
+print("\n* Jenkins disconnected before build could finish, started a new build\n")
+print("\n* not sure\n")
+for j in jobs_colors["red"]:
+    name = j["name"]
+    url = j["url"]
+    if not alwaysFailsBecause.has_key(name):
+        print("    * [![Build Status](%s/badge/icon)](%s) [%s](%s)\n" % (url, url, name, url))
+'
