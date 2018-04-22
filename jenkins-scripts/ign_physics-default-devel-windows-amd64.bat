31129d133bc6b7766ac47b4328351715962dfd23
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-04-22 21:02:17 +0200

    Fix colcon package name

--- jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
@@ -5,7 +5,7 @@ set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
 :: This needs to be migrated to DSL to get multi-major versions correctly
-set COLCON_PACKAGE=ign-physics0
+set COLCON_PACKAGE=ignition-physics0
 set GAZEBODISTRO_FILE=%COLCON_PACKAGE%.yaml
 
 call "%SCRIPT_DIR%/lib/colcon-default-devel-windows.bat"
