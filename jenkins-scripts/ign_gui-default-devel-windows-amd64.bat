7c506d1a54dcda2307950d7b2b343969d9e7dbff
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-04-24 18:37:07 +0200

    Migrate ign-gui to colcon

--- jenkins-scripts/ign_gui-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_gui-default-devel-windows-amd64.bat
@@ -1,11 +1,13 @@
 @echo on
 set SCRIPT_DIR="%~dp0"
 
-call "%SCRIPT_DIR%\lib\windows_configuration.bat"
-
 set VCS_DIRECTORY=ign-gui
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
-set DEPEN_PKGS=qt5-x64-static-release.zip qwt_6.1.2~osrf_qt5.zip
 
-call "%SCRIPT_DIR%\lib\project-default-devel-windows.bat"
+set DEPEN_PKGS="qt qwt protobuf tinyxml2"
+:: This needs to be migrated to DSL to get multi-major versions correctly
+set COLCON_PACKAGE=ignition-gui0
+set GAZEBODISTRO_FILE=ign-gui0.yaml
+
+call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
