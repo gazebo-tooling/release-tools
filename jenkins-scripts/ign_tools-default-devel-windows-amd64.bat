ac268dc1f71e6c9f850b4e772c2e62e5fb39d6a5
Steve Peters <scpeters@openrobotics.org>
2018-05-11 17:18:36 -0700

    Backed out changeset f8a999a41d93

--- jenkins-scripts/ign_tools-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_tools-default-devel-windows-amd64.bat
@@ -3,5 +3,6 @@ set SCRIPT_DIR="%~dp0"
 set VCS_DIRECTORY=ign-tools
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
+set ENABLE_TESTS=false
 
 call "%SCRIPT_DIR%/lib/generic-default-devel-windows.bat"
