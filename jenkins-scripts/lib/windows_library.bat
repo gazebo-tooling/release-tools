ec92a2300a1bb4cddd1c4368657d5a4548e4ed3e
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-05-21 17:47:15 +0200

    Improve error reporting

--- jenkins-scripts/lib/windows_library.bat
+++ jenkins-scripts/lib/windows_library.bat
@@ -176,7 +176,7 @@ goto :EOF
 set LIB_DIR="%~dp0"
 call %LIB_DIR%\windows_env_vars.bat
 
-colcon build --event-handler console_cohesion+ --cmake-args " -DCMAKE_TOOLCHAIN_FILE=%VCPKG_CMAKE_TOOLCHAIN_FILE%" || goto :error
+colcon build --event-handler console_cohesion+ --cmake-args " -DCMAKE_TOOLCHAIN_FILE=%VCPKG_CMAKE_TOOLCHAIN_FILE%" || type %HOMEPATH%/.colcon/latest & goto :error
 goto :EOF
 
 :: ##################################
