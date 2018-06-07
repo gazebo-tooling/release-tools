74a7a1d26c79d1826fb0623c238d68aaa9bbac74
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-06-07 13:58:05 +0200

    Typo

--- jenkins-scripts/docker/release-tools-checker.bash
+++ jenkins-scripts/docker/release-tools-checker.bash
@@ -19,7 +19,7 @@ RESULT_DIR=${WORKSPACE}/cppcheck_results
 mkdir \${RESULT_DIR}
 
 echo '# BEGIN SECTION: run shellcheck'
-find ${WORKSPACE}/release-tools -name '*.bash' -exec shellcheck --shell=bash --exclude SC2034 --formatcheckstyle -- {} \; >  ${RESULT_DIR}/*.xml
+find ${WORKSPACE}/release-tools -name '*.bash' -exec shellcheck --shell=bash --exclude SC2034 --format checkstyle -- {} \; >  ${RESULT_DIR}/*.xml
 echo '# END SECTION'
 DELIM
 
