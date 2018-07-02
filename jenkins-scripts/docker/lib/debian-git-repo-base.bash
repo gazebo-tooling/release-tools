6a4fa54af60647d1e063042075c436b871578bc0
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-07-02 17:21:42 +0200

    Replace all ~

--- jenkins-scripts/docker/lib/debian-git-repo-base.bash
+++ jenkins-scripts/docker/lib/debian-git-repo-base.bash
@@ -62,7 +62,7 @@ OSRF_VERSION=\$VERSION\osrf${RELEASE_VERSION}~${DISTRO}${RELEASE_ARCH_VERSION}
 echo "# BEGIN SECTION: check that pristine-tar is updated"
 git checkout -f pristine-tar || { echo "W: probably miss the pristine-tar branch" && exit 1; }
 # The tilde (~) is not allow in git tag and changed to underscore (_)
-PRISTINE_VERSION_NO_REVISION=\$(echo \${VERSION_NO_REVISION} | sed 's:~:_:')
+PRISTINE_VERSION_NO_REVISION=\$(echo \${VERSION_NO_REVISION} | sed 's:~:_:g')
 if [[ -z \$(git tag | grep upstream/\${PRISTINE_VERSION_NO_REVISION}) ]]; then
    echo "W: \${PRISTINE_VERSION_NO_REVISION} commit was not found in pristine-tar"
    exit 1
