8b5eb0db73326849a2b36e499fc4f64ec32f5b5f
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-07-06 00:43:15 +0200

    Error in merge

--- jenkins-scripts/lib/windows_library.bat
+++ jenkins-scripts/lib/windows_library.bat
@@ -159,6 +159,77 @@ nmake || goto :error
 nmake install || goto :error
 goto :EOF
 
+:: ##################################
+:get_source_from_gazebodistro
+::
+:: arg1: Name of the yaml file in the gazebodistro repro
+:: arg2: directory destination (default .)
+set gzdistro_dir=gazebodistro
+
+if exist %gzdistro_dir% (rmdir /s /q %gzdistro_dir%)
+hg clone https://bitbucket.org/osrf/gazebodistro %gzdistro_dir%
+vcs import --retry 5  < "%gzdistro_dir%\%1" "%2" || goto :error
+goto :EOF
+
+:: ##################################
+:_colcon_build_cmd
+::
+:: The CMAKE_BUILD_TYPE is needed to workaround on issue
+:: https://github.com/Microsoft/vcpkg/issues/1626
+::
+:: arg1 colcon command line extra arguments
+:: arg2 colcon cmake extra command line arguments
+set LIB_DIR="%~dp0"
+call %LIB_DIR%\windows_env_vars.bat
+
+set COLCON_EXTRA_ARGS=%1
+set COLCON_EXTRA_CMAKE_ARGS=%2
+
+colcon build --build-base "build"^
+             --install-base "install"^
+             %EXTRA_COLCON_ARGS%^
+             --cmake-args " -DCMAKE_BUILD_TYPE=%BUILD_TYPE%"^
+                          " -DCMAKE_TOOLCHAIN_FILE=%VCPKG_CMAKE_TOOLCHAIN_FILE%"^
+                          " -DVCPKG_TARGET_TRIPLET=%VCPKG_TARGET_TRIPLET%"^
+                          %COLCON_EXTRA_CMAKE_ARGS%^
+             --event-handler console_cohesion+ || type %HOMEPATH%/.colcon/latest & goto :error
+
+:: ##################################
+::
+:: Build all the workspaces packages except the package provided in arg1
+::
+:: arg1: name of the colcon package excluded from building
+:build_workspace
+
+set COLCON_PACKAGE=%1
+:: two runs to get the dependencies built with testing and the package under
+:: test build with tests
+call :_colcon_build_cmd "--packages-skip %COLCON_PACKAGE%" " -DBUILD_TESTING=0"
+call :_colcon_build_cmd "--packages-select %COLCON_PACKAGE%" " -DBUILD_TESTING=1"
+goto :EOF
+
+:: ##################################
+:list_workspace_pkgs
+colcon list -g || goto :error
+goto :EOF
+
+:: ##################################
+:tests_in_workspace
+:: arg1: package whitelist to test
+set COLCON_PACKAGE=%1
+
+colcon test --build-base "build" --event-handler console_direct+ --install-base "install" --packages-select %COLCON_PACKAGE% --executor sequential || goto :error
+colcon test-result --build-base "build" --all
+goto :EOF
+
+:: ##################################
+:install_vcpkg_package
+:: arg1: package to install
+set LIB_DIR=%~dp0
+call %LIB_DIR%\windows_env_vars.bat
+
+%VCPKG_CMD% install "%1"
+goto :EOF
 
 :: ##################################
 :error - error routine
