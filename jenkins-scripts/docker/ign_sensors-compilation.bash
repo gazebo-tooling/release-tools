b44abb38fc5f3b44ab2a60fc4d8d90fc8faa4299
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-07-25 12:47:43 +0200

    Implement ign-sensor support

--- jenkins-scripts/docker/ign_sensors-compilation.bash
+++ jenkins-scripts/docker/ign_sensors-compilation.bash
@@ -14,9 +14,25 @@ if [[ -z ${DISTRO} ]]; then
   exit 1
 fi
 
+. "${SCRIPT_DIR}/lib/_gz11_hook.bash"
+
 export BUILDING_SOFTWARE_DIRECTORY="ign-sensors"
-export BUILDING_PKG_DEPENDENCIES_VAR_NAME="IGN_SENSORS_DEPENDENCIES"
-export BUILDING_JOB_REPOSITORIES="stable"
+
+if ${NEEDS_GZ11_SUPPORT}; then
+  # all dependencies of ign-sensors are ignition/sdformat
+  export BUILD_IGN_CMAKE=true
+  export BUILD_IGN_TOOLS=true
+  export BUILD_IGN_MATH=true
+  export BUILD_IGN_SDFORMAT=true
+  export BUILD_IGN_COMMON=true
+  export BUILD_IGN_MSGS=true
+  export BUILD_IGN_TRANSPORT=true
+  export BUILD_IGN_RENDERING=true
+else
+  export BUILDING_PKG_DEPENDENCIES_VAR_NAME="IGN_SENSORS_DEPENDENCIES"
+  export BUILDING_JOB_REPOSITORIES="stable"
+fi
+
 if [[ $(date +%Y%m%d) -le 20180831 ]]; then
   ## need prerelease repo to get ignition-cmake1 for ign-rendering
   export BUILDING_JOB_REPOSITORIES="${BUILDING_JOB_REPOSITORIES} prerelease"
