0e0e295e104cbb0f9cbd15da4a4aebd20c987f49
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-07-31 15:50:52 +0200

    Support for ign-physics using ign-plugins and sdf

--- jenkins-scripts/docker/ign_physics-compilation.bash
+++ jenkins-scripts/docker/ign_physics-compilation.bash
@@ -14,10 +14,20 @@ if [[ -z ${DISTRO} ]]; then
   exit 1
 fi
 
-export BUILDING_SOFTWARE_DIRECTORY="ign-physics"
 export BUILDING_JOB_REPOSITORIES="stable"
+# gz11 hook will take of adding prerelease
+. "${SCRIPT_DIR}/lib/_gz11_hook.bash"
+
+export BUILDING_SOFTWARE_DIRECTORY="ign-physics"
 export BUILDING_PKG_DEPENDENCIES_VAR_NAME="IGN_PHYSICS_DEPENDENCIES"
 export DART_FROM_PKGS="true"
+
+if ${NEEDS_GZ11_SUPPORT}; then
+  export BUILD_IGN_CMAKE=true
+  export BUILD_IGN_MATH=true
+  export BUILD_IGN_PLUGINS=true
+  export BUILD_SDFORMAT=true
+fi
 
 if [[ $(date +%Y%m%d) -le 20180831 ]]; then
   ## need prerelease repo to get ignition-cmake during the development cycle
