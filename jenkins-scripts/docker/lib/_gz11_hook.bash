251bc969f6939c86e562d3091fbf54ca52b65ac2
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-08-02 12:20:50 +0200

    Exclude GZ11 support if NEED_GZ11_SUPPORT is false

--- jenkins-scripts/docker/lib/_gz11_hook.bash
+++ jenkins-scripts/docker/lib/_gz11_hook.bash
@@ -1,20 +1,20 @@
-# calculate NEEDS_GZ11_SUPPORT 
+# calculate NEEDS_GZ11_SUPPORT
 export NEEDS_GZ11_SUPPORT=false
 
 if [[ ${DEST_BRANCH} == 'gz11' ]] || [[ ${SRC_BRANCH} == 'gz11' ]]; then
   export NEEDS_GZ11_SUPPORT=true
+
+  # needs gcc8 for c++17 features
+  export USE_GCC8=true
+
+  # define gz11 branches for using when enabled by BUILD_IGN_* variables
+  export IGN_CMAKE_BRANCH="gz11"
+  export IGN_MATH_BRANCH="gz11"
+  export IGN_MSGS_BRANCH="gz11"
+  export IGN_COMMON_BRANCH="gz11"
+  export IGN_TRANSPORT_BRANCH="gz11"
+  export IGN_RENDERING_BRANCH="gz11"
+
+  # Need stable + prerelease repositories for gz11 packages
+  export BUILDING_JOB_REPOSITORIES="stable prerelease"
 fi
-
-# needs gcc8 for c++17 features
-export USE_GCC8=true
-
-# define gz11 branches for using when enabled by BUILD_IGN_* variables
-export IGN_CMAKE_BRANCH="gz11"
-export IGN_MATH_BRANCH="gz11"
-export IGN_MSGS_BRANCH="gz11"
-export IGN_COMMON_BRANCH="gz11"
-export IGN_TRANSPORT_BRANCH="gz11"
-export IGN_RENDERING_BRANCH="gz11"
-  
-# Need stable + prerelease repositories for gz11 packages
-export BUILDING_JOB_REPOSITORIES="stable prerelease"
