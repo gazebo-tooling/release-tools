6f0d94b0fafedce9fa9f806e8ded93ee932a4189
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-08-06 19:33:40 +0200

    Use cppzmq and zeromq as dependencies for ign-transport

--- jenkins-scripts/ign_gui-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_gui-default-devel-windows-amd64.bat
@@ -5,7 +5,7 @@ set VCS_DIRECTORY=ign-gui
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
-set DEPEN_PKGS="qt5 qwt protobuf tinyxml2"
+set DEPEN_PKGS="qt5 qwt protobuf tinyxml2 cppzmq zeromq"
 :: This needs to be migrated to DSL to get multi-major versions correctly
 set COLCON_PACKAGE=ignition-gui0
 set GAZEBODISTRO_FILE=ign-gui0.yaml
