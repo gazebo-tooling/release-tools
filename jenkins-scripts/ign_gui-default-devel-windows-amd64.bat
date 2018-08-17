cdb6f0f6de5f83b1696b027b6c5d5e963a339c52
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-08-17 17:09:23 +0000

    Install boost for sdformat

--- jenkins-scripts/ign_gui-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_gui-default-devel-windows-amd64.bat
@@ -5,7 +5,8 @@ set VCS_DIRECTORY=ign-gui
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
-set DEPEN_PKGS="qt5 qwt protobuf tinyxml2 cppzmq zeromq"
+:: boost for sdforamt
+set DEPEN_PKGS="qt5 qwt protobuf tinyxml2 cppzmq zeromq boost-any boost-variant"
 :: This needs to be migrated to DSL to get multi-major versions correctly
 set COLCON_PACKAGE=ignition-gui0
 set GAZEBODISTRO_FILE=ign-gui0.yaml
