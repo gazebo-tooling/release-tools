af4a25bfa98447ae7acf33775cee3f063a49b8b6
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-08-28 01:07:52 +0200

    Typo

--- jenkins-scripts/docker/lib/_ros_setup_buildsh.bash
+++ jenkins-scripts/docker/lib/_ros_setup_buildsh.bash
@@ -105,7 +105,7 @@ echo '# BEGIN SECTION: running tests'
 # some tests needs to source install before running
 . install/setup.bash
 echo \$LD_LIBRARY_PATH
-echo \GAZEBO_PLUGIN_PATH
+echo \$GAZEBO_PLUGIN_PATH
 find . -name libRosFlashLightPlugin.so || true
 ${CMD_CATKIN_TEST}
 ${CMD_CATKIN_TEST_RESULTS}
