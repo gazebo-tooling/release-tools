c3965b7cf38f323e49fded515337e91d7debb967
Nate Koenig <natekoenig@gmail.com>
2018-09-17 12:18:14 -0700

    Stable and prerelease for sdformat8

--- jenkins-scripts/docker/sdformat-compilation.bash
+++ jenkins-scripts/docker/sdformat-compilation.bash
@@ -25,10 +25,12 @@ fi
 
 if [[ ${SDFORMAT_MAJOR_VERSION} -ge 8 ]]; then
   export USE_GCC8=true
+  export BUILDING_JOB_REPOSITORIES="stable prerelease"
+else
+  export BUILDING_JOB_REPOSITORIES="stable"
 fi
 
 # default and major branches compilations
 export BUILDING_PKG_DEPENDENCIES_VAR_NAME="SDFORMAT_BASE_DEPENDENCIES"
-export BUILDING_JOB_REPOSITORIES="stable"
 
 . "${SCRIPT_DIR}/lib/generic-building-base.bash"
