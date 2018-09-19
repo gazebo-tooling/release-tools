57ca4e94c16e9d87c261ad1b932e8b2d456543f4
Steve Peters <scpeters@openrobotics.org>
2018-09-19 14:16:13 -0700

    clean up math and plugin scripts

--- jenkins-scripts/docker/ign_plugin-compilation.bash
+++ jenkins-scripts/docker/ign_plugin-compilation.bash
@@ -18,6 +18,23 @@ export BUILDING_SOFTWARE_DIRECTORY="ign-plugin"
 export BUILDING_JOB_REPOSITORIES="stable"
 export BUILDING_PKG_DEPENDENCIES_VAR_NAME="IGN_PLUGIN_DEPENDENCIES"
 
+# Identify IGN_PLUGIN_MAJOR_VERSION to help with dependency resolution
+IGN_PLUGIN_MAJOR_VERSION=$(\
+  python ${SCRIPT_DIR}/../tools/detect_cmake_major_version.py \
+  ${WORKSPACE}/ign-plugin/CMakeLists.txt)
+
+# Check IGN_PLUGIN version is integer
+if ! [[ ${IGN_PLUGIN_MAJOR_VERSION} =~ ^-?[0-9]+$ ]]; then
+  echo "Error! IGN_PLUGIN_MAJOR_VERSION is not an integer, check the detection"
+  exit -1
+fi
+
+if [[ ${IGN_PLUGIN_MAJOR_VERSION} -ge 6 ]]; then
+  export NEEDS_GZ11_SUPPORT=true
+fi
+
+. "${SCRIPT_DIR}/lib/_gz11_hook.bash"
+
 if [[ $(date +%Y%m%d) -le 20181231 ]]; then
   ## need prerelease repo to get ignition-cmake during the development cycle
   export BUILDING_JOB_REPOSITORIES="${BUILDING_JOB_REPOSITORIES} prerelease"
