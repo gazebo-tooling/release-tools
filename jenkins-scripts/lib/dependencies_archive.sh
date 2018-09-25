e22b7216d877d13613d4adef8f31698adb5c9cbc
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-09-26 00:32:27 +0200

    Include gflags in dependencies

--- jenkins-scripts/lib/dependencies_archive.sh
+++ jenkins-scripts/lib/dependencies_archive.sh
@@ -707,6 +707,7 @@ IGN_SENSORS_DEPENDENCIES="libignition-common3-dev     \\
 IGN_GAZEBO_DEPENDENCIES="libignition-common3-dev     \\
                          libignition-cmake2-dev \\
                          libignition-gui-dev \\
+                         libgflags-dev \\
                          libignition-math6-dev      \\
                          libignition-math6-eigen3-dev      \\
                          libignition-msgs3-dev       \\
