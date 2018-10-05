42b142e241c996b5d4c64c0c1d44fd5f90eb6a6b
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-10-05 20:38:19 +0200

    Do not provide explicit gazebodistro file

--- jenkins-scripts/ign_gui-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_gui-default-devel-windows-amd64.bat
@@ -1,5 +1,5 @@
 @echo on
-set SCRIPT_DIR="%~dp0"
+set SCRIPT_DIR=%~dp0
 
 set VCS_DIRECTORY=ign-gui
 set PLATFORM_TO_BUILD=x86_amd64
@@ -8,6 +8,5 @@ set IGN_CLEAN_WORKSPACE=true
 set DEPEN_PKGS="qt5 qwt protobuf tinyxml2"
 :: This needs to be migrated to DSL to get multi-major versions correctly
 set COLCON_PACKAGE=ignition-gui0
-set GAZEBODISTRO_FILE=ign-gui0.yaml
 
 call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
