6f178d284165cdc7de5c38e520fdb68b4fc96db5
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-10-05 22:04:24 +0200

    Support for ign-rendering

--- jenkins-scripts/ign_rendering-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_rendering-default-devel-windows-amd64.bat
@@ -1,5 +1,5 @@
 @echo on
-set SCRIPT_DIR="%~dp0"
+set SCRIPT_DIR=%~dp0
 
 set VCS_DIRECTORY=ign-rendering
 set PLATFORM_TO_BUILD=x86_amd64
@@ -8,6 +8,5 @@ set IGN_CLEAN_WORKSPACE=true
 set DEPEN_PKGS="dlfcn-win32 cuda freeimage ogre gts glib"
 :: This needs to be migrated to DSL to get multi-major versions correctly
 set COLCON_PACKAGE=ignition-rendering0
-set GAZEBODISTRO_FILE=ign-rendering0.yaml
 
 call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
