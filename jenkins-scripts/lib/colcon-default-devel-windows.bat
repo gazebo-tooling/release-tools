baf81d817fe6cbd2be2dc020250be8ac0f62ae90
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-10-19 12:52:13 +0200

    Fix comparisson

--- jenkins-scripts/lib/colcon-default-devel-windows.bat
+++ jenkins-scripts/lib/colcon-default-devel-windows.bat
@@ -29,7 +29,7 @@ set LOCAL_WS_BUILD=%WORKSPACE%\build
 @if "%COLCON_AUTO_MAJOR_VERSION%" == "" set COLCON_AUTO_MAJOR_VERSION=false
 
 setlocal ENABLEDELAYEDEXPANSION
-if %COLCON_AUTO_MAJOR_VERSION% == "true" (
+if "%COLCON_AUTO_MAJOR_VERSION%" == "true" (
    for /f %%i in ('python "%SCRIPT_DIR%\tools\detect_cmake_major_version.py" "%WORKSPACE%\%VCS_DIRECTORY%\CMakeLists.txt"') do set PKG_MAJOR_VERSION=%%i
    set COLCON_PACKAGE=%COLCON_PACKAGE%!PKG_MAJOR_VERSION!
    echo "MAJOR_VERSION detected: !PKG_MAJOR_VERSION!"
