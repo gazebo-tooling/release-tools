7d6d57f6fb9c9449c65ca861f342a0690c9a1080
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-10-19 12:28:57 +0200

    Use right colcon param

--- jenkins-scripts/lib/windows_library.bat
+++ jenkins-scripts/lib/windows_library.bat
@@ -192,12 +192,12 @@ set COLCON_EXTRA_CMAKE_ARGS=%2
 colcon build --build-base "build"^
              --install-base "install"^
              --parallel-workers %MAKE_JOBS%^
-	     --no-notify^
              %EXTRA_COLCON_ARGS%^
              --cmake-args " -DCMAKE_BUILD_TYPE=%BUILD_TYPE%"^
                           " -DCMAKE_TOOLCHAIN_FILE=%VCPKG_CMAKE_TOOLCHAIN_FILE%"^
                           " -DVCPKG_TARGET_TRIPLET=%VCPKG_DEFAULT_TRIPLET%"^
                           %COLCON_EXTRA_CMAKE_ARGS%^
+             --event-handler desktop_notification-^
              --event-handler console_cohesion+ || type %HOMEPATH%/.colcon/latest & goto :error
 
 :: ##################################
