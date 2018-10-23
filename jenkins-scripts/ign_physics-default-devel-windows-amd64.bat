edb52d9893f425aca27d1641b490b2f407d04ae5
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-10-23 20:46:03 +0200

    Include ign_physics in COLCON_AUTO_MAJOR_VERSION

--- jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
@@ -6,7 +6,7 @@ set IGN_CLEAN_WORKSPACE=true
 
 set DEPEN_PKGS="eigen3"
 :: This needs to be migrated to DSL to get multi-major versions correctly
-set COLCON_PACKAGE=ignition-physics0
-set GAZEBODISTRO_FILE=ign-physics0.yaml
+set COLCON_PACKAGE=ignition-physics
+set COLCON_AUTO_MAJOR_VERSION=true
 
 call "%SCRIPT_DIR%/lib/colcon-default-devel-windows.bat"
