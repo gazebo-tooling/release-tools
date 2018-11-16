bf715426ebb8fb677ba8084247ba88d62684e534
Steve Peters <scpeters@openrobotics.org>
2018-11-15 16:38:42 -0800

    build ign-rendering0 from source for ign-gui0 docker builds

--- jenkins-scripts/docker/ign_gui-compilation.bash
+++ jenkins-scripts/docker/ign_gui-compilation.bash
@@ -31,6 +31,9 @@ fi
 
 if [[ ${IGN_GUI_MAJOR_VERSION} -ge 1 ]]; then
   export NEEDS_GZ11_SUPPORT=true
+else
+  # need to build ign-rendering0 for ign-gui0
+  export BUILD_IGN_RENDERING=true
 fi
 
 . "${SCRIPT_DIR}/lib/_gz11_hook.bash"
