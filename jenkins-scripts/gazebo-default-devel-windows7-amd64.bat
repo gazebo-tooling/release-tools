337f6da9cc4b71751c5b8646b1829b447f754fcb
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-12-26 18:15:12 +0100

    Missing VCS_DIRECTORY variable

--- jenkins-scripts/gazebo-default-devel-windows7-amd64.bat
+++ jenkins-scripts/gazebo-default-devel-windows7-amd64.bat
@@ -1,5 +1,6 @@
 set SCRIPT_DIR=%~dp0
 set PLATFORM_TO_BUILD=amd64
+set VCS_DIRECTORY=gazebo
 
 for /f %%i in ('python "%SCRIPT_DIR%\tools\detect_cmake_major_version.py" "%WORKSPACE%\%VCS_DIRECTORY%\CMakeLists.txt"') do set GAZEBO_MAJOR_VERSION=%%i
 
