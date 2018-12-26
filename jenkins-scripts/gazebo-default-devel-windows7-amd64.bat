511176aedb30fe7da9cfd6568dee187a15052021
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-12-26 18:17:16 +0100

    Fix var name

--- jenkins-scripts/gazebo-default-devel-windows7-amd64.bat
+++ jenkins-scripts/gazebo-default-devel-windows7-amd64.bat
@@ -4,7 +4,7 @@ set VCS_DIRECTORY=gazebo
 
 for /f %%i in ('python "%SCRIPT_DIR%\tools\detect_cmake_major_version.py" "%WORKSPACE%\%VCS_DIRECTORY%\CMakeLists.txt"') do set GAZEBO_MAJOR_VERSION=%%i
 
-if %IGN_TRANSPORT_MAJOR_VERSION% GEQ 9 (
+if %GAZEBO_MAJOR_VERSION% GEQ 9 (
   call "%SCRIPT_DIR%/lib/gazebo-base-windows.bat"
 ) else (
   call "%SCRIPT_DIR%/lib/gazebo8-base-windows.bat"
