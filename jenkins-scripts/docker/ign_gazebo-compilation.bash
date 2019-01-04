346b789c525d955073610587fe29eb6b467378c0
Steve Peters <scpeters@openrobotics.org>
2019-01-04 09:33:37 -0800

    ign-gazebo: build fuel-tools from source

--- jenkins-scripts/docker/ign_gazebo-compilation.bash
+++ jenkins-scripts/docker/ign_gazebo-compilation.bash
@@ -21,6 +21,8 @@ export BUILDING_PKG_DEPENDENCIES_VAR_NAME="IGN_GAZEBO_DEPENDENCIES"
 export NEEDS_GZ11_SUPPORT=true
 . "${SCRIPT_DIR}/lib/_gz11_hook.bash"
 
+export BUILD_IGN_FUEL_TOOLS=true
+
 if [[ $(date +%Y%m%d) -le 20190201 ]]; then
   ## need prerelease repo to get ignition-cmake2 for ign-gazebo
   export BUILDING_JOB_REPOSITORIES="${BUILDING_JOB_REPOSITORIES} prerelease"
