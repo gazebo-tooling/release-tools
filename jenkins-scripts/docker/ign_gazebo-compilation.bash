4710d71b3a12403537707a351c50e86621471e38
chapulina <burajiru.no.chapulina@gmail.com>
2019-03-27 16:28:34 -0700

    Ign-gazebo Blueprint dependencies from source

--- jenkins-scripts/docker/ign_gazebo-compilation.bash
+++ jenkins-scripts/docker/ign_gazebo-compilation.bash
@@ -23,6 +23,25 @@ if [[ $(date +%Y%m%d) -le 20190401 ]]; then
   export BUILDING_JOB_REPOSITORIES="${BUILDING_JOB_REPOSITORIES} prerelease"
 fi
 
+# Identify IGN_GAZEBO_MAJOR_VERSION to help with dependency resolution
+IGN_GAZEBO_MAJOR_VERSION=$(\
+  python ${SCRIPT_DIR}/../tools/detect_cmake_major_version.py \
+  ${WORKSPACE}/ign-sensors/CMakeLists.txt)
+
+# Check IGN_GAZEBO version is integer
+if ! [[ ${IGN_GAZEBO_MAJOR_VERSION} =~ ^-?[0-9]+$ ]]; then
+  echo "Error! IGN_GAZEBO_MAJOR_VERSION is not an integer, check the detection"
+  exit -1
+fi
+
+# Build dependencies from source while we don't have nightlies for Blueprint
+if [[ ${IGN_GAZEBO_MAJOR_VERSION} -ge 2 ]]; then
+  export BUILD_IGN_RENDERING=true
+  export IGN_RENDERING_BRANCH=default
+  export BUILD_IGN_SENSORS=true
+  export IGN_SENSORS_BRANCH=default
+fi
+
 export USE_GCC8=true
 
 export GPU_SUPPORT_NEEDED=true
