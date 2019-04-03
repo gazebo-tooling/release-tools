4e5bd82b55989e8d7792f758f2812d5553fca6bb
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-04-03 14:13:03 +0200

    Missing gflagss package

--- jenkins-scripts/ign_launch-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_launch-default-devel-windows-amd64.bat
@@ -4,7 +4,7 @@ set VCS_DIRECTORY=ign-launch
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
-set DEPEN_PKGS="curl jsoncpp qt5 tinyxml2 libyaml libzip"
+set DEPEN_PKGS="curl jsoncpp qt5 tinyxml2 gflags libyaml libzip"
 set COLCON_PACKAGE=ignition-launch
 set COLCON_AUTO_MAJOR_VERSION=true
 
