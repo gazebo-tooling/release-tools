04ae0ee10a7ffbf1caa84a64b5a3d785b256e425
Steve Peters <scpeters@openrobotics.org>
2019-05-09 16:04:10 -0700

    ign-transport: keep using prereleases but no nightlies

--- jenkins-scripts/docker/ign_transport-compilation.bash
+++ jenkins-scripts/docker/ign_transport-compilation.bash
@@ -35,7 +35,7 @@ fi
 
 if [[ ${IGN_TRANSPORT_MAJOR_VERSION} -ge 7 ]]; then
   if [[ $(date +%Y%m%d) -le 20190514 ]]; then
-    export BUILDING_JOB_REPOSITORIES="${BUILDING_JOB_REPOSITORIES} prerelease nightly"
+    export BUILDING_JOB_REPOSITORIES="${BUILDING_JOB_REPOSITORIES} prerelease"
   fi
 fi
 
