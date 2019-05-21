b712b613ad44237b0341c8873751a0e15a030ebf
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-05-21 17:30:43 +0200

    Fix sdf file name

--- jenkins-scripts/docker/lib/_gazebo_utils.sh
+++ jenkins-scripts/docker/lib/_gazebo_utils.sh
@@ -19,10 +19,9 @@ tar -xf /tmp/default.tar.gz -C ~/.gazebo/models --strip 1
 rm /tmp/default.tar.gz"""
 
 IGN_GAZEBO_RUNTIME_TEST="""
-${GAZEBO_MODEL_INSTALLATION}
 echo '# BEGIN SECTION: test the script'
 TEST_START=\`date +%s\`
-timeout --preserve-status 180 ign gazebo -v -r camera.sdf || true
+timeout --preserve-status 180 ign gazebo -v -r camera_sensor.sdf || true
 TEST_END=\`date +%s\`
 DIFF=\`echo \"\$TEST_END - \$TEST_START\" | bc\`
 
