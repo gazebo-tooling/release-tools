ca4bd55483facb77ebb826222eddfa0443266bd7
Steve Peters <scpeters@openrobotics.org>
2019-05-21 12:54:40 -0700

    ignition-abichecker.bash: set USE_GCC8 according to package and version

--- jenkins-scripts/docker/ignition-abichecker.bash
+++ jenkins-scripts/docker/ignition-abichecker.bash
@@ -27,9 +27,25 @@ ABI_JOB_PKG_DEPENDENCIES_VAR_NAME=${IGN_NAME_PREFIX}_DEPENDENCIES
 DART_FROM_PKGS_VAR_NAME=${IGN_NAME_PREFIX}_DART_FROM_PKGS
 
 # Identify IGN_MSGS_MAJOR_VERSION to help with dependency resolution
-export ${IGN_NAME_PREFIX}_MAJOR_VERSION=$(\
+export IGN_NAME_PREFIX_MAJOR_VERSION=$(\
   python ${SCRIPT_DIR}/../tools/detect_cmake_major_version.py \
   ${WORKSPACE}/${ABI_JOB_SOFTWARE_NAME}/CMakeLists.txt)
+export ${IGN_NAME_PREFIX}_MAJOR_VERSION=${IGN_NAME_PREFIX_MAJOR_VERSION}
+
+# check if USE_GCC8 should be set
+if [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-gazebo" ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-physics" ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-sensors" ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-common"  ]] && [[ ${IGN_NAME_PREFIX_MAJOR_VERSION} -ge 3 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-gui"     ]] && [[ ${IGN_NAME_PREFIX_MAJOR_VERSION} -ge 1 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-math"    ]] && [[ ${IGN_NAME_PREFIX_MAJOR_VERSION} -ge 6 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-msgs"    ]] && [[ ${IGN_NAME_PREFIX_MAJOR_VERSION} -ge 3 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-plugin"  ]] && [[ ${IGN_NAME_PREFIX_MAJOR_VERSION} -ge 1 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-rendering" ]] && [[ ${IGN_NAME_PREFIX_MAJOR_VERSION} -ge 1 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-transport" ]] && [[ ${IGN_NAME_PREFIX_MAJOR_VERSION} -ge 6 ]]
+then
+  export USE_GCC8=true
+fi
 
 export ABI_JOB_REPOS="stable"
 
