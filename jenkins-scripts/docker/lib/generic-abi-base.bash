921a818979dbeeb4447fe25771167e75212e17f7
Steve Peters <scpeters@openrobotics.org>
2019-05-24 16:50:25 -0700

    use c++17 if USE_GCC8 is true

--- jenkins-scripts/docker/lib/generic-abi-base.bash
+++ jenkins-scripts/docker/lib/generic-abi-base.bash
@@ -32,6 +32,11 @@ if [[ -n "${DART_FROM_PKGS_VAR_NAME}" ]]; then
   eval DART_FROM_PKGS="\$${DART_FROM_PKGS_VAR_NAME}"
 fi
 
+ABI_CXX_STANDARD=c++11
+if [[ "${USE_GCC8}" == "true" ]]; then
+  ABI_CXX_STANDARD=c++17
+fi
+
 cat > build.sh << DELIM
 #!/bin/bash
 
@@ -129,7 +134,7 @@ cat >> pkg.xml << CURRENT_DELIM_LIBS
  </libs>
 
  <gcc_options>
-     -std=c++11
+     -std=${ABI_CXX_STANDARD}
  </gcc_options>
 CURRENT_DELIM_LIBS
 
@@ -157,7 +162,7 @@ cat >> devel.xml << DEVEL_DELIM_LIBS
  </libs>
 
  <gcc_options>
-     -std=c++11
+     -std=${ABI_CXX_STANDARD}
  </gcc_options>
 DEVEL_DELIM_LIBS
 echo '# END SECTION'
