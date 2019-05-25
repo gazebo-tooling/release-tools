fa8b54ff8201da154a0d47269e759b79e9c04f6a
Steve Peters <scpeters@openrobotics.org>
2019-05-24 17:15:26 -0700

    duplicate command rather than trying to use a variable

--- jenkins-scripts/docker/lib/generic-abi-base.bash
+++ jenkins-scripts/docker/lib/generic-abi-base.bash
@@ -180,14 +180,12 @@ REPORTS_DIR=$WORKSPACE/reports/
 rm -fr \${REPORTS_DIR} && mkdir -p \${REPORTS_DIR}
 rm -fr compat_reports/
 # run report tool
-REPORT_CMD="abi-compliance-checker \
-  -lang C++ -lib ${ABI_JOB_SOFTWARE_NAME} -old pkg.xml -new devel.xml"
-${REPORT_CMD} || true
+abi-compliance-checker -lang C++ -lib ${ABI_JOB_SOFTWARE_NAME} -old pkg.xml -new devel.xml || true
 
 # if report was not generated, run again with -quick
 if !ls "compat_reports/${ABI_JOB_SOFTWARE_NAME}"
 then
-  ${REPORT_CMD} -quick || true
+  abi-compliance-checker -lang C++ -lib ${ABI_JOB_SOFTWARE_NAME} -old pkg.xml -new devel.xml -quick || true
 fi
 
 # copy method version independant ( cp ... /*/ ... was not working)
