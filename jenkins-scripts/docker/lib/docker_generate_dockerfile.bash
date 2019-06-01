113421d649ad512c5d8af01bc2db1d42bab55152
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-06-02 00:19:51 +0200

    Use new ROS key

--- jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
+++ jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
@@ -183,7 +183,7 @@ cat >> Dockerfile << DELIM_ROS_REPO
 # Note that ROS uses ubuntu hardcoded in the paths of repositories
 RUN echo "deb http://packages.ros.org/${ROS_REPO_NAME}/ubuntu ${DISTRO} main" > \\
                                                 /etc/apt/sources.list.d/ros.list
-RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 421C365BD9FF1F717815A3895523BAEEB01FA116
+RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 DELIM_ROS_REPO
 # Need ros stable for the cases of ros-shadow-fixed or ros-shadow
 if [[ ${ROS_REPO_NAME} != "ros" ]]; then
