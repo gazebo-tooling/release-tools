2498beec497a312c888112ce09d03f1eddd0f0f9
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-06-26 21:17:19 +0200

    Need python3

--- jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
+++ jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
@@ -152,7 +152,7 @@ fi
 # dirmngr from Yaketty on needed by apt-key
 cat >> Dockerfile << DELIM_DOCKER_DIRMNGR
 RUN apt-get update && \\
-    apt-get install -y dirmngr git
+    apt-get install -y dirmngr git python3
 DELIM_DOCKER_DIRMNGR
 
 if [[ -n ${GZDEV_PROJECT_NAME} ]]; then
