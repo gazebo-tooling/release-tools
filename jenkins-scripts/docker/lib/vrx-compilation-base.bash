47a9c4b48819ed5017fa3bf07f1e457ad3993d5d
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-07-22 13:16:52 +0200

    Miss wget

--- jenkins-scripts/docker/lib/vrx-compilation-base.bash
+++ jenkins-scripts/docker/lib/vrx-compilation-base.bash
@@ -38,7 +38,7 @@ ${VRX_SMOKE_TEST}
 # Generate the first part of the build.sh file for ROS
 . ${SCRIPT_DIR}/lib/_ros_setup_buildsh.bash "vrx"
 
-DEPENDENCY_PKGS="git ruby libeigen3-dev pkg-config python ros-${ROS_DISTRO}-gazebo-plugins ros-${ROS_DISTRO}-gazebo-ros ros-${ROS_DISTRO}-hector-gazebo-plugins ros-${ROS_DISTRO}-joy ros-${ROS_DISTRO}-joy-teleop ros-${ROS_DISTRO}-key-teleop ros-${ROS_DISTRO}-robot-localization ros-${ROS_DISTRO}-robot-state-publisher ros-${ROS_DISTRO}-rviz ros-${ROS_DISTRO}-ros-base ros-${ROS_DISTRO}-teleop-tools ros-${ROS_DISTRO}-teleop-twist-keyboard ros-${ROS_DISTRO}-velodyne-simulator ros-${ROS_DISTRO}-xacro ros-${ROS_DISTRO}-rqt ros-${ROS_DISTRO}-rqt-common-plugins protobuf-compiler"
+DEPENDENCY_PKGS="wget git ruby libeigen3-dev pkg-config python ros-${ROS_DISTRO}-gazebo-plugins ros-${ROS_DISTRO}-gazebo-ros ros-${ROS_DISTRO}-hector-gazebo-plugins ros-${ROS_DISTRO}-joy ros-${ROS_DISTRO}-joy-teleop ros-${ROS_DISTRO}-key-teleop ros-${ROS_DISTRO}-robot-localization ros-${ROS_DISTRO}-robot-state-publisher ros-${ROS_DISTRO}-rviz ros-${ROS_DISTRO}-ros-base ros-${ROS_DISTRO}-teleop-tools ros-${ROS_DISTRO}-teleop-twist-keyboard ros-${ROS_DISTRO}-velodyne-simulator ros-${ROS_DISTRO}-xacro ros-${ROS_DISTRO}-rqt ros-${ROS_DISTRO}-rqt-common-plugins protobuf-compiler"
 
 # ROS packages come from the mirror in the own vrx repository
 USE_ROS_REPO=true
