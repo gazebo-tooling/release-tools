9625d42ff3dcb6499452d0b2d438e6f587e4629b
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-07-26 22:25:20 +0200

    Default branch to repository

--- jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
+++ jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
@@ -30,7 +30,7 @@ fi
 [[ -z ${USE_GCC8} ]] && USE_GCC8=false
 
 GZDEV_DIR=${WORKSPACE}/gzdev
-GZDEV_BRANCH=repository
+GZDEV_BRANCH=${GZDEV_BRANCH:-repository}
 
 dockerfile_install_gzdev_repos()
 {
@@ -41,13 +41,11 @@ DELIM_OSRF_REPO_GIT
 if [[ -n ${GZDEV_PROJECT_NAME} ]]; then
 cat >> Dockerfile << DELIM_OSRF_REPO_GZDEV
 RUN ${GZDEV_DIR}/gzdev.py repository enable --project=${GZDEV_PROJECT_NAME}
-RUN cat /etc/apt/sources.list.d/_gzdev_osrf_*.list
 DELIM_OSRF_REPO_GZDEV
 else
 for repo in ${OSRF_REPOS_TO_USE}; do
 cat >> Dockerfile << DELIM_OSRF_REPO
 RUN ${GZDEV_DIR}/gzdev.py repository enable osrf ${repo}
-RUN cat /etc/apt/sources.list.d/_gzdev_osrf_*.list
 DELIM_OSRF_REPO
 done
 fi
