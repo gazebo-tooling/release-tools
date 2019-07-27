086df36997bdcb80c6623fad210edbf0fe1fcba6
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-07-27 21:57:02 +0200

    Ignore empty quotes

--- jenkins-scripts/docker/lib/_subt_utils.sh
+++ jenkins-scripts/docker/lib/_subt_utils.sh
@@ -6,7 +6,7 @@ fi
 
 TEST_TIMEOUT=\${TEST_TIMEOUT:-180}
 TEST_START=\$(date +%s)
-timeout --preserve-status \$TEST_TIMEOUT ign launch -v 4 competition.ign 2> >(grep -v '[QT]')
+timeout --preserve-status \$TEST_TIMEOUT ign launch -v 4 competition.ign 2> >(grep -v -e '[QT]' -e '\"\"')
 TEST_END=\$(date +%s)
 DIFF=\$(expr \$TEST_END - \$TEST_START)
 
