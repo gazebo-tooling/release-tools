13ea7d79c85963fd75f9876a7ad6751039fab562
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-07-27 20:43:25 +0200

    Ignore QT warning messages

--- jenkins-scripts/docker/lib/_subt_utils.sh
+++ jenkins-scripts/docker/lib/_subt_utils.sh
@@ -1,7 +1,7 @@
 SUBT_COMPETITION_TEST="""
 TEST_TIMEOUT=\${TEST_TIMEOUT:-180}
 TEST_START=\$(date +%s)
-timeout --preserve-status \$TEST_TIMEOUT ign launch -v 4 competition.ign
+timeout --preserve-status \$TEST_TIMEOUT ign launch -v 4 competition.ign 2> >(grep -v '[QT]')
 TEST_END=\$(date +%s)
 DIFF=\$(expr \$TEST_END - \$TEST_START)
 
