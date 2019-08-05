8656adfe114b64ce37d478da4f7b9f924c1cfad0
Steve Peters <scpeters@openrobotics.org>
2019-08-05 14:52:39 -0700

    set MAJOR_VERSION variables

--- jenkins-scripts/docker/ign_gazebo-compilation.bash
+++ jenkins-scripts/docker/ign_gazebo-compilation.bash
@@ -30,10 +30,13 @@ fi
 
 if [[ ${IGN_GUI_MAJOR_VERSION} -eq 3 ]]; then
   export BUILD_IGN_RENDERING=true
+  export IGN_RENDERING_MAJOR_VERSION=3
   export IGN_RENDERING_BRANCH=default
   export BUILD_IGN_SENSORS=true
+  export IGN_SENSORS_MAJOR_VERSION=3
   export IGN_SENSORS_BRANCH=default
   export BUILD_IGN_GUI=true
+  export IGN_GUI_MAJOR_VERSION=3
   export IGN_GUI_BRANCH=default
 fi
 
