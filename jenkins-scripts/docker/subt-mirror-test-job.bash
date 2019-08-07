9a4520a656162281bd74c13f3ed334500da16276
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-08-07 12:35:01 +0200

    Debug messages

--- jenkins-scripts/docker/subt-mirror-test-job.bash
+++ jenkins-scripts/docker/subt-mirror-test-job.bash
@@ -13,7 +13,6 @@ export TIMEOUT=${TIMEOUT:-180}
 . ${SCRIPT_DIR}/lib/_install_nvidia_docker.sh
 
 export ROS_SETUP_POSTINSTALL_HOOK="""
-echo '# BEGIN SECTION: testing the cloudsim mirror installation'
 
 ${INSTALL_NVIDIA_DOCKER2}
 
@@ -24,18 +23,23 @@ sed -i -e s:--uid\\ \\$\\{user_id\\}:: cloudsim_sim/Dockerfile
 sed -i -e s:--uid\\ \\$\\{user_id\\}:: cloudsim_bridge/Dockerfile
 
 chmod +x build.bash run.bash join.bash
-
-# sim and client build
+echo '# BEGIN SECTION: build cloudsim_sim'
 ./build.bash cloudsim_sim --no-cache
+echo '# END SECTION'
+echo '# BEGIN SECTION: build cloudsim_bridge'
 ./build.bash cloudsim_bridge --no-cache
+echo '# END SECTION'
 
+echo '# BEGIN SECTION: run cloudsim_sim'
 ./run.bash cloudsim_sim cloudsim_sim.ign robotName1:=X1 robotConfig1:=X1_SENSOR_CONFIG1 robotName2:=X2 robotConfig2:=X2_SENSOR_CONFIG2
+echo '# END SECTION'
+echo '# BEGIN SECTION: run cloudsim_bridge'
 ./run.bash cloudsim_bridge cloudsim_bridge.ign robotName1:=X1 robotConfig1:=X1_SENSOR_CONFIG1 robotName2:=X2 robotConfig2:=X2_SENSOR_CONFIG2
+echo '# END SECTION'
 roslaunch subt_example example_robot.launch name:=X1
 roslaunch subt_example example_robot.launch name:=X2
 rostopic pub /X2/comm std_msgs/String 'X1'
 rostopic pub /X1/cmd_vel geometry_msgs/Twist '{linear:  {x: 0.1, y: 0.0, z: 0.0}}'
-echo '# END SECTION'
 """
 
 export DEPENDENCY_PKGS="${DEPENDENCY_PKGS} software-properties-common xauth x11-xserver-utils"
