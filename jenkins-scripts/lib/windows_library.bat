a40b1414d407cd61f5e9e7d3a6585dac718bbad0
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-11-19 18:12:34 +0100

    Improve debug

--- jenkins-scripts/lib/windows_library.bat
+++ jenkins-scripts/lib/windows_library.bat
@@ -40,12 +40,12 @@ set MSVC_ON_WIN32_C=C:\Program Files\Microsoft Visual Studio\2017\Community\VC\A
 set LIB_DIR="%~dp0"
 call %LIB_DIR%\windows_env_vars.bat
 
+echo "Check _VCPKG_PATH_SET"
 if not defined _VCPKG_PATH_SET (
-  echo PATH 
   set PATH=%PATH%;%VCPKG_DIR%\installed\%VCPKG_DEFAULT_TRIPLET%\bin
-  echo PATH is ok
+  echo "PATH IS OK"
   set _VCPKG_PATH_SET=1
-  echo PATH is ok2
+  echo "SET IS OK"
 )
 
 IF exist "%MSVC_ON_WIN64_E%" (
