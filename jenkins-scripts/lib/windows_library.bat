31f66477a899f9421f513d31620730c231c247ec
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-11-21 12:13:41 +0100

    Do not use underscore

--- jenkins-scripts/lib/windows_library.bat
+++ jenkins-scripts/lib/windows_library.bat
@@ -40,9 +40,9 @@ set MSVC_ON_WIN32_C=C:\Program Files\Microsoft Visual Studio\2017\Community\VC\A
 set LIB_DIR="%~dp0"
 call %LIB_DIR%\windows_env_vars.bat
 
-if "%_VCPKG_PATH_SET%"=="" (
+if "%VCPKG_PATH_SET%"=="" (
   set PATH=%PATH%;%VCPKG_DIR%\installed\%VCPKG_DEFAULT_TRIPLET%\bin
-  set _VCPKG_PATH_SET=1
+  set VCPKG_PATH_SET=1
 )
 
 IF exist "%MSVC_ON_WIN64_E%" (
