64b1027d294cd660dd8a73fdbad65dd7ed624818
Nate Koenig <natekoenig@gmail.com>
2019-12-02 13:44:00 -0800

    Add libtinyxml2-dev to IGN_MSGS dependencies

--- jenkins-scripts/lib/dependencies_archive.sh
+++ jenkins-scripts/lib/dependencies_archive.sh
@@ -538,6 +538,7 @@ IGN_MSGS_DEPENDENCIES="libignition-tools-dev \\
                        libprotobuf-dev       \\
                        libprotoc-dev         \\
                        protobuf-compiler     \\
+                       libtinyxml2-dev       \\
                        ruby                  \\
                        ruby-dev"
 
