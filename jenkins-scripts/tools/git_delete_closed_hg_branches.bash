ad355dd916ddf6409c6d48a7199172872c882ed2
Steve Peters <scpeters@openrobotics.org>
2020-01-27 21:46:56 -0800

    git_delete_closed_hg_branches.bash: check for each closed branch on git remote and push --delete if it is found there

--- jenkins-scripts/tools/git_delete_closed_hg_branches.bash
+++ jenkins-scripts/tools/git_delete_closed_hg_branches.bash
@@ -7,6 +7,14 @@ hg clone $HG_REPO $HG_TMP
 echo =========== Done Cloning HG Repository
 echo
 echo =========== Identify Closed HG Branches
-hg -R $HG_TMP log -r "closed()" --template '{branch}\n' | sort
+CLOSED_BRANCHES=$(\
+  hg -R $HG_TMP log -r "closed()" --template '{branch}\n' | sort)
+for b in $CLOSED_BRANCHES
+do
+  git show remotes/origin/$b &> /dev/null || \
+    { echo remote git branch not found: $b && continue; }
+  echo remote git branch found: $b
+  git push --delete origin $b
+done
 
 
