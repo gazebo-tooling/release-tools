61688d2b5122f0b6ccc926262b9f076f5a299dc7
chapulina <burajiru.no.chapulina@gmail.com>
2020-02-27 15:23:49 -0800

    ign-launch3: build dome deps from src

--- jenkins-scripts/docker/ign_launch-compilation.bash
+++ jenkins-scripts/docker/ign_launch-compilation.bash
@@ -28,6 +28,28 @@ if ! [[ ${IGN_LAUNCH_MAJOR_VERSION} =~ ^-?[0-9]+$ ]]; then
   exit -1
 fi
 
+if [[ ${IGN_LAUNCH_MAJOR_VERSION} -ge 3 ]]; then
+  export BUILD_IGN_COMMON=true
+  export IGN_COMMON_MAJOR_VERSION=3
+  export IGN_COMMON_BRANCH=ign-common3
+
+  export BUILD_IGN_SENSORS=true
+  export IGN_SENSORS_MAJOR_VERSION=4
+  export IGN_SENSORS_BRANCH=default
+
+  export BUILD_IGN_GUI=true
+  export IGN_GUI_MAJOR_VERSION=4
+  export IGN_GUI_BRANCH=default
+
+  export BUILD_IGN_RENDERING=true
+  export IGN_RENDERING_MAJOR_VERSION=4
+  export IGN_RENDERING_BRANCH=default
+
+  export BUILD_IGN_GAZEBO=true
+  export IGN_GAZEBO_MAJOR_VERSION=4
+  export IGN_GAZEBO_BRANCH=default
+fi
+
 export USE_GCC8=true
 
 export GZDEV_PROJECT_NAME="ignition-launch${IGN_LAUNCH_MAJOR_VERSION}"
