385b1df82c02a5a060827766999e17dd52552a9f
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-04-17 11:50:55 +0200

    Remove set_status_script

--- jenkins-scripts/dsl/_configs_/OSRFCIWorkFlowMultiAny.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFCIWorkFlowMultiAny.groovy
@@ -12,39 +12,6 @@ import javaposse.jobdsl.dsl.Job
 
 class OSRFCIWorkFlowMultiAny
 {
-  static String script_code_build_any(String job_name)
-  {
-    return """\
-      String result_URL = env.JENKINS_URL + '/job/${job_name}/'
-      jenkins_pipeline_job_result['$job_name']  = 'SUCCESS'
-      String bitbucket_publish_job_result = 'ok'
-
-      compilation_job = null
-
-      node("lightweight-linux")
-      {
-        compilation_job = build job: '${job_name}',
-            propagate: false, wait: true,
-            parameters:
-             [[\$class: 'StringParameterValue',  name: 'RTOOLS_BRANCH',   value: "\$RTOOLS_BRANCH"],
-              [\$class: 'BooleanParameterValue', name: 'NO_MAILS',        value: false],
-              [\$class: 'StringParameterValue',  name: 'SRC_REPO',        value: "\$SRC_REPO"],
-              [\$class: 'StringParameterValue',  name: 'SRC_BRANCH',      value: "\$SRC_BRANCH"],
-              [\$class: 'StringParameterValue',  name: 'JOB_DESCRIPTION', value: "\$JOB_DESCRIPTION"],
-              [\$class: 'StringParameterValue',  name: 'DEST_BRANCH',     value: "\$DEST_BRANCH"]]
-      }
-
-      result_URL = result_URL + compilation_job.getNumber()
-
-      if (compilation_job.getResult() != 'SUCCESS')
-      {
-        // any non success is a failure in bitbucket status
-        jenkins_pipeline_job_result['$job_name'] = compilation_job.getResult()
-        bitbucket_publish_job_result = 'failed'
-      }
-    """
-  }
-
   static void create(Job job, String build_any_job_name)
   {
     OSRFCIWorkFlowMultiAny.create(job, [build_any_job_name])
@@ -93,14 +60,6 @@ class OSRFCIWorkFlowMultiAny
       }
 
       build_jobs_with_status = parallel_init +
-      OSRFCIWorkFlow.script_code_set_code(build_status : '"inprogress"',
-                                           build_desc   : '"Testing in progress"',
-                                           build_name   : "'${build_any_job_name}'") + // different order of quotes!
-      OSRFCIWorkFlowMultiAny.script_code_build_any(build_any_job_name) +
-      OSRFCIWorkFlow.script_code_set_code(build_status : '"$bitbucket_publish_job_result"',
-                                           build_desc  : '"Testing is finished"',
-                                           build_name  : "'${build_any_job_name}'", // different order of quotes!
-                                           build_url   : '"$result_URL"') +
       parallel_end
     }
 
