1a78c1b3135204ad442e6bfe97e098bfdc28a537
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-05-01 20:25:50 +0200

    Use string for whiteListTargetBranches

--- jenkins-scripts/dsl/_configs_/GenericAnyJobGitHub.groovy
+++ jenkins-scripts/dsl/_configs_/GenericAnyJobGitHub.groovy
@@ -56,7 +56,7 @@ class GenericAnyJobGitHub
             allowMembersOfWhitelistedOrgsAsAdmin()
             // Only will be triggered in supported_branches
             if (supported_branches) {
-              whiteListTargetBranches(supported_branches)
+              whiteListTargetBranches(supported_branches.join(" "))
             }
             permitAll(true)
             extensions {
