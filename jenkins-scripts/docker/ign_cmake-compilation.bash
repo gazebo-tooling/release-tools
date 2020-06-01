e90219400dce06d829ef06e2d93f4b4c20a46f2e
Michael Carroll <michael@openrobotics.org>
2020-06-01 12:19:40 -0500

    Enable long-running ign-cmake tests on Linux
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- jenkins-scripts/docker/ign_cmake-compilation.bash
+++ jenkins-scripts/docker/ign_cmake-compilation.bash
@@ -17,6 +17,9 @@ fi
 export BUILDING_SOFTWARE_DIRECTORY="ign-cmake"
 export BUILDING_DEPENDENCIES="pkg-config"
 
+# Enable long-running ign-cmake tests in CI.
+export BUILDING_EXTRA_CMAKE_PARAMS="-DBUILDSYSTEM_TESTING=True"
+
 # Identify IGN_CMAKE_MAJOR_VERSION to help with dependency resolution
 IGN_CMAKE_MAJOR_VERSION=$(\
   python ${SCRIPT_DIR}/../tools/detect_cmake_major_version.py \
