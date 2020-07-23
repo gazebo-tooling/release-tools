a7bfbeda779887e28b2d0fc9cde705ecbad21757
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-07-23 21:46:12 +0200

    Avoid lintian in debbuilds on ARM (hangs because syscall not implemented in qemu) (#245)
    * No lintian in gbp

--- jenkins-scripts/docker/lib/debian-git-repo-base.bash
+++ jenkins-scripts/docker/lib/debian-git-repo-base.bash
@@ -52,6 +52,11 @@ ${DEBIAN_GIT_PREINSTALL_HOOK}
 echo '# END SECTION'
 fi
 
+if [[ ${DISTRO} == 'focal' && ${ARCH} == 'arm64' ]]; then
+    # Did not find the way of avoid lintian in gbp call
+    ln -sf /bin/true /usr/bin/lintian
+fi
+
 echo '# BEGIN SECTION: install build dependencies'
 cat debian/changelog
 mk-build-deps -r -i debian/control --tool 'apt-get --yes -o Debug::pkgProblemResolver=yes -o  Debug::BuildDeps=yes'
