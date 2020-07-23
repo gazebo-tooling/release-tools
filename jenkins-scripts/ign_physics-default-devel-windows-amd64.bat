956024953aa1f792765cd5372cf9c5e828797097
Steve Peters <scpeters@openrobotics.org>
2020-07-22 23:01:39 -0700

    add boost

--- jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
@@ -4,7 +4,9 @@ set VCS_DIRECTORY=ign-physics
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
-set DEPEN_PKGS="bullet3 eigen3 tinyxml2 urdfdom"
+set DART_BOOST_PKGS="boost-algorithm boost-filesystem boost-lexical-cast boost-math boost-system"
+set DART_DEPEN_PKGS="%DART_BOOST_PKGS% assimp bullet3 ccd fcl tinyxml2 urdfdom"
+set DEPEN_PKGS="eigen3 tinyxml2 %DART_DEPEN_PKGS%"
 :: This needs to be migrated to DSL to get multi-major versions correctly
 set COLCON_PACKAGE=ignition-physics
 set COLCON_AUTO_MAJOR_VERSION=true
