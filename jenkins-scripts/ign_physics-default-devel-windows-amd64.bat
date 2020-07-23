eeb451a048bf97895fd784d3eba3b480f3016efd
Steve Peters <scpeters@openrobotics.org>
2020-07-23 00:21:40 -0700

    fix quotes

--- jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
@@ -5,8 +5,8 @@ set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
 set DART_BOOST_PKGS="boost-algorithm boost-filesystem boost-lexical-cast boost-math boost-system"
-set DART_DEPEN_PKGS="%DART_BOOST_PKGS% assimp bullet3 ccd fcl tinyxml2 urdfdom"
-set DEPEN_PKGS="eigen3 tinyxml2 %DART_DEPEN_PKGS%"
+set DART_DEPEN_PKGS=%DART_BOOST_PKGS% "assimp bullet3 ccd fcl tinyxml2 urdfdom"
+set DEPEN_PKGS=%DART_DEPEN_PKGS% "eigen3 tinyxml2"
 :: This needs to be migrated to DSL to get multi-major versions correctly
 set COLCON_PACKAGE=ignition-physics
 set COLCON_AUTO_MAJOR_VERSION=true
