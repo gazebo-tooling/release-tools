4c4d9b77c66b281f6b9ed3653721d90ded156f15
Steve Peters <scpeters@openrobotics.org>
2020-07-22 20:57:49 -0700

    Add tinyxml2 dependency for windows builds
    sdformat10 now uses tinyxml2, so add it as a vcpkg
    dependency for ign-physics, sensors, and gazebo builds.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- jenkins-scripts/ign_sensors-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_sensors-default-devel-windows-amd64.bat
@@ -5,7 +5,7 @@ set VCS_DIRECTORY=ign-sensors
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
-set DEPEN_PKGS="protobuf"
+set DEPEN_PKGS="protobuf tinyxml2"
 :: This needs to be migrated to DSL to get multi-major versions correctly
 set COLCON_PACKAGE=ignition-sensors
 set COLCON_AUTO_MAJOR_VERSION=true
