18f9201ba5e63e4eafc5d62aa0fbe397720ff399
Steve Peters <scpeters@openrobotics.org>
2020-10-05 12:07:40 -0700

    use dartsim vcpkg package

--- jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
@@ -4,9 +4,7 @@ set VCS_DIRECTORY=ign-physics
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
-set DART_BOOST_PKGS="boost-algorithm boost-filesystem boost-lexical-cast boost-math boost-system"
-set DART_DEPEN_PKGS=%DART_BOOST_PKGS% "assimp bullet3 ccd fcl tinyxml2 urdfdom"
-set DEPEN_PKGS=%DART_DEPEN_PKGS% "eigen3 tinyxml2"
+set DEPEN_PKGS="dartsim eigen3 tinyxml2"
 :: This needs to be migrated to DSL to get multi-major versions correctly
 set COLCON_PACKAGE=ignition-physics
 set COLCON_AUTO_MAJOR_VERSION=true
