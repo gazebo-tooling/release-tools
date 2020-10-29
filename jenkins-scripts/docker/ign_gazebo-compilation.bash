d050b25b6d738e4874fde8feb3f6153e2652954e
Louise Poubel <louise@openrobotics.org>
2020-10-28 19:42:41 -0700

    Bump in edifice: ign-rendering5
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- jenkins-scripts/docker/ign_gazebo-compilation.bash
+++ jenkins-scripts/docker/ign_gazebo-compilation.bash
@@ -31,6 +31,19 @@ fi
 export NEED_C17_COMPILER=true
 export GPU_SUPPORT_NEEDED=true
 
+if [[ ${IGN_GAZEBO_MAJOR_VERSION} -eq 5 ]]; then
+  export BUILD_IGN_RENDERING=true
+  export IGN_RENDERING_MAJOR_VERSION=5
+  export IGN_RENDERING_BRANCH=main
+
+  export BUILD_IGN_GUI=true
+  export IGN_GUI_MAJOR_VERSION=5
+  export IGN_GUI_BRANCH=main
+
+  export BUILD_IGN_SENSORS=true
+  export IGN_SENSORS_MAJOR_VERSION=5
+  export IGN_SENSORS_BRANCH=main
+fi
 export GZDEV_PROJECT_NAME="ignition-gazebo${IGN_GAZEBO_MAJOR_VERSION}"
 
 . ${SCRIPT_DIR}/lib/generic-building-base.bash
