e3a9af930029ae047c1d41eed104465204bd2bef
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-11-05 00:35:19 +0100

    Inject PYTHONPATH
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/docker/reprepro_updater.bash
+++ jenkins-scripts/docker/reprepro_updater.bash
@@ -44,7 +44,7 @@ export GNUPGHOME=/var/lib/jenkins/.gnupg
 
 echo '# BEGIN SECTION: run reprepro'
 cd "${REPREPRO_REPO_PATH}/scripts"
-sudo -E bash -c "python3 import_upstream.py ${REPREPRO_PARAMS} \
+sudo -E bash -c "PYTHONPATH=${REPREPRO_REPO_PATH}/src python3 import_upstream.py ${REPREPRO_PARAMS} \
   ${UPLOAD_TO_REPO:-:_} \
   ${REPREPRO_REPO_PATH}/config/packages.osrfoundation.org/${REPREPRO_IMPORT_YAML_FILE}"
 echo '# END SECTION'
