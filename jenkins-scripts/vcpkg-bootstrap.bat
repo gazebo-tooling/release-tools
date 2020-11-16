8161cfb3d576e0fecffc4debe7cfa20582efa332
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-11-16 17:39:16 +0100

    Do not use -C on clone
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/vcpkg-bootstrap.bat
+++ jenkins-scripts/vcpkg-bootstrap.bat
@@ -13,7 +13,7 @@ echo # BEGIN SECTION: Bootstrap vcpkg
 rd /s /q %VCPKG_DIR%
 
 :: Clone and use the new version of vcpkg
-git clone https://github.com/microsoft/vcpkg -C %VCPKG_DIR% || goto :error
+git clone https://github.com/microsoft/vcpkg %VCPKG_DIR% || goto :error
 echo "Using SNAPSHOT: %VCPKG_SNAPSHOT%"
 git checkout %VCPKG_SNAPSHOT% -C %VCPKG_DIR% || goto :error
 :: Bootstrap vcpkg.exe
