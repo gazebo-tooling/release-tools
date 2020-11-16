6a274c12dcd6526d6197cad6c92545b3539dd521
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-11-16 17:11:39 +0100

    Rename

---
+++ jenkins-scripts/vcpkg-bootstrap.bat
@@ -0,0 +1,20 @@
+set SCRIPT_DIR=%~dp0
+
+set win_lib=%SCRIPT_DIR%\lib\windows_library.bat
+
+:: Call vcvarsall and all the friends
+echo # BEGIN SECTION: configure the MSVC compiler
+call %win_lib% :configure_msvc2019_compiler
+echo # END SECTION
+
+echo # BEGIN SECTION: Bootstrap vcpkg
+:: Remove previous vcpkg installation
+rd /s /q %VCPKG_DIR%
+
+:: Clone and use the new version of vcpkg
+git clone https://github.com/microsoft/vcpkg -C %VCPKG_DIR% || goto :error
+echo "Using SNAPSHOT: %VCPKG_SNAPSHOT%"
+git checkout %VCPKG_SNAPSHOT% -C %VCPKG_DIR% || goto :error
+:: Bootstrap vcpkg.exe
+%VCPKG_DIR%\bootstrap-vcpkg.bat || goto :error
+echo # END SECTION
