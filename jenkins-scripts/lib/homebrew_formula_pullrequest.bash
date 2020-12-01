8a8c8bc67d36103a7fbb7564fbb78f9206820c48
Steve Peters <scpeters@openrobotics.org>
2020-12-01 02:02:29 -0800

    Switch to brew bump-formula-pr
    There is a bug in 'brew ruby' that has broken our script for
    creating homebrew formula pull requests. I have been meaning
    to switch to 'brew bump-formula-pr' for a while, so let's try
    that now.
    
    Fixes #74.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- jenkins-scripts/lib/homebrew_formula_pullrequest.bash
+++ jenkins-scripts/lib/homebrew_formula_pullrequest.bash
@@ -31,62 +31,24 @@ PULL_REQUEST_HEAD_REPO=git@github.com:osrfbuild/homebrew-simulation.git
 . ${SCRIPT_LIBDIR}/_homebrew_github_setup.bash
 . ${SCRIPT_LIBDIR}/_homebrew_github_get_formula_path.bash
 
-echo '# BEGIN SECTION: calculating the SHA hash and changing the formula'
-# get stable uri and its line number
-URI=`${BREW} ruby -e "puts \"${PACKAGE_ALIAS}\".f.stable.url"`
-echo Changing url from
-echo ${URI} to
-echo ${SOURCE_TARBALL_URI}
-URI_LINE=`grep -n ${URI} ${FORMULA_PATH} \
-  | head -1 \
-  | sed -e 's@:.*@@'`
-echo on line number ${URI_LINE}
-sed -i -e "${URI_LINE}c\  url \"${SOURCE_TARBALL_URI}\"" ${FORMULA_PATH}
-
-echo
-# get line with formula version
-VERSION_LINE=$(awk \
-  "/^  version ['\"]/ {print FNR}" ${FORMULA_PATH} | head -1)
-# check if version can be correctly auto-detected from url
+echo '# BEGIN SECTION: checking if version needs to be passed'
 if ${BREW} ruby -e "exit Version.parse(\"${SOURCE_TARBALL_URI}\").to_s == \"${VERSION}\""
 then
   echo Version can be correctly auto-detected from URL
-  if [ -n "${VERSION_LINE}" ]; then
-    echo remove explicit version on line number ${VERSION_LINE}
-    sed -i -e "${VERSION_LINE}d" ${FORMULA_PATH}
-  fi
 else
-  if [ -z "${VERSION_LINE}" ]; then
-    # Need to insert explicit version tag after url
-    echo Adding explicit version tag after URL
-    sed -i -e "${URI_LINE}a\  version \"${VERSION}\"" ${FORMULA_PATH}
-  else
-    echo Changing version to
-    echo ${VERSION}
-    echo on line number ${VERSION_LINE}
-    sed -i -e "${VERSION_LINE}c\  version \"${VERSION}\"" ${FORMULA_PATH}
-  fi
+  echo Adding explicit version tag
+  EXPLICIT_VERSION="--version=${VERSION}"
 fi
+echo '# END SECTION'
 
-echo
-# get stable sha256 and its line number
-SHA=`${BREW} ruby -e "puts \"${PACKAGE_ALIAS}\".f.stable.checksum"`
-echo Changing sha256 from
-echo ${SHA} to
-echo ${SOURCE_TARBALL_SHA}
-SHA_LINE=`awk "/${SHA}/ {print FNR}" ${FORMULA_PATH} | head -1`
-echo on line number ${SHA_LINE}
-sed -i -e "${SHA_LINE}c\  sha256 \"${SOURCE_TARBALL_SHA}\"" ${FORMULA_PATH}
-
-echo
-# revision line if it's nonzero
-FORMULA_REVISION=$(${BREW} ruby -e "puts \"${PACKAGE_ALIAS}\".f.pkg_version.revision")
-if [ "$FORMULA_REVISION" -gt 0 ]; then
-  echo Deleting formula revision $FORMULA_REVISION
-  FORMULA_REVISION_LINE=$(awk "/  revision ${FORMULA_REVISION}/ {print FNR}" ${FORMULA_PATH} | head -1)
-  echo on line number ${FORMULA_REVISION_LINE}
-  sed -i -e "${FORMULA_REVISION_LINE}d" ${FORMULA_PATH}
-fi
+echo '# BEGIN SECTION: call brew bump-formula-pr --dry-run'
+${BREW} bump-formula-pr ${PACKAGE_ALIAS} --dry-run \
+    --url="${SOURCE_TARBALL_URI}" --sha256="${SOURCE_TARBALL_SHA}" ${EXPLICIT_VERSION}
+echo '# END SECTION'
+echo '# BEGIN SECTION: call brew bump-formula-pr --write'
+${BREW} bump-formula-pr ${PACKAGE_ALIAS} --write \
+    --url="${SOURCE_TARBALL_URI}" --sha256="${SOURCE_TARBALL_SHA}" ${EXPLICIT_VERSION}
+echo '# END SECTION'
 
 # create branch with name and sanitized version string
 PULL_REQUEST_BRANCH="${PACKAGE_ALIAS}_`echo ${VERSION} | tr ' ~:^?*[' '_'`"
