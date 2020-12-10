a2cd766a3d8f88ff9965f50c0ce9073fdf745ae0
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-12-10 15:34:36 +0100

    Missing deps in fuel, sdformat, msgs

--- jenkins-scripts/ign_fuel-tools-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_fuel-tools-default-devel-windows-amd64.bat
@@ -5,6 +5,6 @@ set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
 :: tinyxml2 from msgs
-set DEPEN_PKGS="tinyxml2"
+set DEPEN_PKGS="libyaml libzip tinyxml2"
 
 call "%SCRIPT_DIR%/lib/generic-default-devel-windows.bat"
