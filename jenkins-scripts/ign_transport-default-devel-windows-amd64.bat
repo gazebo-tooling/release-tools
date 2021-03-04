0baaa33511921d413250a615213e228ed7efc6a4
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-03-04 14:16:59 +0100

    Migrate transport to colcon
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/ign_transport-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_transport-default-devel-windows-amd64.bat
@@ -6,9 +6,8 @@ set VCS_DIRECTORY=ign-transport
 set PLATFORM_TO_BUILD=amd64
 set IGN_CLEAN_WORKSPACE=true
 
-for /f %%i in ('python "%SCRIPT_DIR%\tools\detect_cmake_major_version.py" "%WORKSPACE%\%VCS_DIRECTORY%\CMakeLists.txt"') do set IGN_TRANSPORT_MAJOR_VERSION=%%i
-if %IGN_TRANSPORT_MAJOR_VERSION% GEQ 4 (
-  call "%SCRIPT_DIR%/lib/generic-default-devel-windows.bat"
-) else (
-  call "%SCRIPT_DIR%/lib/ign_transport-base-windows.bat"
-)
+set DEPEN_PKGS="dlfcn-win32 cppzmq sqlite protobuf tinyxml2 zeromq"
+set COLCON_PACKAGE=ignition-transport
+set COLCON_AUTO_MAJOR_VERSION=true
+
+call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
