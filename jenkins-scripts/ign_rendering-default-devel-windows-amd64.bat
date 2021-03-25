ed1fb5835fe356de35b8a0c1d1a6bad234e2e800
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-03-25 17:03:26 +0100

    Make ogre22 conditional to rendering5 or above
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/ign_rendering-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_rendering-default-devel-windows-amd64.bat
@@ -4,10 +4,16 @@ set SCRIPT_DIR=%~dp0
 set VCS_DIRECTORY=ign-rendering
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
-:: ogre2 from osrf vcpkg-ports
-set DEPEN_PKGS="dlfcn-win32 cuda freeimage ogre ogre22 gts glib"
+set DEPEN_PKGS="dlfcn-win32 cuda freeimage ogre gts glib"
+for /f %%i in ('python "%SCRIPT_DIR%\tools\detect_cmake_major_version.py" "%WORKSPACE%\%VCS_DIRECTORY%\CMakeLists.txt"') do set IGN_MAJOR_VERSION=%%i
+if %IGN_MAJOR_VERSION% GEQ 5 (
+  set DEPEN_PKGS="%DEPEN_PKGS% ogre22"
+)
 :: This needs to be migrated to DSL to get multi-major versions correctly
 set COLCON_PACKAGE=ignition-rendering
 set COLCON_AUTO_MAJOR_VERSION=true
+
+
+
 
 call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
