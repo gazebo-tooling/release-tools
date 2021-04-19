655584843d24cb283d5066f51f2d44bb97bf4e43
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-04-19 16:30:28 +0200

    Do not use quotes for set on Windows

--- jenkins-scripts/ign_rendering-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_rendering-default-devel-windows-amd64.bat
@@ -4,10 +4,10 @@ set SCRIPT_DIR=%~dp0
 set VCS_DIRECTORY=ign-rendering
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
-set DEPEN_PKGS="dlfcn-win32 cuda freeimage ogre gts glib"
+set DEPEN_PKGS=dlfcn-win32 cuda freeimage ogre gts glib
 for /f %%i in ('python "%SCRIPT_DIR%\tools\detect_cmake_major_version.py" "%WORKSPACE%\%VCS_DIRECTORY%\CMakeLists.txt"') do set IGN_MAJOR_VERSION=%%i
 if %IGN_MAJOR_VERSION% GEQ 5 (
-  set DEPEN_PKGS="%DEPEN_PKGS% ogre22"
+  set DEPEN_PKGS=%DEPEN_PKGS% ogre22
 )
 :: This needs to be migrated to DSL to get multi-major versions correctly
 set COLCON_PACKAGE=ignition-rendering
