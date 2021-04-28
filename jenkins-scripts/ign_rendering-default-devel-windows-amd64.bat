611c02c0e770b8419abd44cce1fd0cb407482789
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-04-28 11:34:13 -0700

    Boost is required to build ign-rendering.
    This is an attempt to resolve the following error as seen on the build
    machine Locutus:
    
    ```
      Could not find pkg-config information for IgnOGRE.  It was not provided by
      the find-module for the package, nor was it explicitly passed into the call
      to ign_find_package(~).  This is most likely an error in this project's use
      of ign-cmake.
    Call Stack (most recent call first):
      CMakeLists.txt:66 (ign_find_package)
    This warning is for project developers.  Use -Wno-dev to suppress it.
    -- Could NOT find Boost (missing: Boost_INCLUDE_DIR)
    ```

--- jenkins-scripts/ign_rendering-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_rendering-default-devel-windows-amd64.bat
@@ -5,7 +5,7 @@ set VCS_DIRECTORY=ign-rendering
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 :: ogre2 from osrf vcpkg-ports
-set DEPEN_PKGS="dlfcn-win32 cuda freeimage ogre ogre2 gts glib"
+set DEPEN_PKGS="boost dlfcn-win32 cuda freeimage ogre ogre2 gts glib"
 :: This needs to be migrated to DSL to get multi-major versions correctly
 set COLCON_PACKAGE=ignition-rendering
 set COLCON_AUTO_MAJOR_VERSION=true
