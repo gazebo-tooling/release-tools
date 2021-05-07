1b6abc31af256a712603f667402b5e3274383308
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-05-07 12:31:26 +0200

    Migrate ign-common to colcon builds

--- jenkins-scripts/ign_common-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_common-default-devel-windows-amd64.bat
@@ -5,5 +5,7 @@ set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
 set DEPEN_PKGS="ffmpeg freeimage gts tinyxml2"
+set COLCON_PACKAGE=ignition-common
+set COLCON_AUTO_MAJOR_VERSION=true
 
-call "%SCRIPT_DIR%/lib/generic-default-devel-windows.bat"
+call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
