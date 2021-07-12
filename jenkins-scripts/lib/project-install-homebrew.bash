251e4b97ba2a60316d58ce041791ffcba0271022
Steven Peters <scpeters@openrobotics.org>
2021-07-12 10:48:24 -0700

    brew: skip brew doctor checks if sdl is installed
    The sdl formula was recently deprecated, which is breaking
    out `brew doctor` checks for any formulae that install dartsim,
    since sdl is an indirect dependency of dartsim, via open-scene-graph.
    For now, skip `brew doctor` checks if sdl is installed.
    
    Signed-off-by: Steven Peters <scpeters@openrobotics.org>

--- jenkins-scripts/lib/project-install-homebrew.bash
+++ jenkins-scripts/lib/project-install-homebrew.bash
@@ -73,7 +73,10 @@ echo '# END SECTION'
 
 echo "#BEGIN SECTION: brew doctor analysis"
 brew missing || brew install $(brew missing | awk '{print $2}') && brew missing
-brew doctor
+# if sdl installed, skip brew doctor
+# remove this line when open-scene-graph stops depending on sdl
+# https://github.com/Homebrew/homebrew-core/pull/81101
+brew list | grep '^sdl$' || brew doctor
 echo '# END SECTION'
 
 echo "# BEGIN SECTION: re-add group write permissions"
