c9b3af93152affd6925a597a9a4c88d71cce2ff5
ahcorde <ahcorde@gmail.com>
2021-07-13 22:46:13 +0200

    fix
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- jenkins-scripts/lib/windows_library.bat
+++ jenkins-scripts/lib/windows_library.bat
@@ -63,7 +63,7 @@ FOR /F "tokens=2 delims=:" %%i IN ('C:\Program Files (x86)\Microsoft Visual Stud
 FOR /F "tokens=2 delims=:" %%i IN ('C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe -prerelease ^| findstr "channelId:"') DO set "CHANNEL=%%i"
 FOR /F "tokens=3 delims=:" %%i IN ('C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe -prerelease ^| findstr "installationPath:"') DO set "INSTALLPATH=C:%%i"
 
-C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installershell.exe modify --add Microsoft.VisualStudio.Component.VC.ATLMFC --add Microsoft.VisualStudio.Component.VC.ATL --productId %PRODUCT% --channelId %CHANNEL% -q --installPath "%INSTALLPATH%"
+"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installershell.exe" modify --add Microsoft.VisualStudio.Component.VC.ATLMFC --add Microsoft.VisualStudio.Component.VC.ATL --productId %PRODUCT% --channelId %CHANNEL% -q --installPath "%INSTALLPATH%"
 
 goto :EOF
 
