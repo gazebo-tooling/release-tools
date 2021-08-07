00008a2ec0a612433df5bc7a07c0c8802b37c9ad
Steve Peters <scpeters@openrobotics.org>
2021-08-07 02:30:01 -0700

    ign-plugin windows script: use colcon
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- jenkins-scripts/ign_plugin-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_plugin-default-devel-windows-amd64.bat
@@ -4,4 +4,9 @@ set VCS_DIRECTORY=ign-plugin
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
-call "%SCRIPT_DIR%/lib/generic-default-devel-windows.bat"
+set DEPEN_PKGS="dlfcn-win32"
+
+set COLCON_PACKAGE=ignition-plugin
+set COLCON_AUTO_MAJOR_VERSION=true
+
+call "%SCRIPT_DIR%/lib/colcon-default-devel-windows.bat"
