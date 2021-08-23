f23ba3fa0a4737b27140ecc4477b37976fc7c4b2
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-08-24 01:30:53 +0200

    Use sudo to craete schroot
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/docker/debian-ratt-builder.bash
+++ jenkins-scripts/docker/debian-ratt-builder.bash
@@ -31,7 +31,7 @@ debuild --no-sign
 echo '# END SECTION'
 
 echo '# BEGIN SECTION: create experimental chroot'
-sbuild-createchroot unstable /srv/chroot/exp-amd64-sbuild http://deb.debian.org/debian
+sudo sbuild-createchroot unstable /srv/chroot/exp-amd64-sbuild http://deb.debian.org/debian
 echo '# END SECTION'
 
 echo '# BEGIN SECTION: run ratt for ${DEB_PACKAGE}'
