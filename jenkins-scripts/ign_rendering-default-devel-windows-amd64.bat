bc7edfabd834528ea93d7ad6a880ed53de515f50
Louise Poubel <louise@openrobotics.org>
2021-09-21 22:14:56 -0700

    ign-rendering depend on Eigen (#519)
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- jenkins-scripts/ign_rendering-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_rendering-default-devel-windows-amd64.bat
@@ -5,7 +5,7 @@ set VCS_DIRECTORY=ign-rendering
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
-set DEPEN_PKGS=boost-core dlfcn-win32 cuda freeimage ogre ogre2 gts glib
+set DEPEN_PKGS=boost-core dlfcn-win32 cuda eigen3 freeimage ogre ogre2 gts glib
 for /f %%i in ('python "%SCRIPT_DIR%\tools\detect_cmake_major_version.py" "%WORKSPACE%\%VCS_DIRECTORY%\CMakeLists.txt"') do set IGN_MAJOR_VERSION=%%i
 if %IGN_MAJOR_VERSION% GEQ 5 (
   set DEPEN_PKGS=%DEPEN_PKGS% ogre22
