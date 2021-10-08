8003de9d379d0a7efe8123a22849033a986fa611
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-10-08 18:21:03 +0200

    Fix dashboard to include packages without Source field (i.e ignition-fortress) (#533)
    * Fix dashboard to include packages with Source filed (i.e ignition-fortress)
    
    * remove tabs
    
    Co-authored-by: Louise Poubel <louise@openrobotics.org>

--- terminal-dashboard/table.bash
+++ terminal-dashboard/table.bash
@@ -140,7 +140,11 @@ do
         if [[ $ARCH == "i386" && $VER == "focal" ]]; then
           PKG_VERSION="disabled"
         else
-          PKG_VERSION=$(wget -qO- http://packages.osrfoundation.org/gazebo/${DISTRO}-${PACKAGE_REPO}/dists/${VER}/main/binary-${ARCH}/Packages | grep -1 "Source: ${LIB}" | sed -n 's/^Version: \(.*\)/\1/p' | uniq)
+          # The Source field is not mandatory and it is probably not present when
+          # the binary package has the same name than the source package
+          PKG_VERSION=$(wget -qO- http://packages.osrfoundation.org/gazebo/${DISTRO}-${PACKAGE_REPO}/dists/${VER}/main/binary-${ARCH}/Packages | \
+            grep -1 -m 1 -e "Source: ${LIB}" -e "Package: ${LIB}" | \
+            sed -n 's/^Version: \(.*\)/\1/p' | uniq)
         fi
 
         PKG_VERSION=${PKG_VERSION%%~*}
