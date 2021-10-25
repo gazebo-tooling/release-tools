d83d624452c59062b280c094b71218ad1336aab6
Steve Peters <scpeters@openrobotics.org>
2021-10-25 10:38:14 -0700

    ignition.dsl: colcon for ign-tools on windows (#537)
    * enable tests in ign-tools
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- jenkins-scripts/ign_tools-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_tools-default-devel-windows-amd64.bat
@@ -3,6 +3,8 @@ set SCRIPT_DIR=%~dp0
 set VCS_DIRECTORY=ign-tools
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
-set ENABLE_TESTS=false
 
-call "%SCRIPT_DIR%/lib/generic-default-devel-windows.bat"
+set COLCON_PACKAGE=ignition-tools
+set COLCON_AUTO_MAJOR_VERSION=true
+
+call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
