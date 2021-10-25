1798c648f4e47f4c683ada43c6b4d96805013675
Steve Peters <scpeters@openrobotics.org>
2021-10-25 10:59:53 -0700

    ign-tools windows CI: don't major version number (#540)
    The ignition-tools cmake package name doesn't include its major
    version, so disable COLCON_AUTO_MAJOR_VERSION
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- jenkins-scripts/ign_tools-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_tools-default-devel-windows-amd64.bat
@@ -5,6 +5,6 @@ set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
 set COLCON_PACKAGE=ignition-tools
-set COLCON_AUTO_MAJOR_VERSION=true
+set COLCON_AUTO_MAJOR_VERSION=false
 
 call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
