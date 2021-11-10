0611ca2f75ae88587e04194e65dc481d3ee619b2
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-11-10 15:43:06 +0100

    Refactor collection libs

--- terminal-dashboard/table.bash
+++ terminal-dashboard/table.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
 # Arguments
 #
 # 1. <collection>: Required: A supported collection, i.e. "citadel", "fortesss", etc.
@@ -13,95 +17,12 @@
 #
 #  bash table.bash edifice prerelease
 
+# shellcheck source=./_dashboard_lib.sh
+. ${SCRIPT_DIR}/_dashboard_lib.sh
+
 COLLECTION=$1
-
 PACKAGE_REPO=${2:-stable}
-
-if [ "$COLLECTION" = "citadel" ]; then
-  LIBS=(
-    "ignition-cmake2"
-    "ignition-math6"
-    "ignition-tools"
-    "ignition-common3"
-    "ignition-msgs5"
-    "ignition-transport8"
-    "ignition-fuel-tools4"
-    "ignition-plugin"
-    "ignition-rendering3"
-    "sdformat9"
-    "ignition-physics2"
-    "ignition-sensors3"
-    "ignition-gui3"
-    "ignition-gazebo3"
-    "ignition-launch2"
-    "ignition-citadel"
-  )
-elif [ "$COLLECTION" = "dome" ]; then
-  LIBS=(
-    "ignition-cmake2"
-    "ignition-math6"
-    "ignition-tools"
-    "ignition-common3"
-    "ignition-msgs6"
-    "ignition-transport9"
-    "ignition-fuel-tools5"
-    "ignition-plugin"
-    "ignition-rendering4"
-    "sdformat10"
-    "ignition-physics3"
-    "ignition-sensors4"
-    "ignition-gui4"
-    "ignition-gazebo4"
-    "ignition-launch3"
-    "ignition-dome"
-  )
-elif [ "$COLLECTION" = "edifice" ]; then
-  LIBS=(
-    "ignition-cmake2"
-    "ignition-math6"
-    "ignition-utils1"
-    "ignition-tools"
-    "ignition-common4"
-    "ignition-msgs7"
-    "ignition-transport10"
-    "ignition-fuel-tools6"
-    "ignition-plugin"
-    "ignition-rendering5"
-    "sdformat11"
-    "ignition-physics4"
-    "ignition-sensors5"
-    "ignition-gui5"
-    "ignition-gazebo5"
-    "ignition-launch4"
-    "ignition-edifice"
-  )
-elif [ "$COLLECTION" = "fortress" ]; then
-  LIBS=(
-    "ignition-cmake2"
-    "ignition-math6"
-    "ignition-utils1"
-    "ignition-tools"
-    "ignition-common4"
-    "ignition-msgs8"
-    "ignition-transport11"
-    "ignition-fuel-tools7"
-    "ignition-plugin"
-    "ignition-rendering6"
-    "sdformat12"
-    "ignition-physics5"
-    "ignition-sensors6"
-    "ignition-gui6"
-    "ignition-gazebo6"
-    "ignition-launch5"
-    "ignition-fortress"
-  )
-else
-  echo "Missing collection"
-  exit
-fi
-
 ARCHS=( "amd64" "i386" "arm64" "armhf")
-
 DISTROS=( "ubuntu" "debian" )
 
 COLUMN="        "
@@ -110,8 +31,7 @@ GREEN="\e[42m"
 YELLOW="\e[43m"
 RED="\e[101m"
 
-for LIB in "${LIBS[@]}"
-do
+for LIB in $(get_libraries_by_collection "${COLLECTION}" ); do
   echo -e "\e[107m\e[90m${LIB}\e[49m\e[39m"
 
   LIB_VER=""
--- terminal-dashboard/unreleased.bash
+++ terminal-dashboard/unreleased.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
 # Helper script to check if there are unreleased changes for supported versions.
 # The script prints a list of links that can be clicked to see if there are new
 # changes since the latest release.
@@ -16,91 +20,13 @@
 #
 #  bash unreleased.bash edifice
 
-COLLECTION=$1
+# shellcheck source=./_dashboard_lib.sh
+. "${SCRIPT_DIR}"/_dashboard_lib.sh
 
+COLLECTION=$1
 PACKAGE_REPO=${2:-stable}
 
-if [ "$COLLECTION" = "citadel" ]; then
-  LIBS=(
-    "ignition-cmake2"
-    "ignition-math6"
-    "ignition-tools"
-    "ignition-common3"
-    "ignition-msgs5"
-    "ignition-transport8"
-    "ignition-fuel-tools4"
-    "ignition-plugin"
-    "ignition-rendering3"
-    "sdformat9"
-    "ignition-physics2"
-    "ignition-sensors3"
-    "ignition-gui3"
-    "ignition-gazebo3"
-    "ignition-launch2"
-  )
-elif [ "$COLLECTION" = "dome" ]; then
-  LIBS=(
-    "ignition-cmake2"
-    "ignition-math6"
-    "ignition-tools"
-    "ignition-common3"
-    "ignition-msgs6"
-    "ignition-transport9"
-    "ignition-fuel-tools5"
-    "ignition-plugin"
-    "ignition-rendering4"
-    "sdformat10"
-    "ignition-physics3"
-    "ignition-sensors4"
-    "ignition-gui4"
-    "ignition-gazebo4"
-    "ignition-launch3"
-  )
-elif [ "$COLLECTION" = "edifice" ]; then
-  LIBS=(
-    "ignition-cmake2"
-    "ignition-math6"
-    "ignition-utils1"
-    "ignition-tools"
-    "ignition-common4"
-    "ignition-msgs7"
-    "ignition-transport10"
-    "ignition-fuel-tools6"
-    "ignition-plugin"
-    "ignition-rendering5"
-    "sdformat11"
-    "ignition-physics4"
-    "ignition-sensors5"
-    "ignition-gui5"
-    "ignition-gazebo5"
-    "ignition-launch4"
-  )
-elif [ "$COLLECTION" = "fortress" ]; then
-  LIBS=(
-    "ignition-cmake2"
-    "ignition-math6"
-    "ignition-utils1"
-    "ignition-tools"
-    "ignition-common4"
-    "ignition-msgs8"
-    "ignition-transport11"
-    "ignition-fuel-tools7"
-    "ignition-plugin"
-    "ignition-rendering6"
-    "sdformat12"
-    "ignition-physics5"
-    "ignition-sensors6"
-    "ignition-gui6"
-    "ignition-gazebo6"
-    "ignition-launch5"
-  )
-else
-  echo "Missing collection"
-  exit
-fi
-
-for LIB in "${LIBS[@]}"
-do
+for LIB in $(get_libraries_by_collection "${COLLECTION}" ); do
   LIB_SHORT="${LIB/ignition-/ign-}"
   LIB_NAME=${LIB_SHORT//[[:digit:]]/}
 
