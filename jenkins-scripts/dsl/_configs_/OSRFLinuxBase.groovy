31f48d5821a07c3e516d1486b6097c2aceea6a3e
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-11-11 17:56:21 +0100

    Do not fail if linux jobs don't have Dockefile

--- jenkins-scripts/dsl/_configs_/OSRFLinuxBase.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFLinuxBase.groovy
@@ -23,6 +23,10 @@ class OSRFLinuxBase
           archiveArtifacts {
             pattern('Dockerfile')
             pattern('build.sh')
+            // there are no guarantees that a job using OSRFLinuxBase generate
+            // these files (i.e: testing job). Do not fail if they are not
+            // present
+            allowEmpty(true)
         }
       }
     }
