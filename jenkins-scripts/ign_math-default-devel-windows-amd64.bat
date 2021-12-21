b9bfc6b11622951e9544049a21cfdff23d6b7c56
Michael Carroll <michael@openrobotics.org>
2021-12-21 15:48:52 -0600

    ignition.dsl: colcon for ign-math on windows (#602)
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- jenkins-scripts/ign_math-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_math-default-devel-windows-amd64.bat
@@ -3,5 +3,7 @@ set SCRIPT_DIR=%~dp0
 set VCS_DIRECTORY=ign-math
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
+set COLCON_PACKAGE=ignition-math
+set COLCON_AUTO_MAJOR_VERSION=true
 
-call "%SCRIPT_DIR%/lib/generic-default-devel-windows.bat"
+call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
