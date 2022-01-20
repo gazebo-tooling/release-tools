b0c944496ae022ce7b24cd041b9c74d955bfd229
Steve Peters <scpeters@openrobotics.org>
2022-01-20 11:41:49 -0800

    Ignore brew doctor proj@7 warning for install jobs
    Repeat of #626 for install_bottle jobs.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- jenkins-scripts/lib/project-install-homebrew.bash
+++ jenkins-scripts/lib/project-install-homebrew.bash
@@ -73,10 +73,10 @@ echo '# END SECTION'
 
 echo "#BEGIN SECTION: brew doctor analysis"
 brew missing || brew install $(brew missing | awk '{print $2}') && brew missing
-# if sdl installed, skip brew doctor
-# remove this line when open-scene-graph stops depending on sdl
-# https://github.com/Homebrew/homebrew-core/pull/81101
-brew list | grep '^sdl$' || brew doctor
+# if proj@7 installed, skip brew doctor
+# remove this line when gdal stops depending on a deprecated version of proj
+# https://github.com/Homebrew/homebrew-core/issues/82441
+brew list | grep '^proj@7$' || brew doctor
 echo '# END SECTION'
 
 echo "# BEGIN SECTION: re-add group write permissions"
