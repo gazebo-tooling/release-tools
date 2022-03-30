a20784dc16d8bb8793cb81fa65367615f1170e9f
Steve Peters <scpeters@openrobotics.org>
2022-03-29 22:21:10 -0700

    brew: try to rescue if `brew list` fails
    Sometimes `brew list` fails if the brew installation
    on a build machine has been corrupted. This will
    check for failures and invoke restore_brew if needed.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- jenkins-scripts/lib/_homebrew_cleanup.bash
+++ jenkins-scripts/lib/_homebrew_cleanup.bash
@@ -15,6 +15,7 @@ export HOMEBREW_UPDATE_TO_TAG=1
 ${BREW_BINARY} up || { restore_brew && ${BREW_BINARY} up ; }
 
 # Clear all installed homebrew packages, links, taps, and kegs
+${BREW_BINARY} list --formula | wc || { restore_brew && ${BREW_BINARY} list --formula | wc; }
 BREW_LIST=$(${BREW_BINARY} list --formula)
 if [[ -n "${BREW_LIST}" ]]; then
   ${BREW_BINARY} remove --force --ignore-dependencies ${BREW_LIST}
