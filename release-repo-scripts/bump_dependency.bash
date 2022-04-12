88436da43e3db319d1f697fc8ef56d0793676edb
methylDragon <methylDragon@gmail.com>
2022-04-11 17:07:53 -0700

    Fix some bump_dependency edgecases (#699)
    * Add comments to standard src case
    
    * Handle IGN_<LIB>_VER <N-1> case
    
    * Remove version numbers
    
    Adapted from: https://github.com/ignition-tooling/release-tools/commit/b2990ee2af7c5aa63d661fa41a386c69d71b356b
    
    * Handle VERSION X.X.X. case
    
    Adapted/fixed from: https://github.com/ignition-tooling/release-tools/commit/b2990ee2af7c5aa63d661fa41a386c69d71b356b
    
    Preserves other args
    
    * Increase scope of find_package matching

--- release-repo-scripts/bump_dependency.bash
+++ release-repo-scripts/bump_dependency.bash
@@ -455,7 +455,28 @@ for ((i = 0; i < "${#LIBRARIES[@]}"; i++)); do
     DEP_VER=${VERSIONS[$j]}
     DEP_PREV_VER="$((${DEP_VER}-1))"
 
+    # Rule: *plugin2 -> *plugin3
+    # Replace lines like: "find_package(ignition-cmake2)"
+    #               with: "find_package(ignition-cmake3)"
     find . -type f ! -name 'Changelog.md' ! -name 'Migration.md' -print0 | xargs -0 sed -i "s ${DEP_LIB}${DEP_PREV_VER} ${DEP_LIB}${DEP_VER} g"
+
+    # Rule: IGN_PLUGIN_VER 2 -> IGN_PLUGIN_VER 3
+    # Replace lines like: "set(IGN_PLUGIN_VER 2)"
+    #               with: "set(IGN_PLUGIN_VER 3)"
+    find . -type f ! -name 'Changelog.md' ! -name 'Migration.md' -print0 | xargs -0 sed -i "s@IGN_${DEP_LIB}_VER ${DEP_PREV_VER}@\UIGN_${DEP_LIB}_VER ${DEP_VER}@ig"
+
+    # Replace lines like "find_package(ignition-cmake2 2.0.0)"
+    #               with "find_package(ignition-cmake3)"
+    find . -type f -name 'CMakeLists.txt' -print0 | xargs -0 sed -i "s@\(find_package.*${DEP_LIB}\)${DEP_PREV_VER} \+${DEP_PREV_VER}[^ )]*@\1${DEP_VER}@g"
+
+    # Replace lines like "ign_find_package(ignition-math6 VERSION 6.5.0)"
+    #               with "ign_find_package(ignition-math7)"
+    # Preserves other args and handles edge cases:
+    #               like "ign_find_package(ignition-math6 VERSION 6.5.0 REQUIRED)"
+    #               with "ign_find_package(ignition-math6 REQUIRED)"
+    #               like "ign_find_package(ignition-math6 REQUIRED COMPONENTS VERSION 6.10 eigen3)"
+    #               with "ign_find_package(ignition-math7 REQUIRED COMPONENTS eigen3)"
+    find . -type f -name 'CMakeLists.txt' -print0 | xargs -0 sed -i "s@\(find_package.*${DEP_LIB}\)${DEP_PREV_VER}\(.*\) \+VERSION \+${DEP_PREV_VER}[^ )]*@\1${DEP_VER}\2@g"
 
     # Replace collection yaml branch names with main
     if [[ "${LIB}" == "ign-${COLLECTION}" ]]; then
@@ -490,4 +511,3 @@ for ((i = 0; i < "${#LIBRARIES[@]}"; i++)); do
   commitAndPR ${TOOLING_ORG} master
 
 done
-
