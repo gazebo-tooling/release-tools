7e679f9c5823bc15957f26a7201268e092c7fe2b
Louise Poubel <louise@openrobotics.org>
2022-04-14 13:49:01 -0700

    Bumps in garden: ign-tools2
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- jenkins-scripts/ign_tools-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_tools-default-devel-windows-amd64.bat
@@ -5,6 +5,6 @@ set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
 set COLCON_PACKAGE=ignition-tools
-set COLCON_AUTO_MAJOR_VERSION=false
+set COLCON_AUTO_MAJOR_VERSION=true
 
 call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
