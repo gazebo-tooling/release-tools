2e405856b401cb3399b9955034433e47674e8508
Jose Luis Rivero <jrivero@osrfoundation.org>
2022-04-22 21:38:39 +0200

    Exclude OpenAL errors from failing

--- jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
+++ jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
@@ -342,16 +342,16 @@ cat >> Dockerfile << DELIM_NVIDIA_GPU
 DELIM_NVIDIA_GPU
    else
 # NVIDIA is using nvidia_docker2 integration
-GLVND_VERSION=1.2.0
-if [[ ${LINUX_DISTRO} == 'ubuntu' && ( ${DISTRO} != 'bionic' && ${DISTRO} != 'focal' ) ]]; then
-  GLVND_VERSION=1.3.4
-fi
 cat >> Dockerfile << DELIM_NVIDIA2_GPU
   # nvidia-container-runtime
   ENV NVIDIA_VISIBLE_DEVICES \
     ${NVIDIA_VISIBLE_DEVICES:-all}
   ENV NVIDIA_DRIVER_CAPABILITIES \
     ${NVIDIA_DRIVER_CAPABILITIES:+$NVIDIA_DRIVER_CAPABILITIES,}graphics
+DELIM_NVIDIA2_GPU
+
+if [[ ${LINUX_DISTRO} == 'ubuntu' ]] && [[ ${DISTRO} == 'bionic' && ${DISTRO} == 'focal' ]]; then
+cat >> Dockerfile << DELIM_NVIDIA3_GPU
 # Install libglvnd for OpenGL using nvidia-docker2
 RUN apt-get update && apt-get install -y --no-install-recommends \
         git \
@@ -367,13 +367,14 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
         x11proto-gl-dev && \
     rm -rf /var/lib/apt/lists/*
 RUN mkdir -p /opt/libglvnd && cd /opt/libglvnd && \
-    git clone -b v${GLVND_VERSION} https://github.com/NVIDIA/libglvnd.git . && \
+    git clone -b v1.2.0 https://github.com/NVIDIA/libglvnd.git . && \
     ./autogen.sh && \
     ./configure --prefix=/usr/local --libdir=/usr/local/lib/x86_64-linux-gnu && \
     make install-strip && \
     find /usr/local/lib/x86_64-linux-gnu -type f -name 'lib*.la' -delete
 ENV LD_LIBRARY_PATH /usr/local/lib/x86_64-linux-gnu\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}
-DELIM_NVIDIA2_GPU
+DELIM_NVIDIA3_GPU
+fi
   fi
  else
   # No NVIDIA cards needs to have the same X stack than the host
