987a45738a2b2276603d4bd2266380d4d4424ebc
Jorge Perez <jjperez@ekumenlabs.com>
2022-04-26 10:49:25 -0500

    Fix build regression on ign-tools1 for windows on CI (#713)
    * Fix build regression on ign-tools1 for windows on CI
    
    Signed-off-by: Jorge Perez <jjperez@ekumenlabs.com>

--- jenkins-scripts/ign_tools-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_tools-default-devel-windows-amd64.bat
@@ -5,6 +5,17 @@ set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
 set COLCON_PACKAGE=ignition-tools
-set COLCON_AUTO_MAJOR_VERSION=true
+
+:: override logic @ colcon-default-devel-windows.bat to handle ign-tools1 case on windows
+setlocal ENABLEDELAYEDEXPANSION
+for /f %%i in ('python "%SCRIPT_DIR%\tools\detect_cmake_major_version.py" "%WORKSPACE%\%VCS_DIRECTORY%\CMakeLists.txt"') do set PKG_MAJOR_VERSION=%%i
+echo "MAJOR_VERSION detected: !PKG_MAJOR_VERSION!"
+if "!PKG_MAJOR_VERSION!" == "1" (
+   set COLCON_PACKAGE=%COLCON_PACKAGE%
+) else (
+   set COLCON_PACKAGE=%COLCON_PACKAGE%!PKG_MAJOR_VERSION!
+)
+
+set COLCON_AUTO_MAJOR_VERSION=false
 
 call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
