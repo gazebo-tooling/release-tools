266d10818231b5edefd6d789f794cccdb78cabd8
Louise Poubel <louise@openrobotics.org>
2022-06-17 09:17:16 -0700

    Support gz- repos on source_changelog script (#741)
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- source-repo-scripts/source_changelog.bash
+++ source-repo-scripts/source_changelog.bash
@@ -1,15 +1,23 @@
 #!/bin/bash
+# bash source_changelog.bash <PREV_VER>
 
 PREV_TAG=$1
 
 git fetch --tags
 
 REPO=$(basename `git rev-parse --show-toplevel`)
-REPO_FULL="${REPO/ign-/ignition-}"
+REPO="${REPO/ign-/gz-}"
 MAJOR=${PREV_TAG%.*.*}
 BRANCH=${REPO/sdformat/sdf}${MAJOR}
+TAG="${REPO}${MAJOR}"
 
-COMMITS=$(git log ${BRANCH}...${REPO_FULL}${MAJOR}_${PREV_TAG}  --pretty=format:"%h")
+# TODO(chapulina) Support Garden tags and branches, which will start with gz-
+TAG="${TAG/gz-/ignition-}"
+TAG="${TAG/sim/gazebo}"
+BRANCH="${BRANCH/gz-/ign-}"
+BRANCH="${BRANCH/sim/gazebo}"
+
+COMMITS=$(git log ${BRANCH}...${TAG}_${PREV_TAG}  --pretty=format:"%h")
 
 for COMMIT in $COMMITS
 do
@@ -19,6 +27,6 @@ do
   PR=${PR%)}
 
   echo "1. $TITLE"
-  echo "    * [Pull request #$PR](https://github.com/ignitionrobotics/$REPO/pull/$PR)"
+  echo "    * [Pull request #$PR](https://github.com/gazebosim/$REPO/pull/$PR)"
   echo ""
 done
