6efef1f3fd70ebb84e4f5f533080e3c58f3be007
Addisu Z. Taddese <addisu@openrobotics.org>
2022-06-29 10:46:49 -0500

    Add pytest to list of pip installed dependencies for sdformat
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- jenkins-scripts/sdformat-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/sdformat-default-devel-homebrew-amd64.bash
@@ -4,7 +4,7 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-PIP_PACKAGES_NEEDED="psutil"
+PIP_PACKAGES_NEEDED="psutil pytest"
 
 . ${SCRIPT_DIR}/lib/project-default-devel-homebrew-amd64.bash sdformat
 
