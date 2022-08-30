3e61ba8c96fb1c1b0f9d8f96e3e463f935551b34
Jose Luis Rivero <jrivero@osrfoundation.org>
2022-08-30 19:47:01 +0200

    Need colcon package name

--- jenkins-scripts/ign_cmake-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_cmake-default-devel-windows-amd64.bat
@@ -4,6 +4,9 @@ set VCS_DIRECTORY=gz-cmake
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
+:: This needs to be migrated to DSL to get multi-major versions correctly
+set COLCON_PACKAGE=gz-cmake
+set COLCON_AUTO_MAJOR_VERSION=true
 set COLCON_EXTRA_CMAKE_ARGS=-DBUILDSYSTEM_TESTING:BOOL=True
 
 call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
