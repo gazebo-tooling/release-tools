a32c867fe45c7b86af5602a7c35ee37aa08abca6
Jose Luis Rivero <jrivero@osrfoundation.org>
2022-09-08 00:26:51 +0200

    Import changes from 797

--- release-repo-scripts/rename_ignition_to_gazebo.bash
+++ release-repo-scripts/rename_ignition_to_gazebo.bash
@@ -24,17 +24,23 @@ find . -name control -type f | while IFS= read -r f; do
   echo "Processing control file: ${f}"
   ignition_pkgs=$(grep-dctrl -sPackage -n '' ${f})
   echo " - Replace ignition ocurrences"
-  # Rename urls and packages
-  sed -i -e 's:ignitionrobotics:gazebosim:g' "${f}"
-  sed -i -e 's:libignition:libgz:g' "${f}"
-  sed -i -e 's:ignition:gz:g' "${f}"
-  sed -i -e 's:Ignition:Gazebo:g' "${f}"
-  sed -i -e 's:ign-:gz-:g' "${f}"
+  # Rename packages and sources
+  sed -i -e 's/Package: python3-ignition-gazebo/Package: python3-gz-sim/g' "${f}"
+  sed -i -e 's/Package: ignition-gazebo/Package: gz-sim/g' "${f}"
+  sed -i -e 's/Package: libignition-gazebo/Package: libgz-sim/g' "${f}"
+  sed -i -e 's/Source: ignition-gazebo/Source: gz-sim/g' "${f}"
+
+  sed -i -e 's/Package: python3-ignition/Package: python3-gz/g' "${f}"
+  sed -i -e 's/Package: ruby-ignition/Package: ruby-gz/g' "${f}"
+  sed -i -e 's/Package: ignition/Package: gz/g' "${f}"
+  sed -i -e 's/Package: libignition/Package: libgz/g' "${f}"
+  sed -i -e 's/Source: ignition/Source: gz/g' "${f}"
   # Inject transitional (alias) packages
   echo " - Transitional packages in control file"
   for pkg in ${ignition_pkgs}; do
     echo "  * Processing transitional package for ${pkg}"
     new_pkg_name=${pkg/ignition/gz}
+    new_pkg_name=${new_pkg_name/gazebo/sim}
     cat << EOF >> "${f}"
 
 Package: ${pkg}
@@ -96,6 +102,11 @@ done
 echo
 
 # 6. Rename all ignition filenames not symlinks
+find . -name '*ignition-gazebo*' -type f | while IFS= read -r f; do
+  echo "Renaming ${f} -> ${f/ignition-gazebo/gz-sim}"
+  git mv "${f}" "${f/ignition-gazebo/gz-sim}"
+done
+
 find . -name '*ignition*' -type f | while IFS= read -r f; do
   echo "Renaming ${f} -> ${f/ignition-/gz-}"
   git mv "${f}" "${f/ignition-/gz-}"
@@ -111,9 +122,14 @@ done
 
 echo
 
-# 7. Revert Provides, Breaks, Replaces
-find . -name control -type f | while IFS= read -r f; do
-  sed -i -e 's:\(Breaks\:.*\)gz-\(.*\):\1ignition-\2:' "${f}"
-  sed -i -e 's:\(Provides\:.*\)gz-\(.*\):\1ignition-\2:' "${f}"
-  sed -i -e 's:\(Replaces\:.*\)gz-\(.*\):\1ignition-\2:' "${f}"
-done
+# # 7. Revert Provides, Breaks, Replaces
+# find . -name control -type f | while IFS= read -r f; do
+#   sed -i -e 's:\(Breaks\:.*\)gz-\(.*\):\1ignition-\2:' "${f}"
+#   sed -i -e 's:\(Provides\:.*\)gz-\(.*\):\1ignition-\2:' "${f}"
+#   sed -i -e 's:\(Replaces\:.*\)gz-\(.*\):\1ignition-\2:' "${f}"
+# done
+
+echo "Do manually:"
+echo " * Update depends from the same library"
+echo " * Add breaks / replaces for previous nightly"
+echo " * Update rules"
