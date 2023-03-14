6a028f6e9fa7af3f29e692eef941ce18f60fe308
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-03-14 13:59:43 +0100

    Add dartsim to rendering, gazebo and launch
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/ign_launch-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_launch-default-devel-windows-amd64.bat
@@ -4,7 +4,7 @@ set VCS_DIRECTORY=gz-launch
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
-set DEPEN_PKGS=assimp boost-core ccd cppzmq cuda curl dlfcn-win32 eigen3 fcl ffmpeg freeimage gflags glib gts jsoncpp libyaml libzip ogre ogre2 ogre22 openssl protobuf qt5 qwt tinyxml2 zeromq
+set DEPEN_PKGS=assimp boost-core ccd cppzmq cuda dartsim curl dlfcn-win32 eigen3 fcl ffmpeg freeimage gflags glib gts jsoncpp libyaml libzip ogre ogre2 ogre22 openssl protobuf qt5 qwt tinyxml2 zeromq
 for /f %%i in ('python "%SCRIPT_DIR%\tools\detect_cmake_major_version.py" "%WORKSPACE%\%VCS_DIRECTORY%\CMakeLists.txt"') do set GZ_MAJOR_VERSION=%%i
 if %GZ_MAJOR_VERSION% GEQ 6 (
   set DEPEN_PKGS=%DEPEN_PKGS% gdal
