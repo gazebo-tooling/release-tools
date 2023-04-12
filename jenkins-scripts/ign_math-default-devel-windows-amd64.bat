d44e63e03157bd17801e0ea8c22d63fe43583319
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-04-12 13:09:44 +0200

    Remove DEPEN_PKGS references to vcpkg
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/ign_math-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_math-default-devel-windows-amd64.bat
@@ -3,7 +3,6 @@ set SCRIPT_DIR=%~dp0
 set VCS_DIRECTORY=gz-math
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
-set DEPEN_PKGS=pybind11
 set COLCON_PACKAGE=gz-math
 set COLCON_AUTO_MAJOR_VERSION=true
 
