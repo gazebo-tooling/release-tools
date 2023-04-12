7c8c8aa9df7edbd8930e8ab111b995aebdd97b4a
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-04-13 00:04:27 +0200

    Remove death code
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/ign_common-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_common-default-devel-windows-amd64.bat
@@ -5,8 +5,6 @@ set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
 for /f %%i in ('python "%SCRIPT_DIR%\tools\detect_cmake_major_version.py" "%WORKSPACE%\%VCS_DIRECTORY%\CMakeLists.txt"') do set GZ_MAJOR_VERSION=%%i
-if %GZ_MAJOR_VERSION% GEQ 5 (
-)
 
 set COLCON_PACKAGE=gz-common
 set COLCON_AUTO_MAJOR_VERSION=true
