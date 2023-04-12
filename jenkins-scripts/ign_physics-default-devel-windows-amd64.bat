a8ddb76e237d0164e70b5fe53800d4f77a54b9ba
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-04-13 00:08:49 +0200

    Remove death code in physics
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_physics-default-devel-windows-amd64.bat
@@ -3,11 +3,6 @@ set SCRIPT_DIR=%~dp0
 set VCS_DIRECTORY=gz-physics
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
-
-for /f %%i in ('python "%SCRIPT_DIR%\tools\detect_cmake_major_version.py" "%WORKSPACE%\%VCS_DIRECTORY%\CMakeLists.txt"') do set GZ_MAJOR_VERSION=%%i
-if %GZ_MAJOR_VERSION% GEQ 6 (
-)
-:: This needs to be migrated to DSL to get multi-major versions correctly
 set COLCON_PACKAGE=gz-physics
 set COLCON_AUTO_MAJOR_VERSION=true
 
