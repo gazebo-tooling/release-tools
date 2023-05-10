2e105877b00883efa084aeb402521ae304741982
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-05-10 17:35:53 +0200

    Move release-bloom.py wrapper to parent dir
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- bloom/ros_gazebo_pkgs/ros_gazebo_pkgs-release.py.bash
+++ bloom/ros_gazebo_pkgs/ros_gazebo_pkgs-release.py.bash
@@ -14,5 +14,5 @@ if [[ ${1%-*} != "${1}" ]]; then
 fi
 
 for p in gazebo-dev gazebo-msgs gazebo-plugins gazebo-ros gazebo-ros-control gazebo-ros-pkgs; do
-    ./release-bloom.py ${p} $(for i in $@; do echo -n "$i "; done)
+    ../release-bloom.py ${p} $(for i in $@; do echo -n "$i "; done)
 done
