48cff444c4a0e57b790739ce3f3e8e415af35c1b
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-05-16 14:20:46 +0200

    Disable launch
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/docker/ros_gz-install-test-job.bash
+++ jenkins-scripts/docker/ros_gz-install-test-job.bash
@@ -16,20 +16,20 @@ fi
 export INSTALL_JOB_POSTINSTALL_HOOK="""
 echo '# BEGIN SECTION: test gz_sim via ros2 launch'
 . /opt/ros/${ROS_DISTRO}/setup.bash
-TEST_START=\`date +%s\`
-timeout --preserve-status 180 ros2 launch ros_gz_sim gz_sim.launch.py gz_args:=shapes.sdf
-TEST_END=\`date +%s\`
-DIFF=\`echo \"\$TEST_END - \$TEST_START\" | bc\`
+# TEST_START=\`date +%s\`
+# timeout --preserve-status 180 ros2 launch ros_gz_sim gz_sim.launch.py gz_args:=shapes.sdf
+# TEST_END=\`date +%s\`
+# DIFF=\`echo \"\$TEST_END - \$TEST_START\" | bc\`
 
-if [ \$DIFF -lt 180 ]; then
-   echo 'The test took less than 180s. Something bad happened'
-   exit 1
-fi
-echo '# END SECTION'
+# if [ \$DIFF -lt 180 ]; then
+#   echo 'The test took less than 180s. Something bad happened'
+#   exit 1
+# fi
+# echo '# END SECTION'
 echo '# BEGIN SECTION: ros_gz talker/listener'
-ros2 run ros_gz_bridge parameter_bridge /chatter@std_msgs/msg/String@ignition.msgs.StringMsg &
+ros2 run ros_gz_bridge parameter_bridge /chatter@std_msgs/msg/String@gz.msgs.StringMsg &
 ros2 topic echo /chatter > /tmp/echo_chatter
-ign topic -t /chatter -m ignition.msgs.StringMsg -p 'data:\"Hello\"' &
+gz topic -t /chatter -m gz.msgs.StringMsg -p 'data:\"Hello\"' &
 if [[ -z \$(cat /tmp/echo_chatter) ]]; then
   echo 'chatter log file is empty'
   exit 1
