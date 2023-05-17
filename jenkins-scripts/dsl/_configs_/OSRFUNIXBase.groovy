d9797c71c40041c378b10f1b3aca0dc705eb9968
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2023-05-17 15:22:24 -0500

    Remove code comments
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- jenkins-scripts/dsl/_configs_/OSRFUNIXBase.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFUNIXBase.groovy
@@ -79,20 +79,10 @@ class OSRFUNIXBase extends OSRFBase
           onlyIfBuildSucceeds(false)
           onlyIfBuildFails(true)
         }
-        // Manual insertion of xml for Naginator plugin because of this issue https://issues.jenkins.io/browse/JENKINS-66458
-        // configure { project -> 
-        //   project / publishers / 'com.chikli.hudson.plugin.naginator.NaginatorPublisher' {
-        //     regexpForRerun("nvml error: driver/library version mismatch")
-        //     checkRegexp(true)
-        //     maxSchedule(1)
-        //     delay(class: 'com.chikli.hudson.plugin.naginator.FixedDelay') {
-        //       delay(70)
-        //     }
-        //   }
-        // }
       }
     }
 
+    // Add the new regex to naginator parameter
     RetryFailures.update_retry_parameters(job, 'nvml error driver/library version mismatch')
   }
 }
