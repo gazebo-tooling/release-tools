382d6d3157f680964070b258c075b9566224ec0a
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-05-24 23:02:57 +0200

    Fix setup tool
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/dsl/tools/setup_local_generation.bash
+++ jenkins-scripts/dsl/tools/setup_local_generation.bash
@@ -6,17 +6,17 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SNAKEYAML_PATH="lib/snakeyaml.jar"
 JOB_DSL_VER="1.77"
 
-pushd "${SCRIPT_DIR}" 2>/dev/null
+pushd "${SCRIPT_DIR}" >/dev/null
 echo " * Download job-dsl-core jar"
-curl -ssl https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/${JOB_DSL_VER}/job-dsl-core-${JOB_DSL_VER}-standalone.jar -o jobdsl.jar
+curl -sSL https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/${JOB_DSL_VER}/job-dsl-core-${JOB_DSL_VER}-standalone.jar -o jobdsl.jar
 echo " * Download snakeyaml jar"
 # need to inject the yaml module into the jar file. lib subdir is needed to ineject into
 # tar in the right place.
-mkdir lib
-curl -ssl https://repo1.maven.org/maven2/org/yaml/snakeyaml/1.32/snakeyaml-1.32.jar -o ${SNAKEYAML_PATH}
+mkdir -p lib
+curl -sSL https://repo1.maven.org/maven2/org/yaml/snakeyaml/1.32/snakeyaml-1.32.jar -o ${SNAKEYAML_PATH}
 echo " * Inject snakeyaml in job-dsl-core jar"
-tar uf jobdsl.jar ${SNAKEYAML_PATH}
+jar uf jobdsl.jar ${SNAKEYAML_PATH}
 rm -fr ${SNAKEYAML_PATH}
 rmdir lib
 echo " >> Run 'java -jar ${PWD}/jobdsl.jar <file.dsl>' to generate locally the Jenkins XML config"
-popd "${SCRIPT_DIR}" 2>/dev/null
+popd >/dev/null
