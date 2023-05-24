fa45fc24d79851766a809188ef0a55b11edfd5b3
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-05-24 18:24:03 +0200

    Use the helper script in github actions
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/dsl/tools/setup_local_generation.bash
+++ jenkins-scripts/dsl/tools/setup_local_generation.bash
@@ -1,9 +1,12 @@
 #!/bin/bash 
 set -e
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 SNAKEYAML_PATH="lib/snakeyaml.jar"
 JOB_DSL_VER="1.77"
 
+pushd "${SCRIPT_DIR}" 2>/dev/null
 echo " * Download job-dsl-core jar"
 curl -ssl https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/${JOB_DSL_VER}/job-dsl-core-${JOB_DSL_VER}-standalone.jar -o jobdsl.jar
 echo " * Download snakeyaml jar"
@@ -16,3 +19,4 @@ tar uf jobdsl.jar ${SNAKEYAML_PATH}
 rm -fr ${SNAKEYAML_PATH}
 rmdir lib
 echo " >> Run 'java -jar ${PWD}/jobdsl.jar <file.dsl>' to generate locally the Jenkins XML config"
+popd "${SCRIPT_DIR}" 2>/dev/null
