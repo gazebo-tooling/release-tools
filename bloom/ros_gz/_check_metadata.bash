8238d40eaaa3b557b27fad34b4c704b489a2496d
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-06-14 19:14:46 +0200

    Add a check for metadata in ros_gz-release.py.bash
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- bloom/ros_gz/_check_metadata.bash
+++ bloom/ros_gz/_check_metadata.bash
@@ -11,19 +11,23 @@ fi
 RELEASE_REPO_URL=${1}
 ROS_DISTRO=${2}
 
+echo " * Checking that ${RELEASE_REPO_URL} has a correct gzgarden metadata for ROS ${ROS_DISTRO}"
+
 TMP_DIR=$(mktemp -d) 
-git clone "${RELEASE_REPO_URL}" "${TMP_DIR}"
-pushd "${TMP_DIR}" 2> /dev/null || exit
-cd "${TMP_DIR}" || exit
+git clone -q "${RELEASE_REPO_URL}" "${TMP_DIR}"
+pushd "${TMP_DIR}" 2> /dev/null > /dev/null || exit
 DEBIAN_LATEST_TAG=$(git for-each-ref --sort=creatordate --format '%(refname)' refs/tags | \
           grep "debian/ros-${ROS_DISTRO}" | \
           tail -1 | sed 's:refs/tags/::')
-git checkout "${DEBIAN_LATEST_TAG}"
-dpkg-parsechangelog -SSource 
-if ! dpkg-parsechangelog -SSource | grep -q 'gzgarden' > /dev/null; then
-  echo "Latest changelog entry does not have gzgarden name"
-  echo "in the repo tag ${DEBIAN_LATEST_TAG} for repo ${RELEASE_REPO_URL}"
-  echo "Maybe the rename in the bloom templates was not done."
+git checkout -q "${DEBIAN_LATEST_TAG}"
+if ! dpkg-parsechangelog -SSource 2> /dev/null | grep -q 'gzgarden'; then
+  echo " !! Latest changelog entry does not have gzgarden name"
+  echo " !! in the repo tag '${DEBIAN_LATEST_TAG}' for repo '${RELEASE_REPO_URL}'"
+  echo -n " !! Latest changelog is:"
+  dpkg-parsechangelog -SSource 2> /dev/null || exit
+  echo " !! Maybe the rename in the bloom templates was not done."
   exit 1
 fi
-popd 2>/dev/null || exit
+popd >/dev/null || exit
+
+echo "  + Changelog has correct gzgarden metadata for ${DEBIAN_LATEST_TAG}"
