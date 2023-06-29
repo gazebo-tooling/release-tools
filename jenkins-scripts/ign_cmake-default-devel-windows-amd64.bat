608be8d584710d793ab8f0a8ece0a880f24417b0
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-06-29 17:41:40 +0200

    Enable GZ_ENABLE_RELOCATABLE_INSTALL in CI for gz-cmake3
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/ign_cmake-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_cmake-default-devel-windows-amd64.bat
@@ -9,4 +9,10 @@ set COLCON_PACKAGE=gz-cmake
 set COLCON_AUTO_MAJOR_VERSION=true
 set COLCON_PACKAGE_EXTRA_CMAKE_ARGS="-DBUILDSYSTEM_TESTING:BOOL=True"
 
+setlocal ENABLEDELAYEDEXPANSION
+for /f %%i in ('python "%SCRIPT_DIR%\tools\detect_cmake_major_version.py" "%WORKSPACE%\%VCS_DIRECTORY%\CMakeLists.txt"') do set PKG_MAJOR_VERSION=%%i
+if !PKG_MAJOR_VERSION! gtr 3 (
+  set COLCON_PACKAGE_EXTRA_CMAKE_ARGS="%COLCON_PACKAGE_EXTRA_CMAKE_ARGS% -DGZ_ENABLE_RELOCATABLE_INSTALL=True"
+)
+
 call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
