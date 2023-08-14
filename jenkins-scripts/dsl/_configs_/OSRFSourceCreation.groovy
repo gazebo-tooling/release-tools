4c54f8ce7f970edab12b1f4688511579e0b6406f
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-08-14 13:14:45 +0200

    DSL class for the tarball generation
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

---
+++ jenkins-scripts/dsl/_configs_/OSRFSourceCreation.groovy
@@ -0,0 +1,60 @@
+package _configs_
+
+import javaposse.jobdsl.dsl.Job
+import _configs_.Globals
+
+class OSRFSourceCreation
+{
+  static void addParameters(Job job, Map default_params = [:])
+  {
+    job.with
+    {
+      parameters {
+        stringParam("VERSION",
+                    default_params.find{ it.key == "VERSION"}?.value,
+                    "Packages version to be built or nightly (enable nightly build mode)")
+        stringParam("RELEASE_VERSION",
+                    default_params.find{ it.key == "RELEASE_VERSION"}?.value,
+                    "Packages release version")
+        stringParam("RELEASE_REPO_BRANCH",
+                    default_params.find{ it.key == "RELEASE_REPO_BRANCH"}?.value,
+                    "Branch from the -release repo to be used")
+        stringParam("UPLOAD_TO_REPO",
+                    default_params.find{ it.key == "UPLOAD_TO_REPO"}?.value,
+                    "OSRF repo name to upload the package to: stable | prerelease | nightly | none (for testing proposes)")
+      }
+    }
+  }
+
+  static void create(Job job, Map default_params = [:])
+  {
+    OSRFLinuxBuildPkgBase.create(job)
+    GenericRemoteToken.create(job)
+    OSRFSourceCreation.addParameters(job, default_params)
+
+    job.with
+    {
+      wrappers {
+        preBuildCleanup()
+      }
+
+      properties {
+        priority 100
+      }
+
+      steps {
+        systemGroovyCommand("""\
+          build.setDescription(
+          '<b>' + build.buildVariableResolver.resolve('VERSION') + '-' +
+          build.buildVariableResolver.resolve('RELEASE_VERSION') + '</b>' +
+          '<br />' +
+          'branch: ' + build.buildVariableResolver.resolve('RELEASE_REPO_BRANCH') + ' | ' +
+          'upload to: ' + build.buildVariableResolver.resolve('UPLOAD_TO_REPO') +
+          '<br />' +
+          'RTOOLS_BRANCH: ' + build.buildVariableResolver.resolve('RTOOLS_BRANCH'));
+          """.stripIndent()
+        )
+      }
+    }
+  }
+}
