1cf7426aef9fff16b6f444337b888221314e034e
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-08-30 19:21:30 +0200

    Support for GAZEBO_DISTRO in ros_gz bloom
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- bloom/ros_gz/_check_metadata.bash
+++ bloom/ros_gz/_check_metadata.bash
@@ -1,17 +1,15 @@
-#!/bin/bash
-
-set -e
-
+#!/bin/bash -e
 
 if [[ $# -lt 2 ]]; then
-  echo "Usage: $0 <RELEASE_REPO_URL> <ROS_DISTRO>"
+  echo "Usage: $0 <RELEASE_REPO_URL> <ROS_DISTRO> <GAZEBO_DISTRO>"
   exit 1
 fi
 
 RELEASE_REPO_URL=${1}
 ROS_DISTRO=${2}
+GAZEBO_DISTRO=${3}
 
-echo " * Checking that ${RELEASE_REPO_URL} has a correct gzgarden metadata for ROS ${ROS_DISTRO}"
+echo " * Checking that ${RELEASE_REPO_URL} has a correct gz${GAZEBO_DISTRO} metadata for ROS ${ROS_DISTRO}"
 
 TMP_DIR=$(mktemp -d) 
 git clone -q "${RELEASE_REPO_URL}" "${TMP_DIR}"
@@ -20,8 +18,8 @@ DEBIAN_LATEST_TAG=$(git for-each-ref --sort=creatordate --format '%(refname)' re
           grep "debian/ros-${ROS_DISTRO}" | \
           tail -1 | sed 's:refs/tags/::')
 git checkout -q "${DEBIAN_LATEST_TAG}"
-if ! dpkg-parsechangelog -SSource 2> /dev/null | grep -q 'gzgarden'; then
-  echo " !! Latest changelog entry does not have gzgarden name"
+if ! dpkg-parsechangelog -SSource 2> /dev/null | grep -q "gz${GAZEBO_DISTRO}"; then
+  echo " !! Latest changelog entry does not have gz${GAZEBO_DISTRO} name"
   echo " !! in the repo tag '${DEBIAN_LATEST_TAG}' for repo '${RELEASE_REPO_URL}'"
   echo -n " !! Latest changelog is:"
   dpkg-parsechangelog -SSource 2> /dev/null || exit
@@ -30,4 +28,4 @@ if ! dpkg-parsechangelog -SSource 2> /dev/null | grep -q 'gzgarden'; then
 fi
 popd >/dev/null || exit
 
-echo "  + Changelog has correct gzgarden metadata for ${DEBIAN_LATEST_TAG}"
+echo "  + Changelog has correct gz${GAZEBO_DISTRO} metadata for ${DEBIAN_LATEST_TAG}"
