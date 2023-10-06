eccf9ef18143bccad295451550ffe0584b4319e8
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-10-06 19:32:12 +0200

    Update abichecker to work with gz-*
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/docker/ignition-abichecker.bash
+++ jenkins-scripts/docker/ignition-abichecker.bash
@@ -14,13 +14,13 @@ if [[ -z ${DISTRO} ]]; then
   exit 1
 fi
 
-# In ignition this variable is passed directly from the DSL job creation
+# In Gazebo this variable is passed directly from the DSL job creation
 if [[ -z ${ABI_JOB_SOFTWARE_NAME} ]]; then
   echo "ABI_JOB_SOFTWARE_NAME not set!"
   exit 1
 fi
 
-# convert from ign-package to GZ_PACKAGE_DEPENDENCIES
+# convert from gz-package to GZ_PACKAGE_DEPENDENCIES
 GZ_NAME_PREFIX=$(\
   echo ${ABI_JOB_SOFTWARE_NAME} | tr '[:lower:]-' '[:upper:]_')
 ABI_JOB_PKG_DEPENDENCIES_VAR_NAME=${GZ_NAME_PREFIX}_DEPENDENCIES
@@ -42,7 +42,7 @@ export ${GZ_NAME_PREFIX}_MAJOR_VERSION=${GZ_NAME_PREFIX_MAJOR_VERSION}
 #
 # Set ABI_JOB_HEADER_PREFIX to match the last subfolder:
 #   gazebo6, math6, math7, sim7
-if [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-gazebo" && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 7 ]]
+if [[ "${ABI_JOB_SOFTWARE_NAME}" = "gz-gazebo" && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 7 ]]
 then
   # special case for gz-sim7+
   export ABI_JOB_HEADER_PREFIX=sim[0-9]*
@@ -51,29 +51,29 @@ else
   export ABI_JOB_HEADER_PREFIX=${ABI_JOB_SOFTWARE_NAME/[ignz]*-/}[0-9]*
 fi
 
-# check if NEED_C17_COMPILER should be set
+# check if NEED_C17_COMPILER should be set. Only needed for Bionic so only care about Citadel
+# versions
 if [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-gazebo" ]] || \
   [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-physics" ]] || \
   [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-sensors" ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-common"    && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 3 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-gui"       && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 1 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-math"      && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 6 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-msgs"      && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 3 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-plugin"    && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 1 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-rendering" && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 1 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-transport" && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 6 ]]
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-common" ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-gui" ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-math" ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-msgs" ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-plugin" ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-rendering" ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-transport" ]]
 then
   export NEED_C17_COMPILER=true
 fi
 
 # check if OGRE-2.2 include paths are needed
-if [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-gazebo"   && ${GZ_NAME_PREFIX_MAJOR_VERSION} -eq 6 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-gui"       && ${GZ_NAME_PREFIX_MAJOR_VERSION} -eq 6 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-launch"    && ${GZ_NAME_PREFIX_MAJOR_VERSION} -eq 5 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-sensors"   && ${GZ_NAME_PREFIX_MAJOR_VERSION} -eq 6 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-rendering" && ${GZ_NAME_PREFIX_MAJOR_VERSION} -eq 6 ]]
+if [[ "${ABI_JOB_SOFTWARE_NAME}" = "gz-sim"      && ${GZ_NAME_PREFIX_MAJOR_VERSION} -eq 6 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "gz-gui"       && ${GZ_NAME_PREFIX_MAJOR_VERSION} -eq 6 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "gz-launch"    && ${GZ_NAME_PREFIX_MAJOR_VERSION} -eq 5 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "gz-sensors"   && ${GZ_NAME_PREFIX_MAJOR_VERSION} -eq 6 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "gz-rendering" && ${GZ_NAME_PREFIX_MAJOR_VERSION} -eq 6 ]]
 then
-
   # OGRE 2.2 is packaged as "Ogre-Next" on jammy
   if [[ "${DISTRO}" == "jammy" ]]
   then
@@ -92,11 +92,11 @@ then
   fi
 fi
 
-if [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-gazebo"   && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 6 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-gui"       && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 7 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-launch"    && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 6 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-sensors"   && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 7 ]] || \
-  [[ "${ABI_JOB_SOFTWARE_NAME}" = "ign-rendering" && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 7 ]]
+if [[ "${ABI_JOB_SOFTWARE_NAME}" = "gz-sim"      && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 6 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "gz-gui"       && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 7 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "gz-launch"    && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 6 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "gz-sensors"   && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 7 ]] || \
+  [[ "${ABI_JOB_SOFTWARE_NAME}" = "gz-rendering" && ${GZ_NAME_PREFIX_MAJOR_VERSION} -ge 7 ]]
 then
   # -fPIC needed to compile Qt
   export ABI_JOB_EXTRA_GCC_OPTIONS="-fPIC"
