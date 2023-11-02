035f4280fa5329cc650d295bb07cc78f6de847aa
Steve Peters <scpeters@openrobotics.org>
2023-11-02 13:27:01 -0700

    Remove unneeded variable check
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- jenkins-scripts/lib/project-install-homebrew.bash
+++ jenkins-scripts/lib/project-install-homebrew.bash
@@ -46,8 +46,7 @@ echo '# BEGIN SECTION: setup the osrf/simulation tap'
 brew tap osrf/simulation
 echo '# END SECTION'
 
-if [[ -n "${RTOOLS_BRANCH}" ]] && \
-   python3 "${SCRIPT_DIR}/tools/detect_ci_matching_branch.py" "${RTOOLS_BRANCH}"
+if python3 "${SCRIPT_DIR}/tools/detect_ci_matching_branch.py" "${RTOOLS_BRANCH}"
 then
   echo "# BEGIN SECTION: trying to checkout branch ${RTOOLS_BRANCH} from osrf/simulation"
   pushd "$(brew --repo osrf/simulation)"
