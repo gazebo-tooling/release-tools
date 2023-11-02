bf87798e8a0b94410fb9a41e896a22edabfb7c9f
Steve Peters <scpeters@openrobotics.org>
2023-11-02 13:18:28 -0700

    Apply linting fixes from code review
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>
    
    Co-authored-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/lib/project-install-homebrew.bash
+++ jenkins-scripts/lib/project-install-homebrew.bash
@@ -47,12 +47,12 @@ brew tap osrf/simulation
 echo '# END SECTION'
 
 if [[ -n "${RTOOLS_BRANCH}" ]] && \
-   python3 ${SCRIPT_DIR}/tools/detect_ci_matching_branch.py "${RTOOLS_BRANCH}"
+   python3 "${SCRIPT_DIR}/tools/detect_ci_matching_branch.py" "${RTOOLS_BRANCH}"
 then
   echo "# BEGIN SECTION: trying to checkout branch ${RTOOLS_BRANCH} from osrf/simulation"
-  pushd $(brew --repo osrf/simulation)
-  git fetch origin ${RTOOLS_BRANCH} || true
-  git checkout ${RTOOLS_BRANCH} || true
+  pushd "$(brew --repo osrf/simulation)"
+  git fetch origin "${RTOOLS_BRANCH}" || true
+  git checkout "${RTOOLS_BRANCH}" || true
   popd
   echo '# END SECTION'
 fi
