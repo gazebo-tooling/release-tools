429d6d46254541b3e210abb27c90f243d0c2dbb2
Jose Luis Rivero <jrivero@osrfoundation.org>
2024-01-10 18:18:11 +0100

    Use gz- prefix for -source jobs
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- release.py
+++ release.py
@@ -523,6 +523,8 @@ def go(argv):
     if not args.release_version:
         args.release_version = 1
 
+    package_alias_force_gz = args.package_alias.replace('ignition-','gz-')
+
     print(f"Downloading releasing info for {args.package}")
     # Sanity checks and dicover supported distributions before proceed.
     repo_dir, args.release_repo_branch = download_release_repository(args.package, args.release_repo_branch)
@@ -634,7 +636,7 @@ def go(argv):
         params['SOURCE_REPO_REF'] = tag_repo(args) \
             if not args.source_repo_ref else args.source_repo_ref
 
-        call_jenkins_build(f"{args.package_alias}-source",
+        call_jenkins_build(f'{package_alias_force_gz}-source',
                            params,
                            'Source',
                            args.version)
