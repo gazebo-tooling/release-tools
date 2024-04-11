29fc8b37fcd98267a151cacbebd3c183bebb794b
Addisu Z. Taddese <addisu@openrobotics.org>
2024-04-11 15:54:15 -0500

    Integrate cmake_to_gtest_junit_output script
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- jenkins-scripts/docker/lib/_generic_linux_compilation_build.sh.bash
+++ jenkins-scripts/docker/lib/_generic_linux_compilation_build.sh.bash
@@ -96,7 +96,8 @@ if $GENERIC_ENABLE_TESTS; then
   echo '# BEGIN SECTION: running tests'
   init_stopwatch TEST
   mkdir -p \$HOME
-  make test ARGS="-VV ${BUILDING_EXTRA_MAKETEST_PARAMS}" || true
+  make test ARGS="-VV ${BUILDING_EXTRA_MAKETEST_PARAMS} --output-junit cmake_junit_output.xml" || true
+  python3 tools/cmake_to_gtest_junit_output.py cmake_junit_output.xml test_results  || true
   stop_stopwatch TEST
   echo '# END SECTION'
 else
