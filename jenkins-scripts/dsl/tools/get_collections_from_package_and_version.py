3c62cde6ba4103eb7ff4f73e6eb6335997a63ccf
Jose Luis Rivero <jrivero@osrfoundation.org>
2024-06-07 13:15:11 +0200

    Add the __main__ function
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/dsl/tools/get_collections_from_package_and_version.py
+++ jenkins-scripts/dsl/tools/get_collections_from_package_and_version.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 import sys
 import yaml
 
 
 # Function to find the collection name based on lib name and major version
-def find_collection(data, lib_name, major_version):
+def find_collection(data, lib_name, major_version) -> list:
   instances = []
 
   for collection in data['collections']:
@@ -15,21 +14,29 @@ def find_collection(data, lib_name, major_version):
   return instances
 
 
-def get_major_version(version):
+def get_major_version(version) -> int:
   elements = version.split('.')
   return int(elements[0])
 
 
-if len(sys.argv) < 3:
-  print(f"Usage: {sys.argv[0]} <lib_name> <major_version> <collection-yaml-file>")
-  sys.exit(1)
+def main() -> int:
+  if len(sys.argv) < 3:
+    print(f"Usage: {sys.argv[0]} <lib_name> <major_version> <collection-yaml-file>")
+    return 2
 
-lib_name = sys.argv[1]
-version = sys.argv[2]
-yaml_file = sys.argv[3]
+  lib_name = sys.argv[1]
+  version = sys.argv[2]
+  yaml_file = sys.argv[3]
 
-with open(yaml_file, 'r') as file:
-  data = yaml.safe_load(file)
+  with open(yaml_file, 'r') as file:
+    data = yaml.safe_load(file)
 
-collection_names = find_collection(data, lib_name, get_major_version(version))
-print(f"{' '.join(collection_names)}")
+  collection_names = find_collection(data, lib_name, get_major_version(version))
+  if not collection_names:
+    return 1
+  print(f"{' '.join(collection_names)}")
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main())
