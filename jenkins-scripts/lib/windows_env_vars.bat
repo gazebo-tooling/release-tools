afd013afcd7604566eceba5e5461805c4718d2bd
Jose Luis Rivero <jrivero@osrfoundation.org>
2024-11-07 17:47:46 +0100

    Fix var declaration
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/lib/windows_env_vars.bat
+++ jenkins-scripts/lib/windows_env_vars.bat
@@ -1,33 +1,32 @@
-if exist D:\vcpkg (
-  set VCPKG_DIR=D:\vcpkg
-) else if exist C:\vcpkg (
-    set VCPKG_DIR=C:\vcpkg
+if defined USE_PIXI (
+  set "PIXI_VERSION=0.30.0"
+  set "PIXI_URL=https://github.com/prefix-dev/pixi/releases/download/v%PIXI_VERSION%/pixi-x86_64-pc-windows-msvc.exe"
+  set "PIXI_TMPDIR=%TMP%\pixi-%RANDOM%"
+  set "PIXI_TMP=%PIXI_TMPDIR%\pixi.exe"
+  set "PIXI_PROJECT_PATH=%TMP%\pixi\project"
 ) else (
-   echo "Can not find a vcpkg installation"
-   exit -1
-)
+  if exist D:\vcpkg (
+    set VCPKG_DIR=D:\vcpkg
+  ) else if exist C:\vcpkg (
+      set VCPKG_DIR=C:\vcpkg
+  ) else (
+     echo "Can not find a vcpkg installation"
+     exit -1
+  )
 
-if DEFINED MAKE_JOBS (
-  set VCPKG_MAX_CONCURRENCY=%MAKE_JOBS%
+  if DEFINED MAKE_JOBS (
+    set VCPKG_MAX_CONCURRENCY=%MAKE_JOBS%
+  )
+  set VCPKG_OSRF_DIR=%VCPKG_DIR%\osrf_vcpkg_ports
+  set VCPKG_INSTALLED_FILES_DIR=%VCPKG_DIR%\installed
+  set VCPKG_CMD=%VCPKG_DIR%\vcpkg.exe
+  set VCPKG_CMAKE_TOOLCHAIN_FILE=%VCPKG_DIR%/scripts/buildsystems/vcpkg.cmake
+  if NOT DEFINED VCPKG_SNAPSHOT (
+    :: see https://github.com/microsoft/vcpkg/releases
+    set VCPKG_SNAPSHOT=2022.02.23
+  )
+  :: Set of common gz dependencies expected up to Garden
+  set VCPKG_DEPENDENCIES_LEGACY=assimp boost-core bullet3 ccd console-bridge cppzmq cuda curl dlfcn-win32 eigen3 fcl ffmpeg freeimage gdal gflags glib gts jsoncpp libyaml libzip ogre ogre2 ogre22 openssl protobuf pybind11 qt5 qt5-winextras qwt spdlog sqlite3 tinyxml2 zeromq
 )
-set VCPKG_OSRF_DIR=%VCPKG_DIR%\osrf_vcpkg_ports
-set VCPKG_INSTALLED_FILES_DIR=%VCPKG_DIR%\installed
-set VCPKG_CMD=%VCPKG_DIR%\vcpkg.exe
-set VCPKG_CMAKE_TOOLCHAIN_FILE=%VCPKG_DIR%/scripts/buildsystems/vcpkg.cmake
-if NOT DEFINED VCPKG_SNAPSHOT (
-  :: see https://github.com/microsoft/vcpkg/releases
-  set VCPKG_SNAPSHOT=2022.02.23
-)
-:: Set of common gz dependencies expected up to Garden
-set VCPKG_DEPENDENCIES_LEGACY=assimp boost-core bullet3 ccd console-bridge cppzmq cuda curl dlfcn-win32 eigen3 fcl ffmpeg freeimage gdal gflags glib gts jsoncpp libyaml libzip ogre ogre2 ogre22 openssl protobuf pybind11 qt5 qt5-winextras qwt spdlog sqlite3 tinyxml2 zeromq
-
-set "CONDA_BASE_PATH=%TMP%\conda"
-set "CONDA_CMD=%miniforge_install%\condabin\conda.bat"
-
-set "PIXI_VERSION=0.30.0"
-set "PIXI_URL=https://github.com/prefix-dev/pixi/releases/download/v%PIXI_VERSION%/pixi-x86_64-pc-windows-msvc.exe"
-set "PIXI_TMPDIR=%TMP%\pixi-%RANDOM%"
-set "PIXI_TMP=%PIXI_TMPDIR%\pixi.exe"
-set "PIXI_PROJECT_PATH=%CONDA_BASE_PATH%\project"
 
 goto :EOF
