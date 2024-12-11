a269d2401181230d7fad2d180647368eb7c5c8a7
Jose Luis Rivero <jrivero@honurobotics.com>
2024-12-11 17:35:27 +0100

    Do not fail if DBG_LAST_BUILD_FILES in unset
    Signed-off-by: Jose Luis Rivero <jrivero@honurobotics.com>

--- jenkins-scripts/lib/windows_library.bat
+++ jenkins-scripts/lib/windows_library.bat
@@ -426,7 +426,12 @@ call hooks.bat
 :: ERRORS in hooks will make the build to fail. Be permissive
 :: if errorlevel 1 exit %EXTRA_EXIT_PARAM% 1
 popd
-echo call %PIXI_PROJECT_PATH%\hooks.bat > %DBG_LAST_BUILD_FILE%
+
+:: TODO: DBG_LAST_BUILD_FILE support for debugging only if local_build.bat
+:: was called. Support the reproducible in Jenkins too
+if defined DBG_LAST_BUILD_FILE (
+  echo call %PIXI_PROJECT_PATH%\hooks.bat > %DBG_LAST_BUILD_FILE%
+)
 goto :EOF
 
 :: ##################################
