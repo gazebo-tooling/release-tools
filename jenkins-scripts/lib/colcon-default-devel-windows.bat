8a27594e73b5863dbd4dad38bbc4c10dac9e9201
Jose Luis Rivero <jrivero@honurobotics.com>
2024-12-12 12:26:27 +0100

    Scape CONDA_PREFIX be sure if it working
    Signed-off-by: Jose Luis Rivero <jrivero@honurobotics.com>

--- jenkins-scripts/lib/colcon-default-devel-windows.bat
+++ jenkins-scripts/lib/colcon-default-devel-windows.bat
@@ -91,8 +91,15 @@ if defined USE_PIXI (
 
 
   echo # BEGIN SECTION: pixi: custom environment variable for gz
-  set OGRE_RESOURCE_PATH=%CONDA_PREFIX%\Library\bin
-  set OGRE2_RESOURCE_PATH=%CONDA_PREFIX%\Library\bin\OGRE-Next
+  set "OGRE_RESOURCE_PATH=%CONDA_PREFIX%\Library\bin"
+  if "%%CONDA_PREFIX%%"=="" (
+    echo # BEGIN SECTION: ERROR: CONDA_PREFIX is not set
+    echo CONDA_PREFIX variable was not set. Please set it before calling this script
+    echo # END SECTION
+    goto :error
+  )
+  set OGRE_RESOURCE_PATH=%%CONDA_PREFIX%%\Library\bin
+  set OGRE2_RESOURCE_PATH=%%CONDA_PREFIX%%\Library\bin\OGRE-Next
   echo # END SECTION
 ) else (
   :: Call vcvarsall and all the friends
