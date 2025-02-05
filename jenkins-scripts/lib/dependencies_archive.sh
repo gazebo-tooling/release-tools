077fcad52089858460f0073142edb903f7374373
Jose Luis Rivero <jrivero@osrfoundation.org>
2025-02-05 16:05:36 +0100

    Remove jobs and configurations from Gazebo
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/lib/dependencies_archive.sh
+++ jenkins-scripts/lib/dependencies_archive.sh
@@ -3,19 +3,6 @@
 # **** WARNING ***** : when modifying this file
 # **** WARNING ***** : any trailing whitespaces will break dependencies scapes
 
-# Dart flags. Enable it by default unless compiled from source
-if [ -z ${DART_COMPILE_FROM_SOURCE} ]; then
-   DART_COMPILE_FROM_SOURCE=false
-fi
-
-if [ -z ${DART_FROM_PKGS} ]; then
-  DART_FROM_PKGS=false
-fi
-
-if ${DART_COMPILE_FROM_SOURCE}; then
-    DART_FROM_PKGS=false
-fi
-
 pythonv="python3"
 if [[ ${DISTRO} == 'trusty' ]]; then
   pythonv="python"
@@ -99,107 +86,10 @@ else
     SDFORMAT_BASE_DEPENDENCIES="${SDFORMAT_NO_GZ_DEPENDENCIES}"
 fi
 
-# GAZEBO related dependencies. Default value points to the development version
-# of gazebo, it is being used by compile from source tutorial
-if [[ -z ${GAZEBO_MAJOR_VERSION} ]]; then
-    GAZEBO_MAJOR_VERSION=11
-fi
-
-# Need to explicit define to use old sdformat package
-if [[ -z ${USE_OLD_SDFORMAT} ]]; then
-    USE_OLD_SDFORMAT=false
-fi
-
-if ${USE_OLD_SDFORMAT}; then
-    sdformat_pkg="sdformat"
-elif [[ ${GAZEBO_MAJOR_VERSION} -ge 11 ]]; then
-    sdformat_pkg="libsdformat9-dev"
-elif [[ ${GAZEBO_MAJOR_VERSION} -ge 9 ]]; then
-    sdformat_pkg="libsdformat6-dev"
-fi
-
 ogre_pkg="libogre-1.9-dev"
 if [[ ${DISTRO} != 'xenial' ]]; then
   ogre_pkg="libogre-1.9-dev libogre-2.1-dev"
 fi
-
-# Starting from utopic, we are using the bullet provided by ubuntu
-bullet_pkg="libbullet-dev"
-
-# choose dart version
-if $DART_FROM_PKGS; then
-  dart_pkgs="libdart-utils-urdf-dev \
-             libdart-external-odelcpsolver-dev \
-             libdart-external-ikfast-dev"
-  if [[ ${GAZEBO_MAJOR_VERSION} -ge 11 ]]; then
-    dart_pkgs="${dart_pkgs} libdart-collision-bullet-dev \
-               libdart-collision-ode-dev"
-  fi
-  # If on Bionic/Xenial use custom dart packages, these have a different name
-  # dart -> dart6
-  if [[ ${DISTRO} == 'bionic' ]] || [[ ${DISTRO} == 'xenial' ]]; then
-    dart_pkgs="${dart_pkgs//dart/dart6}"
-  fi
-fi
-
-# --------------------------------------
-# GAZEBO classic
-# --------------------------------------
-
-GAZEBO_BASE_DEPENDENCIES_NO_SDFORMAT="libfreeimage-dev     \\
-                          libprotoc-dev                    \\
-                          libprotobuf-dev                  \\
-                          protobuf-compiler                \\
-                          freeglut3-dev                    \\
-                          libcurl4-openssl-dev             \\
-                          libtinyxml-dev                   \\
-                          libtinyxml2-dev                  \\
-                          libtar-dev                       \\
-                          libtbb-dev                       \\
-                          ${ogre_pkg}                      \\
-                          libxml2-dev                      \\
-                          pkg-config                       \\
-                          qtbase5-dev                      \\
-                          libqwt-qt5-dev                   \\
-                          libltdl-dev                      \\
-                          libgts-dev                       \\
-                          libboost-thread-dev              \\
-                          libboost-system-dev              \\
-                          libboost-filesystem-dev          \\
-                          libboost-program-options-dev     \\
-                          libboost-regex-dev               \\
-                          libboost-iostreams-dev           \\
-                          ${bullet_pkg}                    \\
-                          libsimbody-dev                   \\
-                          ${dart_pkgs}"
-
-if [[ ${GAZEBO_MAJOR_VERSION} -ge 11 ]]; then
-    GAZEBO_BASE_DEPENDENCIES_NO_SDFORMAT="${GAZEBO_BASE_DEPENDENCIES_NO_SDFORMAT} \\
-                                         libignition-common3-dev \\
-                                         libignition-fuel-tools4-dev \\
-                                         libignition-transport8-dev \\
-                                         libignition-math6-dev \\
-                                         libignition-msgs5-dev"
-elif [[ ${GAZEBO_MAJOR_VERSION} -ge 9 ]]; then
-    GAZEBO_BASE_DEPENDENCIES_NO_SDFORMAT="${GAZEBO_BASE_DEPENDENCIES_NO_SDFORMAT} \\
-                                         libignition-common-dev \\
-                                         libignition-fuel-tools1-dev \\
-                                         libignition-transport4-dev \\
-                                         libignition-math4-dev \\
-                                         libignition-msgs-dev"
-fi
-
-GAZEBO_BASE_DEPENDENCIES="${GAZEBO_BASE_DEPENDENCIES_NO_SDFORMAT} \\
-                          ${sdformat_pkg}"
-
-GAZEBO_EXTRA_DEPENDENCIES="libavformat-dev  \\
-                           libavcodec-dev   \\
-                           libgdal-dev      \\
-                           libgraphviz-dev  \\
-                           libswscale-dev   \\
-                           libavdevice-dev   \\
-                           ruby-ronn"
-
 
 if [[ -z $ROS_DISTRO ]]; then
   echo "------------------------------------------------------------"
@@ -207,39 +97,6 @@ if [[ -z $ROS_DISTRO ]]; then
   echo "skipping ROS related variables"
   echo "------------------------------------------------------------"
 else
-  # default versions for every ROS distribution
-  if [[ -z ${GAZEBO_VERSION_FOR_ROS} ]]; then
-    case ${ROS_DISTRO} in
-      melodic)
-        GAZEBO_VERSION_FOR_ROS="9"
-      ;;
-      noetic)
-        GAZEBO_VERSION_FOR_ROS="11"
-      ;;
-      # ROS 2
-      foxy)
-        GAZEBO_VERSION_FOR_ROS="11"
-      ;;
-      rolling)
-        GAZEBO_VERSION_FOR_ROS="11"
-    esac
-  fi
-
-  case ${GAZEBO_VERSION_FOR_ROS} in
-     *)
-      # Rolling is supported only on Jammy. Gazebo11 packages from Jammy
-      # are coming directly from Ubuntu repositories, package names are
-      # unversioned
-      if [[ ${DISTRO} == 'jammy' ]]; then
-        _GZ_ROS_PACKAGES="libgazebo-dev gazebo"
-      else
-        # both packages see  http://answers.ros.org/question/217970
-        _GZ_ROS_PACKAGES="libgazebo${GAZEBO_VERSION_FOR_ROS}-dev \\
-                         gazebo${GAZEBO_VERSION_FOR_ROS}"
-      fi
-    ;;
-  esac
-
   # colcon has no python2 candidate
   if ${ROS2}; then
     ROS_CATKIN_BASE="${pythonv}-dev                      \\
@@ -262,110 +119,6 @@ else
                     ${pythonv}-rospkg            \\
                     ${pythonv}-vcstools"
   fi
-
-  #
-  # ROS_GAZEBO_PKGS DEPENDECIES
-  #
-  ROS_GAZEBO_PKGS_COMMON_DEPS="${ROS_CATKIN_BASE}                \\
-                               libtinyxml-dev                    \\
-                               ros-${ROS_DISTRO}-std-msgs        \\
-                               ros-${ROS_DISTRO}-trajectory-msgs \\
-                               ros-${ROS_DISTRO}-sensor-msgs     \\
-                               ros-${ROS_DISTRO}-geometry-msgs"
-
-  if ${ROS2}; then
-    # Most of the base deps (ament, lint, rclcpp) are already included in
-    # ROS_CATKIN_BASE. TODO: change var name
-    ROS_GAZEBO_PKGS_DEPENDENCIES="${ROS_GAZEBO_PKGS_COMMON_DEPS}              \\
-                                  ros-${ROS_DISTRO}-builtin-interfaces        \\
-                                  ros-${ROS_DISTRO}-rosidl-default-runtime    \\
-                                  ros-${ROS_DISTRO}-rosidl-default-generators"
-  else
-    #
-    # ROS1
-    #
-    ROS_GAZEBO_PKGS_DEPENDENCIES="${ROS_GAZEBO_PKGS_COMMON_DEPS}            \\
-                                  ros-${ROS_DISTRO}-ros                     \\
-                                  ros-${ROS_DISTRO}-catkin                  \\
-                                  ros-${ROS_DISTRO}-pluginlib               \\
-                                  ros-${ROS_DISTRO}-roscpp                  \\
-                                  ros-${ROS_DISTRO}-angles                  \\
-                                  ros-${ROS_DISTRO}-camera-info-manager     \\
-                                  ros-${ROS_DISTRO}-cmake-modules           \\
-                                  ros-${ROS_DISTRO}-controller-manager      \\
-                                  ros-${ROS_DISTRO}-control-toolbox         \\
-                                  ros-${ROS_DISTRO}-tf                      \\
-                                  ros-${ROS_DISTRO}-cv-bridge               \\
-                                  ros-${ROS_DISTRO}-diagnostic-updater      \\
-                                  ros-${ROS_DISTRO}-dynamic-reconfigure     \\
-                                  ros-${ROS_DISTRO}-image-transport         \\
-                                  ros-${ROS_DISTRO}-joint-limits-interface  \\
-                                  ros-${ROS_DISTRO}-message-generation      \\
-                                  ros-${ROS_DISTRO}-nav-msgs                \\
-                                  ros-${ROS_DISTRO}-nodelet                 \\
-                                  ros-${ROS_DISTRO}-pcl-conversions         \\
-                                  ros-${ROS_DISTRO}-polled-camera           \\
-                                  ros-${ROS_DISTRO}-rosconsole              \\
-                                  ros-${ROS_DISTRO}-rosgraph-msgs           \\
-                                  ros-${ROS_DISTRO}-std-srvs                \\
-                                  ros-${ROS_DISTRO}-tf                      \\
-                                  ros-${ROS_DISTRO}-tf2-ros                 \\
-                                  ros-${ROS_DISTRO}-transmission-interface  \\
-                                  ros-${ROS_DISTRO}-urdf                    \\
-                                  ros-${ROS_DISTRO}-xacro"
-
-    ROS_GAZEBO_PKGS_EXAMPLE_DEPS="ros-${ROS_DISTRO}-xacro \\
-                                 ${ROS_GAZEBO_PKGS_EXAMPLE_DEPS}"
-  fi
-
-  ROS_GAZEBO_PKGS_DEPENDENCIES="${ROS_GAZEBO_PKGS_DEPENDENCIES} \\
-                                ${_GZ_ROS_PACKAGES}"
-
-  if [[ -n ${USE_DEFAULT_GAZEBO_VERSION_FOR_ROS} ]] && ${USE_DEFAULT_GAZEBO_VERSION_FOR_ROS}; then
-    ROS_GAZEBO_PKGS="ros-${ROS_DISTRO}-gazebo-msgs \
-                     ros-${ROS_DISTRO}-gazebo-plugins \
-                     ros-${ROS_DISTRO}-gazebo-ros \
-                     ros-${ROS_DISTRO}-gazebo-ros-pkgs"
-    # no ros-control in ROS2 yet
-    if ! ${ROS2}; then
-        # TODO(sloretz) Install gazebo-ros-control when it's ported to ROS 2
-        ROS_GAZEBO_PKGS="${ROS_GAZEBO_PKGS} \
-                     ros-${ROS_DISTRO}-gazebo-ros-control"
-    fi
-  else
-    ROS_GAZEBO_PKGS="ros-${ROS_DISTRO}-gazebo${GAZEBO_VERSION_FOR_ROS}-msgs \
-                     ros-${ROS_DISTRO}-gazebo${GAZEBO_VERSION_FOR_ROS}-plugins \
-                     ros-${ROS_DISTRO}-gazebo${GAZEBO_VERSION_FOR_ROS}-ros \
-                     ros-${ROS_DISTRO}-gazebo${GAZEBO_VERSION_FOR_ROS}-ros-pkgs"
-    # no ros-control in ROS2 yet
-    if ! ${ROS2}; then
-        # TODO(sloretz) Install gazebo-ros-control when it's ported to ROS 2
-        ROS_GAZEBO_PKGS="${ROS_GAZEBO_PKGS} \
-                     ros-${ROS_DISTRO}-gazebo${GAZEBO_VERSION_FOR_ROS}-ros-control"
-    fi
-  fi
-fi
-
-#
-# DART dependencies
-#
-DART_DEPENDENCIES="libflann-dev            \\
-                   libgtest-dev            \\
-                   libeigen3-dev           \\
-                   libassimp-dev           \\
-                   freeglut3-dev           \\
-                   libxi-dev               \\
-                   libxmu-dev              \\
-                   libtinyxml-dev          \\
-                   libtinyxml2-dev         \\
-                   libfcl-dev              \\
-                   liburdfdom-dev          \\
-                   libboost-system-dev     \\
-                   libboost-filesystem-dev"
-
-if ${DART_COMPILE_FROM_SOURCE}; then
-    GAZEBO_EXTRA_DEPENDENCIES="$GAZEBO_EXTRA_DEPENDENCIES \\
-                               $DART_DEPENDENCIES"
 fi
 
 #
