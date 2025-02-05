914f8e1f61f24a427632ecbb2ceef586992bf7fc
Jose Luis Rivero <jrivero@osrfoundation.org>
2025-02-05 16:05:36 +0100

    Remove jobs and configurations from Gazebo
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- jenkins-scripts/docker/lib/_gazebo_utils.sh
+++ jenkins-scripts/docker/lib/_gazebo_utils.sh
@@ -1,28 +1,3 @@
-GAZEBO_RUNTIME_TEST="""
-echo '# BEGIN SECTION: test the script'
-wget -P /tmp/ https://github.com/osrf/gazebo_models/archive/master.tar.gz
-mkdir -p ~/.gazebo/models
-tar -xvf /tmp/master.tar.gz -C ~/.gazebo/models --strip 1
-rm /tmp/master.tar.gz
-
-TEST_START=\`date +%s\`
-timeout --preserve-status 180 gazebo --verbose || true
-TEST_END=\`date +%s\`
-DIFF=\`echo \"\$TEST_END - \$TEST_START\" | bc\`
-
-if [ \$DIFF -lt 180 ]; then
-   echo 'The test took less than 180s. Something bad happened'
-   exit 1
-fi
-echo '# END SECTION'
-"""
-
-GAZEBO_MODEL_INSTALLATION="""
-wget -q https://github.com/osrf/gazebo_models/archive/master.tar.gz -O /tmp/master.tar.gz
-mkdir -p ~/.gazebo/models
-tar -xf /tmp/master.tar.gz -C ~/.gazebo/models --strip 1 >/dev/null 2>&1
-rm /tmp/master.tar.gz"""
-
 GZ_SIM_RUNTIME_TEST="""
 if \${GZ_SIM_RUNTIME_TEST_USE_IGN}; then
   CMD_TOOLS='ign'
