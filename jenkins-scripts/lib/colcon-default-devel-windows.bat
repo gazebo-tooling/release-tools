4cdde844ea77cbb6f7bce5059204ee0813d44ecd
Jose Luis Rivero <jrivero@osrfoundation.org>
2025-03-14 17:49:27 +0100

    Fix bad merge of code

--- jenkins-scripts/lib/colcon-default-devel-windows.bat
+++ jenkins-scripts/lib/colcon-default-devel-windows.bat
@@ -48,8 +48,7 @@ if not exist %WORKSPACE%\%VCS_DIRECTORY% (
 set DXDIAG_FILE=%WORKSPACE%\dxdiag.txt
 if "%GPU_SUPPORT_NEEDED%" == "true" (
   echo # BEGIN SECTION: dxdiag info
-  set DXDIAG_FILE=%WORKSPACE%\dxdiag.txt
-  dxdiag /t %DXDIAG_FILE%%
+  dxdiag /t %DXDIAG_FILE%
   :: if errorlevel 1 ( dxdiag \t !DXDIAG_FILE!)
   type %DXDIAG_FILE%
   echo Checking for correct NVIDIA GPU support %DXDIAG_FILE%
