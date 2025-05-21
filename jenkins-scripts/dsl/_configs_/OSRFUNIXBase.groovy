2a05f641c6a0608897da5be2df803dde6d250ef1
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2025-05-21 09:48:53 -0500

    Try using newer instructions from DSL
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- jenkins-scripts/dsl/_configs_/OSRFUNIXBase.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFUNIXBase.groovy
@@ -41,40 +41,44 @@ class OSRFUNIXBase extends OSRFBase
       publishers {
         postBuildScript {
           buildSteps {
-            conditionalSteps {
-              condition {
-                expression("(.)* gpu-nvidia (.)*",'${NODE_LABELS}')
-              }
-              steps {
-                systemGroovyCommand('''\
-                  import hudson.model.Cause.UpstreamCause;
-                  import hudson.model.*;
-
-                  def node = build.getBuiltOn()
-                  def old_labels = node.getLabelString()
-
-                  println("Checking if nvidia mismatch error is present in log")
-                  if (!(build.getLog(1000) =~ "nvml error: driver/library version mismatch")) {
-                    println(" NVIDIA driver/library version mismatch not detected in the log - Not performing any automatic recovery steps")
-                    return 1;
-                  } else {
-                    println("# BEGIN SECTION: NVIDIA MISMATCH RECOVERY")
-                    try {
-                      println(" PROBLEM: NVIDIA driver/library version mismatch was detected in the log. Try to automatically resolve it:")
-                      println("Removing labels and adding 'recovery-process' label to node")
-                      node.setLabelString("recovery-process")
-                    } catch (Exception ex) {
-                      println("ERROR - CANNOT PERFORM RECOVERY ACTIONS FOR NVIDIA ERROR")
-                      println("Restoring to previous state")
-                      node.setLabelString(old_labels)
-                      throw ex
-                    }
-                    println("# END SECTION: NVIDIA MISMATCH RECOVERY")
+            postBuildStep {
+              buildSteps {
+                conditionalSteps {
+                  condition {
+                    expression("(.)* gpu-nvidia (.)*",'${NODE_LABELS}')
                   }
-                  '''.stripIndent()
-                )
-                shell("""sudo shutdown -r +1""")
-              }
+                  steps {
+                    systemGroovyCommand('''\
+                      import hudson.model.Cause.UpstreamCause;
+                      import hudson.model.*;
+
+                      def node = build.getBuiltOn()
+                      def old_labels = node.getLabelString()
+
+                      println("Checking if nvidia mismatch error is present in log")
+                      if (!(build.getLog(1000) =~ "nvml error: driver/library version mismatch")) {
+                        println(" NVIDIA driver/library version mismatch not detected in the log - Not performing any automatic recovery steps")
+                        return 1;
+                      } else {
+                        println("# BEGIN SECTION: NVIDIA MISMATCH RECOVERY")
+                        try {
+                          println(" PROBLEM: NVIDIA driver/library version mismatch was detected in the log. Try to automatically resolve it:")
+                          println("Removing labels and adding 'recovery-process' label to node")
+                          node.setLabelString("recovery-process")
+                        } catch (Exception ex) {
+                          println("ERROR - CANNOT PERFORM RECOVERY ACTIONS FOR NVIDIA ERROR")
+                          println("Restoring to previous state")
+                          node.setLabelString(old_labels)
+                          throw ex
+                        }
+                        println("# END SECTION: NVIDIA MISMATCH RECOVERY")
+                      }
+                      '''.stripIndent()
+                    )
+                    shell("""sudo shutdown -r +1""")
+                  }
+                }
+              }           
             }
           }
           // onlyIfBuildSucceeds(false)
