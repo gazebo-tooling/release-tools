6eeb1bb4ddbafb2d5c0c4fc5d34f330f9c087789
Jose Luis Rivero <jrivero@honurobotics.com>
2025-07-09 19:20:49 +0200

    Right API call
    Signed-off-by: Jose Luis Rivero <jrivero@honurobotics.com>

--- jenkins-scripts/dsl/_configs_/OSRFDslBase.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFDslBase.groovy
@@ -16,7 +16,7 @@ class OSRFDslBase extends OSRFBase {
 
       steps {
         dsl {
-          text(readFileFromWorkspace(target_dsl_scripts))
+          external(target_dsl_scripts)
           removeAction('DISABLE')
           removeViewAction('DELETE')
         }
