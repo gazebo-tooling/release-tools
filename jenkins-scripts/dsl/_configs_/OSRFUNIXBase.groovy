0e1eaa257e8713fc0963b785ae209af721266239
Cristóbal Arroyo <cristobal.arroyo@ekumenlabs.com>
2025-07-09 12:43:39 -0500

    Fix publishers using the configure block in PostBuild
    Remove the publisher block from configure block dedicated to postbuild
    
    ---
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>
    Signed-off-by: Jose Luis Rivero <jrivero@honurobotics.com>
    Co-authored-by: Jose Luis Rivero <jrivero@honurobotics.com>

--- jenkins-scripts/dsl/_configs_/OSRFUNIXBase.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFUNIXBase.groovy
@@ -38,29 +38,44 @@ class OSRFUNIXBase extends OSRFBase
              git clone https://github.com/gazebo-tooling/release-tools scripts -b \$RTOOLS_BRANCH
              """.stripIndent())
       }
+
       publishers {
-        configure { project ->
-          project / 'publishers' / 'org.jenkinsci.plugins.postbuildscript.PostBuildScript' << {
+        // Add the new regex to naginator tag
+        // There is no need to specify checkRegexp and maxSchedule because they are the default values
+        HelperRetryFailures.create(job, [
+          regexpForRerun: "nvml error: driver/library version mismatch",
+          delay: 70
+        ])
+      }
+
+      configure { project ->
+        project / 'publishers' / 'org.jenkinsci.plugins.postbuildscript.PostBuildScript' << {
+          config {
             buildSteps {
-              'org.jenkinsci.plugins.conditionalbuildstep.ConditionalBuilder' {
-                runner(class: 'org.jenkins_ci.plugins.run_condition.BuildStepRunner$Fail')
-                conditionalbuilders {
-                  'hudson.plugins.groovy.SystemGroovy' {
-                    bindings()
-                    source(class: 'hudson.plugins.groovy.StringSystemScriptSource') {
-                      script {
-                        script('''\
-import hudson.model.Cause.UpstreamCause;
-import hudson.model.*;
+              'org.jenkinsci.plugins.postbuildscript.model.PostBuildStep' {
+                results {
+                  string('FAILURE')
+                }
+                buildSteps {
+                  'org.jenkinsci.plugins.conditionalbuildstep.ConditionalBuilder' {
+                    runner(class: 'org.jenkins_ci.plugins.run_condition.BuildStepRunner$Fail')
+                    conditionalbuilders {
+                      'hudson.plugins.groovy.SystemGroovy' {
+                        bindings()
+                        source(class: 'hudson.plugins.groovy.StringSystemScriptSource') {
+                          script {
+                            script('''\
+  import hudson.model.Cause.UpstreamCause;
+  import hudson.model.*;
 
-def node = build.getBuiltOn()
-def old_labels = node.getLabelString()
+  def node = build.getBuiltOn()
+  def old_labels = node.getLabelString()
 
-println("Checking if nvidia mismatch error is present in log")
-if (!(build.getLog(1000) =~ "nvml error: driver/library version mismatch")) {
+  println("Checking if nvidia mismatch error is present in log")
+  if (!(build.getLog(1000) =~ "nvml error: driver/library version mismatch")) {
   println(" NVIDIA driver/library version mismatch not detected in the log - Not performing any automatic recovery steps")
   return 1;
-} else {
+  } else {
   println("# BEGIN SECTION: NVIDIA MISMATCH RECOVERY")
   try {
     println(" PROBLEM: NVIDIA driver/library version mismatch was detected in the log. Try to automatically resolve it:")
@@ -73,36 +88,28 @@ if (!(build.getLog(1000) =~ "nvml error: driver/library version mismatch")) {
     throw ex
   }
   println("# END SECTION: NVIDIA MISMATCH RECOVERY")
-}
-''')
-                        sandbox('false')
-                        classpath()
+  }
+  ''')
+                            sandbox('false')
+                            classpath()
+                          }
+                        }
+                      }
+                      'hudson.tasks.Shell' {
+                        command('sudo shutdown -r +1')
                       }
                     }
+                    runCondition(class: 'org.jenkins_ci.plugins.run_condition.core.ExpressionCondition') {
+                      expression('(.)* gpu-nvidia (.)*')
+                      delegate.label('${NODE_LABELS}')
+                    }
                   }
-                  'hudson.tasks.Shell' {
-                    command('sudo shutdown -r +1')
-                  }
-                }
-                runCondition(class: 'org.jenkins_ci.plugins.run_condition.core.ExpressionCondition') {
-                  expression('(.)* gpu-nvidia (.)*')
-                  delegate.label('${NODE_LABELS}')
                 }
               }
             }
-            scriptOnlyIfSuccess('false')
-            scriptOnlyIfFailure('true')
-            markBuildUnstable('false')
           }
         }
       }
     }
-
-    // Add the new regex to naginator tag
-    // There is no need to specify checkRegexp and maxSchedule because they are the default values
-    HelperRetryFailures.create(job, [
-      regexpForRerun: "nvml error: driver/library version mismatch",
-      delay: 70
-    ])
   }
 }
