9589f4e239184e72a0e8453be5adfda6260e5c1b
Jose Luis Rivero <jrivero@honurobotics.com>
2025-07-18 13:33:09 +0200

    sha256sum from coreutils

--- jenkins-scripts/dsl/_configs_/OSRFSourceCreation.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFSourceCreation.groovy
@@ -121,7 +121,7 @@ class OSRFSourceCreation
             fi
           fi
 
-          SOURCE_TARBALL_SHA256=\$(shasum --algorithm 256 \${tarball})
+          SOURCE_TARBALL_SHA256=\$(sha256sum \${tarball})
           echo tarball sha256 is \${SOURCE_TARBALL_SHA256}
 
           echo "S3_FILES_TO_UPLOAD=\${tarball}" >> ${properties_file}
