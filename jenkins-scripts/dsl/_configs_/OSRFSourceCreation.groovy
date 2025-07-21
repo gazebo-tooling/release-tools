43d7241307d8a3aff1b04c47f1362349054484de
Steve Peters <scpeters@openrobotics.org>
2025-07-21 04:14:48 -0700

    OSRFSourceCreation: calculate tarball sha256 (#1353)
    * OSRFSourceCreation: calculate tarball sha256
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>
    
    ---------
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>
    Co-authored-by: Jose Luis Rivero <jrivero@honurobotics.com>

--- jenkins-scripts/dsl/_configs_/OSRFSourceCreation.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFSourceCreation.groovy
@@ -121,8 +121,12 @@ class OSRFSourceCreation
             fi
           fi
 
+          SOURCE_TARBALL_SHA256=\$(sha256sum \${WORKSPACE}/${pkg_sources_dir}/\${tarball} | awk '{print \$1}')
+          echo tarball sha256 is \${SOURCE_TARBALL_SHA256}
+
           echo "S3_FILES_TO_UPLOAD=\${tarball}" >> ${properties_file}
           echo "SOURCE_TARBALL_URI=$s3_download_url_basedir/\${tarball}" >> ${properties_file}
+          echo "SOURCE_TARBALL_SHA256=\${SOURCE_TARBALL_SHA256}" >> ${properties_file}
           """.stripIndent()
         )
       }
