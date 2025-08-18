a38d57e402c66365907c7c120a7cebbedbf566db
Steve Peters <scpeters@openrobotics.org>
2025-08-18 14:19:07 -0700

    use if instead of exit
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- jenkins-scripts/lib/_homebrew_path_setup.sh
+++ jenkins-scripts/lib/_homebrew_path_setup.sh
@@ -1,14 +1,16 @@
 #/bin/sh +x
 set -e
 
-# exit if brew command is already in the PATH
-command -v brew && exit 0
+# skip logic if brew command is already in the PATH
+if ! brew --prefix; then
 
-echo brew not in PATH, checking standard locations
+  echo brew not in PATH, checking standard locations
 
-# otherwise check in standard brew locations using the precedence order from
-# https://docs.brew.sh/Tips-and-Tricks
-BREW=$(PATH="/opt/homebrew/bin:/home/linuxbrew/.linuxbrew/bin:/usr/local/bin" command -v brew)
-which $BREW && eval "$($BREW shellenv)"
+  # otherwise check in standard brew locations using the precedence order from
+  # https://docs.brew.sh/Tips-and-Tricks
+  BREW=$(PATH="/opt/homebrew/bin:/home/linuxbrew/.linuxbrew/bin:/usr/local/bin" command -v brew)
+  which -s $BREW && eval "$($BREW shellenv)"
 
-echo brew found at $(command -v brew)
+  echo brew found at $(command -v brew)
+
+fi
