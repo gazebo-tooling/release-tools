83b0f7e4145c9774d12f926b4659b4a9d3332299
Jose Luis Rivero <jrivero@honurobotics.com>
2025-11-11 11:51:36 +0100

    Implement the Conda auto-environment selection (#1408)
    * Implementation of conda auto-environment selection
    * Relocate the find config script
    
    ----
    Signed-off-by: Jose Luis Rivero <jrivero@honurobotics.com>

--- jenkins-scripts/lib/windows_env_vars.bat
+++ jenkins-scripts/lib/windows_env_vars.bat
@@ -1,11 +1,15 @@
+
 set "PIXI_VERSION=0.44.0"
 set "PIXI_URL=https://github.com/prefix-dev/pixi/releases/download/v%PIXI_VERSION%/pixi-x86_64-pc-windows-msvc.exe"
 if NOT DEFINED PIXI_PROJECT_PATH (
   set "PIXI_PROJECT_PATH=%TMP%\pixi\project"
 )
+set "PIXI_BOOTSTRAP_PROJECT_PATH=%TMP%\pixi\bootstrap_project"
+
 set "PIXI_TMPDIR=%TMP%\pixi"
 set "PIXI_TMP=%PIXI_TMPDIR%\pixi.exe"
-set "CONDA_ENVS_DIR=%SCRIPT_DIR%\..\conda\envs\"
+set "CONDA_ROOT_DIR=%LIB_DIR%\..\..\conda\"
+set "CONDA_ENVS_DIR=%CONDA_ROOT_DIR%\envs\"
 
 if NOT DEFINED EXIT_ON_ERROR (
   set EXIT_ON_ERROR=
