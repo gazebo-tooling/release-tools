032f5a3319e7a9372fbc52e8b8b5207d003ab3bb
Steve Peters <scpeters@intrinsic.ai>
2026-04-20 11:51:58 -0700

    add comment with bug link
    Signed-off-by: Steve Peters <scpeters@intrinsic.ai>

--- jenkins-scripts/dsl/_configs_/OSRFBase.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFBase.groovy
@@ -60,6 +60,7 @@ class OSRFBase
           maxSchedule: 2
         ])
 
+        // Homebrew installation problems, see https://github.com/gazebo-tooling/release-tools/issues/1479
         HelperRetryFailures.create(job, [
           regexpForRerun: "Error: Empty installation",
           checkRegexp: true,
